aff080431c848adb82e2b9a7151b9d3c38ff21e9
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-05-06 09:36:29 -0400

    use . as TBranch delimeter instead of / to make root life easier

---
+++ offline/framework/phool/phooldefs.h
@@ -0,0 +1,13 @@
+#ifndef PHOOLDEFS_H__
+#define PHOOLDEFS_H__
+
+#include <string>
+
+namespace phooldefs
+{
+  static const std::string branchpathdelim = ".";
+  static const std::string nodetreepathdelim = "/";
+};
+
+#endif
+
