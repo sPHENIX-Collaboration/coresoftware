177bf9aee52792c09d60a6a3eb6b0624b8ca9bc3
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-05-27 12:36:52 -0400

    oops missed virtual calls

--- simulation/g4simulation/g4jets/JetMap.h
+++ simulation/g4simulation/g4jets/JetMap.h
@@ -23,51 +23,51 @@ public:
    
   virtual ~JetMap() {}
 
-  void identify(std::ostream &os = std::cout) const;
-  void Reset() {}
-  int  isValid() const {return 0;}
+  virtual void identify(std::ostream &os = std::cout) const;
+  virtual void Reset() {}
+  virtual int  isValid() const {return 0;}
 
   // map content info ----------------------------------------------------------
   
-  void      set_algo(Jet::ALGO algo) {return;}
-  Jet::ALGO get_algo() const         {return Jet::NONE;}
+  virtual void      set_algo(Jet::ALGO algo) {return;}
+  virtual Jet::ALGO get_algo() const         {return Jet::NONE;}
   
-  void  set_par(float par) {return;}
-  float get_par() const    {return NAN;}
+  virtual void  set_par(float par) {return;}
+  virtual float get_par() const    {return NAN;}
 
   // set access to list of source identifiers ----------------------------------
   
-  bool empty_src() const        {return true;}
-  void insert_src(Jet::SRC src) {return;}
+  virtual bool empty_src() const        {return true;}
+  virtual void insert_src(Jet::SRC src) {return;}
  
-  ConstSrcIter begin_src()             const {return std::set<Jet::SRC>().end();}
-  ConstSrcIter  find_src(Jet::SRC src) const {return std::set<Jet::SRC>().end();}
-  ConstSrcIter   end_src()             const {return std::set<Jet::SRC>().end();}
+  virtual ConstSrcIter begin_src()             const {return std::set<Jet::SRC>().end();}
+  virtual ConstSrcIter  find_src(Jet::SRC src) const {return std::set<Jet::SRC>().end();}
+  virtual ConstSrcIter   end_src()             const {return std::set<Jet::SRC>().end();}
 
-  SrcIter begin_src()             {return std::set<Jet::SRC>().end();}
-  SrcIter  find_src(Jet::SRC src) {return std::set<Jet::SRC>().end();}
-  SrcIter   end_src()             {return std::set<Jet::SRC>().end();}
+  virtual SrcIter begin_src()             {return std::set<Jet::SRC>().end();}
+  virtual SrcIter  find_src(Jet::SRC src) {return std::set<Jet::SRC>().end();}
+  virtual SrcIter   end_src()             {return std::set<Jet::SRC>().end();}
   
   // map access to jets --------------------------------------------------------
  
-  bool   empty()                   const {return true;}
-  size_t  size()                   const {return 0;}
-  size_t count(unsigned int idkey) const {return 0;}
-  void   clear()                         {return;}
+  virtual bool   empty()                   const {return true;}
+  virtual size_t  size()                   const {return 0;}
+  virtual size_t count(unsigned int idkey) const {return 0;}
+  virtual void   clear()                         {return;}
   
-  const Jet* get(unsigned int idkey) const {return NULL;}
-        Jet* get(unsigned int idkey)       {return NULL;} 
+  virtual const Jet* get(unsigned int idkey) const {return NULL;}
+  virtual       Jet* get(unsigned int idkey)       {return NULL;} 
 
-  Jet*   insert(Jet* jet)          {return NULL;}
-  size_t erase(unsigned int idkey) {return 0;}
+  virtual Jet*   insert(Jet* jet)          {return NULL;}
+  virtual size_t erase(unsigned int idkey) {return 0;}
 
-  ConstIter begin()                  const {return typ_JetMap().end();}
-  ConstIter find(unsigned int idkey) const {return typ_JetMap().end();}
-  ConstIter end()                    const {return typ_JetMap().end();}
+  virtual ConstIter begin()                  const {return typ_JetMap().end();}
+  virtual ConstIter find(unsigned int idkey) const {return typ_JetMap().end();}
+  virtual ConstIter end()                    const {return typ_JetMap().end();}
 
-  Iter begin()                   {return typ_JetMap().end();}
-  Iter  find(unsigned int idkey) {return typ_JetMap().end();}
-  Iter   end()                   {return typ_JetMap().end();}
+  virtual Iter begin()                   {return typ_JetMap().end();}
+  virtual Iter  find(unsigned int idkey) {return typ_JetMap().end();}
+  virtual Iter   end()                   {return typ_JetMap().end();}
 
 protected:
   JetMap();
