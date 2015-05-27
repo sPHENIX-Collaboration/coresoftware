fe01286ad796d537e0ea095c7badee8ef995307c
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-05-27 11:36:49 -0400

    MPM + versioned storage container in case this gets extended with new attributes

--- simulation/g4simulation/g4jets/JetMap.C
+++ simulation/g4simulation/g4jets/JetMap.C
@@ -8,59 +8,9 @@ using namespace std;
 
 ClassImp(JetMap)
 
-JetMap::JetMap()
-: _algo(Jet::NONE),
-  _par(NAN),
-  _src(),
-  _map() {
-}
+JetMap::JetMap() {}
 
-JetMap::~JetMap() {
-  JetMap::Reset();
-}
-
-void JetMap::Reset() {
-  _algo = Jet::NONE;  
-  _par = NAN;
-  _src.clear();
-//  _map.clear();
-
-  while(_map.begin() != _map.end())
-    {
-      delete _map.begin()->second;
-      _map.erase(_map.begin());
-    }
-}
-
-void
-JetMap::identify(ostream& os) const
-{
-  os << "JetMap: size = " << _map.size() << endl;
-  os << "        par = " << _par << endl;
-  os << "        source = ";
-  for (SrcIter i = begin_src(); i != end_src(); ++i)
-    os << (*i) << ",";
-  os << endl;
-
+void JetMap::identify(ostream& os) const {
+  os << "JetMap" << endl;
   return;
-}
-
-const Jet* JetMap::get(unsigned int id) const {
-  ConstIter iter = _map.find(id);
-  if (iter == _map.end()) return NULL;  
-  return iter->second;
-}
-
-Jet* JetMap::get(unsigned int id) {
-  Iter iter = _map.find(id);
-  if (iter == _map.end()) return NULL;
-  return iter->second;
-}
-
-Jet* JetMap::insert(Jet* jet) {
-  unsigned int index = 0;
-  if (!_map.empty()) index = _map.rbegin()->first + 1;
-  _map.insert(make_pair( index , jet ));
-  _map[index]->set_id(index);
-  return (_map[index]);
 }
--- simulation/g4simulation/g4jets/JetMap.h
+++ simulation/g4simulation/g4jets/JetMap.h
@@ -20,62 +20,59 @@ public:
   // source identifier iterators
   typedef std::set<Jet::SRC>::const_iterator ConstSrcIter;
   typedef std::set<Jet::SRC>::iterator            SrcIter;
-  
-  JetMap();
-  virtual ~JetMap();
+   
+  virtual ~JetMap() {}
 
   void identify(std::ostream &os = std::cout) const;
-  void Reset();
-  int  isValid() const {return 1;}
+  void Reset() {}
+  int  isValid() const {return 0;}
 
-  // map content info
+  // map content info ----------------------------------------------------------
   
-  void set_algo(Jet::ALGO algo) {_algo = algo;}
-  Jet::ALGO get_algo() const    {return _algo;}
+  void      set_algo(Jet::ALGO algo) {return;}
+  Jet::ALGO get_algo() const         {return Jet::NONE;}
   
-  void  set_par(float par) {_par = par;}
-  float get_par() const    {return _par;}
+  void  set_par(float par) {return;}
+  float get_par() const    {return NAN;}
 
-  // set access to list of source identifiers
+  // set access to list of source identifiers ----------------------------------
   
-  bool empty_src() const        {return _src.empty();}
-  void insert_src(Jet::SRC src) {_src.insert(src);}
+  bool empty_src() const        {return true;}
+  void insert_src(Jet::SRC src) {return;}
  
-  ConstSrcIter begin_src()             const {return _src.begin();}
-  ConstSrcIter  find_src(Jet::SRC src) const {return _src.find(src);}
-  ConstSrcIter   end_src()             const {return _src.end();}
+  ConstSrcIter begin_src()             const {return std::set<Jet::SRC>().end();}
+  ConstSrcIter  find_src(Jet::SRC src) const {return std::set<Jet::SRC>().end();}
+  ConstSrcIter   end_src()             const {return std::set<Jet::SRC>().end();}
 
-  SrcIter begin_src()             {return _src.begin();}
-  SrcIter  find_src(Jet::SRC src) {return _src.find(src);}
-  SrcIter   end_src()             {return _src.end();}
+  SrcIter begin_src()             {return std::set<Jet::SRC>().end();}
+  SrcIter  find_src(Jet::SRC src) {return std::set<Jet::SRC>().end();}
+  SrcIter   end_src()             {return std::set<Jet::SRC>().end();}
   
-  // map access to jets
+  // map access to jets --------------------------------------------------------
  
-  bool   empty()                   const {return _map.empty();}
-  size_t  size()                   const {return _map.size();}
-  size_t count(unsigned int idkey) const {return _map.count(idkey);}
-  void   clear()                         {Reset();}
+  bool   empty()                   const {return true;}
+  size_t  size()                   const {return 0;}
+  size_t count(unsigned int idkey) const {return 0;}
+  void   clear()                         {return;}
   
-  const Jet*   get(unsigned int idkey) const;
-        Jet*   get(unsigned int idkey); 
+  const Jet* get(unsigned int idkey) const {return NULL;}
+        Jet* get(unsigned int idkey)       {return NULL;} 
 
-        //! insert Jet to the map. Once inserted, the JetMap own the Jet object
-        Jet*   insert(Jet* jet);
-        size_t erase(unsigned int idkey) {return _map.erase(idkey);}
+  Jet*   insert(Jet* jet)          {return NULL;}
+  size_t erase(unsigned int idkey) {return 0;}
 
-  ConstIter begin()                  const {return _map.begin();}
-  ConstIter find(unsigned int idkey) const {return _map.find(idkey);}
-  ConstIter end()                    const {return _map.end();}
+  ConstIter begin()                  const {return typ_JetMap().end();}
+  ConstIter find(unsigned int idkey) const {return typ_JetMap().end();}
+  ConstIter end()                    const {return typ_JetMap().end();}
 
-  Iter begin()                   {return _map.begin();}
-  Iter  find(unsigned int idkey) {return _map.find(idkey);}
-  Iter   end()                   {return _map.end();}
+  Iter begin()                   {return typ_JetMap().end();}
+  Iter  find(unsigned int idkey) {return typ_JetMap().end();}
+  Iter   end()                   {return typ_JetMap().end();}
+
+protected:
+  JetMap();
   
 private:
-  Jet::ALGO  _algo;                 //< algorithm used to reconstruct jets         
-  float _par;                       //< algorithm parameter setting
-  std::set<Jet::SRC> _src;      //< list of sources (clusters, towers, etc)
-  typ_JetMap _map; //< jet algorithm output storage
     
   ClassDef(JetMap, 1);
 };
