e7e366d782eb7dd814a8f5932f68fcfeb3267585
jlab <joshua.labounty@stonybrook.edu>
2015-05-28 13:46:04 -0400

    Changed geometry to better reflect the PANDA model, using G4Trd to place trapezoidal crystals in an offpoint geometry. Added _inputFile to function so that the user can choose mapping file to be used to construct geometry

--- simulation/g4simulation/g4detectors/PHG4CrystalCalorimeterDetector.cc
+++ simulation/g4simulation/g4detectors/PHG4CrystalCalorimeterDetector.cc
@@ -20,6 +20,7 @@
 #include <Geant4/G4Trap.hh>
 #include <Geant4/G4Cons.hh>
 #include <Geant4/G4Box.hh>
+#include <Geant4/G4Trd.hh>
 
 #include <Geant4/G4VisAttributes.hh>
 #include <Geant4/G4Colour.hh>
@@ -27,9 +28,15 @@
 #include <cmath>
 #include <sstream>
 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+
 using namespace std;
 
 //static double no_overlap = 0.00015 * cm; // added safety margin against overlaps by using same boundary between volumes
+
 
 //_______________________________________________________________________
 PHG4CrystalCalorimeterDetector::PHG4CrystalCalorimeterDetector( PHCompositeNode *Node, const std::string &dnam ):
@@ -53,7 +60,7 @@ PHG4CrystalCalorimeterDetector::PHG4CrystalCalorimeterDetector( PHCompositeNode 
   _materialCrystal( "G4_PbWO4" ),
   _active(1),
   _crystallogicnameprefix("eEcalCrystal"),
-  _superdetector("NONE")
+  _inputFile( "/direct/phenix+u/jlab/github_fork/coresoftware/simulation/g4simulation/g4detectors/mapping/CrystalCalorimeter_Mapping_v001.txt" )
 {
 
 }
@@ -68,19 +75,10 @@ PHG4CrystalCalorimeterDetector::~PHG4CrystalCalorimeterDetector()
 int
 PHG4CrystalCalorimeterDetector::IsInCrystalCalorimeter(G4VPhysicalVolume * volume) const
 {
-
-  // if hit is in absorber material
-  bool isinabsorber = false;
-
   if (volume->GetName().find(_crystallogicnameprefix) != string::npos)
     {
       return 1;
     }
-  else if ( isinabsorber )
-    {
-      return -1;
-    }
-
   return 0;
 }
 
@@ -93,10 +91,10 @@ PHG4CrystalCalorimeterDetector::Construct( G4LogicalVolume* logicWorld )
   G4Material* Air = G4Material::GetMaterial("G4_AIR");
 
   G4VSolid* ecal_envelope_cone = new G4Cons("eEcal_envelope_solid",
-                                            _rMin1, _rMax1,
-                                            _rMin2, _rMax2,
-                                            _dZ/2.,
-                                            _sPhi, _dPhi );
+					    _rMin1, _rMax1,
+					    _rMin2, _rMax2,
+					    _dZ/2.,
+					    _sPhi, _dPhi );
 
   G4LogicalVolume* ecal_envelope_log =  new G4LogicalVolume(ecal_envelope_cone, Air, G4String("eEcal_envelope"), 0, 0, 0);
 
@@ -115,7 +113,7 @@ PHG4CrystalCalorimeterDetector::Construct( G4LogicalVolume* logicWorld )
 
   /* Place envelope cone in simulation */
   new G4PVPlacement( G4Transform3D(ecal_rotm, G4ThreeVector(_place_in_x, _place_in_y, _place_in_z) ),
-                     ecal_envelope_log, "CrystalCalorimeter", logicWorld, 0, false, overlapcheck);
+		     ecal_envelope_log, "CrystalCalorimeter", logicWorld, 0, false, overlapcheck);
 
   /* Construct crystal calorimeter within envelope */
   ConstructCrystals(ecal_envelope_log);
@@ -128,75 +126,130 @@ PHG4CrystalCalorimeterDetector::Construct( G4LogicalVolume* logicWorld )
 int
 PHG4CrystalCalorimeterDetector::ConstructCrystals(G4LogicalVolume* ecalenvelope)
 {
-  /* Simple model: Fill calorimeter envelope with rectangular crystals (front face = back face)
-     which are arranged in chessboard pattern in x-y and oriented parallel to z azis */
-  int n_crystals_j = 58;
-  int n_crystals_k = 58;
 
-  G4double r_min = _rMin2;
-  G4double r_max = _rMax1;
-  G4double crystal_r = sqrt(  _crystal_front_dx * _crystal_front_dx + _crystal_front_dy * _crystal_front_dy );
+	G4int NumberOfLines;		 			//Number of crystals to be created.
+	const G4int NumberOfIndices = 7; 			//Different dimensions needed for crystal placement
+	const string FileName = _inputFile.c_str();		//File in which crystal positions are stored
+	
+	G4int j_cry, k_cry;					//Indices for matrix
+	G4int j_idx, k_idx;					//Indices of each crstals
+	G4double x_cent, y_cent, z_cent, r_theta, r_phi;	//Coordinates of crystal in [x,y,z,theta,phi]
+	G4double dx1 = 50.19*mm; //12.188*mm;			//Half of the extent of the front face of the trapezoid in x
+	G4double dx2 = 59.3154545455*mm; //13.702*mm;		//Half of the extent of the back face of the trapezoid in x
+	G4double dy1 = dx1;					//Half of the extent of the front face of the trapezoid in y
+	G4double dy2 = dx2;					//Half of the extent of the back face of the trapezoid in y
+	G4double dz =  90.000*mm;				//Half of the extent of the crystal in z
 
-  /* define center of crystal with index j=0, k=0 */
-  G4double xpos_j0_k0 = 0.0*mm - ( 0.5 * n_crystals_j - 0.5 ) * _crystal_front_dx;
-  G4double ypos_j0_k0 = 0.0*mm - ( 0.5 * n_crystals_k - 0.5 ) * _crystal_front_dy;
+	//Create single crystal
 
-  G4Material* material_crystal = G4Material::GetMaterial(_materialCrystal.c_str());
+	G4VSolid* crystal_solid = new G4Trd(G4String("eEcal_crystal"),
+		dx1,						//Half length on the small face in x
+		dx2,						//Half length on the large face in x
+		dy1,						//Half length on the small face in y
+		dy2,						//Half length on the large face in y
+		dz);						//Half length in z
 
-  G4VSolid* crystal_solid = new G4Box( G4String("eEcal_crystal"),
-                                       _crystal_front_dx / 2.0,
-                                       _crystal_front_dy / 2.0,
-                                       _crystal_dz / 2.0 );
+	G4Material* material_crystal = G4Material::GetMaterial(_materialCrystal.c_str());
 
-  G4LogicalVolume *crystal_logic = new G4LogicalVolume( crystal_solid,
-                                                        material_crystal,
-                                                        "eEcal_crystal",
-                                                        0, 0, 0);
+	G4LogicalVolume *crystal_logic = new G4LogicalVolume( crystal_solid,
+		material_crystal,
+		"eEcal_crystal",
+		0, 0, 0);
 
-  G4VisAttributes *visattchk = new G4VisAttributes();
-  visattchk->SetVisibility(true);
-  visattchk->SetForceSolid(true);
-  visattchk->SetColour(G4Colour::Cyan());
-  crystal_logic->SetVisAttributes(visattchk);
+	G4VisAttributes *visattchk = new G4VisAttributes();
+	visattchk->SetVisibility(true);
+	visattchk->SetForceSolid(true);
+	visattchk->SetColour(G4Colour::Cyan());
+	crystal_logic->SetVisAttributes(visattchk);
 
-  ostringstream name;
+	ostringstream name;
 
-  /* Place crystal units */
-  G4RotationMatrix *Rot = new G4RotationMatrix();
-  Rot->rotateX(0 * rad);
-  Rot->rotateY(0 * rad);
-  Rot->rotateZ(0 * rad);
+	ifstream datafile;
 
-  for (int idx_j = 0; idx_j < n_crystals_j; idx_j++)
-    {
-      for (int idx_k = 0; idx_k < n_crystals_k; idx_k++)
-        {
-          /* Construct unique name for crystal */
-          name.str("");
-          name << _crystallogicnameprefix << "_j_" << idx_j << "_k_" << idx_k;
+	if (!datafile.is_open())
+	{
+		datafile.open(FileName.c_str());
+		if(!datafile)
+		{
+			cerr << endl << "*******************************************************************" << endl;
+			cerr << "ERROR: Failed to open " << FileName <<" --- Exiting program." << endl;
+			cerr << "*******************************************************************" << endl << endl;
+			exit(1);
+		}
+	}
 
-          /* Calculate center position for crystal */
-          G4double xpos_crystal_jk = xpos_j0_k0 + idx_j * _crystal_front_dx;
-          G4double ypos_crystal_jk = ypos_j0_k0 + idx_k * _crystal_front_dx;
-          G4ThreeVector g4vec(xpos_crystal_jk, ypos_crystal_jk, 0);
+	//Determine the Number of Crystals to be Created
+	NumberOfLines = 0;
+	ifstream in(_inputFile.c_str());
+	std::string unused;
+	while ( std::getline(in, unused) )
+	   ++NumberOfLines;
+	cout << "Number of Crystals: " << NumberOfLines << endl;			
 
-          /* check if crystal extends beyond calorimeter envelope volume */
-          G4double crystal_rpos = sqrt( xpos_crystal_jk * xpos_crystal_jk + ypos_crystal_jk * ypos_crystal_jk );
+	j_cry = NumberOfLines; 		// = Number of Crystals
+	k_cry = NumberOfIndices; 	// = j, k, x, y, z, alpha, beta.
+	
+	cout << "We will now create a " << NumberOfLines << " by " << k_cry << " matrix." << endl;
+	
+	double Crystals[j_cry][k_cry];
 
-          G4double crystal_r_clear_max = crystal_rpos + crystal_r;
-          G4double crystal_r_clear_min = crystal_rpos - crystal_r;
+	cout << "Done! Now filling the matrix" << endl;
+	
+	G4int j = 0;
+	G4int k = 0;
 
-          if ( crystal_r_clear_min < r_min || crystal_r_clear_max > r_max )
-            continue;
+	while (j_cry > j) {
+		while (k_cry > k) {
+			datafile >> Crystals[j][k];
+			k++;
+		}
+		j++;
+		k = 0;
+	}
+	
+	cout << "Array Filled. Now Constructing Crystals." << endl;
+	
+	//Write out matrix to confirm correct input file
+	for(j = 0; j < j_cry; ++j) {
+		for(k = 0; k < k_cry; ++k) 
+			cout << Crystals[j][k] << "|";	
+		cout << endl;
+	}
+		
+	//Construct and place each crystal
 
-          /* If crystal wihtin envelope: place it */
-          new G4PVPlacement( Rot, G4ThreeVector(xpos_crystal_jk , ypos_crystal_jk, 0),
-                             crystal_logic,
-                             name.str().c_str(),
-                             ecalenvelope,
-                             0, 0, overlapcheck);
-        }
-    }
+	G4RotationMatrix *Rot = new G4RotationMatrix(); //rotation matrix for the placement of each crystal
 
-  return 0;
+	j = 0;
+	while (j_cry > j) {
+		j_idx = Crystals[j][0];
+		k_idx = Crystals[j][1];
+		x_cent = Crystals[j][2];
+		y_cent = Crystals[j][3];
+		z_cent = Crystals[j][4];
+		r_theta = Crystals[j][5];
+		r_phi = Crystals[j][6];
+
+		G4ThreeVector Crystal_Center = G4ThreeVector(x_cent*mm, y_cent*mm, z_cent*mm);
+
+		Rot->rotateX(r_theta * rad);
+		Rot->rotateY(r_phi * rad);
+		Rot->rotateZ(0 * rad);
+	
+		name.str("");
+		name << _crystallogicnameprefix << "_j_" << j_idx << "_k_" << k_idx;
+		
+		new G4PVPlacement( Rot, Crystal_Center,
+			crystal_logic,
+			name.str().c_str(),
+			ecalenvelope,
+			0, 0, overlapcheck);
+
+		j++;
+	}
+	
+	cout << endl << "All done..." << endl;
+	datafile.close();
+		
+	return 0;
 }
