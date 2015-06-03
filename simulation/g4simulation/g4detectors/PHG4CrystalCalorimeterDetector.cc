cca683793f7aec2fe366d3c569a4c930a81c3123
Josh LaBounty <joshua.labounty@stonybrook.edu>
2015-06-03 16:08:34 -0400

    Changes _place_in_x on line 343 of PHG4CrystalCalorimeterDetector.cc to _place_in_y

--- simulation/g4simulation/g4detectors/PHG4CrystalCalorimeterDetector.cc
+++ simulation/g4simulation/g4detectors/PHG4CrystalCalorimeterDetector.cc
@@ -340,7 +340,7 @@ PHG4CrystalCalorimeterDetector::ConstructCrystals(G4LogicalVolume* ecalenvelope)
                 j_idx = k_max - Crystals[j][0];
                 k_idx = k_max - Crystals[j][1];
                 x_cent = Crystals[j][2] - _place_in_x;
-                y_cent = -1.0 * (Crystals[j][3] - _place_in_x);
+                y_cent = -1.0 * (Crystals[j][3] - _place_in_y);
                 z_cent = Crystals[j][4] - _place_in_z;
                 r_theta = Crystals[j][5];
                 r_phi = -1.0*Crystals[j][6];
