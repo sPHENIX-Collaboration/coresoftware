a26f667fd2ff5572ae0d0c6c8dc2c818d231064b
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-06-24 09:41:29 -0400

    pair dictionary call added

--- simulation/g4simulation/g4eval/EvalLinksV1LinkDef.h
+++ simulation/g4simulation/g4eval/EvalLinksV1LinkDef.h
@@ -1,7 +1,9 @@
 #ifdef __CINT__
 
+#pragma link C++ class std::string+;
 #pragma link C++ class std::pair<unsigned int,unsigned int>+;
 #pragma link C++ class std::map< std::pair<unsigned int,unsigned int> , float >+;
+#pragma link C++ class std::pair< std::pair<unsigned int,unsigned int> , float >+;
 #pragma link C++ class EvalLinksV1+;
 
 #endif /* __CINT__ */
