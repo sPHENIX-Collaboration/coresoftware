d301833fdc41494975a921c9729d23df6ebbe223
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-06-26 17:16:27 -0400

    breakout the LinkDefs into individual files

---
+++ simulation/g4simulation/g4detectors/PHG4BlockCellRecoLinkDef.h
@@ -0,0 +1,5 @@
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockCellReco-!;
+
+#endif /* __CINT__ */
---
+++ simulation/g4simulation/g4detectors/ePHENIXRICHConstructionLinkDef.h
@@ -0,0 +1,5 @@
+#ifdef __CINT__
+
+#pragma link C++ class ePHENIXRICH::RICH_Geometry-!;
+
+#endif /* __CINT__ */
