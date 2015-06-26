366e5bf68088a6cae612ac526c63320950691896
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-06-26 12:14:37 -0400

    string declaration not needed and results in readback warning

--- simulation/g4simulation/g4eval/EvalLinksV1LinkDef.h
+++ simulation/g4simulation/g4eval/EvalLinksV1LinkDef.h
@@ -1,6 +1,5 @@
 #ifdef __CINT__
 
-#pragma link C++ class std::string+;
 #pragma link C++ class std::pair<unsigned int,unsigned int>+;
 #pragma link C++ class std::map< std::pair<unsigned int,unsigned int> , float >+;
 #pragma link C++ class std::pair< std::pair<unsigned int,unsigned int> , float >+;
