b679d24d23c1789721ce1507a7e3b8b1706f66bf
jlab <joshua.labounty@stonybrook.edu>
2015-07-27 22:36:01 -0400

    Adds new detector PHG4Envelope which functions as a black hole and can be used to pick up on energy which escapes from another detector setup. This detector constists of two endcaps and a cylinder which can be resized/repositioned to encapsulate a detector

---
+++ simulation/g4simulation/g4detectors/PHG4EnvelopeSubsystem.h
@@ -0,0 +1,57 @@
+#ifndef PHG4EnvelopeSubsystem_h
+#define PHG4EnvelopeSubsystem_h
+
+#include <g4main/PHG4Subsystem.h>
+#include <g4main/PHG4Detector.h>
+#include <g4main/PHG4SteppingAction.h>
+
+#include <phool/PHCompositeNode.h>
+
+#include <Geant4/G4Types.hh>
+#include <Geant4/G4String.hh>
+
+class PHG4EnvelopeDetector;
+class PHG4EnvelopeSteppingAction;
+class PHG4EventAction;
+
+class PHG4EnvelopeSubsystem: public PHG4Subsystem
+{
+	public:
+		//Constructor
+		PHG4EnvelopeSubsystem ( const std::string &name = "ENVELOPE_DEFAULT", const int layer = 0 );
+		
+		//Destructor
+		virtual ~PHG4EnvelopeSubsystem ( void )
+		{}
+			
+		/*
+			Creates the detector_ object and place it on the node tree, under "DETECTORS" node (or whatever)
+			Creates the stepping action and place it on the node tree, under "ACTIONS" node
+			Creates relevant hit nodes that will be populated by the stepping action and stored in the output DST
+		*/
+	
+		int Init(PHCompositeNode *);
+	
+		//Event Processing
+		int process_event(PHCompositeNode *);
+	
+		//Accessors (reimplemented)
+		virtual PHG4Detector* GetDetector( void ) const;
+		virtual PHG4SteppingAction* GetSteppingAction( void ) const;
+	
+	private:
+		//Pointer to Geant4 implementation of detector
+		PHG4EnvelopeDetector* detector_;
+	
+		//Stepping Action
+		PHG4EnvelopeSteppingAction* steppingAction_;
+		PHG4EventAction *eventAction_;
+			
+		G4String material;
+		int active;
+	
+		std::string detector_type;
+			
+};
+
+#endif
