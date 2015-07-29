90116af76d7933f2a344eaa474c3b98c79b24351
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-07-29 17:29:14 -0400

    bring in the fastjet dependence

--- simulation/g4simulation/g4jets/PHG4TruthJetReco.C
+++ simulation/g4simulation/g4jets/PHG4TruthJetReco.C
@@ -9,11 +9,19 @@
 #include <fun4all/getClass.h>
 
 // PHENIX Geant4 includes
+#include <g4main/PHG4TruthInfoContainer.h>
+#include <g4main/PHG4Particle.h>
 #include <JetMap.h>
 #include <Jet.h>
 
 // standard includes
 #include <iostream>
+
+// fastjet includes
+#include <fastjet/JetDefinition.hh>
+#include <fastjet/PseudoJet.hh>
+#include <fastjet/ClusterSequence.hh>
+#include <fastjet/SISConePlugin.hh>
 
 using namespace std;
 
@@ -43,25 +51,40 @@ int PHG4TruthJetReco::process_event(PHCompositeNode *topNode) {
   //---------------------------------
   // Get Objects off of the Node Tree
   //---------------------------------
-  /*
-  // Pull the reconstructed track information off the node tree...
-  _g4tracks = findNode::getClass<SvtxTrackMap>(topNode, "SvtxTrackMap");
-  if(!_g4tracks) 
-    {
-      cerr << PHWHERE << " ERROR: Can't find SvtxTrackMap." << endl;
-      return Fun4AllReturnCodes::ABORTEVENT;
-    }
 
-  if (verbosity > 1) {
-    _g4tracks->identify();
-    for (SvtxTrackMap::Iter iter = _g4tracks->begin();
-	 iter != _g4tracks->end();
-	 ++iter) {
-      SvtxTrack *track = &iter->second;
-      track->identify();
-    }
+  // Pull the reconstructed track information off the node tree...
+  PHG4TruthInfoContainer *truthinfo = findNode::getClass<PHG4TruthInfoContainer>(topNode,"G4TruthInfo");
+  if (!truthinfo) {
+    cerr << PHWHERE << " ERROR: Can't find G4TruthInfo" << endl;
+    return Fun4AllReturnCodes::ABORTEVENT;
   }
-  */
+
+  // run fast jet
+  
+  std::vector<fastjet::PseudoJet> particles;
+  PHG4TruthInfoContainer::Map primary_map = truthinfo->GetPrimaryMap();
+  for (PHG4TruthInfoContainer::ConstIterator iter = primary_map.begin(); 
+       iter != primary_map.end(); 
+       ++iter) {
+    PHG4Particle *part = iter->second;
+    fastjet::PseudoJet pseudojet (part->get_px(),
+				  part->get_py(),
+				  part->get_pz(),
+				  part->get_e());
+    pseudojet.set_user_index(part->get_track_id());
+    particles.push_back(pseudojet);
+  }
+
+  fastjet::JetDefinition jetdef(fastjet::antikt_algorithm,0.4,fastjet::Best);
+  fastjet::ClusterSequence jetFinder(particles,jetdef);
+  std::vector<fastjet::PseudoJet> fastjets = jetFinder.inclusive_jets();
+
+  for (unsigned int ijet = 0; ijet < fastjets.size(); ++ijet) {
+    cout << "  "
+	 << fastjets[ijet].perp() << ", "
+	 << fastjets[ijet].eta()  << ", "
+	 << fastjets[ijet].phi()  << endl;
+  }
   
   if (verbosity > 0) cout << "PHG4TruthJetReco::process_event -- exited" << endl;
 
