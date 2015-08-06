adcfeeb9a1bcb9acdfe5cd0b2e158f1bb8549810
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-08-05 20:24:17 -0400

    prep for merge from master

--- simulation/g4simulation/g4jets/JetReco.C
+++ simulation/g4simulation/g4jets/JetReco.C
@@ -154,7 +154,7 @@ void JetReco::FillJetNode(PHCompositeNode *topNode, int ipos, std::vector<Jet*> 
     jetmap->insert(jets[i]); // map takes ownership, sets unique id
   }
 
-  jetmap->identify();
+  jetmap->identify(); // report contents, debug statement
   
   return;
 }
