11f4aae3cb7b95da45bf297f9d072c8b01060c75
Nils Feege <nils.feege@stonybrook.edu>
2015-08-12 01:33:57 -0400

    Make projective crystal calorimeter separate class inheriting from non-projective version

--- simulation/g4simulation/g4detectors/PHG4ProjCrystalCalorimeterDetector.h
+++ simulation/g4simulation/g4detectors/PHG4ProjCrystalCalorimeterDetector.h
@@ -1,7 +1,8 @@
-#ifndef PHG4CrystalCalorimeterDetector_h
-#define PHG4CrystalCalorimeterDetector_h
+#ifndef PHG4ProjCrystalCalorimeterDetector_h
+#define PHG4ProjCrystalCalorimeterDetector_h
 
 #include <g4main/PHG4Detector.h>
+#include "PHG4CrystalCalorimeterDetector.h"
 
 #include <Geant4/globals.hh>
 #include <Geant4/G4Types.hh>
@@ -21,59 +22,67 @@ class G4VSolid;
 
 /**
  * \file ${file_name}
- * \brief Module to build forward sampling Hadron calorimeterr (endcap) in Geant4
+ * \brief Module to build crystal calorimeter (endcap) in Geant4
  * \author Nils Feege <nils.feege@stonybrook.edu>
  */
 
-class PHG4CrystalCalorimeterDetector: public PHG4Detector
+class PHG4ProjCrystalCalorimeterDetector: public PHG4CrystalCalorimeterDetector
 {
 
 public:
 
   //! constructor
-  PHG4CrystalCalorimeterDetector( PHCompositeNode *Node, const std::string &dnam="BLOCK" );
+  PHG4ProjCrystalCalorimeterDetector( PHCompositeNode *Node, const std::string &dnam="BLOCK" );
 
   //! destructor
-  virtual ~PHG4CrystalCalorimeterDetector();
+  virtual ~PHG4ProjCrystalCalorimeterDetector();
 
   //! construct
   virtual void Construct( G4LogicalVolume* world );
 
   //!@name volume accessors
-  virtual int IsInCrystalCalorimeter(G4VPhysicalVolume*) const;
+  int IsInCrystalCalorimeter(G4VPhysicalVolume*) const;
 
 
   //! Select mapping file for calorimeter tower
   virtual void SetTowerMappingFile( std::string filename ) {
-    _mapping_tower_file = filename;
+    _inputFile = filename;
   }
 
   //! Select mapping file for supermodule
   virtual void SetSupermoduleGeometry( std::string filename2 ) {
-    //_inputFile_4x4_construct = filename2;
+    _inputFile_4x4_construct = filename2;
   }
 
+  virtual void CrystalDimensions(G4double& dx_front, G4double& dy_front, G4double& dx_back, G4double& dy_back, G4double& dz);
 
-  void SetCrystalSize(G4double dx, G4double dy, G4double dz) {
-    _crystal_dx = dx;
-    _crystal_dy = dy;
-    _crystal_dz = dz;
+  virtual void SetDimensions(G4double dx_front, G4double dy_front, G4double dx_back, G4double dy_back, G4double dz) {
+  _dx_front = dx_front;
+  _dy_front = dy_front;
+  _dx_back = dx_back;
+  _dy_back = dy_back;
+  _dz_crystal = dz;
   }
+
+  virtual void CarbonFiberAdjustments(G4double& adjust_width, G4double& adjust_length);
+
+  virtual void CarbonFiberSpacing(G4double& CF_width, G4double& Air_CF, G4double& Air_Cry);
 
   void SetPlace( G4double place_in_x, G4double place_in_y, G4double place_in_z) {
     _place_in_x = place_in_x;
     _place_in_y = place_in_y;
     _place_in_z = place_in_z;
   }
-
   void SetXRot( G4double rot_in_x ) { _rot_in_x = rot_in_x; }
   void SetYRot( G4double rot_in_y ) { _rot_in_y = rot_in_y; }
   void SetZRot( G4double rot_in_z ) { _rot_in_z = rot_in_z; }
 
-  void SetMaterialCrystal( G4String material ) { _materialCrystal = material; }
+  void SetMaterial( G4String material ) { _materialCrystal = material; }
 
   void SetActive(const int i = 1) {_active = i;}
   void SetAbsorberActive(const int i = 1) {_absorberactive = i;}
+
+  void SetInput( G4String inFile ) { _inputFile = inFile; }
 
   int IsActive() const {return _active;}
 
@@ -87,8 +96,9 @@ public:
 
 private:
 
-  G4LogicalVolume* ConstructTower();
-  int PlaceTower(G4LogicalVolume* envelope , G4LogicalVolume* tower);
+  int ConstructProjectiveCrystals(G4LogicalVolume* envelope);
+  int Fill4x4Unit(G4LogicalVolume *crystal_logic);
+  int FillSpecialUnit(G4LogicalVolume *crystal_logic, G4int ident);
 
   /* Calorimeter envelope geometry */
   G4double _place_in_x;
@@ -108,10 +118,12 @@ private:
   G4double _sPhi;
   G4double _dPhi;
 
-  /* HCAL tower geometry */
-  G4double _crystal_dx;
-  G4double _crystal_dy;
-  G4double _crystal_dz;
+  /* crystal geometry */
+  G4double _dx_front;
+  G4double _dy_front;
+  G4double _dx_back;
+  G4double _dy_back;
+  G4double _dz_crystal;
 
   G4String _materialCrystal;
 
@@ -120,9 +132,10 @@ private:
   int _layer;
   int _blackhole;
 
-  std::string _towerlogicnameprefix;
+  std::string _crystallogicnameprefix;
   std::string _superdetector;
-  std::string _mapping_tower_file;
+  std::string _inputFile;
+  std::string _inputFile_4x4_construct;
 };
 
 #endif
