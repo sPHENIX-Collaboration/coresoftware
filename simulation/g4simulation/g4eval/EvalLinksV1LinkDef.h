139d19440d1f18c0e982301980f9eaba1738f1e5
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-08-14 20:08:41 -0400

    removing the dst interface, use the new function library to regenerate info as needed

--- simulation/g4simulation/g4eval/EvalLinksV1LinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class std::pair<unsigned long long,unsigned long long>+;
-#pragma link C++ class std::map< std::pair<unsigned long long,unsigned long long> , double >+;
-#pragma link C++ class std::pair< std::pair<unsigned long long,unsigned long long> , double >+;
-#pragma link C++ class EvalLinksV1+;
-
-#endif /* __CINT__ */
