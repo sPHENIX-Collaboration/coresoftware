7898ff3c816220ad55256c9236136160fc18e144
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-08-16 10:35:52 -0400

    compiling vertex eval skeleton

--- simulation/g4simulation/g4eval/SvtxVertexEval.h
+++ simulation/g4simulation/g4eval/SvtxVertexEval.h
@@ -10,6 +10,7 @@
 #include <g4hough/SvtxVertex.h>
 #include <g4main/PHG4Hit.h>
 #include <g4main/PHG4Particle.h>
+#include <g4main/PHG4VtxPoint.h>
 
 #include <set>
 #include <map>
@@ -29,15 +30,15 @@ public:
   SvtxHitEval*     get_hit_eval() {return _trackeval.get_hit_eval();}
   
   // backtrace through to PHG4Hits
-  std::set<PHG4Particles*>  all_truth_particles (SvtxVertex* vertex);
+  std::set<PHG4Particle*>  all_truth_particles (SvtxVertex* vertex);
   
   // backtrace through to PHG4VtxPoints
   std::set<PHG4VtxPoint*> all_truth_points           (SvtxVertex* vertex);
   PHG4VtxPoint*           max_truth_point_by_ntracks (SvtxVertex* vertex);
 
   // forwardtrace through to SvtxVertexs
-  std::set<SvtxVertex*> all_vertexes_from(PHG4VtxPoint* truthpoint);
-  SvtxVertex*           best_vertex_from(PHG4VtxPoint* truthpoint);
+  std::set<SvtxVertex*> all_vertexes_from (PHG4VtxPoint* truthpoint);
+  SvtxVertex*           best_vertex_from  (PHG4VtxPoint* truthpoint);
   
   // overlap calculations
   unsigned int get_ntracks_contribution(SvtxVertex* svtxvertex, PHG4VtxPoint* truthpoint);  
@@ -49,9 +50,9 @@ private:
   std::map<SvtxVertex*,std::set<PHG4Particle*> >               _cache_all_truth_particles;
   std::map<SvtxVertex*,std::set<PHG4VtxPoint*> >               _cache_all_truth_points;
   std::map<SvtxVertex*,PHG4VtxPoint*>                          _cache_max_truth_point_by_ntracks;
-  std::map<PHG4VtxPoint*,std::set<SvtxVertex*> >               _cache_all_vertexes_from_particle;
-  std::map<PHG4VtxPoint*,SvtxVertex* >                         _cache_best_vertex_from_particle;
-  std::map<std::pair<SvtxVertex*,PHG4VtxPoint*>, unsigned int> _cache_get_nclusters_contribution;
+  std::map<PHG4VtxPoint*,std::set<SvtxVertex*> >               _cache_all_vertexes_from_point;
+  std::map<PHG4VtxPoint*,SvtxVertex* >                         _cache_best_vertex_from_point;
+  std::map<std::pair<SvtxVertex*,PHG4VtxPoint*>, unsigned int> _cache_get_ntracks_contribution;
 };
 
 #endif // __SVTXVERTEXEVAL_H__
