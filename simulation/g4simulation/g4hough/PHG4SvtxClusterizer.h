a6e7db5dc9f9afc5da1a2ca849b4e080f846c629
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-08-18 23:05:04 -0400

    replace outdated node search with PHTypeNodeIterator by getClass, use const args where appropriate, use initializer, cleanup

--- simulation/g4simulation/g4hough/PHG4SvtxClusterizer.h
+++ simulation/g4simulation/g4hough/PHG4SvtxClusterizer.h
@@ -13,8 +13,8 @@ class PHG4SvtxClusterizer : public SubsysReco {
 
 public:
 
-  PHG4SvtxClusterizer(const char * name = "PHG4SvtxClusterizer");
-  ~PHG4SvtxClusterizer(){}
+  PHG4SvtxClusterizer(const std::string &name = "PHG4SvtxClusterizer");
+  virtual ~PHG4SvtxClusterizer(){}
   
   //! module initialization
   int Init(PHCompositeNode *topNode){return 0;}
@@ -29,30 +29,30 @@ public:
   int End(PHCompositeNode *topNode){return 0;}
   
   //! set an energy requirement relative to the thickness MIP expectation
-  void set_threshold(float fraction_of_mip) {
+  void set_threshold(const float fraction_of_mip) {
     _fraction_of_mip = fraction_of_mip;
   }
-  float get_threshold_by_layer(int layer) {
+  float get_threshold_by_layer(const int layer) const {
     if (_thresholds_by_layer.find(layer) == _thresholds_by_layer.end()) return 0.0;
-    return _thresholds_by_layer[layer];
+    return _thresholds_by_layer.find(layer)->second;
   }
   
   //! option to turn off z-dimension clustering
-  void set_z_clustering(int layer, bool make_z_clustering) {
+  void set_z_clustering(const int layer, const bool make_z_clustering) {
     _make_z_clustering.insert(std::make_pair(layer,make_z_clustering));
   }
-  bool get_z_clustering(int layer) {
+  bool get_z_clustering(const int layer) const {
     if (_make_z_clustering.find(layer) == _make_z_clustering.end()) return true;
-    return _make_z_clustering[layer];
+    return _make_z_clustering.find(layer)->second;
   }
 
   //! option to turn on/off energy weighted clustering
-  void set_energy_weighting(int layer, bool make_e_weights) {
+  void set_energy_weighting(const int layer, const bool make_e_weights) {
     _make_e_weights.insert(std::make_pair(layer,make_e_weights));
   }
-  bool get_energy_weighting(int layer) {
+  bool get_energy_weighting(const int layer) const {
     if (_make_e_weights.find(layer) == _make_e_weights.end()) return false;
-    return _make_e_weights[layer];
+    return _make_e_weights.find(layer)->second;
   }  
 
 private:
