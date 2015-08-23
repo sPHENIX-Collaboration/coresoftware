ec6bc8ff6f1bcc9c672e815e60842bb827bf3033
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-08-23 02:54:47 -0400

    optimized node access for svtxhiteval

--- simulation/g4simulation/g4eval/SvtxHitEval.C
+++ simulation/g4simulation/g4eval/SvtxHitEval.C
@@ -20,7 +20,12 @@
 using namespace std;
 
 SvtxHitEval::SvtxHitEval(PHCompositeNode* topNode)
-  : _topNode(topNode),
+  : _hitmap(NULL),
+    _g4cells_svtx(NULL),
+    _g4cells_tracker(NULL),
+    _g4hits_svtx(NULL),
+    _g4hits_tracker(NULL),
+    _truthinfo(NULL),
     _do_cache(true),
     _cache_all_truth_hits(),
     _cache_max_truth_hit_by_energy(),
@@ -31,6 +36,7 @@ SvtxHitEval::SvtxHitEval(PHCompositeNode* topNode)
     _cache_best_hit_from_g4hit(),
     _cache_get_energy_contribution_g4particle(),
     _cache_get_energy_contribution_g4hit() {
+  get_node_pointers(topNode);
 }
 
 void SvtxHitEval::next_event(PHCompositeNode* topNode) {
@@ -45,23 +51,15 @@ void SvtxHitEval::next_event(PHCompositeNode* topNode) {
   _cache_get_energy_contribution_g4particle.clear();
   _cache_get_energy_contribution_g4hit.clear();
 
-  _topNode = topNode;  
+  get_node_pointers(topNode);
 }
 
 PHG4CylinderCell* SvtxHitEval::get_cell(SvtxHit* hit) {
-
-  // need things off of the DST...
-  PHG4CylinderCellContainer* g4cells_svtx    = findNode::getClass<PHG4CylinderCellContainer>(_topNode,"G4CELL_SVTX");
-  PHG4CylinderCellContainer* g4cells_tracker = findNode::getClass<PHG4CylinderCellContainer>(_topNode,"G4CELL_SILICON_TRACKER");
-  if (!g4cells_svtx && !g4cells_tracker) {
-    cerr << PHWHERE << " ERROR: Can't find G4CELL_SVTX or G4CELL_SILICON_TRACKER" << endl;
-    exit(-1);
-  }
- 
+  
   // hop from reco hit to g4cell
   PHG4CylinderCell* cell = NULL;
-  if (!cell&&g4cells_svtx)    cell = g4cells_svtx->findCylinderCell(hit->get_cellid());
-  if (!cell&&g4cells_tracker) cell = g4cells_tracker->findCylinderCell(hit->get_cellid()); 
+  if (!cell&&_g4cells_svtx)    cell = _g4cells_svtx->findCylinderCell(hit->get_cellid());
+  if (!cell&&_g4cells_tracker) cell = _g4cells_tracker->findCylinderCell(hit->get_cellid()); 
   return cell;
 }
 
@@ -71,27 +69,12 @@ std::set<PHG4Hit*> SvtxHitEval::all_truth_hits(SvtxHit* hit) {
     return _cache_all_truth_hits[hit];
   }
   
-  // need things off of the DST...
-  PHG4CylinderCellContainer* g4cells_svtx    = findNode::getClass<PHG4CylinderCellContainer>(_topNode,"G4CELL_SVTX");
-  PHG4CylinderCellContainer* g4cells_tracker = findNode::getClass<PHG4CylinderCellContainer>(_topNode,"G4CELL_SILICON_TRACKER");
-  if (!g4cells_svtx && !g4cells_tracker) {
-    cerr << PHWHERE << " ERROR: Can't find G4CELL_SVTX or G4CELL_SILICON_TRACKER" << endl;
-    exit(-1);
-  }
-    
-  PHG4HitContainer* g4hits_svtx    = findNode::getClass<PHG4HitContainer>(_topNode,"G4HIT_SVTX");
-  PHG4HitContainer* g4hits_tracker = findNode::getClass<PHG4HitContainer>(_topNode,"G4HIT_SILICON_TRACKER");
-  if (!g4hits_svtx && !g4hits_tracker) {
-    cerr << PHWHERE << " ERROR: Can't find G4HIT_SVTX or G4HIT_SILICON_TRACKER" << endl;
-    exit(-1);
-  }
-
   std::set<PHG4Hit*> truth_hits;
   
   // hop from reco hit to g4cell
   PHG4CylinderCell *cell = NULL;
-  if (!cell&&g4cells_svtx)    cell = g4cells_svtx->findCylinderCell(hit->get_cellid());
-  if (!cell&&g4cells_tracker) cell = g4cells_tracker->findCylinderCell(hit->get_cellid());
+  if (!cell&&_g4cells_svtx)    cell = _g4cells_svtx->findCylinderCell(hit->get_cellid());
+  if (!cell&&_g4cells_tracker) cell = _g4cells_tracker->findCylinderCell(hit->get_cellid());
   if (!cell) return truth_hits;
 
   // loop over all the g4hits in this cell
@@ -100,8 +83,8 @@ std::set<PHG4Hit*> SvtxHitEval::all_truth_hits(SvtxHit* hit) {
        ++g4iter) {
       
     PHG4Hit* g4hit = NULL;
-    if (!g4hit&&g4hits_svtx)    g4hit = g4hits_svtx->findHit(g4iter->first);
-    if (!g4hit&&g4hits_tracker) g4hit = g4hits_tracker->findHit(g4iter->first);
+    if (!g4hit&&_g4hits_svtx)    g4hit = _g4hits_svtx->findHit(g4iter->first);
+    if (!g4hit&&_g4hits_tracker) g4hit = _g4hits_tracker->findHit(g4iter->first);
     if (!g4hit) continue;
     
     // fill output set
@@ -149,18 +132,12 @@ std::set<PHG4Particle*> SvtxHitEval::all_truth_particles(SvtxHit* hit) {
   
   std::set<PHG4Hit*> g4hits = all_truth_hits(hit);
   if (g4hits.empty()) return truth_particles;
-  
-  PHG4TruthInfoContainer* truthinfo = findNode::getClass<PHG4TruthInfoContainer>(_topNode,"G4TruthInfo");
-  if (!truthinfo) {
-    cerr << PHWHERE << " ERROR: Can't find G4TruthInfo" << endl;
-    exit(-1);
-  }
 
   for (std::set<PHG4Hit*>::iterator iter = g4hits.begin();
        iter != g4hits.end();
        ++iter) {
     PHG4Hit* g4hit = *iter;
-    PHG4Particle* particle = truthinfo->GetHit( g4hit->get_trkid() );
+    PHG4Particle* particle = _truthinfo->GetHit( g4hit->get_trkid() );
     if (!particle) continue;
     truth_particles.insert(particle);
   }
@@ -205,19 +182,12 @@ std::set<SvtxHit*> SvtxHitEval::all_hits_from(PHG4Particle* g4particle) {
       (_cache_all_hits_from_particle.find(g4particle) != _cache_all_hits_from_particle.end())) {
     return _cache_all_hits_from_particle[g4particle];
   }
-  
-  // need things off of the DST...
-  SvtxHitMap* hitmap = findNode::getClass<SvtxHitMap>(_topNode,"SvtxHitMap");
-  if (!hitmap) {
-    cerr << PHWHERE << " ERROR: Can't find SvtxHitMap" << endl;
-    exit(-1);
-  }
-
+ 
   std::set<SvtxHit*> hits;
   
   // loop over all the hits
-  for (SvtxHitMap::Iter iter = hitmap->begin();
-       iter != hitmap->end();
+  for (SvtxHitMap::Iter iter = _hitmap->begin();
+       iter != _hitmap->end();
        ++iter) {
 
     SvtxHit* hit = &iter->second;
@@ -246,18 +216,11 @@ std::set<SvtxHit*> SvtxHitEval::all_hits_from(PHG4Hit* g4hit) {
     return _cache_all_hits_from_g4hit[g4hit];
   }
   
-  // need things off of the DST...
-  SvtxHitMap* hitmap = findNode::getClass<SvtxHitMap>(_topNode,"SvtxHitMap");
-  if (!hitmap) {
-    cerr << PHWHERE << " ERROR: Can't find SvtxHitMap" << endl;
-    exit(-1);
-  }
-
   std::set<SvtxHit*> hits;
   
   // loop over all the hits
-  for (SvtxHitMap::Iter iter = hitmap->begin();
-       iter != hitmap->end();
+  for (SvtxHitMap::Iter iter = _hitmap->begin();
+       iter != _hitmap->end();
        ++iter) {
 
     SvtxHit* hit = &iter->second;
@@ -284,13 +247,6 @@ SvtxHit* SvtxHitEval::best_hit_from(PHG4Hit* g4hit) {
   if ((_do_cache) &&
       (_cache_best_hit_from_g4hit.find(g4hit) != _cache_best_hit_from_g4hit.end())) {
     return _cache_best_hit_from_g4hit[g4hit];
-  }
-  
-  // need things off of the DST...
-  SvtxHitMap* hitmap = findNode::getClass<SvtxHitMap>(_topNode,"SvtxHitMap");
-  if (!hitmap) {
-    cerr << PHWHERE << " ERROR: Can't find SvtxHitMap" << endl;
-    exit(-1);
   }
 
   SvtxHit* best_hit = NULL;
@@ -361,4 +317,37 @@ float SvtxHitEval::get_energy_contribution(SvtxHit* hit, PHG4Hit* g4hit) {
   if (_do_cache) _cache_get_energy_contribution_g4hit.insert(make_pair(make_pair(hit,g4hit),energy));
   
   return energy;
+}
+
+void SvtxHitEval::get_node_pointers(PHCompositeNode* topNode) {
+
+  // need things off of the DST...
+  _hitmap = findNode::getClass<SvtxHitMap>(topNode,"SvtxHitMap");
+  if (!_hitmap) {
+    cerr << PHWHERE << " ERROR: Can't find SvtxHitMap" << endl;
+    exit(-1);
+  }
+
+  // need things off of the DST...
+  _g4cells_svtx    = findNode::getClass<PHG4CylinderCellContainer>(topNode,"G4CELL_SVTX");
+  _g4cells_tracker = findNode::getClass<PHG4CylinderCellContainer>(topNode,"G4CELL_SILICON_TRACKER");
+  if (!_g4cells_svtx && !_g4cells_tracker) {
+    cerr << PHWHERE << " ERROR: Can't find G4CELL_SVTX or G4CELL_SILICON_TRACKER" << endl;
+    exit(-1);
+  }
+    
+  _g4hits_svtx    = findNode::getClass<PHG4HitContainer>(topNode,"G4HIT_SVTX");
+  _g4hits_tracker = findNode::getClass<PHG4HitContainer>(topNode,"G4HIT_SILICON_TRACKER");
+  if (!_g4hits_svtx && !_g4hits_tracker) {
+    cerr << PHWHERE << " ERROR: Can't find G4HIT_SVTX or G4HIT_SILICON_TRACKER" << endl;
+    exit(-1);
+  }
+  
+  _truthinfo = findNode::getClass<PHG4TruthInfoContainer>(topNode,"G4TruthInfo");
+  if (!_truthinfo) {
+    cerr << PHWHERE << " ERROR: Can't find G4TruthInfo" << endl;
+    exit(-1);
+  }
+  
+  return;
 }
