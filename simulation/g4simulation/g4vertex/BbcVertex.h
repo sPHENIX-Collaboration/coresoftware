3083a6ff87e849f42139894d0566be3f13293e22
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-08-28 04:01:11 -0400

    versioned BbcVertex

--- simulation/g4simulation/g4vertex/BbcVertex.h
+++ simulation/g4simulation/g4vertex/BbcVertex.h
@@ -2,47 +2,43 @@
 #define __BBCVERTEX_H__
 
 #include <phool/PHObject.h>
-#include <vector>
-#include <set>
+#include <cmath>
 #include <iostream>
 
 class BbcVertex : public PHObject {
 
 public:
   
-  BbcVertex();
-  virtual ~BbcVertex();
+  virtual ~BbcVertex() {}
 
   // PHObject virtual overloads
   
-  void         identify(std::ostream& os = std::cout) const;
-  void         Reset();
-  int          IsValid() const;
+  virtual void         identify(std::ostream& os = std::cout) const {os << "BbcVertex base" << std::endl;}
+  virtual BbcVertex*   Clone()                                      {return NULL;}
+  virtual void         Reset()                                      {}
+  virtual int          IsValid() const                              {return 0;}
 
   // vertex info
   
-  unsigned int get_id() const                        {return _id;}
-  void         set_id(unsigned int id)               {_id = id;}
+  virtual unsigned int get_id() const           {return 0xFFFFFFFF;}
+  virtual void         set_id(unsigned int id)  {}
   
-  float        get_t0() const                        {return _t0;}
-  void         set_t0(float t0)                      {_t0 = t0;}
+  virtual float        get_t0() const           {return NAN;}
+  virtual void         set_t0(float t0)         {}
 
-  float        get_t0_err() const                    {return _t0_err;}
-  void         set_t0_err(float t0_err)              {_t0_err = t0_err;}
+  virtual float        get_t0_err() const       {return NAN;}
+  virtual void         set_t0_err(float t0_err) {}
   
-  float        get_z() const                         {return _z;}
-  void         set_z(float z)                        {_z = z;}
+  virtual float        get_z() const            {return NAN;}
+  virtual void         set_z(float z)           {}
 
-  float        get_z_err() const                     {return _z_err;}
-  void         set_z_err(float z_err)                {_z_err = z_err;}
+  virtual float        get_z_err() const        {return NAN;}
+  virtual void         set_z_err(float z_err)   {}
+
+protected:
+  BbcVertex() {}
   
 private:
-  
-  unsigned int                     _id;        //< unique identifier within container
-  float                            _t0;        //< collision time
-  float                            _t0_err;    //< collision time uncertainty
-  float                            _z;         //< collision position z
-  float                            _z_err;     //< collision position z uncertainty
   
   ClassDef(BbcVertex, 1);
 };
---
+++ simulation/g4simulation/g4vertex/BbcVertex_v1.h
@@ -0,0 +1,54 @@
+#ifndef __BBCVERTEX_V1_H__
+#define __BBCVERTEX_V1_H__
+
+#include "BbcVertex.h"
+
+#include <phool/PHObject.h>
+#include <vector>
+#include <set>
+#include <iostream>
+
+class BbcVertex_v1 : public BbcVertex {
+
+public:
+  
+  BbcVertex_v1();
+  virtual ~BbcVertex_v1();
+
+  // PHObject virtual overloads
+  
+  void         identify(std::ostream& os = std::cout) const;
+  BbcVertex*   Clone();
+  void         Reset();
+  int          IsValid() const;
+
+  // vertex info
+  
+  unsigned int get_id() const                        {return _id;}
+  void         set_id(unsigned int id)               {_id = id;}
+  
+  float        get_t0() const                        {return _t0;}
+  void         set_t0(float t0)                      {_t0 = t0;}
+
+  float        get_t0_err() const                    {return _t0_err;}
+  void         set_t0_err(float t0_err)              {_t0_err = t0_err;}
+  
+  float        get_z() const                         {return _z;}
+  void         set_z(float z)                        {_z = z;}
+
+  float        get_z_err() const                     {return _z_err;}
+  void         set_z_err(float z_err)                {_z_err = z_err;}
+  
+private:
+  
+  unsigned int                     _id;        //< unique identifier within container
+  float                            _t0;        //< collision time
+  float                            _t0_err;    //< collision time uncertainty
+  float                            _z;         //< collision position z
+  float                            _z_err;     //< collision position z uncertainty
+  
+  ClassDef(BbcVertex_v1, 1);
+};
+
+#endif
+
