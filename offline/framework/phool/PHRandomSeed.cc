87632827dda03816f4d5540db95786a11df17450
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-09-01 15:52:36 -0400

    get random seed from /dev/urandom

---
+++ offline/framework/phool/PHRandomSeed.cc
@@ -0,0 +1,27 @@
+#include "PHRandomSeed.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+unsigned int PHRandomSeed()
+{
+  //  that's when we switch to c++11
+  //  std::random_device rdev;
+  //  uint32_t random_seed = rdev();
+  unsigned int random_seed;
+  // use /dev/urandom which unlike /dev/random is non blocking 
+  // even if entropy is too low. Not for use in cryptographic apps but
+  // good enough for a random seed
+  FILE *fp = fopen("/dev/urandom", "r");
+  if (!fp)
+    {
+      cout << "could not open /dev/urandom for seed" << endl;
+      exit(1);
+    }
+  fread(&random_seed, sizeof(random_seed), 1,fp);
+  fclose(fp);
+  return random_seed;
+}
