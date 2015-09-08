a992ddb6213ae7403d516517023a0f3ab6c99c3c
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-09-08 11:48:11 -0400

    g4main & g4field verbosity raising by one unit

--- simulation/g4simulation/g4main/PHG4Reco.cc
+++ simulation/g4simulation/g4main/PHG4Reco.cc
@@ -137,7 +137,7 @@ PHG4Reco::~PHG4Reco( void )
 //_________________________________________________________________
 int PHG4Reco::Init( PHCompositeNode* topNode )
 {
-  if (verbosity >= 0) {
+  if (verbosity > 0) {
     cout << "========================= PHG4Reco::Init() ================================" << endl;
   }
   recoConsts *rc = recoConsts::instance();
@@ -150,18 +150,18 @@ int PHG4Reco::Init( PHCompositeNode* topNode )
       G4Seed(PHRandomSeed());
     }
   // create GEANT run manager
-  if (verbosity > 0) cout << "PHG4Reco::Init - create run manager" << endl;
+  if (verbosity > 1) cout << "PHG4Reco::Init - create run manager" << endl;
   runManager_ = new G4RunManager();
 
   DefineMaterials();
 
   //setup the constant field
-  if (verbosity > 0) cout << "PHG4Reco::Init - create magnetic field setup" << endl;
+  if (verbosity > 1) cout << "PHG4Reco::Init - create magnetic field setup" << endl;
   if (fieldmapfile != "NONE")
     {
       field_ = new G4TBMagneticFieldSetup(fieldmapfile, mapdim) ;
       magfield = field_->get_magfield_at_000(2); // get the z coordinate at 0/0/0
-      if (verbosity > 0)
+      if (verbosity > 1)
 	{
 	  cout << "magfield in PHG4Reco: " << magfield << endl;
 	}
@@ -236,7 +236,7 @@ int PHG4Reco::Init( PHCompositeNode* topNode )
     }
 
   // create phenix detector, add subsystems, and register to GEANT
-  if (verbosity > 0) cout << "PHG4Reco::Init - create detector" << endl;
+  if (verbosity > 1) cout << "PHG4Reco::Init - create detector" << endl;
   detector_ = new PHG4PhenixDetector();
   detector_->Verbosity(verbosity);
   detector_->SetWorldSizeX(WorldSize[0]*cm);
@@ -276,7 +276,7 @@ int PHG4Reco::Init( PHCompositeNode* topNode )
       PHG4SteppingAction *action = g4sub->GetSteppingAction();
       if (action)
 	{
-	  if (verbosity > 0)
+	  if (verbosity > 1)
 	    {
 	      cout << "Adding steppingaction for " << g4sub->Name() << endl;
 	    }
@@ -362,7 +362,7 @@ int PHG4Reco::Init( PHCompositeNode* topNode )
   G4HadronicProcessStore::Instance()->SetVerbose(0);
   G4LossTableManager::Instance()->SetVerbose(0);
 
-  if (verbosity >= 0) {
+  if (verbosity > 0) {
     cout << "===========================================================================" << endl;
   }
   
@@ -801,7 +801,7 @@ PMMA      -3  12.01 1.008 15.99  6.  1.  8.  1.19  3.6  5.7  1.4
       LiF->AddElement(Li, natoms = 1);
       LiF->AddElement(F, natoms = 1);
 
-      if (verbosity > 0)
+      if (verbosity > 1)
 	{
 	  cout << "g4_lif: " << g4_lif << endl;
 	  cout << "LiF: " << LiF << endl;
