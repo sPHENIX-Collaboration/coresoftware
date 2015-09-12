03a1a280f01c38539ead26332f613aaf6b8ace36
Jin Huang <jhuang@bnl.gov>
2015-09-12 00:40:28 -0400

    impliment calibration

--- simulation/g4simulation/g4cemc/RawTowerCalibration.cc
+++ simulation/g4simulation/g4cemc/RawTowerCalibration.cc
@@ -16,53 +16,40 @@
 #include <iostream>
 #include <stdexcept>
 #include <map>
+#include <cassert>
 
 using namespace std;
 
-RawTowerCalibration::RawTowerCalibration(const std::string& name):
-  SubsysReco(name),
-  _towers(NULL),
-  rawtowergeom(NULL),
-  detector("NONE"),
-  _cell_binning(PHG4CylinderCellDefs::undefined),
-  emin(1e-6),
-  chkenergyconservation(0),
-  _nlayers(-1),
-  _nphibins(-1),
-  _netabins(-1),
-  _etamin(NAN),
-  _phimin(NAN),
-  _etastep(NAN),
-  _phistep(NAN),
-  _timer( PHTimeServer::get()->insert_new(name) )
-{}
+RawTowerCalibration::RawTowerCalibration(const std::string& name) :
+    SubsysReco(name), _calib_algorithm(kNo_calibration), //
+    _calib_towers(NULL), _raw_towers(NULL), //
+    rawtowergeom(NULL), //
+    detector("NONE"), //
+    _calib_tower_node_prefix("CALIB"), _raw_tower_node_prefix("RAW"), //
+    //! pedstal in unit of ADC
+    _pedstal_ADC(NAN),
+    //! calibration constant in unit of GeV per ADC
+    _calib_const_GeV_ADC(NAN), //
+    _zero_suppression_GeV(0), //
+    _timer(PHTimeServer::get()->insert_new(name))
+{
+}
 
-int 
+int
 RawTowerCalibration::InitRun(PHCompositeNode *topNode)
 {
-  std::string geonodename = "CYLINDERCELLGEOM_" + detector;
-  PHG4CylinderCellGeomContainer* cellgeos = findNode::getClass<PHG4CylinderCellGeomContainer>(topNode, geonodename.c_str());
-  if (!cellgeos)
-    {
-      std::cerr << PHWHERE << " " << geonodename << " Node missing, doing nothing." << std::endl;
-      throw std::runtime_error("Failed to find " + geonodename + " node in RawTowerCalibration::CreateNodes");
-    }
-
-  // fill the number of layers in the calorimeter
-  _nlayers = cellgeos->get_NLayers();
-
-
   PHNodeIterator iter(topNode);
 
   // Looking for the DST node
   PHCompositeNode *dstNode;
-  dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "DST"));
+  dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode",
+      "DST"));
   if (!dstNode)
     {
-      std::cout << PHWHERE << "DST Node missing, doing nothing." << std::endl;
+      std::cout << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << "DST Node missing, doing nothing." << std::endl;
       exit(1);
     }
-
 
   try
     {
@@ -71,237 +58,99 @@ RawTowerCalibration::InitRun(PHCompositeNode *topNode)
   catch (std::exception& e)
     {
       std::cout << e.what() << std::endl;
-      //exit(1);
+      return Fun4AllReturnCodes::ABORTRUN;
     }
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-int 
+int
 RawTowerCalibration::process_event(PHCompositeNode *topNode)
 {
   if (verbosity)
     {
-      std::cout << PHWHERE << "Process event entered" << std::endl;
+      std::cout << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__<< "Process event entered" << std::endl;
     }
 
+  RawTowerContainer::ConstRange begin_end = _raw_towers->getTowers();
+  RawTowerContainer::ConstIterator rtiter;
+  for (rtiter = begin_end.first; rtiter != begin_end.second; ++rtiter)
+    {
+      const RawTowerDefs::keytype key = rtiter->first;
+      const RawTower *raw_tower = rtiter->second;
+      assert(raw_tower);
 
+      if (_calib_algorithm == kNo_calibration)
+        {
+          if (raw_tower->get_energy() > _zero_suppression_GeV)
+            {
+              _calib_towers->AddTower(key, new RawTowerv1(*raw_tower));
+            }
+        }
+      else if (_calib_algorithm == kSimple_linear_calibration)
+        {
+          const double raw_energy = raw_tower->get_energy();
+          const double calib_energy = (raw_energy - _pedstal_ADC)
+              * _calib_const_GeV_ADC;
 
+          if (calib_energy > _zero_suppression_GeV)
+            {
+              RawTower *calib_tower = new RawTowerv1(*raw_tower);
+              calib_tower->set_energy(calib_energy);
+              _calib_towers->AddTower(key, calib_tower);
+            }
+        }
+      else
+        {
+          std::cout << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+              << " invalid calibration algorithm #" << _calib_algorithm
+              << std::endl;
+
+          return Fun4AllReturnCodes::ABORTRUN;
+        }
+    } //  for (rtiter = begin_end.first; rtiter != begin_end.second; ++rtiter)
+
+  if (verbosity)
+    {
+      std::cout << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << "input sum energy = " << _raw_towers->getTotalEdep()
+          << ", output sum digitalized value = " << _calib_towers->getTotalEdep()
+          << std::endl;
+    }
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-int 
+int
 RawTowerCalibration::End(PHCompositeNode *topNode)
 {
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-void 
+void
 RawTowerCalibration::CreateNodes(PHCompositeNode *topNode)
 {
 
   PHNodeIterator iter(topNode);
-  PHCompositeNode *runNode = static_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "RUN"));
+  PHCompositeNode *runNode = static_cast<PHCompositeNode*>(iter.findFirst(
+      "PHCompositeNode", "RUN"));
   if (!runNode)
     {
-      std::cerr << PHWHERE << "Run Node missing, doing nothing." << std::endl;
-      throw std::runtime_error("Failed to find Run node in RawTowerCalibration::CreateNodes");
+      std::cerr << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << "Run Node missing, doing nothing." << std::endl;
+      throw std::runtime_error(
+          "Failed to find Run node in RawTowerCalibration::CreateNodes");
     }
 
-  // get the cell geometry and build up the tower geometry object
-  std::string geonodename = "CYLINDERCELLGEOM_" + detector;
-  PHG4CylinderCellGeomContainer* cellgeos = findNode::getClass<PHG4CylinderCellGeomContainer>(topNode, geonodename.c_str());
-  if (!cellgeos)
-    {
-      std::cerr << PHWHERE << " " << geonodename << " Node missing, doing nothing." << std::endl;
-      throw std::runtime_error("Failed to find " + geonodename + " node in RawTowerCalibration::CreateNodes");
-    }
   TowerGeomNodeName = "TOWERGEOM_" + detector;
-  rawtowergeom =  findNode::getClass<RawTowerGeom>(topNode, TowerGeomNodeName.c_str());
-  if (! rawtowergeom)
+  rawtowergeom = findNode::getClass<RawTowerGeom>(topNode,
+      TowerGeomNodeName.c_str());
+  if (!rawtowergeom)
     {
-
-      rawtowergeom = new RawTowerGeomv2();
-      PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(rawtowergeom, TowerGeomNodeName.c_str(), "PHObject");
-      runNode->addNode(newNode);
-    }
-  // fill the number of layers in the calorimeter
-  _nlayers = cellgeos->get_NLayers();
-
-  // Create the tower nodes on the tree
-  std::map<int, PHG4CylinderCellGeom *>::const_iterator miter;
-  std::pair <std::map<int, PHG4CylinderCellGeom *>::const_iterator, std::map<int, PHG4CylinderCellGeom *>::const_iterator> begin_end = cellgeos->get_begin_end();
-  int ifirst = 1;
-  int first_layer = -1;
-  PHG4CylinderCellGeom * first_cellgeo = NULL;
-  double inner_radius = 0;
-  double thickness = 0;
-  for (miter = begin_end.first; miter != begin_end.second; ++miter)
-    {
-      PHG4CylinderCellGeom *cellgeo = miter->second;
-      first_cellgeo = miter->second;
-
-      if (verbosity)
-	{
-	  cellgeo->identify();
-	}
-      thickness += cellgeo->get_thickness();
-      if (ifirst)
-	{
-	  _cell_binning = cellgeo->get_binning();
-	  _nphibins = cellgeo->get_phibins();
-	  _phimin = cellgeo->get_phimin();
-	  _phistep = cellgeo->get_phistep();
-          if (_cell_binning == PHG4CylinderCellDefs::etaphibinning
-              or _cell_binning == PHG4CylinderCellDefs::etaslatbinning)
-            {
-              _netabins = cellgeo->get_etabins();
-              _etamin = cellgeo->get_etamin();
-              _etastep = cellgeo->get_etastep();
-            }
-          else if (_cell_binning == PHG4CylinderCellDefs::sizebinning)
-            {
-              _netabins = cellgeo->get_zbins();// bin eta in the same number of z bins
-            }
-          else if (_cell_binning == PHG4CylinderCellDefs::spacalbinning)
-            {
-              // use eta definiton for each row of towers
-              _netabins = cellgeo->get_etabins();
-            }
-          else
-            {
-              cout <<"RawTowerCalibration::CreateNodes::"<<Name()
-		   <<" - Fatal Error - unsupported cell binning method "<<_cell_binning<<endl;
-            }
-	  inner_radius = cellgeo->get_radius();
-	  first_layer = cellgeo->get_layer();
-	  ifirst = 0;
-	}
-      else
-	{
-          if (_cell_binning != cellgeo->get_binning())
-            {
-              cout << "inconsistent binning method from " << _cell_binning
-             << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_binning()
-             << endl;
-              exit(1);
-            }
-	  if (inner_radius > cellgeo->get_radius())
-	    {
-	      cout << "radius of layer " << cellgeo->get_layer() 
-		   << " is " << cellgeo->get_radius()
-		   << " which smaller than radius " << inner_radius
-		   << " of first layer in list " << first_layer
-		   << endl;
-	    }
-	  if (_nphibins != cellgeo->get_phibins())
-	    {
-	      cout << "mixing number of phibins, fisrt layer: " << _nphibins
-		   << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_phibins()
-		   << endl;
-	      exit(1);
-	    }
-	  if ( _phimin != cellgeo->get_phimin())
-	    {
-	      cout << "mixing number of phimin, fisrt layer: " << _phimin
-		   << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_phimin()
-		   << endl;
-	      exit(1);
-	    }
-	  if (_phistep != cellgeo->get_phistep())
-	    {
-	      cout << "mixing phisteps first layer: " << _phistep
-		   << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_phistep()
-		   << " diff: " << _phistep - cellgeo->get_phistep()
-		   << endl;
-	      exit(1);
-	    }
-          if (_cell_binning == PHG4CylinderCellDefs::etaphibinning
-              or _cell_binning == PHG4CylinderCellDefs::etaslatbinning)
-            {
-              if (_netabins != cellgeo->get_etabins())
-                {
-                  cout << "mixing number of_netabins , fisrt layer: " << _netabins
-                 << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_etabins()
-                 << endl;
-                  exit(1);
-                }
-              if (fabs(_etamin - cellgeo->get_etamin()) > 1e-9)
-                {
-                  cout << "mixing etamin, fisrt layer: " << _etamin << " layer "
-                      << cellgeo->get_layer() << ": " << cellgeo->get_etamin()
-                      << " diff: " << _etamin - cellgeo->get_etamin() << endl;
-                  exit(1);
-                }
-              if (fabs(_etastep - cellgeo->get_etastep()) > 1e-9)
-                {
-                  cout << "mixing eta steps first layer: " << _etastep
-                      << " layer " << cellgeo->get_layer() << ": "
-                      << cellgeo->get_etastep() << " diff: "
-                      << _etastep - cellgeo->get_etastep() << endl;
-                  exit(1);
-                }
-            }
-
-          else if (_cell_binning == PHG4CylinderCellDefs::sizebinning)
-            {
-
-              if (_netabins != cellgeo->get_zbins())
-                {
-                  cout << "mixing number of z bins , fisrt layer: " << _netabins
-                 << " layer " << cellgeo->get_layer() << ": " << cellgeo->get_zbins()
-                 << endl;
-                  exit(1);
-                }
-            }
-	}
-    }
-  rawtowergeom->set_radius(inner_radius);
-  rawtowergeom->set_thickness(thickness);
-  rawtowergeom->set_phibins(_nphibins);
-  rawtowergeom->set_phistep(_phistep);
-  rawtowergeom->set_phimin(_phimin);
-  rawtowergeom->set_etabins(_netabins);
-
-  if (first_cellgeo == NULL)
-    {
-      cout <<"RawTowerCalibration::CreateNodes - ERROR - can not find first layer of cells "<<endl;
-
-      exit(1);
-    }
-
-  if (_cell_binning == PHG4CylinderCellDefs::etaphibinning
-      or _cell_binning == PHG4CylinderCellDefs::etaslatbinning
-      or _cell_binning == PHG4CylinderCellDefs::spacalbinning
-      )
-    {
-//  rawtowergeom->set_etamin(_etamin);
-//  rawtowergeom->set_etastep(_etastep);
-      for (int ibin = 0; ibin<first_cellgeo->get_etabins(); ibin++)
-        {
-
-          const pair<double, double> range = first_cellgeo -> get_etabounds(ibin);
-
-          rawtowergeom->set_etabounds(ibin, range);
-        }
-
-    }
-  else if (_cell_binning == PHG4CylinderCellDefs::sizebinning)
-    {
-      for (int ibin = 0; ibin<first_cellgeo->get_zbins(); ibin++)
-        {
-
-          const pair<double, double> z_range = first_cellgeo -> get_zbounds(ibin);
-          const double r = first_cellgeo->get_radius();
-          const double eta1 = -log (tan(atan2(r, z_range.first)/2.) );
-          const double eta2 = -log (tan(atan2(r, z_range.second)/2.) );
-          rawtowergeom->set_etabounds(ibin, make_pair(eta1, eta2));
-        }
-
-    }
-  else
-    {
-      cout <<"RawTowerCalibration::CreateNodes - ERROR - unsupported cell geometry "<<_cell_binning<<endl;
-      exit(1);
+      std::cerr << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << " " << TowerGeomNodeName << " Node missing, doing bail out!"
+          << std::endl;
+      throw std::runtime_error(
+          "Failed to find " + TowerGeomNodeName
+              + " node in RawTowerCalibration::CreateNodes");
     }
 
   if (verbosity >= 1)
@@ -309,22 +158,45 @@ RawTowerCalibration::CreateNodes(PHCompositeNode *topNode)
       rawtowergeom->identify();
     }
 
-  PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "DST"));
+  PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst(
+      "PHCompositeNode", "DST"));
   if (!dstNode)
     {
-      std::cerr << PHWHERE << "DST Node missing, doing nothing." << std::endl;
-      throw std::runtime_error("Failed to find DST node in RawTowerCalibration::CreateNodes");
+      std::cerr << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << "DST Node missing, doing nothing." << std::endl;
+      throw std::runtime_error(
+          "Failed to find DST node in RawTowerCalibration::CreateNodes");
+    }
+
+  RawTowerNodeName = "TOWER_" + _raw_tower_node_prefix + "_" + detector;
+  _raw_towers = findNode::getClass<RawTowerContainer>(dstNode,
+      RawTowerNodeName.c_str());
+  if (!_raw_towers)
+    {
+      std::cerr << Name() << "::" << detector << "::" << __PRETTY_FUNCTION__
+          << " " << RawTowerNodeName << " Node missing, doing bail out!"
+          << std::endl;
+      throw std::runtime_error(
+          "Failed to find " + RawTowerNodeName
+              + " node in RawTowerCalibration::CreateNodes");
     }
 
   // Create the tower nodes on the tree
-  _towers = new RawTowerContainer();
-  CaliTowerNodeName = "TOWER_SIM_" + detector;
-  PHIODataNode<PHObject> *towerNode = new PHIODataNode<PHObject>(_towers, CaliTowerNodeName.c_str(), "PHObject");
-  dstNode->addNode(towerNode);
+  PHNodeIterator dstiter(dstNode);
+  PHCompositeNode *DetNode = dynamic_cast<PHCompositeNode*>(dstiter.findFirst(
+      "PHCompositeNode", detector));
+  if (!DetNode)
+    {
+      DetNode = new PHCompositeNode(detector);
+      dstNode->addNode(DetNode);
+    }
+
+  _calib_towers = new RawTowerContainer();
+  CaliTowerNodeName = "TOWER_" + _calib_tower_node_prefix + "_" + detector;
+  PHIODataNode<PHObject> *towerNode = new PHIODataNode<PHObject>(_calib_towers,
+      CaliTowerNodeName.c_str(), "PHObject");
+  DetNode->addNode(towerNode);
 
   return;
 }
-
-
-
 
--- simulation/g4simulation/g4cemc/RawTowerCalibration.h
+++ simulation/g4simulation/g4cemc/RawTowerCalibration.h
@@ -10,24 +10,120 @@ class PHCompositeNode;
 class RawTowerContainer;
 class RawTowerGeom;
 
-//! simple calibration for a linear scale and pedstal calibration cross all channels
-class RawTowerCalibration : public SubsysReco {
+//! calibrate ADC value to measured energy deposition in calorimeter towers
+//! default input DST node is TOWER_RAW_DETECTOR
+//! default output DST node is TOWER_CALIB_DETECTOR
+class RawTowerCalibration : public SubsysReco
+{
 
- public:
-  RawTowerCalibration(const std::string& name="RawTowerCalibration");
-  virtual ~RawTowerCalibration(){}
+public:
+  RawTowerCalibration(const std::string& name = "RawTowerCalibration");
+  virtual
+  ~RawTowerCalibration()
+  {
+  }
 
-  int InitRun(PHCompositeNode *topNode);
-  int process_event(PHCompositeNode *topNode);
-  int End(PHCompositeNode *topNode);
-  void Detector(const std::string &d) {detector = d;}
-  void EminCut(const double e) {emin = e;}
-  void checkenergy(const int i = 1) {chkenergyconservation = i;}
+  int
+  InitRun(PHCompositeNode *topNode);
+  int
+  process_event(PHCompositeNode *topNode);
+  int
+  End(PHCompositeNode *topNode);
+  void
+  Detector(const std::string &d)
+  {
+    detector = d;
+  }
 
- protected:
-  void CreateNodes(PHCompositeNode *topNode);
+  enum enu_calib_algorithm
+  {
+    //! directly pass the energy of raw tower to calibrated tower. Zero suppression is applied
+    kNo_calibration = 0,
 
-  RawTowerContainer* _towers;
+    //! simple digitalization with photon statistics, ADC conversion and pedstal
+    kSimple_linear_calibration = 1
+  };
+
+  enu_calib_algorithm
+  get_calib_algorithm() const
+  {
+    return _calib_algorithm;
+  }
+
+  void
+  set_calib_algorithm(enu_calib_algorithm calibAlgorithm)
+  {
+    _calib_algorithm = calibAlgorithm;
+  }
+
+  double
+  get_calib_const_GeV_ADC() const
+  {
+    return _calib_const_GeV_ADC;
+  }
+
+  void
+  set_calib_const_GeV_ADC(double calibConstGeVAdc)
+  {
+    _calib_const_GeV_ADC = calibConstGeVAdc;
+  }
+
+  std::string
+  get_calib_tower_node_prefix() const
+  {
+    return _calib_tower_node_prefix;
+  }
+
+  void
+  set_calib_tower_node_prefix(std::string calibTowerNodePrefix)
+  {
+    _calib_tower_node_prefix = calibTowerNodePrefix;
+  }
+
+  double
+  get_pedstal_ADC() const
+  {
+    return _pedstal_ADC;
+  }
+
+  void
+  set_pedstal_ADC(double pedstalAdc)
+  {
+    _pedstal_ADC = pedstalAdc;
+  }
+
+  std::string
+  get_raw_tower_node_prefix() const
+  {
+    return _raw_tower_node_prefix;
+  }
+
+  void
+  set_raw_tower_node_prefix(std::string rawTowerNodePrefix)
+  {
+    _raw_tower_node_prefix = rawTowerNodePrefix;
+  }
+
+  double
+  get_zero_suppression_GeV() const
+  {
+    return _zero_suppression_GeV;
+  }
+
+  void
+  set_zero_suppression_GeV(double zeroSuppressionGeV)
+  {
+    _zero_suppression_GeV = zeroSuppressionGeV;
+  }
+
+protected:
+  void
+  CreateNodes(PHCompositeNode *topNode);
+
+  enu_calib_algorithm _calib_algorithm;
+
+  RawTowerContainer* _calib_towers;
+  RawTowerContainer* _raw_towers;
   RawTowerGeom *rawtowergeom;
 
   std::string detector;
@@ -35,16 +131,17 @@ class RawTowerCalibration : public SubsysReco {
   std::string CaliTowerNodeName;
   std::string TowerGeomNodeName;
 
-  int _cell_binning;
-  double emin;	
-  int chkenergyconservation;
-  int _nlayers;
-  int _nphibins;
-  int _netabins;
-  double _etamin;
-  double _phimin;
-  double _etastep;
-  double _phistep;
+  std::string _calib_tower_node_prefix;
+  std::string _raw_tower_node_prefix;
+
+  //! pedstal in unit of ADC
+  double _pedstal_ADC;
+
+  //! calibration constant in unit of GeV per ADC
+  double _calib_const_GeV_ADC;
+
+  //! zero suppression in unit of GeV
+  double _zero_suppression_GeV;
 
   PHTimeServer::timer _timer;
 
