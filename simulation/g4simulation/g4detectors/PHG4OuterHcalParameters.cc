55dc97a08aaa358bf63bb0099b0276d7c1726ec8
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-09-17 11:31:55 -0400

    check in intermediate version to fall back to in case I screw up

---
+++ simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.cc
@@ -0,0 +1,34 @@
+#include "PHG4OuterHcalParameters.h"
+
+#include <Geant4/G4SystemOfUnits.hh>
+
+PHG4OuterHcalParameters::PHG4OuterHcalParameters():
+  inner_radius(116 * cm),
+  outer_radius(136 * cm),
+  size_z(175.94 * 2 * cm),
+  scinti_gap(0.85 * cm),
+  tilt_angle(32.5 * deg),
+  n_scinti_plates(5 * 64),
+  n_scinti_tiles(12),
+  scinti_tile_thickness(0.7*cm),
+  scinti_gap_neighbor(0.1*cm),
+  scinti_eta_coverage(1.1),
+  place_in_x(0 * cm),
+  place_in_y(0 * cm),
+  place_in_z(0 * cm),
+  x_rot(0*deg),
+  y_rot(0*deg),
+  z_rot(0*deg),
+  active(0),
+  absorberactive(0),
+  ncross(0),
+  blackhole(0),
+  material("SS310"),
+  steplimits(NAN),
+  light_scint_mode(true),
+  light_balance(false),
+  light_balance_inner_radius(0.0),
+  light_balance_inner_corr(1.0),
+  light_balance_outer_radius(10.0),
+  light_balance_outer_corr(1.0)
+{}
---
+++ simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.h
@@ -0,0 +1,43 @@
+#ifndef PHG4OUTERHCALPARAMETERS_H
+#define PHG4OUTERHCALPARAMETERS_H
+
+#include <Geant4/globals.hh>
+
+// contains parameters in G4 internal units
+class PHG4OuterHcalParameters
+{
+ public:
+  PHG4OuterHcalParameters();
+  virtual ~PHG4OuterHcalParameters() {}
+
+  G4double inner_radius;
+  G4double outer_radius;
+  G4double size_z;
+  G4double scinti_gap;
+  G4double tilt_angle;
+  G4int n_scinti_plates;
+  G4int n_scinti_tiles;
+  G4double scinti_tile_thickness;
+  G4double scinti_gap_neighbor;
+  G4double scinti_eta_coverage;
+  G4double place_in_x;
+  G4double place_in_y;
+  G4double place_in_z;
+  G4double x_rot;
+  G4double y_rot;
+  G4double z_rot;
+  G4int active;
+  G4int absorberactive;
+  G4int ncross;
+  G4int blackhole;
+  G4String material;
+  G4double steplimits;
+  bool  light_scint_mode;
+  bool light_balance;
+  G4double light_balance_inner_radius;
+  G4double light_balance_inner_corr;
+  G4double light_balance_outer_radius;
+  G4double light_balance_outer_corr;
+};
+
+#endif
