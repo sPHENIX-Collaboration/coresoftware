8d5dd83ee19538eed401a304c1632abece87fbb3
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-09-18 10:56:26 -0400

    re-enable G4 printouts independant of verbosity - the overlap messages from G4 seem to go to stdout and they are essential for users to see

--- simulation/g4simulation/g4main/PHG4UIsession.cc
+++ simulation/g4simulation/g4main/PHG4UIsession.cc
@@ -10,7 +10,8 @@ G4UIsession * PHG4UIsession::SessionStart() { return NULL; }
 void PHG4UIsession::PauseSessionStart(const G4String&) {;}
 
 G4int PHG4UIsession::ReceiveG4cout(const G4String& coutString) {
-  if (verbosity > 0) std::cout << coutString << std::flush;
+  //  if (verbosity > 0) std::cout << coutString << std::flush;
+  std::cout << coutString << std::flush;
   return 0;
 }
  
