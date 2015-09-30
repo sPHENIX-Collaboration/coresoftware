aef4305eaa4d0cab915b4cbcaea391dbe98e78aa
Jin Huang <jhuang@bnl.gov>
2015-09-30 12:17:22 -0400

    Support option to remove truth particle and vertex that is not associated with the embedded particles

--- simulation/g4simulation/g4main/PHG4TruthSubsystem.cc
+++ simulation/g4simulation/g4main/PHG4TruthSubsystem.cc
@@ -18,6 +18,9 @@
 #include <Geant4/G4ParticleDefinition.hh>
 #include <Geant4/G4SystemOfUnits.hh>
 
+#include <cassert>
+#include <iostream>
+
 using namespace std;
 
 //_______________________________________________________________________
@@ -25,7 +28,8 @@ PHG4TruthSubsystem::PHG4TruthSubsystem( const string &name ):
   PHG4Subsystem( name ),
   eventAction_( 0 ),
   steppingAction_( 0 ),
-  trackingAction_( 0 )
+  trackingAction_( 0 ),
+  saveOnlyEmbeded_(false)
 {}
 
 //_______________________________________________________________________
@@ -135,6 +139,60 @@ PHG4TruthSubsystem::process_event( PHCompositeNode* topNode )
 
     }
   return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int PHG4TruthSubsystem::process_after_geant(PHCompositeNode * topNode)
+{
+  if (saveOnlyEmbeded_)
+    {
+      if (Verbosity()>1)
+        {
+          cout <<__PRETTY_FUNCTION__<<" - INFO - only save the G4 truth information that is associated with the embedded particle"<<endl;
+        }
+
+      PHG4TruthInfoContainer* truthInfoList =  findNode::getClass<PHG4TruthInfoContainer>( topNode , "G4TruthInfo" );
+      assert(truthInfoList);
+
+      set<int> savevtxlist;
+
+      // remove particle that is not embedd associated
+      PHG4TruthInfoContainer::Range truth_range = truthInfoList->GetHitRange();
+      PHG4TruthInfoContainer::Iterator truthiter = truth_range.first;
+      while (truthiter != truth_range.second)
+        {
+          const int primary_id = (truthiter->second)->get_primary_id();
+          if (truthInfoList->isEmbeded(primary_id) == 0)
+            {
+              // not a embed associated particle
+
+              truthInfoList->delete_hit(truthiter++);
+            }
+          else
+            {
+              // save vertex id for primary particle which leaves no hit
+              // in active area
+              savevtxlist.insert((truthiter->second)->get_vtx_id());
+              ++truthiter;
+            }
+        }
+
+      // remove vertex that is not embedd associated
+      PHG4TruthInfoContainer::VtxRange vtxrange = truthInfoList->GetVtxRange();
+      PHG4TruthInfoContainer::VtxIterator vtxiter = vtxrange.first;
+      while (vtxiter != vtxrange.second)
+        {
+          if (savevtxlist.find(vtxiter->first) == savevtxlist.end())
+            {
+              truthInfoList->delete_vtx(vtxiter++);
+            }
+          else
+            {
+              ++vtxiter;
+            }
+        }
+    }
+
+  return 0;
 }
 
 int
