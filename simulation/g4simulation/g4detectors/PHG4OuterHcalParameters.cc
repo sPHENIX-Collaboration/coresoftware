843c4a890b9037d02bba8156c3835859d54ab724
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-10-08 15:23:58 -0400

    add option to save or not absorber hits in truth info

--- simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.cc
+++ simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.cc
@@ -37,7 +37,8 @@ PHG4OuterHcalParameters::PHG4OuterHcalParameters():
   light_balance_outer_radius(10.0),
   light_balance_outer_corr(1.0),
   magnet_cutout(12.*cm),
-  magnet_cutout_first_scinti(8) // tile start at 0, drawing tile starts at 1
+  magnet_cutout_first_scinti(8), // tile start at 0, drawing tile starts at 1
+  absorbertruth(0)
 {}
 
 void
--- simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.h
+++ simulation/g4simulation/g4detectors/PHG4OuterHcalParameters.h
@@ -41,6 +41,7 @@ class PHG4OuterHcalParameters
   G4double light_balance_outer_corr;
   G4double magnet_cutout;
   G4int magnet_cutout_first_scinti;
+  G4int absorbertruth;
 };
 
 #endif
