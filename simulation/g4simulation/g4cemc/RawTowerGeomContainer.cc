19cb8404fe0fcaf0855012001629cb69fc81ff76
Jin Huang <jhuang@bnl.gov>
2015-11-16 22:15:34 -0500

    Setup based clase for geometry container for towers

--- simulation/g4simulation/g4cemc/RawTowerGeomContainer.cc
+++ simulation/g4simulation/g4cemc/RawTowerGeomContainer.cc
@@ -9,65 +9,9 @@ ClassImp(RawTowerGeomContainer)
 using namespace std;
 
 
-RawTowerGeomContainer::RawTowerGeomContainer( RawTowerDefs::CalorimeterId caloid )
-  {
-    _caloid = caloid;
-  }
-
-RawTowerGeomContainer::~RawTowerGeomContainer()
-{}
-
-RawTowerGeomContainer::ConstRange
-RawTowerGeomContainer::get_tower_geometries( void ) const
-{
-  return make_pair(_geoms.begin(), _geoms.end());
-}
-
-
-RawTowerGeomContainer::Range
-RawTowerGeomContainer::get_tower_geometries( void )
-{
-  return make_pair(_geoms.begin(), _geoms.end());
-}
-
-
-RawTowerGeomContainer::ConstIterator
-RawTowerGeomContainer::add_tower_geometry(RawTowerGeom *geo)
-{
-  _geoms[geo->get_id()] = geo;
-  return _geoms.find(geo->get_id());
-}
-
-RawTowerGeom *
-RawTowerGeomContainer::get_tower_geometry(RawTowerDefs::keytype key)
-{
-  Iterator it = _geoms.find(key);
-  if (it != _geoms.end())
-    {
-      return it->second;
-    }
-  return NULL;
-}
-
-
-int
-RawTowerGeomContainer::isValid() const
-{
-  return (!_geoms.empty());
-}
-
-void
-RawTowerGeomContainer::Reset()
-{
-  while (_geoms.begin() != _geoms.end())
-    {
-      delete _geoms.begin()->second;
-      _geoms.erase(_geoms.begin());
-    }
-}
 
 void
 RawTowerGeomContainer::identify(std::ostream& os) const
 {
-  os << "RawTowerGeomContainer, number of tower geometries: " << size() << std::endl;
+  os << "Base class RawTowerGeomContainer."<< std::endl;
 }
--- simulation/g4simulation/g4cemc/RawTowerGeomContainer.h
+++ simulation/g4simulation/g4cemc/RawTowerGeomContainer.h
@@ -20,30 +20,57 @@ class RawTowerGeomContainer : public PHObject
   typedef std::pair<Iterator, Iterator> Range;
   typedef std::pair<ConstIterator, ConstIterator> ConstRange;
 
-  RawTowerGeomContainer( RawTowerDefs::CalorimeterId caloid );
-  virtual ~RawTowerGeomContainer();
+  //! default constructor for ROOT IO
+  virtual ~RawTowerGeomContainer() {}
 
-  void Reset();
-  int isValid() const;
   void identify(std::ostream& os=std::cout) const;
 
-  void set_calorimeter_id( RawTowerDefs::CalorimeterId caloid ) { _caloid = caloid; }
-  RawTowerDefs::CalorimeterId get_calorimeter_id( ) { return _caloid; }
+  virtual void set_calorimeter_id( RawTowerDefs::CalorimeterId  ) { PHOOL_VIRTUAL_WARN("set_calorimeter_id()");}
+  virtual RawTowerDefs::CalorimeterId get_calorimeter_id( ) const { PHOOL_VIRTUAL_WARN("get_calorimeter_id()");return RawTowerDefs::NONE; }
 
-  ConstIterator add_tower_geometry(RawTowerGeom *geo);
-  RawTowerGeom *get_tower_geometry(RawTowerDefs::keytype key);
+  virtual ConstIterator add_tower_geometry(RawTowerGeom *geo) {PHOOL_VIRTUAL_WARN("add_tower_geometry()");}
+  virtual RawTowerGeom *get_tower_geometry(RawTowerDefs::keytype key) {PHOOL_VIRTUAL_WARN("get_tower_geometry()");return NULL;}
 
   //! return all tower geometries
-  ConstRange get_tower_geometries( void ) const;
-  Range get_tower_geometries( void );
+  virtual ConstRange get_tower_geometries( void ) const {PHOOL_VIRTUAL_WARN("get_tower_geometries()");return ConstRange(Map().begin(), Map().end()); };
+  virtual Range get_tower_geometries( void ) {PHOOL_VIRTUAL_WARN("get_tower_geometries()");return Range(Map().begin(), Map().end()); };
 
-  unsigned int size() const {return _geoms.size();}
+  virtual unsigned int size() const {PHOOL_VIRTUAL_WARN("size()"); return 0;}
+
+  // for cylindrical calorimeter only as implemented in RawTowerGeomContainer_Cylinderv1
+  virtual double get_radius() const {PHOOL_VIRTUAL_WARN("get_radius()"); return NAN;}
+  virtual double get_thickness() const  {PHOOL_VIRTUAL_WARN("get_thickness()"); return NAN;}
+  virtual int get_phibins() const{PHOOL_VIRTUAL_WARN("get_phibins()"); return -1;}
+  virtual double get_phistep() const {PHOOL_VIRTUAL_WARN("get_phistep()"); return NAN;}
+  virtual double get_phimin() const {PHOOL_VIRTUAL_WARN("get_phimin()"); return NAN;}
+  virtual int get_etabins() const {PHOOL_VIRTUAL_WARN("get_etabins()"); return -1;}
+  virtual double get_etastep() const {PHOOL_VIRTUAL_WARN("get_etastep()"); return NAN;}
+  virtual double get_etamin() const {PHOOL_VIRTUAL_WARN("get_etamin()"); return NAN;}
+
+  virtual std::pair<double, double> get_phibounds(const int ibin) const {PHOOL_VIRTUAL_WARN("get_phibounds(const int)"); return std::make_pair(NAN,NAN);}
+  virtual std::pair<double, double> get_etabounds(const int ibin) const {PHOOL_VIRTUAL_WARN("get_etabounds(const int)"); return std::make_pair(NAN,NAN);}
+  virtual double get_etacenter(const int ibin) const {PHOOL_VIRTUAL_WARN("get_etacenter(const int)"); return NAN;}
+  virtual double get_phicenter(const int ibin) const {PHOOL_VIRTUAL_WARN("get_phicenter(const int)"); return NAN;}
+
+  virtual int get_etabin(const double eta) const {PHOOL_VIRTUAL_WARN("get_etabin(const double)"); return -1;}
+  virtual int get_phibin(const double phi) const {PHOOL_VIRTUAL_WARN("get_phibin(const double)"); return -1;}
+
+  virtual void set_radius(const double r) {PHOOL_VIRTUAL_WARN("set_radius(const double)");}
+  virtual void set_thickness(const double t) {PHOOL_VIRTUAL_WARN("set_thickness(const double)");}
+  virtual void set_phibins(const int i) {PHOOL_VIRTUAL_WARN("set_phibins(const int)");}
+  virtual void set_phistep(const double phi) {PHOOL_VIRTUAL_WARN("set_phistep(const double)");}
+  virtual void set_phimin(const double phi) {PHOOL_VIRTUAL_WARN("set_phimin(const double)");}
+  virtual void set_etabins(const int i) {PHOOL_VIRTUAL_WARN("set_etabins(const int)");}
+  virtual void set_etamin(const double z) {PHOOL_VIRTUAL_WARN("set_etamin(const double)");}
+  virtual void set_etastep(const double z) {PHOOL_VIRTUAL_WARN("set_etastep(const double)");}
+  virtual void set_etabounds(const int ibin, const std::pair<double, double> & bounds) {PHOOL_VIRTUAL_WARN("set_etabounds(const int ibin, const std::pair<double, double> & bounds)");}
+
 
  protected:
-  RawTowerDefs::CalorimeterId _caloid;
-  Map _geoms;
+  //! this class is not for use. Base class only
+  RawTowerGeomContainer() {}
 
-  ClassDef(RawTowerGeomContainer,1)
+  ClassDef(RawTowerGeomContainer,2)
 };
 
 #endif /* NEWGEOMCONTAINER_H__ */
