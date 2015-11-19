653b4e4ad64b30cd3a7911538b76ea8c198923e6
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-11-19 15:13:11 -0500

    new parameter class compiles

--- simulation/g4simulation/g4detectors/PHG4InnerHcalSubsystem.h
+++ simulation/g4simulation/g4detectors/PHG4InnerHcalSubsystem.h
@@ -6,8 +6,12 @@
 #include <Geant4/G4Types.hh>
 #include <Geant4/G4String.hh>
 
+#include <map>
+#include <set>
+#include <string>
+
 class PHG4InnerHcalDetector;
-class PHG4InnerHcalParameters;
+class PHG4Parameters;
 class PHG4InnerHcalSteppingAction;
 class PHG4EventAction;
 
@@ -24,6 +28,8 @@ class PHG4InnerHcalSubsystem: public PHG4Subsystem
   {}
 
   //! init
+  int Init(PHCompositeNode *);
+
   /*!
   creates the detector_ object and place it on the node tree, under "DETECTORS" node (or whatever)
   reates the stepping action and place it on the node tree, under "ACTIONS" node
@@ -52,7 +58,10 @@ class PHG4InnerHcalSubsystem: public PHG4Subsystem
   const std::string SuperDetector() {return superdetector;}
 
   void BlackHole(const int i=1);
-  PHG4InnerHcalParameters *GetParameters();
+  void set_double_param(const std::string &name, const double dval);
+  void set_int_param(const std::string &name, const int ival);
+  void set_string_param(const std::string &name, const std::string &sval);
+  void SetDefaultParameters();
 
   protected:
 
@@ -68,11 +77,15 @@ class PHG4InnerHcalSubsystem: public PHG4Subsystem
   /*! derives from PHG4EventAction */
   PHG4EventAction *eventAction_;
 
-  PHG4InnerHcalParameters *params;
+  PHG4Parameters *params;
 
   int layer;
   std::string detector_type;
   std::string superdetector;
+  std::map<const std::string, double> dparams;
+  std::map<const std::string, int> iparams;
+  std::map<const std::string, std::string> cparams;
+
 };
 
 #endif
