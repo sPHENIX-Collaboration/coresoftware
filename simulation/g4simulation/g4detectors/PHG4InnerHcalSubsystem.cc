6841d7a11b5c813a6bd8c0f0c3d9c165698f2502
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-11-19 15:13:11 -0500

    new parameter class compiles

--- simulation/g4simulation/g4detectors/PHG4InnerHcalSubsystem.cc
+++ simulation/g4simulation/g4detectors/PHG4InnerHcalSubsystem.cc
@@ -2,18 +2,27 @@
 #include "PHG4InnerHcalDetector.h"
 #include "PHG4EventActionClearZeroEdep.h"
 #include "PHG4InnerHcalSteppingAction.h"
-#include "PHG4InnerHcalParameters.h"
+#include "PHG4Parameters.h"
 
 #include <g4main/PHG4HitContainer.h>
+
+#include <pdbcalbase/PdbParameterMap.h>
+
 #include <phool/getClass.h>
 
 #include <Geant4/globals.hh>
 
 #include <boost/foreach.hpp>
 
+#include <set>
 #include <sstream>
 
 using namespace std;
+
+map<const string,double> default_double;
+map<const string, int> default_int;
+map<const string, string> default_string;
+
 
 //_______________________________________________________________________
 PHG4InnerHcalSubsystem::PHG4InnerHcalSubsystem( const std::string &name, const int lyr ):
@@ -31,21 +40,43 @@ PHG4InnerHcalSubsystem::PHG4InnerHcalSubsystem( const std::string &name, const i
   ostringstream nam;
   nam << name << "_" << lyr;
   Name(nam.str().c_str());
-  params = new PHG4InnerHcalParameters();
+}
+
+int 
+PHG4InnerHcalSubsystem::Init(PHCompositeNode* topNode)
+{
+  params = new PHG4Parameters(superdetector);
+  SetDefaultParameters();
+  return 0;
 }
 
 //_______________________________________________________________________
-int PHG4InnerHcalSubsystem::InitRun( PHCompositeNode* topNode )
+int 
+PHG4InnerHcalSubsystem::InitRun( PHCompositeNode* topNode )
 {
   PHNodeIterator iter( topNode );
   PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "DST" ));
 
+  PHCompositeNode *parNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "RUN" ));
+  string g4geonodename = "G4GEO_" + superdetector;
+  string paramnodename = "G4GEOPARAM_" + superdetector;
+
+  PdbParameterMap *nodeparams = findNode::getClass<PdbParameterMap>(topNode,paramnodename);
+  if (nodeparams)
+    {
+      params->FillFrom(nodeparams);
+    }
+  else
+    {
+      params->SaveToNodeTree(parNode,paramnodename);
+    }
+  parNode->addNode(new PHDataNode<PHG4Parameters>(params,g4geonodename));
   // create detector
-  detector_ = new PHG4InnerHcalDetector(topNode, params, Name());
+  detector_ = new PHG4InnerHcalDetector(topNode, NULL, Name());
   detector_->SuperDetector(superdetector);
   detector_->OverlapCheck(overlapcheck);
   set<string> nodes;
-  if (params->IsActive())
+  if (params->get_int_param("active"))
     {
       PHCompositeNode *DetNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode",superdetector));
       if (! DetNode)
@@ -64,7 +95,7 @@ int PHG4InnerHcalSubsystem::InitRun( PHCompositeNode* topNode )
 	  nodename <<  "G4HIT_" << detector_type << "_" << layer;
 	}
       nodes.insert(nodename.str());
-      if (params->IsAbsorberactive())
+      if (params->get_int_param("absorberactive"))
 	{
 	  nodename.str("");
 	  if (superdetector != "NONE")
@@ -105,7 +136,7 @@ int PHG4InnerHcalSubsystem::InitRun( PHCompositeNode* topNode )
   else
     {
       // if this is a black hole it does not have to be active
-      if (params->IsBlackHole())
+      if (params->get_int_param("blackhole"))
 	{
 	  steppingAction_ = new PHG4InnerHcalSteppingAction(detector_, params);
 	}
@@ -152,26 +183,105 @@ PHG4SteppingAction* PHG4InnerHcalSubsystem::GetSteppingAction( void ) const
   return steppingAction_;
 }
 
-PHG4InnerHcalParameters *
-PHG4InnerHcalSubsystem::GetParameters()
-{
-  return params;
-}
-
 void
 PHG4InnerHcalSubsystem::SetActive(const int i)
 {
-  params->SetActive(i);
+  iparams["active"] = i;
 }
 
 void
 PHG4InnerHcalSubsystem::SetAbsorberActive(const int i)
 {
-  params->SetAbsorberactive(i);
+  iparams["absorberactive"] = i;
 }
 
 void
 PHG4InnerHcalSubsystem::BlackHole(const int i)
 {
-  params->BlackHole(i);
+  iparams["blackhole"] = i;
+}
+
+void
+PHG4InnerHcalSubsystem::set_double_param(const std::string &name, const double dval)
+{
+  if (default_double.find(name) == default_double.end())
+    {
+      cout << "double parameter " << name << " not implemented" << endl;
+      cout << "implemented double parameters are:" << endl;
+      for (map<const string, double>::const_iterator iter = default_double.begin(); iter != default_double.end(); ++iter)
+	{
+	  cout << iter->first << endl;
+	}
+      return;
+    }
+  dparams[name] = dval;
+}
+
+void
+PHG4InnerHcalSubsystem::set_int_param(const std::string &name, const int ival)
+{
+  if (default_int.find(name) == default_int.end())
+    {
+      cout << "integer parameter " << name << " not implemented" << endl;
+      cout << "implemented integer parameters are:" << endl;
+      for (map<const string, int>::const_iterator iter = default_int.begin(); iter != default_int.end(); ++iter)
+	{
+	  cout << iter->first << endl;
+	}
+      return;
+    }
+  iparams[name] = ival;
+}
+
+void
+PHG4InnerHcalSubsystem::set_string_param(const std::string &name, const string &sval)
+{
+  if (default_string.find(name) == default_string.end())
+    {
+      cout << "string parameter " << name << " not implemented" << endl;
+      cout << "implemented string parameters are:" << endl;
+      for (map<const string, string>::const_iterator iter = default_string.begin(); iter != default_string.end(); ++iter)
+	{
+	  cout << iter->first << endl;
+	}
+      return;
+    }
+  cparams[name] = sval;
+}
+
+void
+PHG4InnerHcalSubsystem::SetDefaultParameters()
+{
+
+  default_double["inner_radius"] = 116.;
+  default_double["light_balance_inner_corr"] = NAN;
+  default_double["light_balance_inner_radius"] = NAN;
+  default_double["light_balance_outer_corr"] = NAN;
+  default_double["light_balance_outer_radius"] = NAN;
+  default_double["outer_radius"] = 136.;
+  default_double["place_x"] = 0.;
+  default_double["place_y"] = 0.;
+  default_double["place_z"] = 0.;
+  default_double["rot_x"] = 0.;
+  default_double["rot_y"] = 0.;
+  default_double["rot_z"] = 0.;
+  default_double["scinti_eta_coverage"] = 1.1;
+  default_double["scinti_gap"] = 0.85;
+  default_double["scinti_gap_neighbor"] = 0.1;
+  default_double["scinti_tile_thickness"] = 0.7;
+  default_double["size_z"] = 175.94 * 2;
+  default_double["steplimits"] = NAN;
+  default_double["tilt_angle"] = 32.5;
+  default_double["timing_cut"] = 100.;
+
+  default_int["absorberactive"] = 0;
+  default_int["absorbertruth"] = 0;
+  default_int["active"] = 0;
+  default_int["blackhole"] = 0;
+  default_int["light_scint_model"] = 1;
+  default_int["ncross"] = 4;
+  default_int["n_scinti_plates"] = 5*64;
+  default_int["n_scinti_tiles"] = 12;
+
+  default_string["material"] = "SS310";
 }
