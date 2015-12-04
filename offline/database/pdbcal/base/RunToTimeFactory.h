bd4d3d4231e07071b58fc30d7f660e4e3aba0393
Chris Pinkenburg <pinkenburg@bnl.gov>
2015-12-04 16:26:53 -0500

    remove unused runtotime factory

--- offline/database/pdbcal/base/RunToTimeFactory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RunToTimeFactory_hh__
-#define __RunToTimeFactory_hh__
-
-#include "PHGenericFactoryT.h"
-#include "RunToTime.h"
-
-typedef PHGenericFactoryT<RunToTime> RunToTimeFactory;
-
-#endif
