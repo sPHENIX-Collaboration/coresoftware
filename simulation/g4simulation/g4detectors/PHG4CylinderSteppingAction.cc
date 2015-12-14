31e021252d836745f780adc553dd55531791ca2a
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-12-14 12:55:33 -0500

    comment out shower hit id construction for now

--- simulation/g4simulation/g4detectors/PHG4CylinderSteppingAction.cc
+++ simulation/g4simulation/g4detectors/PHG4CylinderSteppingAction.cc
@@ -106,6 +106,18 @@ bool PHG4CylinderSteppingAction::UserSteppingAction( const G4Step* aStep, bool )
           // Now add the hit
 	  //	  hit->print();
           hits_->AddHit(layer_id, hit);
+
+	  // {
+	  //   if ( G4VUserTrackInformation* p = aTrack->GetUserInformation() )
+	  //     {
+	  // 	if ( PHG4TrackUserInfoV1* pp = dynamic_cast<PHG4TrackUserInfoV1*>(p) )
+	  // 	  {
+	  // 	    pp->GetShower()->add_g4hit_id(hit->get_hit_id());
+	  // 	  }
+	  //     }
+	  // }
+
+	  
 	  if (hit->get_z(0) > zmax || hit->get_z(0) < zmin)
 	    {
 	      cout << "PHG4CylinderSteppingAction: hit outside acceptance, layer: " << layer_id << endl;
