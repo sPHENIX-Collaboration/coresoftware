833a9fc4532c38fd8a9d104e742b589f0539d7fe
Michael P. McCumber <michael.p.mccumber@gmail.com>
2015-12-19 18:08:29 -0500

    pass shower information from cell,g4hit up to tower

--- simulation/g4simulation/g4cemc/RawTowerBuilderByHitIndex.cc
+++ simulation/g4simulation/g4cemc/RawTowerBuilderByHitIndex.cc
@@ -122,8 +122,10 @@ RawTowerBuilderByHitIndex::process_event(PHCompositeNode *topNode)
           towers_->AddTower( tower->get_id() , tower );
         }
       tower->add_ecell( (g4hit_i->get_index_j()<<16) + g4hit_i->get_index_k(), g4hit_i->get_edep());
-      tower->set_energy( tower->get_energy() + g4hit_i->get_edep() );
 
+      tower->add_eshower(g4hit_i->get_shower_id(), g4hit_i->get_edep());
+            
+      tower->set_energy( tower->get_energy() + g4hit_i->get_edep() );
     }
 
   float towerE = 0.;
