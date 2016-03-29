3a688569c70895f4508d5335371b4e0654011b96
Chris Pinkenburg <pinkenburg@bnl.gov>
2016-03-29 17:01:32 -0400

    add prototype tower builder and treemaker

---
+++ simulation/g4simulation/g4histos/G4RootScintillatorTower.cc
@@ -0,0 +1,42 @@
+#include "G4RootScintillatorTower.h"
+
+#include <g4cemc/RawTower.h>
+
+#include <cmath>
+#include <iostream>
+
+
+using namespace std;
+
+ClassImp(G4RootScintillatorTower)
+
+G4RootScintillatorTower::G4RootScintillatorTower() : 
+  row(-1),
+  column(-1),
+  energy(0)
+{}
+
+G4RootScintillatorTower::G4RootScintillatorTower(const RawTower &tower) :
+  row(tower.get_bineta()),
+  column(tower.get_binphi()),
+  energy(tower.get_energy())
+{}
+
+void G4RootScintillatorTower::Reset()
+{
+  row = -1;
+  column = -1;
+  energy = 0;
+}
+
+int G4RootScintillatorTower::isValid() const
+{
+  return (row >= 0);
+}
+
+void G4RootScintillatorTower::identify(std::ostream& os) const
+{
+  os << "G4RootScintillatorTower: row: " << row << ", column: " << column 
+     << " energy=" << energy << std::endl;
+}
+
