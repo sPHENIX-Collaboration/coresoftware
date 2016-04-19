bd1a831762ec36f49b04ab897b9b948763a62703
Chris Pinkenburg <pinkenburg@bnl.gov>
2016-04-19 12:15:39 -0400

    fix bug in adding shower for absorber hit (used active volume hit container instead of absorber container)

--- simulation/g4simulation/g4detectors/PHG4HcalPrototypeSteppingAction.cc
+++ simulation/g4simulation/g4detectors/PHG4HcalPrototypeSteppingAction.cc
@@ -188,7 +188,7 @@ bool PHG4HcalPrototypeSteppingAction::UserSteppingAction( const G4Step* aStep, b
 		  {
 		    if ( PHG4TrackUserInfoV1* pp = dynamic_cast<PHG4TrackUserInfoV1*>(p) )
 		      {
-			pp->GetShower()->add_g4hit_id(hits_->GetID(),hit->get_hit_id());
+			pp->GetShower()->add_g4hit_id(absorberhits_->GetID(),hit->get_hit_id());
 		      }
 		  }
 	      }
