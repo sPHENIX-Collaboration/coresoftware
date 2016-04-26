a5cdc92df7d7492d17282e366094b9719fdd42fd
HaiwangYu <yuhw.pku@gmail.com>
2016-04-26 01:07:26 -0400

    set input/output nodes

--- simulation/g4simulation/g4hough/PHG4TrackKalmanFitter.C
+++ simulation/g4simulation/g4hough/PHG4TrackKalmanFitter.C
@@ -1,47 +1,27 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// This module is desgined to grab svtx tracks and put truth and cluster
-// information into a TTree for GenFit testing
-//
-////////////////////////////////////////////////////////////////////////////////
-//
-// Darren McGlinchey
-// 1 Apr 2016
-//
-////////////////////////////////////////////////////////////////////////////////
-
+/*!
+ *  \file		PHG4TrackKalmanFitter.C
+ *  \brief		Refit SvtxTracks with PHGenFit.
+ *  \details	Refit SvtxTracks with PHGenFit.
+ *  \author		Haiwang Yu <yuhw@nmsu.edu>
+ */
 
 #include "PHG4TrackKalmanFitter.h"
 
-#include <phool/phool.h>
-#include <phool/getClass.h>
-#include <g4main/PHG4HitContainer.h>
-#include <g4main/PHG4TruthInfoContainer.h>
-#include <g4main/PHG4Particle.h>
-#include <g4main/PHG4Hit.h>
-#include <g4main/PHG4VtxPoint.h>
+#include <fun4all/Fun4AllReturnCodes.h>
 #include <fun4all/PHTFileServer.h>
-#include <fun4all/Fun4AllServer.h>
-
-#include <g4hough/SvtxVertexMap.h>
-#include <g4hough/SvtxVertex.h>
-#include <g4hough/SvtxTrackMap.h>
-#include <g4hough/SvtxTrack.h>
 #include <g4hough/SvtxClusterMap.h>
-#include <g4hough/SvtxCluster.h>
-#include <g4hough/SvtxHitMap.h>
-#include <g4hough/SvtxHit.h>
-
-#include <g4eval/SvtxEvalStack.h>
-#include <g4eval/SvtxTrackEval.h>
-#include <g4eval/SvtxClusterEval.h>
-#include <g4eval/SvtxTruthEval.h>
-#include <g4eval/SvtxVertexEval.h>
-#include <g4eval/SvtxHitEval.h>
-
-#include <TTree.h>
-
+#include <g4main/PHG4TruthInfoContainer.h>
+#include <phool/getClass.h>
+#include <phool/phool.h>
+#include <phool/PHCompositeNode.h>
+#include <phool/PHIODataNode.h>
+#include <phool/PHNodeIterator.h>
 #include <iostream>
+
+#include "TTree.h"
+#include "SvtxTrackMap_v1.h"
+#include "SvtxVertexMap_v1.h"
+
 
 using namespace std;
 
@@ -49,55 +29,52 @@ using namespace std;
 //-- Constructor:
 //--  simple initialization
 //----------------------------------------------------------------------------//
-PHG4TrackKalmanFitter::PHG4TrackKalmanFitter(const string &name):
-  SubsysReco( name ),
-  _flags( NONE ),
-  _tracks( NULL ),
-  _svtxevalstack( NULL )
-{
-  //initialize
-  _event = 0;
-  _outfile = "PHG4TrackKalmanFitter.root";
+PHG4TrackKalmanFitter::PHG4TrackKalmanFitter(const string &name) :
+		SubsysReco(name), _do_eval(false), _flags(NONE), _eval_tree( NULL) {
+	//initialize
+	_event = 0;
+	_eval_outname = "PHG4TrackKalmanFitter_eval.root";
 }
 
 //----------------------------------------------------------------------------//
 //-- Init():
 //--   Intialize all histograms, trees, and ntuples
 //----------------------------------------------------------------------------//
-int PHG4TrackKalmanFitter::Init(PHCompositeNode *topNode)
-{
-  cout << PHWHERE << " Openning file " << _outfile << endl;
-  PHTFileServer::get().open( _outfile, "RECREATE");
+int PHG4TrackKalmanFitter::Init(PHCompositeNode *topNode) {
+	cout << PHWHERE << " Openning file " << _eval_outname << endl;
 
+	CreateNodes(topNode);
 
-  // create TTree
-  _tracks = new TTree("tracks", "Svtx Tracks");
-  _tracks->Branch("event", &event, "event/I");
-  _tracks->Branch("gtrackID", &gtrackID, "gtrackID/I");
-  _tracks->Branch("gflavor", &gflavor, "gflavor/I");
-  _tracks->Branch("gpx", &gpx, "gpx/F");
-  _tracks->Branch("gpy", &gpy, "gpy/F");
-  _tracks->Branch("gpz", &gpz, "gpz/F");
-  _tracks->Branch("gvx", &gvx, "gvx/F");
-  _tracks->Branch("gvy", &gvy, "gvy/F");
-  _tracks->Branch("gvz", &gvz, "gvz/F");
-  _tracks->Branch("trackID", &trackID, "trackID/I");
-  _tracks->Branch("charge", &charge, "charge/I");
-  _tracks->Branch("nhits", &nhits, "nhits/I");
-  _tracks->Branch("px", &px, "px/F");
-  _tracks->Branch("py", &py, "py/F");
-  _tracks->Branch("pz", &pz, "pz/F");
-  _tracks->Branch("dca2d", &dca2d, "dca2d/F");
-  _tracks->Branch("clusterID", &clusterID, "clusterID[nhits]/I");
-  _tracks->Branch("layer", &layer, "layer[nhits]/I");
-  _tracks->Branch("x", &x, "x[nhits]/F");
-  _tracks->Branch("y", &y, "y[nhits]/F");
-  _tracks->Branch("z", &z, "z[nhits]/F");
-  _tracks->Branch("size_dphi", &size_dphi, "size_dphi[nhits]/F");
-  _tracks->Branch("size_dz", &size_dz, "size_dz[nhits]/F");
+	if (_do_eval) {
+		PHTFileServer::get().open(_eval_outname, "RECREATE");
+		// create TTree
+		_eval_tree = new TTree("tracks", "Svtx Tracks");
+		_eval_tree->Branch("event", &event, "event/I");
+		_eval_tree->Branch("gtrackID", &gtrackID, "gtrackID/I");
+		_eval_tree->Branch("gflavor", &gflavor, "gflavor/I");
+		_eval_tree->Branch("gpx", &gpx, "gpx/F");
+		_eval_tree->Branch("gpy", &gpy, "gpy/F");
+		_eval_tree->Branch("gpz", &gpz, "gpz/F");
+		_eval_tree->Branch("gvx", &gvx, "gvx/F");
+		_eval_tree->Branch("gvy", &gvy, "gvy/F");
+		_eval_tree->Branch("gvz", &gvz, "gvz/F");
+		_eval_tree->Branch("trackID", &trackID, "trackID/I");
+		_eval_tree->Branch("charge", &charge, "charge/I");
+		_eval_tree->Branch("nhits", &nhits, "nhits/I");
+		_eval_tree->Branch("px", &px, "px/F");
+		_eval_tree->Branch("py", &py, "py/F");
+		_eval_tree->Branch("pz", &pz, "pz/F");
+		_eval_tree->Branch("dca2d", &dca2d, "dca2d/F");
+		_eval_tree->Branch("clusterID", &clusterID, "clusterID[nhits]/I");
+		_eval_tree->Branch("layer", &layer, "layer[nhits]/I");
+		_eval_tree->Branch("x", &x, "x[nhits]/F");
+		_eval_tree->Branch("y", &y, "y[nhits]/F");
+		_eval_tree->Branch("z", &z, "z[nhits]/F");
+		_eval_tree->Branch("size_dphi", &size_dphi, "size_dphi[nhits]/F");
+		_eval_tree->Branch("size_dz", &size_dz, "size_dz[nhits]/F");
+	}
 
-
-  return 0;
+	return Fun4AllReturnCodes::EVENT_OK;
 }
 
 //----------------------------------------------------------------------------//
@@ -105,121 +82,46 @@ int PHG4TrackKalmanFitter::Init(PHCompositeNode *topNode)
 //--   Call user instructions for every event.
 //--   This function contains the analysis structure.
 //----------------------------------------------------------------------------//
-int PHG4TrackKalmanFitter::process_event(PHCompositeNode *topNode)
-{
-  _event++;
-  if (_event % 1000 == 0)
-    cout << PHWHERE << "Events processed: " << _event << endl;
+int PHG4TrackKalmanFitter::process_event(PHCompositeNode *topNode) {
+	_event++;
+	if (_event % 1000 == 0)
+		cout << PHWHERE << "Events processed: " << _event << endl;
 
-  GetNodes(topNode);
+	GetNodes(topNode);
 
-  if (!_svtxevalstack) {
-    _svtxevalstack = new SvtxEvalStack(topNode);
-    _svtxevalstack->set_strict(false);
-    _svtxevalstack->set_verbosity(verbosity + 1);
-  } else {
-    _svtxevalstack->next_event(topNode);
-  }
 
-  fill_tree(topNode);
+	if (_do_eval) {
+		fill_tree(topNode);
+	}
 
-  return 0;
+	return Fun4AllReturnCodes::EVENT_OK;
 }
 
 //----------------------------------------------------------------------------//
 //-- End():
 //--   End method, wrap everything up
 //----------------------------------------------------------------------------//
-int PHG4TrackKalmanFitter::End(PHCompositeNode *topNode)
-{
+int PHG4TrackKalmanFitter::End(PHCompositeNode *topNode) {
 
-  PHTFileServer::get().cd( _outfile );
+	if (_do_eval) {
+		PHTFileServer::get().cd(_eval_outname);
+		_eval_tree->Write();
+	}
 
-  _tracks->Write();
-
-  if (_svtxevalstack) delete _svtxevalstack;
-
-  return 0;
+	return Fun4AllReturnCodes::EVENT_OK;
 }
-
 
 //----------------------------------------------------------------------------//
 //-- fill_tree():
 //--   Fill the trees with truth, track fit, and cluster information
 //----------------------------------------------------------------------------//
-void PHG4TrackKalmanFitter::fill_tree(PHCompositeNode *topNode)
-{
-  // Make sure to reset all the TTree variables before trying to set them.
-  reset_variables();
+void PHG4TrackKalmanFitter::fill_tree(PHCompositeNode *topNode) {
+	//! Make sure to reset all the TTree variables before trying to set them.
+	reset_variables();
 
-  // get evaluators
-  SvtxTrackEval*     trackeval = _svtxevalstack->get_track_eval();
-  SvtxTruthEval*     trutheval = _svtxevalstack->get_truth_eval();
+	_eval_tree->Fill();
 
-  if (_truth_container)
-  {
-
-    PHG4TruthInfoContainer::ConstRange range =
-      _truth_container->GetPrimaryParticleRange();
-
-    for (PHG4TruthInfoContainer::ConstIterator iter = range.first;
-         iter != range.second;
-         ++iter)
-    {
-
-      PHG4Particle* g4particle = iter->second;
-
-      gtrackID = g4particle->get_track_id();
-      gflavor  = g4particle->get_pid();
-
-      PHG4VtxPoint* vtx = trutheval->get_vertex(g4particle);
-      gvx      = vtx->get_x();
-      gvy      = vtx->get_y();
-      gvz      = vtx->get_z();
-
-			gpx			 = g4particle->get_px();
-			gpy			 = g4particle->get_py();
-			gpz			 = g4particle->get_pz();
-
-      SvtxTrack* track = trackeval->best_track_from(g4particle);
-      if (track)
-      {
-        trackID   = track->get_id();
-        charge    = track->get_charge();
-        nhits     = track->size_clusters();
-        px        = track->get_px();
-        py        = track->get_py();
-        pz        = track->get_pz();
-        dca2d     = track->get_dca2d();
-
-
-        int iclus = 0;
-        for (SvtxTrack::ConstClusterIter iter = track->begin_clusters();
-             iter != track->end_clusters();
-             ++iter)
-        {
-          unsigned int cluster_id = *iter;
-          SvtxCluster* cluster = _clustermap->get(cluster_id);
-          unsigned int l = cluster->get_layer();
-
-          clusterID[iclus] = (int)cluster_id;
-          layer[iclus] = (int)l;
-          x[iclus] = cluster->get_x();
-          y[iclus] = cluster->get_y();
-          z[iclus] = cluster->get_z();
-          size_dphi[iclus] = cluster->get_phi_size();
-          size_dz[iclus] = cluster->get_z_size();
-
-          ++iclus;
-        }
-
-      } // if(track)
-    } // for( iter)
-  } //if (_truth_container)
-
-  _tracks->Fill();
-  return;
-
+	return;
 }
 
 //----------------------------------------------------------------------------//
@@ -227,70 +129,128 @@ void PHG4TrackKalmanFitter::fill_tree(PHCompositeNode *topNode)
 //--   Reset all the tree variables to their default values.
 //--   Needs to be called at the start of every event
 //----------------------------------------------------------------------------//
-void PHG4TrackKalmanFitter::reset_variables()
-{
-  event = -9999;
+void PHG4TrackKalmanFitter::reset_variables() {
+	event = -9999;
 
-  //-- truth
-  gtrackID = -9999;
-  gflavor = -9999;
-  gpx = -9999;
-  gpy = -9999;
-  gpz = -9999;
-  gvx = -9999;
-  gvy = -9999;
-  gvz = -9999;
+	//-- truth
+	gtrackID = -9999;
+	gflavor = -9999;
+	gpx = -9999;
+	gpy = -9999;
+	gpz = -9999;
+	gvx = -9999;
+	gvy = -9999;
+	gvz = -9999;
 
-  //-- reco
-  trackID = -9999;
-  charge = -9999;
-  nhits = -9999;
-  px = -9999;
-  py = -9999;
-  pz = -9999;
-  dca2d = -9999;
+	//-- reco
+	trackID = -9999;
+	charge = -9999;
+	nhits = -9999;
+	px = -9999;
+	py = -9999;
+	pz = -9999;
+	dca2d = -9999;
 
-  //-- clusters
-  for (int i = 0; i < 7; i++)
-  {
-    clusterID[i] = -9999;
-    layer[i] = -9999;
-    x[i] = -9999;
-    y[i] = -9999;
-    z[i] = -9999;
-    size_dphi[i] = -9999;
-    size_dz[i] = -9999;
-  }
+	//-- clusters
+	for (int i = 0; i < 7; i++) {
+		clusterID[i] = -9999;
+		layer[i] = -9999;
+		x[i] = -9999;
+		y[i] = -9999;
+		z[i] = -9999;
+		size_dphi[i] = -9999;
+		size_dz[i] = -9999;
+	}
 
+}
+
+int PHG4TrackKalmanFitter::CreateNodes(PHCompositeNode *topNode) {
+	// create nodes...
+	PHNodeIterator iter(topNode);
+
+	PHCompositeNode *dstNode = static_cast<PHCompositeNode*>(iter.findFirst(
+			"PHCompositeNode", "DST"));
+	if (!dstNode) {
+		cerr << PHWHERE << "DST Node missing, doing nothing." << endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
+
+	// Create the SVTX node
+	PHCompositeNode* tb_node = dynamic_cast<PHCompositeNode*>(iter.findFirst(
+			"PHCompositeNode", "SVTX"));
+	if (!tb_node) {
+		tb_node = new PHCompositeNode("SVTX");
+		dstNode->addNode(tb_node);
+		if (verbosity > 0)
+			cout << "SVTX node added" << endl;
+	}
+
+	_trackmap_refit = new SvtxTrackMap_v1;
+	PHIODataNode<PHObject>* tracks_node = new PHIODataNode<PHObject>(
+			_trackmap_refit, "SvtxTrackMapRefit", "PHObject");
+	tb_node->addNode(tracks_node);
+	if (verbosity > 0)
+		cout << "Svtx/SvtxTrackMap node added" << endl;
+
+	_vertexmap_refit = new SvtxVertexMap_v1;
+	PHIODataNode<PHObject>* vertexes_node = new PHIODataNode<PHObject>(
+			_vertexmap_refit, "SvtxVertexMapRefit", "PHObject");
+	tb_node->addNode(vertexes_node);
+	if (verbosity > 0)
+		cout << "Svtx/SvtxVertexMap node added" << endl;
+
+	return Fun4AllReturnCodes::EVENT_OK;
 }
 
 //----------------------------------------------------------------------------//
 //-- GetNodes():
 //--   Get all the all the required nodes off the node tree
 //----------------------------------------------------------------------------//
-void PHG4TrackKalmanFitter::GetNodes(PHCompositeNode * topNode)
-{
-  //DST objects
-  //Truth container
-  _truth_container = findNode::getClass<PHG4TruthInfoContainer>(topNode, "G4TruthInfo");
-  if (!_truth_container && _event < 2)
-  {
-    cout << PHWHERE
-         << " PHG4TruthInfoContainer node not found on node tree"
-         << endl;
-  }
+int PHG4TrackKalmanFitter::GetNodes(PHCompositeNode * topNode) {
+	//DST objects
+	//Truth container
+	_truth_container = findNode::getClass<PHG4TruthInfoContainer>(topNode,
+			"G4TruthInfo");
+	if (!_truth_container && _event < 2) {
+		cout << PHWHERE << " PHG4TruthInfoContainer node not found on node tree"
+				<< endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
 
-  //Svtx Clusters
-  _clustermap = findNode::getClass<SvtxClusterMap>(topNode, "SvtxClusterMap");
-  if (!_clustermap && _event < 2)
-  {
-    cout << PHWHERE
-         << " SvtxClusterMap node not found on node tree"
-         << endl;
-  }
+	//! Input Svtx Clusters
+	_clustermap = findNode::getClass<SvtxClusterMap>(topNode, "SvtxClusterMap");
+	if (!_clustermap && _event < 2) {
+		cout << PHWHERE << " SvtxClusterMap node not found on node tree"
+				<< endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
 
+	//! Input Svtx Tracks
+	_trackmap = findNode::getClass<SvtxTrackMap>(topNode, "SvtxTrackMap");
+	if (!_trackmap && _event < 2) {
+		cout << PHWHERE << " SvtxClusterMap node not found on node tree"
+				<< endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
+
+	//! Output Svtx Tracks
+	_trackmap_refit = findNode::getClass<SvtxTrackMap>(topNode,
+			"SvtxTrackMapRefit");
+	if (!_trackmap_refit && _event < 2) {
+		cout << PHWHERE << " SvtxTrackMapRefit node not found on node tree"
+				<< endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
+
+	//! Output Svtx Vertices
+	_vertexmap_refit = findNode::getClass<SvtxVertexMap>(topNode,
+			"SvtxVertexMapRefit");
+	if (!_vertexmap_refit && _event < 2) {
+		cout << PHWHERE << " SvtxVertexMapRefit node not found on node tree"
+				<< endl;
+		return Fun4AllReturnCodes::ABORTEVENT;
+	}
+
+	return Fun4AllReturnCodes::EVENT_OK;
 }
-
-
-
 
--- simulation/g4simulation/g4hough/PHG4TrackKalmanFitter.h
+++ simulation/g4simulation/g4hough/PHG4TrackKalmanFitter.h
@@ -1,10 +1,18 @@
+/*!
+ *  \file		PHG4TrackKalmanFitter.h
+ *  \brief		Refit SvtxTracks with PHGenFit.
+ *  \details	Refit SvtxTracks with PHGenFit.
+ *  \author		Haiwang Yu <yuhw@nmsu.edu>
+ */
+
 #ifndef __PHG4TrackKalmanFitter_H__
 #define __PHG4TrackKalmanFitter_H__
 
 #include <fun4all/SubsysReco.h>
 #include <string>
 
-//Forward declerations
+class SvtxTrackMap;
+class SvtxVertexMap;
 class PHCompositeNode;
 class PHG4TruthInfoContainer;
 class SvtxClusterMap;
@@ -12,95 +20,115 @@ class SvtxEvalStack;
 class TFile;
 class TTree;
 
+//! \brief		Refit SvtxTracks with PHGenFit.
+class PHG4TrackKalmanFitter: public SubsysReco {
+public:
+	//!Default constructor
+	PHG4TrackKalmanFitter(const std::string &name = "PHG4TrackKalmanFitter");
 
-//Brief: basic ntuple and histogram creation for sim evaluation
-class PHG4TrackKalmanFitter: public SubsysReco
-{
- public: 
-  //Default constructor
-  PHG4TrackKalmanFitter(const std::string &name="PHG4TrackKalmanFitter");
+	//!Initialization, called for initialization
+	int Init(PHCompositeNode *);
 
-  //Initialization, called for initialization
-  int Init(PHCompositeNode *);
+	//!Process Event, called for each event
+	int process_event(PHCompositeNode *);
 
-  //Process Event, called for each event
-  int process_event(PHCompositeNode *);
+	//!End, write and close files
+	int End(PHCompositeNode *);
 
-  //End, write and close files
-  int End(PHCompositeNode *);
+	  /// set verbosity
+	  void Verbosity(int verb) {
+	    verbosity = verb; // SubsysReco verbosity
+	  }
 
-  //Change output filename
-  void set_filename(const char* file)
-  { if(file) _outfile = file; }
+	//Change output filename
+	void set_filename(const char* file) {
+		if (file)
+			_eval_outname = file;
+	}
 
-  //Flags of different kinds of outputs
-  enum Flag
-  {
-    //all disabled
-    NONE = 0,
-  };
+	//Flags of different kinds of outputs
+	enum Flag {
+		//all disabled
+		NONE = 0,
+	};
 
-  //Set the flag
-  //Flags should be set like set_flag(PHG4TrackKalmanFitter::TRUTH, true) from macro
-  void set_flag(const Flag& flag, const bool& value)
-  {
-   if(value) _flags |= flag;
-   else _flags &= (~flag);
-  }
+	//Set the flag
+	//Flags should be set like set_flag(PHG4TrackKalmanFitter::TRUTH, true) from macro
+	void set_flag(const Flag& flag, const bool& value) {
+		if (value)
+			_flags |= flag;
+		else
+			_flags &= (~flag);
+	}
 
-  //User modules
-  void fill_tree(PHCompositeNode*);
-  void reset_variables();
+	//User modules
+	void fill_tree(PHCompositeNode*);
+	void reset_variables();
 
- private:
-  //output filename
-  std::string _outfile;
-   
-  //Event counter
-  int _event;
+	bool is_do_eval() const {
+		return _do_eval;
+	}
 
-  //Get all the nodes
-  void GetNodes(PHCompositeNode *);
-  
-  //flags
-  unsigned int _flags;
+	void set_do_eval(bool doEval) {
+		_do_eval = doEval;
+	}
 
-  //TTrees
-  TTree* _tracks;
-  int event;
-  //-- truth
-  int gtrackID;
-  int gflavor;
-  float gpx;
-  float gpy;
-  float gpz;
-  float gvx;
-  float gvy;
-  float gvz;
-  //-- reco
-  int trackID;
-  int charge;
-  int nhits;
-  float px;
-  float py;
-  float pz;
-  float dca2d;
-  //-- clusters
-  int clusterID[7];
-  int layer[7];
-  float x[7];
-  float y[7];
-  float z[7];
-  float size_dphi[7];
-  float size_dz[7];
+private:
+	//! switch eval out
+	bool _do_eval;
 
-  //Node pointers
-  PHG4TruthInfoContainer* _truth_container;
-  SvtxClusterMap* _clustermap;
+	//!output filename
+	std::string _eval_outname;
 
-  // eval stack
-  SvtxEvalStack* _svtxevalstack;
+	//Event counter
+	int _event;
 
+	//!Get all the nodes
+	int GetNodes(PHCompositeNode *);
+
+	//!Create New nodes
+	int CreateNodes(PHCompositeNode *topNode);
+
+	//!flags
+	unsigned int _flags;
+
+	//TTrees
+	TTree* _eval_tree;
+	int event;
+	//-- truth
+	int gtrackID;
+	int gflavor;
+	float gpx;
+	float gpy;
+	float gpz;
+	float gvx;
+	float gvy;
+	float gvz;
+	//-- reco
+	int trackID;
+	int charge;
+	int nhits;
+	float px;
+	float py;
+	float pz;
+	float dca2d;
+	//-- clusters
+	int clusterID[7];
+	int layer[7];
+	float x[7];
+	float y[7];
+	float z[7];
+	float size_dphi[7];
+	float size_dz[7];
+
+	//! Input Node pointers
+	PHG4TruthInfoContainer* _truth_container;
+	SvtxClusterMap* _clustermap;
+	SvtxTrackMap* _trackmap;
+
+	//! Output Node pointers
+	SvtxTrackMap* _trackmap_refit;
+	SvtxVertexMap* _vertexmap_refit;
 };
 
 #endif //* __PHG4TrackKalmanFitter_H__ *//
