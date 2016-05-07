2fde52f71c9d1cdb377d8879b324732ad7dee005
Chris Pinkenburg <pinkenburg@bnl.gov>
2016-05-07 13:20:28 -0400

    Add missing PHG4Shower.h include

--- simulation/g4simulation/g4detectors/PHG4OuterHcalSteppingAction.cc
+++ simulation/g4simulation/g4detectors/PHG4OuterHcalSteppingAction.cc
@@ -10,6 +10,7 @@
 #include <g4main/PHG4HitContainer.h>
 #include <g4main/PHG4Hit.h>
 #include <g4main/PHG4Hitv1.h>
+#include <g4main/PHG4Shower.h>
 #include <g4main/PHG4TrackUserInfoV1.h>
 
 #include <phool/getClass.h>
