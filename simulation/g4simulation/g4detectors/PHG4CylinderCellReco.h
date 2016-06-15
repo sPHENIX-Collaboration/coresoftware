a024014b9e3ce8d4aaf25c1edb414c83748e19d8
Michael P. McCumber <michael.p.mccumber@gmail.com>
2016-06-15 09:14:26 -0400

    layer dependent integration times for cylinder cells

--- simulation/g4simulation/g4detectors/PHG4CylinderCellReco.h
+++ simulation/g4simulation/g4detectors/PHG4CylinderCellReco.h
@@ -35,16 +35,11 @@ class PHG4CylinderCellReco : public SubsysReco
   void checkenergy(const int i=1) {chkenergyconservation = i;}
   void OutputDetector(const std::string &d) {outdetector = d;}
 
-  double get_timing_window_min() {return timing_min;}
-  double get_timing_window_max() {return timing_max;}
-  void set_timing_window(const double tmin, const double tmax) {
-    timing_min = tmin; timing_max = tmax;
+  double get_timing_window_min(const int i) {return tmin_max[i].first;}
+  double get_timing_window_max(const int i) {return tmin_max[i].second;}
+  void   set_timing_window(const int i, const double tmin, const double tmax) {
+    tmin_max[i] = std::make_pair(tmin,tmax);
   }
-  
-  //! get timing window size in ns.
-  double get_timing_window_size() const {return timing_max - timing_min;}
-  //! set timing window size in ns. This is for a simple simulation of the ADC integration window starting from 0ns to this value. Default to infinity, i.e. include all hits
-  void set_timing_window_size(const double s) {set_timing_window(0.0,s);}
 
  protected:
   void set_size(const int i, const double sizeA, const double sizeB, const int what);
@@ -74,8 +69,9 @@ class PHG4CylinderCellReco : public SubsysReco
   int chkenergyconservation;
 
   //! timing window size in ns. This is for a simple simulation of the ADC integration window starting from 0ns to this value. Default to infinity, i.e. include all hits
-  double timing_min;
-  double timing_max;
+  double tmin_default;
+  double tmax_default;
+  std::map<int, std::pair<double,double> > tmin_max;
 };
 
 #endif
