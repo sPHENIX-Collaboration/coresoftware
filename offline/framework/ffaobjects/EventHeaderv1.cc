cb8dca751d47b483ea7b12cc3e55629d17366f0e
Jin Huang <jhuang@bnl.gov>
2016-07-13 23:14:18 -0400

    Add a generic event header

---
+++ offline/framework/ffaobjects/EventHeaderv1.cc
@@ -0,0 +1,36 @@
+#include "EventHeaderv1.h"
+
+using namespace std;
+
+ClassImp(EventHeaderv1)
+
+EventHeaderv1::EventHeaderv1()
+{
+  Reset();
+  return;
+}
+
+void EventHeaderv1::Reset()
+{
+  TimeStamp = 0;
+  EvtSequence = -999999;
+  EvtType = -999999;
+  return;
+}
+
+void EventHeaderv1::identify(ostream& out) const
+{
+  out << "identify yourself: I am an EventHeaderv1 Object" << endl;
+  out << "Event no: " << EvtSequence 
+      << ", Type: " << EvtType 
+      << ", DAQ arrival time: " << ctime(&TimeStamp)
+      << endl;
+
+  return;
+}
+
+int EventHeaderv1::isValid() const
+{
+  return((TimeStamp) ? 1:0); // return 1 if TimeStamp is not zero
+}
+
