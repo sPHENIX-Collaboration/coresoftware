f9bd79c9b5aa429087ad0909301fafebc7c86c88
Sookhyun <shlee@bnl.gov>
2016-09-18 22:30:29 -0400

    modified to read detector geometry from DST file

--- offline/packages/Display/interface/PHEventDisplay.cxx
+++ offline/packages/Display/interface/PHEventDisplay.cxx
@@ -2,8 +2,8 @@
         \file PHEventDisplay.cxx
         \author Sookhyun Lee
         \brief event display interface,
-	       parameters/switches set, detector modules called, control display.
-        \version $Revision: 1.1 $
+	       set parameters/switches, call detector modules, control display.
+        \version $Revision: 1.2 $
         \date    $Date: 07/26/2016
 */
 
@@ -15,11 +15,28 @@
 // PHOOL and Fun4All includes
 #include <fun4all/Fun4AllReturnCodes.h>
 #include <fun4all/Fun4AllServer.h>
+#include <phool/getClass.h>
+#include <phool/phool.h>
 #include <phool/PHIODataNode.h>
+#include <phool/PHNodeIterator.h>
 #include <phool/PHTimer.h>
 #include <phool/PHCompositeNode.h>
 //#include <TMutNode.h>
 #include <phool/PHTimeServer.h>
+#include <phgeom/PHGeomUtility.h>
+#include <phgeom/PHGeomTGeo.h>
+
+
+// EVE framework includes
+#include "TEveManager.h"
+#include "TEveBrowser.h"
+#include "TEveWindow.h"
+#include "TGeoManager.h"
+#include "TGeoVolume.h"
+#include "TGLAutoRotator.h"
+#include "TGLViewer.h"
+#include "TFile.h"
+
 
 // sPHENIX Event Display
 #include <PHEveDisplay.h>
@@ -30,11 +47,8 @@
 #include <mCaloEveDisplay.h>
 #include <mJetEveDisplay.h>
 
-// EVE framework includes
-#include "TEveManager.h"
-#include <TGLViewer.h>
-#include <TGLAutoRotator.h>
 #include <TThread.h>
+#include <TStyle.h>
 
 #include <PHEventDisplay.h>
 
@@ -42,33 +56,54 @@ using boost::bind;
 
 PHEventDisplay::PHEventDisplay(int w = 1920,
 				 int h = 1080,
-				 bool use_fieldmap = true,
-				 const std::string& mapname = "sPHEBIX.2d.root",
-				 const std::string& geoname = "geo.root",
-				 float jet_pt_threshold = 5.,
-				 float jet_e_scale = 20.,
-				 float calo_e_threshold = 0.2,
-				 bool is_svtx_on = true,
-				 bool is_calo_on = true,
-				 bool is_jet_on = true) :
+				 bool _use_fieldmap = true,
+				 bool _use_geofile = true,
+				 const std::string& _mapname = "sPHEBIX.2d.root",
+				 const std::string& _geoname = "geo.root") :
   SubsysReco("PHEventDisplay"),
   _pending_update(false),
   _modules(),
   _mutex(PTHREAD_MUTEX_INITIALIZER),
   _update_thread(),
-  _PHEveDisplay(new PHEveDisplay(w, h, use_fieldmap, mapname, geoname)),
-  _eve_manager(NULL),
+  _PHEveDisplay(new PHEveDisplay(w, h, _use_fieldmap, _use_geofile, _mapname, _geoname, verbosity)),
   _rot(NULL),
   _status_thread(),
-  _is_svtx_on(is_svtx_on),
-  _is_calo_on(is_calo_on),
-  _is_jet_on(is_jet_on)
+  jet_pt_threshold(5.),
+  jet_e_scale(30.),
+  calo_e_threshold(0.2),
+  is_svtx_on(true),
+  is_cemc_on(true),
+  is_hcalin_on(true),
+  is_hcalout_on(true),
+  is_jet_on(true),
+  is_truth_on(true),
+  use_fieldmap(_use_fieldmap),
+  use_geofile(_use_geofile),
+  width(w),
+  height(h),
+  mapname(_mapname),
+  geoname(_geoname),
+  nevent(0),
+  verbosity(0)
 {
-  _eve_manager = _PHEveDisplay->get_eve_instance();
-  _PHEveDisplay->set_jet_pt_threshold(jet_pt_threshold);
-  _PHEveDisplay->set_jet_e_scale(jet_e_scale);
-  _PHEveDisplay->set_calo_e_threshold(calo_e_threshold);
-  std::cout << "PHEventDisplay initialized.. "<<std::endl;  
+
+  TEveManager::Create(kTRUE,"V");
+  gEve->GetBrowser()->HideBottomTab();
+  TEveWindow::SetMainFrameDefWidth(width);
+  TEveWindow::SetMainFrameDefHeight(height);
+  TFile::SetCacheFileDir(".");
+
+  gEve->FullRedraw3D(kTRUE);  
+  TGLViewer*  v = gEve->GetDefaultGLViewer();
+  //  v->ColorSet().Background().SetColor(kMagenta+4);
+        v->SetGuideState(TGLUtil::kAxesOrigin, kTRUE, kFALSE, 0);
+  v->RefreshPadEditor(v);
+  v->CurrentCamera().RotateRad(0.0, 1.5707);// theta, phi
+  v->DoDraw();
+
+  _PHEveDisplay->set_eve_manager(gEve);
+
+  if (verbosity) std::cout << "PHEventDisplay instantiated."<<std::endl;  
 }
 
 PHEventDisplay::~PHEventDisplay()
@@ -79,33 +114,68 @@ int PHEventDisplay::Init(PHCompositeNode *topNode)
 {
   SubsysReco::Init(topNode);
 
-  register_module<mG4EveDisplay>();
-  std::cout<<"mG4EveDisplay module registered.."<<std::endl;
-  if(_is_svtx_on){ 
-  register_module<mSvtxEveDisplay>();
-  std::cout<<"mSvtxEveDisplay module registered.."<<std::endl;}
-  if(_is_calo_on){
-  register_module<mCaloEveDisplay>();
-  std::cout<<"mCaloEveDisplay module registered.."<<std::endl;}
-  if(_is_jet_on){
-  register_module<mJetEveDisplay>();
-  std::cout<<"mJetEveDisplay module registered.."<<std::endl;}
+  _PHEveDisplay->set_jet_pt_threshold(jet_pt_threshold);
+  _PHEveDisplay->set_jet_e_scale(jet_e_scale);
+  _PHEveDisplay->set_calo_e_threshold(calo_e_threshold);
 
-  std::for_each(_modules.begin(),_modules.end(),
-		bind(&mPHEveModuleBase::init,_1, topNode));
-  std::cout<<"all modules registered.. "<<std::endl;
+  std::cout.precision(5);
+  std::cout<< "jet pt threshold: "<< jet_pt_threshold<<std::endl;
+  std::cout<< "jet e scale: " << jet_e_scale<< std::endl;
+  std::cout<< "calo e threshold: " << calo_e_threshold << std::endl;
+  std::cout<< "svtx :" << is_svtx_on << ", cemc : "<< is_cemc_on << ", hcalin : "<< is_hcalin_on << ", hcalout : "<< is_hcalout_on << ", jet : "<<is_jet_on <<", truth : "<< is_truth_on <<", verbosity : " << verbosity <<std::endl;
+
   return 0;
 }
  
 int PHEventDisplay::InitRun(PHCompositeNode *topNode)
 {
-  std::cout<<"PHEventDisplay - initialize run.. "<<std::endl;
+
+  if (verbosity) std::cout<<"PHEventDisplay - initialize run. "<<std::endl;
+
+  try
+    {
+
+    if (verbosity) std::cout<<"PHEventDisplay - load_geometry() begins. "<<std::endl;
+    _PHEveDisplay->load_geometry(topNode, gEve);
+    if (verbosity) std::cout<<"PHEventDisplay - add_elements() begins."<<std::endl;
+    _PHEveDisplay->add_elements(gEve);
+    if (verbosity) std::cout<<"PHEventDisplay - config_bfield() begins."<<std::endl;
+    _PHEveDisplay->config_bfields();
+
+    if(is_truth_on){
+ 	register_module<mG4EveDisplay>();
+	if(verbosity)
+	std::cout<<"PHEventDisplay - mG4EveDisplay module registered."<<std::endl;}
+    if(is_svtx_on){
+	register_module<mSvtxEveDisplay>();
+	if (verbosity) 
+	std::cout<<"PHEventDisplay - mSvtxEveDisplay module registered."<<std::endl;}
+    if(is_cemc_on || is_hcalin_on || is_hcalout_on){
+	register_module<mCaloEveDisplay>();
+	if(verbosity)
+	std::cout<<"PHEventDisplay - mCaloEveDisplay module registered."<<std::endl;}
+    if(is_jet_on){
+	register_module<mJetEveDisplay>();
+	if(verbosity)
+	std::cout<<"PHEventDisplay - mJetEveDisplay module registered."<<std::endl;}
+	std::for_each(_modules.begin(),_modules.end(),
+                bind(&mPHEveModuleBase::init,_1, topNode));
+    if(verbosity) std::cout<<"PHEventDisplay - all modules registered. "<<std::endl;
+
+    }
+  catch(std::exception &e)
+    {
+        std::cout << "Exception caught while initializing sPHENIX event display: " << e.what() << std::endl;
+    }
+
+
   return 0;
 }
 
 int PHEventDisplay::process_event(PHCompositeNode *topNode)
 {
-  std::cout<<"PHEventDisplay - setting up to process event.." <<std::endl;
+  if (verbosity) std::cout<<"PHEventDisplay - setting up to process event." <<std::endl;
+
   std::for_each(_modules.begin(),
 		_modules.end(),
 		bind(&mPHEveModuleBase::event,
@@ -114,52 +184,68 @@ int PHEventDisplay::process_event(PHCompositeNode *topNode)
   return 0;
 }
 
-
-int PHEventDisplay::ResetEvent(PHCompositeNode *topNode)
-{
-  return 0;
-}
-
 int PHEventDisplay::End(PHCompositeNode *topNode)
 {
   return 0;
 }
 
-TEveManager* PHEventDisplay::get_eve_instance()
-{
-  return _PHEveDisplay->get_eve_instance();
-}
-
 void PHEventDisplay::update_scene()
 {
+  if (verbosity) std::cout << "PHEventDisplay - update_scene() nevent = " <<nevent<<std::endl;
   TThread::Lock(); // Keep the autorotator from segfaulting
   std::for_each(_modules.begin(),_modules.end(),
-		bind(&mPHEveModuleBase::draw_event,_1));
-  _eve_manager->Redraw3D(kFALSE);
+		bind(&mPHEveModuleBase::clear,_1));
+  draw_default();
   TThread::UnLock();
 }
 
 void PHEventDisplay::run_evt_in_thread()
 {
+  if(verbosity) std::cout << "PHEventDisplay - run_evt_in_thread() nevent = "<<nevent<<std::endl;
   if( !pthread_mutex_trylock(&_mutex) ) 
   {
-    if( _pending_update ) update_scene();
+    if( _pending_update ){
+	update_scene();
+    }
     _pending_update = false;
     _update_thread = boost::make_shared<boost::thread>(bind(&PHEventDisplay::reco_thread, this));
   }
 }
 
+
 void PHEventDisplay::reco_thread()
 {
+  if (verbosity) std::cout<< "PHEventDisplay - reco_thread() nevent = "<<nevent<<std::endl;
+  nevent++;
   Fun4AllServer* se = Fun4AllServer::instance();
   se->run(1);
+  if (verbosity>1) std::cout <<"reco_thread() run(1) complete."<<std::endl;
   _pending_update = true;
+  if (verbosity>1) std::cout<< "reco_thread() pendinding update "<<std::endl;
   pthread_mutex_unlock(&_mutex);
+  if (verbosity>1) std::cout <<"reco_thread() mutex unlock()"<<std::endl;
+
 }
+
+void PHEventDisplay::draw_default()
+{
+  if (verbosity) std::cout<<"PHEventDisplay - draw_default() begins."<<std::endl;
+  if (verbosity>1) std::cout<<"draw_default() FullRedraw3D." <<std::endl;
+  gEve->FullRedraw3D(kTRUE);
+  if (verbosity>1) std::cout<<"draw_default() TGLViewer." <<std::endl;
+  TGLViewer*  v = gEve->GetDefaultGLViewer();
+  //  v->ColorSet().Background().SetColor(kMagenta+4);
+  v->SetGuideState(TGLUtil::kAxesOrigin, kTRUE, kFALSE, 0);
+  v->RefreshPadEditor(v);
+  v->CurrentCamera().RotateRad(0.0, 1.5707);// theta, phi
+  v->DoDraw();
+
+}
+
 
 void PHEventDisplay::start_rotation()
 {
-  _rot = _eve_manager->GetDefaultGLViewer()->GetAutoRotator();
+  _rot = gEve->GetDefaultGLViewer()->GetAutoRotator();
   _rot->SetADolly(0.0);
   _rot->SetATheta(0.0);
   _rot->SetWTheta(0.075);
@@ -170,6 +256,6 @@ void PHEventDisplay::start_rotation()
 void
 PHEventDisplay::go_fullscreen()
 {
-  _PHEveDisplay->go_fullscreen();
+  _PHEveDisplay->go_fullscreen(gEve);
 }
 
--- offline/packages/Display/interface/PHEventDisplay.h
+++ offline/packages/Display/interface/PHEventDisplay.h
@@ -2,8 +2,8 @@
         \file PHEventDisplay.h
         \author Sookhyun Lee
         \brief event display interface,
-               parameters/switches set, detector modules called, control display.
-        \version $Revision: 1.1 $
+              set parameters/switches, call detector modules, control display.
+        \version $Revision: 1.2 $
         \date    $Date: 07/26/2016
 */
 
@@ -26,6 +26,7 @@ class shared_ptr;
 class PHEveDisplay;
 class PHCompositeNode;
 class TEveManager;
+class TGLViewer;
 class TGLAutoRotator;
 class mPHEveModuleBase;
 
@@ -34,15 +35,10 @@ class PHEventDisplay : public SubsysReco
 public:
   PHEventDisplay(int w, 
 		  int h,
-		  bool use_fieldmap,
-		  const std::string& mapname,
-		  const std::string& geoname,
-		  float jet_pt_threshold,
-		  float jet_e_scale,
-		  float calo_e_threshold,
-		  bool  is_svtx_on,
-		  bool  is_calo_on,
-		  bool  is_jet_on);
+		  bool _use_fieldmap,
+		  bool _use_geofile,
+		  const std::string& _mapname,
+		  const std::string& _geoname);
 
   ~PHEventDisplay();
 
@@ -52,21 +48,28 @@ public:
   int InitRun(PHCompositeNode *topNode);
   /// Event processing
   int process_event(PHCompositeNode *topNode);
-  /// Event reset
-  int ResetEvent(PHCompositeNode *topNode);
   /// End of process
   int End(PHCompositeNode *topNode);
 
-  /// Return a pointer to the underlying TEveManager
-  TEveManager* get_eve_instance();
   /// Threaded access to Fun4All server
   void run_evt_in_thread();
-
   void start_rotation();
   void go_fullscreen();
+  void set_jet_pt_threshold(float pt){jet_pt_threshold = pt;}
+  void set_jet_e_scale(float e_scale){jet_e_scale = e_scale;}
+  void set_calo_e_threshold(float e){calo_e_threshold = e;}
+  void set_svtx_on(bool svtx_on) {is_svtx_on = svtx_on;}
+  void set_cemc_on(bool cemc_on) {is_cemc_on = cemc_on;}
+  void set_hcalin_on(bool hcalin_on){is_hcalin_on = hcalin_on;}
+  void set_hcalout_on(bool hcalout_on){is_hcalout_on = hcalout_on;}
+  void set_jet_on(bool jet_on) {is_jet_on = jet_on;}
+  void set_truth_on(bool truth_on) {is_truth_on = truth_on;}
+  void set_verbosity(int verb) {verbosity = verb;}
+
 
 private:
   void reco_thread();
+  void draw_default();  
   void update_scene();
 
 #ifndef __CINT__
@@ -91,15 +94,28 @@ private:
   boost::shared_ptr<PHEveDisplay> _PHEveDisplay;
 #endif
 
-  TEveManager* _eve_manager;
   TGLAutoRotator* _rot;
 
 #ifndef __CINT__
   boost::shared_ptr<boost::thread> _status_thread;
 #endif
-  bool _is_svtx_on;
-  bool _is_calo_on;
-  bool _is_jet_on;
+  float jet_pt_threshold;
+  float jet_e_scale;
+  float calo_e_threshold;
+  bool is_svtx_on;
+  bool is_cemc_on;
+  bool is_hcalin_on;
+  bool is_hcalout_on;
+  bool is_jet_on;
+  bool is_truth_on;
+  bool use_fieldmap;
+  bool use_geofile;
+  int width;
+  int height;
+  std::string mapname;
+  std::string geoname;
+  int nevent;
+  int verbosity;
 };
 
-#endif // __PHONLINEDISPLAY_H__
+#endif // __PHEVENTDISPLAY_H__
