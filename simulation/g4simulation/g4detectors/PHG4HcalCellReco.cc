878690731f0412fedc598ba782624e5eb61c6eb3
Chris Pinkenburg <pinkenburg@bnl.gov>
2016-11-16 15:14:58 -0500

    new HCal Cell reco code which uses scintillators as given in g4 sims - remove all the hyper complicated leftovers from the cylinder eta/phi binning

--- simulation/g4simulation/g4detectors/PHG4HcalCellReco.cc
+++ simulation/g4simulation/g4detectors/PHG4HcalCellReco.cc
@@ -1,11 +1,7 @@
 #include "PHG4HcalCellReco.h"
-#include "PHG4CylinderGeomContainer.h"
-#include "PHG4CylinderGeom.h"
-#include "PHG4CylinderCellGeomContainer.h"
-#include "PHG4CylinderCellGeom.h"
-#include "PHG4CylinderCellv1.h"
-#include "PHG4CylinderCellContainer.h"
-#include "PHG4CylinderCellDefs.h"
+#include "PHG4ScintillatorSlatv1.h"
+#include "PHG4ScintillatorSlatContainer.h"
+#include "PHG4Parameters.h"
 
 #include <g4main/PHG4Hit.h>
 #include <g4main/PHG4HitContainer.h>
@@ -24,18 +20,21 @@
 #include <limits>       // std::numeric_limits
 
 using namespace std;
+// for hcal dimension
+#define ROWDIM 320
+#define COLUMNDIM 24
+static PHG4ScintillatorSlat *slatarray[ROWDIM][COLUMNDIM];
 
 PHG4HcalCellReco::PHG4HcalCellReco(const string &name) :
   SubsysReco(name),
+  PHG4ParameterInterface(name),
   _timer(PHTimeServer::get()->insert_new(name.c_str())),
-  nslatscombined(1),
-  netabins(24), // that is our default
   chkenergyconservation(0),
-  tmin_default(0.0),  // ns
-  tmax_default(60.0), // ns
-  tmin_max()
+  tmin(NAN),  // ns
+  tmax(NAN) // ns
 {
-  memset(nbins, 0, sizeof(nbins));  
+  memset(slatarray, 0, sizeof(slatarray));
+  InitializeParameters();
 }
 
 int PHG4HcalCellReco::InitRun(PHCompositeNode *topNode)
@@ -43,13 +42,16 @@ int PHG4HcalCellReco::InitRun(PHCompositeNode *topNode)
   PHNodeIterator iter(topNode);
 
   // Looking for the DST node
-  PHCompositeNode *dstNode;
-  dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "DST"));
+  PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "DST"));
   if (!dstNode)
     {
       std::cout << PHWHERE << "DST Node missing, doing nothing." << std::endl;
       exit(1);
     }
+  PHCompositeNode *runNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "RUN" ));
+
+  string paramnodename = "G4CELLPARAM_" + detector;
+  // string paramname = detector + "_cell";
   hitnodename = "G4HIT_" + detector;
   PHG4HitContainer *g4hit = findNode::getClass<PHG4HitContainer>(topNode, hitnodename.c_str());
   if (!g4hit)
@@ -58,100 +60,26 @@ int PHG4HcalCellReco::InitRun(PHCompositeNode *topNode)
       exit(1);
     }
   cellnodename = "G4CELL_" + detector;
-  PHG4CylinderCellContainer *cells = findNode::getClass<PHG4CylinderCellContainer>(topNode , cellnodename);
-  if (!cells)
+  PHG4ScintillatorSlatContainer *slats = findNode::getClass<PHG4ScintillatorSlatContainer>(topNode , cellnodename);
+  if (!slats)
     {
       PHNodeIterator dstiter(dstNode);
       PHCompositeNode *DetNode =
-          dynamic_cast<PHCompositeNode*>(dstiter.findFirst("PHCompositeNode",
-              detector));
+	dynamic_cast<PHCompositeNode*>(dstiter.findFirst("PHCompositeNode",
+							 detector));
       if (!DetNode)
         {
           DetNode = new PHCompositeNode(detector);
           dstNode->addNode(DetNode);
         }
-      cells = new PHG4CylinderCellContainer();
-      PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(cells, cellnodename.c_str() , "PHObject");
+      slats = new PHG4ScintillatorSlatContainer();
+      PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(slats, cellnodename.c_str() , "PHObject");
       DetNode->addNode(newNode);
     }
-
-  geonodename = "CYLINDERGEOM_" + detector;
-  PHG4CylinderGeomContainer *geo =  findNode::getClass<PHG4CylinderGeomContainer>(topNode , geonodename.c_str());
-  if (!geo)
-    {
-      cout << "Could not locate geometry node " << geonodename << endl;
-      exit(1);
-    }
-  if (verbosity > 0)
-    {
-      geo->identify();
-    }
-  seggeonodename = "CYLINDERCELLGEOM_" + detector;
-  PHG4CylinderCellGeomContainer *seggeo = findNode::getClass<PHG4CylinderCellGeomContainer>(topNode , seggeonodename.c_str());
-  if (!seggeo)
-    {
-      seggeo = new PHG4CylinderCellGeomContainer();
-      PHCompositeNode *runNode = dynamic_cast<PHCompositeNode*>(iter.findFirst("PHCompositeNode", "RUN" ));
-      PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(seggeo, seggeonodename.c_str() , "PHObject");
-      runNode->addNode(newNode);
-    }
-
-  map<int, PHG4CylinderGeom *>::const_iterator miter;
-  pair <map<int, PHG4CylinderGeom *>::const_iterator, map<int, PHG4CylinderGeom *>::const_iterator> begin_end = geo->get_begin_end();
-  map<int, std::pair <double, int> >::iterator sizeiter;
-  for (miter = begin_end.first; miter != begin_end.second; ++miter)
-    {
-      PHG4CylinderGeom *layergeom = miter->second;
-      int layer = layergeom->get_layer();
-      int numslats = layergeom->get_nscint();
-      // create geo object and fill with variables common to all binning methods
-      PHG4CylinderCellGeom *layerseggeo = new PHG4CylinderCellGeom();
-      layerseggeo->set_layer(layergeom->get_layer());
-      layerseggeo->set_radius(layergeom->get_radius());
-      layerseggeo->set_thickness(layergeom->get_thickness());
-      int nslatbins = numslats / nslatscombined;
-	  // check if the number of slats is a multiple of our number of slats read out
-	  // if not print out a warning and drop the remaining slats 
-	  // from reconstruction since
-	  // we don't want weird differently sized towers 
-          if (numslats % nslatscombined)
-            {
-	      //              nslatbins++;
-              cout << Name() << ": total number of slats " << numslats 
-                   << " not multiple of readout combined slats "
-                   <<  nslatscombined << " dropping the last " 
-                   << numslats % nslatscombined
-                   << " slats from reconstruction" << endl;
-	      cout << "This will affect clusters which span the rollover phi range" << endl;
-            }
-	  if (verbosity > 1)
-	    {
-	      layergeom->identify();
-	    }
-          layerseggeo->set_binning(PHG4CylinderCellDefs::etaslatbinning);
-          layerseggeo->set_etabins(netabins);
-          layerseggeo->set_etamin(-1.1);
-          layerseggeo->set_etastep((1.1+1.1)/netabins);
-          layerseggeo->set_phimin(layergeom->get_phi_slat_zero());
-	  double deltaphi = 2.*M_PI/layergeom->get_nscint();
-	  layerseggeo->set_phistep(deltaphi*nslatscombined);
-	  layerseggeo->set_phibins(nslatbins);
-          etastep[layer] = (1.1+1.1)/netabins;
-      // add geo object filled by different binning methods
-      seggeo->AddLayerCellGeom(layerseggeo);
-      if (verbosity > 1)
-	{
-	  layerseggeo->identify();
-	}
-    }
-
-  for (std::map<int,int>::iterator iter = binning.begin(); 
-       iter != binning.end(); ++iter) {
-    int layer = iter->first;
-    // if the user doesn't set an integration window, set the default
-    tmin_max.insert(std::make_pair(layer,std::make_pair(tmin_default,tmax_default)));    
-  }
-  
+  UpdateParametersWithMacro();
+  SaveToNodeTree(runNode,paramnodename);
+  tmin = get_double_param("tmin");
+  tmax = get_double_param("tmax");
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
@@ -166,17 +94,10 @@ PHG4HcalCellReco::process_event(PHCompositeNode *topNode)
       cout << "Could not locate g4 hit node " << hitnodename << endl;
       exit(1);
     }
-  PHG4CylinderCellContainer *cells = findNode::getClass<PHG4CylinderCellContainer>(topNode, cellnodename);
-  if (! cells)
+  PHG4ScintillatorSlatContainer *slats = findNode::getClass<PHG4ScintillatorSlatContainer>(topNode, cellnodename);
+  if (! slats)
     {
       cout << "could not locate cell node " << cellnodename << endl;
-      exit(1);
-    }
-
-  PHG4CylinderCellGeomContainer *seggeo = findNode::getClass<PHG4CylinderCellGeomContainer>(topNode , seggeonodename.c_str());
-  if (! seggeo)
-    {
-      cout << "could not locate geo node " << seggeonodename << endl;
       exit(1);
     }
 
@@ -186,74 +107,67 @@ PHG4HcalCellReco::process_event(PHCompositeNode *topNode)
     {
       PHG4HitContainer::ConstIterator hiter;
       PHG4HitContainer::ConstRange hit_begin_end = g4hit->getHits(*layer);
-      PHG4CylinderCellGeom *geo = seggeo->GetLayerCellGeom(*layer);
-      int nslatbins = geo->get_phibins();
       for (hiter = hit_begin_end.first; hiter != hit_begin_end.second; ++hiter)
 	{
+	  if (hiter->second->get_t(0)>tmax) continue;
+	  if (hiter->second->get_t(1)<tmin) continue;
+	  short icolumn = hiter->second->get_scint_id();
+	  short irow = hiter->second->get_row();
+	  if ( irow >= ROWDIM || irow < 0)
+	    {
+	      cout << "row " << irow
+		   << " exceed array size: " << ROWDIM
+		   << " adjust ROWDIM and recompile" << endl;
+	      exit(1);
+	    }
+
+	  if (icolumn >= COLUMNDIM || icolumn < 0)
+	    {
+	      cout << "column: " << icolumn
+		   << " exceed array size: " << COLUMNDIM
+		   << " adjust COLUMNDIM and recompile" << endl;
+	      exit(1);
+	    }
+
+
+	  if (!slatarray[irow][icolumn])
+	    {
+	      slatarray[irow][icolumn] = new PHG4ScintillatorSlatv1();
+	    }
+	  slatarray[irow][icolumn]->add_edep(hiter->second->get_edep(),
+					     hiter->second->get_eion(),
+					     hiter->second->get_light_yield());
+	  slatarray[irow][icolumn]->add_hit_key(hiter->first);
+	  // cout << "row: " << hiter->second->get_row() 
+	  // 	   << ", column: " << hiter->second->get_scint_id() << endl;
 	  // checking ADC timing integration window cut
-	  if (hiter->second->get_t(0)>tmin_max[*layer].second) continue;
-	  if (hiter->second->get_t(1)<tmin_max[*layer].first) continue;
-
-	  int slatno = hiter->second->get_scint_id();
-	  int slatbin;
-	  slatbin = hiter->second->get_layer() / nslatscombined;
-	  if (slatbin < 0 || slatbin > nslatbins)
-	    {
-	      if (slatbin + 1 > nslatbins)
-		{
-		  if (verbosity > 0)
-		    {
-		      cout << "dealing with non fitting slat binning, this one will be dropped" << endl;
-		    }
-		  continue;
-		}
-	      cout << "slatbin out of range: " << slatbin
-		   << ", slatbins 0 - " << nslatbins
-		   << ", slat no: " << hiter->second->get_scint_id()
-		   << endl;
-	    }
-
-	  unsigned int key = (slatbin<<16) + slatno;
-	  if (celllist.find(key) == celllist.end())
-	    {
-	      celllist[key] = new PHG4CylinderCellv1();
-	      celllist[key]->set_layer(*layer);
-	      celllist[key]->set_phibin(slatbin);
-	      celllist[key]->set_etabin(slatno);
-	    }
-
-    celllist[key]->add_edep(hiter->first, hiter->second->get_edep(),hiter->second->get_light_yield());
-    celllist[key]->add_shower_edep(hiter->second->get_shower_id(), hiter->second->get_edep());
 	} // end loop over g4hits
-      int numcells = 0;
-      for (map<unsigned int, PHG4CylinderCell *>::const_iterator mapiter = celllist.begin();mapiter != celllist.end() ; ++mapiter)
+      int nslathits = 0;
+      for (int irow = 0; irow<ROWDIM; irow++)
 	{
-	  cells->AddCylinderCell(*layer, mapiter->second);
-	  numcells++;
-	  if (verbosity > 1)
+	  for (int icolumn = 0; icolumn<COLUMNDIM; icolumn++)
 	    {
-	      cout << "Adding cell in bin slat: " << (mapiter->first >> 16)
-		   << ", eta: " << (mapiter->first & 0xFFFF)
-		   << ", energy dep: " << mapiter->second->get_edep()
-		   << endl;
+	      if (slatarray[irow][icolumn])
+		{
+		  PHG4ScintillatorSlatDefs::keytype key = PHG4ScintillatorSlatDefs::genkey(irow,icolumn);
+		  slats->AddScintillatorSlat(key,slatarray[irow][icolumn]);
+		  slatarray[irow][icolumn] = NULL;
+		  nslathits++;
+		}
 	    }
 	}
-      celllist.clear();
       if (verbosity > 0)
 	{
-	  cout << Name() << ": found " << numcells << " eta/slat cells with energy deposition" << endl;
+	  cout << Name() << ": found " << nslathits << " slats with energy deposition" << endl;
 	}
     }
 
-
-
-
-if (chkenergyconservation)
-  {
-    CheckEnergy(topNode);
-  }
-_timer.get()->stop();
-return Fun4AllReturnCodes::EVENT_OK;
+  if (chkenergyconservation)
+    {
+      CheckEnergy(topNode);
+    }
+  _timer.get()->stop();
+  return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int
@@ -262,40 +176,12 @@ PHG4HcalCellReco::End(PHCompositeNode *topNode)
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-void
-PHG4HcalCellReco::etasize_nslat(const int i, const double deltaeta, const int nslat)
-{
-  if (nslat >= 1)
-    {
-      nslatscombined = nslat;
-      set_size(i, deltaeta, nslat, PHG4CylinderCellDefs::etaslatbinning);
-    }
-  else
-    {
-      cout << PHWHERE << ": bad number of slats to combine: " << nslat << endl;
-      exit(1);
-    }
-  return;
-}
-
-void
-PHG4HcalCellReco::set_size(const int i, const double sizeA, const int sizeB, const int what)
-{
-  if (binning.find(i) != binning.end())
-    {
-      cout << "size for layer " << i << " already set" << endl;
-      return;
-    }
-  binning[i] = what;
-  cell_size[i] = std::make_pair(sizeA, sizeB);
-  return;
-}
 
 int
 PHG4HcalCellReco::CheckEnergy(PHCompositeNode *topNode)
 {
   PHG4HitContainer *g4hit = findNode::getClass<PHG4HitContainer>(topNode, hitnodename.c_str());
-  PHG4CylinderCellContainer *cells = findNode::getClass<PHG4CylinderCellContainer>(topNode, cellnodename);
+  PHG4ScintillatorSlatContainer *slats = findNode::getClass<PHG4ScintillatorSlatContainer>(topNode, cellnodename);
   double sum_energy_g4hit = 0.;
   double sum_energy_cells = 0.;
   PHG4HitContainer::ConstRange hit_begin_end = g4hit->getHits();
@@ -304,8 +190,8 @@ PHG4HcalCellReco::CheckEnergy(PHCompositeNode *topNode)
     {
       sum_energy_g4hit += hiter->second->get_edep();
     }
-  PHG4CylinderCellContainer::ConstRange cell_begin_end = cells->getCylinderCells();
-  PHG4CylinderCellContainer::ConstIterator citer;
+  PHG4ScintillatorSlatContainer::ConstRange cell_begin_end = slats->getScintillatorSlats();
+  PHG4ScintillatorSlatContainer::ConstIterator citer;
   for (citer = cell_begin_end.first; citer != cell_begin_end.second; ++citer)
     {
       sum_energy_cells += citer->second->get_edep();
@@ -314,6 +200,7 @@ PHG4HcalCellReco::CheckEnergy(PHCompositeNode *topNode)
   // the fractional eloss for particles traversing eta bins leads to minute rounding errors
   if (fabs(sum_energy_cells - sum_energy_g4hit) / sum_energy_g4hit > 1e-6)
     {
+      cout << "hint: timing cuts tmin/tmax will do this to you" << endl;
       cout << "energy mismatch between cells: " << sum_energy_cells
 	   << " and hits: " << sum_energy_g4hit
 	   << " diff sum(cells) - sum(hits): " << sum_energy_cells - sum_energy_g4hit
@@ -330,29 +217,10 @@ PHG4HcalCellReco::CheckEnergy(PHCompositeNode *topNode)
   return 0;
 }
 
-
-double
-PHG4HcalCellReco::get_phi_slat_zero_low(const double radius, const double thickness, const double tiltangle)
+void
+PHG4HcalCellReco::SetDefaultParameters()
 {
-  // A/sin(alpha) = C/sin(gamma)
-  // beta = 90-gamma
-
-  double sinalpha = ((radius+thickness/2.)/radius) * sin(tiltangle);
-  double beta =  asin(sinalpha) - tiltangle;
-  cout << "beta: " << beta * 180./M_PI << endl;
-  return beta;
-}
-
-double
-PHG4HcalCellReco::get_phi_slat_zero_up(const double radius, const double thickness, const double tiltangle)
-{
-  // A/sin(alpha) = C/sin(gamma)
-  // beta = 90-gamma
-  double a = radius+thickness;
-  double c = radius+thickness/2.;
-  double alpha = M_PI - tiltangle;
-  double singamma = c/a * sin(alpha);
-  double beta =  M_PI - asin(singamma) - alpha;
-  cout << "beta: " << beta * 180./M_PI << endl;
-  return beta;
+  set_default_double_param("tmax",60.0);
+  set_default_double_param("tmin",0.0);
+  return;
 }
--- simulation/g4simulation/g4detectors/PHG4HcalCellReco.h
+++ simulation/g4simulation/g4detectors/PHG4HcalCellReco.h
@@ -1,5 +1,7 @@
-#ifndef PHG4HCALCELLRECO_H
-#define PHG4HCALCELLRECO_H
+#ifndef PHG4HcalCellReco_H
+#define PHG4HcalCellReco_H
+
+#include "PHG4ParameterInterface.h"
 
 #include <fun4all/SubsysReco.h>
 #include <phool/PHTimeServer.h>
@@ -10,11 +12,11 @@
 class PHCompositeNode;
 class PHG4CylinderCell;
 
-class PHG4HcalCellReco : public SubsysReco
+class PHG4HcalCellReco : public SubsysReco, public PHG4ParameterInterface
 {
  public:
 
-  PHG4HcalCellReco(const std::string &name = "HCALCELLRECO");
+  PHG4HcalCellReco(const std::string &name = "Prototype2HcalCellReco");
 
   virtual ~PHG4HcalCellReco(){}
   
@@ -27,46 +29,23 @@ class PHG4HcalCellReco : public SubsysReco
   //! end of process
   int End(PHCompositeNode *topNode);
   
-  void Detector(const std::string &d) {detector = d;}
-  void etasize_nslat(const int i, const double deltaeta, const int nslat);
-  void checkenergy(const int i=1) {chkenergyconservation = i;}
-  void set_etabins(const int nbins=24) {netabins = nbins;}
+  void SetDefaultParameters();
 
-  double get_timing_window_min(const int i) {return tmin_max[i].first;}
-  double get_timing_window_max(const int i) {return tmin_max[i].second;}
-  void   set_timing_window(const int i, const double tmin, const double tmax) {
-    tmin_max[i] = std::make_pair(tmin,tmax);
-  }
-  void   set_timing_window_defaults(const double tmin, const double tmax) {
-    tmin_default = tmin; tmax_default = tmax;
-  }
+  void Detector(const std::string &d) {detector = d;}
+  void checkenergy(const int i=1) {chkenergyconservation = i;}
+
+  void   set_timing_window(const double tmi, const double tma) {tmin = tmi; tmax = tma;}
   
  protected:
-  void set_size(const int i, const double sizeA, const int sizeB, const int what);
   int CheckEnergy(PHCompositeNode *topNode);
-  double get_phi_slat_zero_low(const double radius, const double thickness, const double tiltangle);
-  double get_phi_slat_zero_up(const double radius, const double thickness, const double tiltangle);
-  static std::pair<double, double> get_etaphi(const double x, const double y, const double z);
-  static double get_eta(const double radius, const double z);
-  std::map<int, int>  binning;
-  std::map<int, std::pair <double,int> > cell_size; // cell size in eta/nslats
-  std::map<int, std::pair <double,double> > zmin_max; // zmin/zmax for each layer for faster lookup
-  std::map<int, double> etastep;
   std::string detector;
   std::string hitnodename;
   std::string cellnodename;
-  std::string geonodename;
-  std::string seggeonodename;
   PHTimeServer::timer _timer;
-  int nbins[2];
-  int nslatscombined;
-  int netabins;
   int chkenergyconservation;
-  std::map<unsigned int, PHG4CylinderCell *> celllist;
 
-  double tmin_default;
-  double tmax_default;
-  std::map<int, std::pair<double,double> > tmin_max;
+  double tmin;
+  double tmax;
 };
 
 #endif
