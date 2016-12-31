44cf53f8d16a4b657209077469aac18b54307d9e
Jin Huang <jhuang@bnl.gov>
2016-12-31 14:48:01 -0500

    allow save PdbParameterMapContainer to files

--- offline/database/pdbcal/base/PdbParameterMapContainer.cc
+++ offline/database/pdbcal/base/PdbParameterMapContainer.cc
@@ -1,11 +1,20 @@
 #include "PdbParameterMapContainer.h"
 #include "PdbParameterMap.h"
+#include "PdbBankID.h"
 
 #include <phool/phool.h>
+#include <phool/PHTimeStamp.h>
 
+#include <TBufferXML.h>
+#include <TFile.h>
 #include <TSystem.h>
 
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -62,4 +71,54 @@ PdbParameterMapContainer::GetParametersToModify(const int layer)
       return NULL;
     }
   return iter->second;
+}
+
+int
+PdbParameterMapContainer::WriteToFile(const std::string &detector_name,
+    const string &extension, const string &dir)
+{
+  //Note the naming convention should be consistent with PHG4Parameters::WriteToFile
+
+  ostringstream fullpath;
+  ostringstream fnamestream;
+  PdbBankID bankID(0); // lets start at zero
+  PHTimeStamp TStart(0);
+  PHTimeStamp TStop(0xffffffff);
+  fullpath << dir;
+  // add / if directory lacks ending /
+  if (*(dir.rbegin()) != '/')
+    {
+      fullpath << "/";
+    }
+  fnamestream << detector_name << "_geoparams" << "-"
+      << bankID.getInternalValue() << "-" << TStart.getTics() << "-"
+      << TStop.getTics() << "-" << time(0) << "." << extension;
+  string fname = fnamestream.str();
+  std::transform(fname.begin(), fname.end(), fname.begin(), ::tolower);
+  fullpath << fname;
+
+  cout << "PdbParameterMapContainer::WriteToFile - save to " << fullpath.str()
+      << endl;
+
+  TFile *f = TFile::Open(fullpath.str().c_str(), "recreate");
+
+  PdbParameterMapContainer * container = new PdbParameterMapContainer();
+  for (std::map<int, PdbParameterMap *>::const_iterator it =
+      parametermap.begin(); it != parametermap.end(); ++it)
+    {
+      PdbParameterMap *myparm = static_cast<PdbParameterMap *> (it->second->Clone());
+      container->AddPdbParameterMap(it->first, myparm);
+    }
+
+  // force xml file writing to use extended precision shown experimentally
+  // to not modify input parameters (.15e)
+  string floatformat = TBufferXML::GetFloatFormat();
+  TBufferXML::SetFloatFormat("%.15e");
+  container->Write("PdbParameterMapContainer");
+  delete f;
+  // restore previous xml float format
+  TBufferXML::SetFloatFormat(floatformat.c_str());
+  cout << "sleeping 1 second to prevent duplicate inserttimes" << endl;
+  sleep(1);
+  return 0;
 }
