af7f1203f20bba6122d0fd084254fb23d7025d0b
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-03-03 16:19:19 -0500

    add shower edep to cell

--- simulation/g4simulation/g4detectors/PHG4HcalCellReco.cc
+++ simulation/g4simulation/g4detectors/PHG4HcalCellReco.cc
@@ -164,6 +164,7 @@ PHG4HcalCellReco::process_event(PHCompositeNode *topNode)
       slatarray[irow][icolumn]->add_eion(hiter->second->get_eion());
       slatarray[irow][icolumn]->add_light_yield(hiter->second->get_light_yield());
       slatarray[irow][icolumn]->add_edep(hiter->first, hiter->second->get_edep());
+      slatarray[irow][icolumn]->add_shower_edep(hiter->second->get_shower_id(), hiter->second->get_edep());
     } // end loop over g4hits
   int nslathits = 0;
   for (int irow = 0; irow<ROWDIM; irow++)
