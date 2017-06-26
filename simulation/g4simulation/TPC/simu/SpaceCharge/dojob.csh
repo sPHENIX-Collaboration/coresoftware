268d8e8db3de19fe74905b28dac85d7605186f32
Carlos Perez Lara <cperez@rcas2066.rcf.bnl.gov>
2017-06-26 01:20:52 -0400

    moving tpc project

--- simulation/g4simulation/TPC/simu/SpaceCharge/dojob.csh
+++
@@ -1,3 +0,0 @@
-#!/bin/tcsh
-./genE $1
-exit
--- simulation/g4simulation/TPC/simu/TPCsimuLinkDef.h
+++
@@ -1,6 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class TPCDetectorSubsystem+;
-#pragma link C++ class TPCSimulationSubsystem+;
-
-#endif /* __CINT__ */
