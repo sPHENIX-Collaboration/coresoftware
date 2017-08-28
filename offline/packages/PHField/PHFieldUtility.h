28aa063e2fe49761f8231050e0c71d99bdb95e84
Jin Huang <jhuang@bnl.gov>
2017-08-27 20:34:40 -0400

    clean up interface functions

--- offline/packages/PHField/PHFieldUtility.h
+++ offline/packages/PHField/PHFieldUtility.h
@@ -1,95 +1,60 @@
-#ifndef PHGeomUtility_HH__
-#define PHGeomUtility_HH__
+#ifndef PHFieldUtility_HH__
+#define PHFieldUtility_HH__
 
 #include <ctime>
+#include <iostream>
 #include <map>
 #include <set>
 #include <string>
-#include <iostream>
+
+#include "PHFieldConfig.h"
 
 class PHCompositeNode;
-class TGeoManager;
-class PHGeomTGeo;
-class PHGeomIOTGeo;
+class PHField;
+class PHFieldConfig;
 
 //! Toolsets to do geometry operations
-class PHGeomUtility
+class PHFieldUtility
 {
-
-protected:
-
+ protected:
   // static tool sets only
-  PHGeomUtility();
-  virtual
-  ~PHGeomUtility();
+  PHFieldUtility();
+  virtual ~PHFieldUtility();
 
-public:
+ public:
 
-  //! Main user interface: DST node -> TGeoManager for downstream use
-  static TGeoManager *
-  GetTGeoManager(PHCompositeNode *topNode);
+  //! Make a default PHFieldConfig
+  static PHFieldConfig * MakeDefaultFieldConfig();
 
-  //! TGeo ROOT/GDML/Macro file -> DST node with automatic file type discrimination based on file names
-  static int
-  ImportGeomFile(PHCompositeNode *topNode, const std::string & geometry_file);
+  //! Get transient PHField from DST nodes. If not found, make a new one based on default_config
+  static PHField *
+  GetFieldMapNode(PHFieldConfig *default_config = nullptr, PHCompositeNode *topNode = nullptr);
 
-  //! gGeoManager -> DST node
-  static int
-  ImportCurrentTGeoManager(PHCompositeNode *topNode);
+  //! Get persistent PHFieldConfig from DST nodes. If not found, make a new one based on default_config
+  static PHFieldConfig *
+  GetGeomIOTGeoNode(PHFieldConfig *default_config = nullptr, PHCompositeNode *topNode = nullptr);
 
-  //! DST node -> TGeoManager -> export files, like gdml, .root or .C formats
-  static void
-  ExportGeomtry(PHCompositeNode *topNode, const std::string & geometry_file);
-
-  //! Get non-persistent PHGeomTGeo from DST nodes. If not found, make a new one
-  static PHGeomTGeo *
-  GetGeomTGeoNode(PHCompositeNode *topNode, bool build_new = true);
-
-  //! Get persistent PHGeomIOTGeo from DST nodes. If not found, make a new one
-  static PHGeomIOTGeo *
-  GetGeomIOTGeoNode(PHCompositeNode *topNode, bool build_new = true);
-
-  //! Update persistent PHGeomIOTGeo node RUN/GEOMETRY_IO based on run-time object PHGeomTGeo at RUN/GEOMETRY
-  //! \return the updated PHGeomIOTGeo from DST tree
-  static PHGeomIOTGeo *
-  UpdateIONode(PHCompositeNode *topNode);
-
-  //! Build or update PHGeomTGeo node RUN/GEOMETRY based on the persistent PHGeomIOTGeo node RUN/GEOMETRY_IO
+  //! Build or update PHField node RUN/GEOMETRY based on the persistent PHGeomIOTGeo node RUN/GEOMETRY_IO
   //! \return the updated PHGeomTGeo from DST tree
-  static PHGeomTGeo *
+  static PHField *
   LoadFromIONode(PHCompositeNode *topNode);
 
-  //! Make a name for tmp geometry file
-  //! Geometry files gain a size of ~10MB and it used in translation from Geant4 to DST format.
-  //! This tmp file should be on a local file system (/tmp/) and write/deletable
+  //! DST node name for RunTime field map object
   static std::string
-  GenerateGeometryFileName(const std::string & filename_extension = "gdml");
-
-  //! delete the geometry file after use
-  static bool
-  RemoveGeometryFile(const std::string & file_name);
-
-  //! Verbosity for geometry IO like, TGeoMangers
-  static void SetVerbosity(int v);
-
-  //! Verbosity for geometry IO like, TGeoMangers
-  static int GetVerbosity();
-
-  //! DST node name for RunTime geometry object
-  static std::string
-  GetDSTNodeName()
+  GetDSTFieldMapNodeName()
   {
-    return std::string("GEOMETRY");
+    return std::string("FIELD_MAP");
   }
 
-  //! DST node name for persistent geometry storage node
+  //! DST node name for persistent field configuration node
   static std::string
-  GetDSTIONodeName()
+  GetDSTConfigNodeName()
   {
-    return std::string("GEOMETRY_IO");
+    return std::string("FIELD_CONFIG");
   }
-protected:
+
+ protected:
 
 };
 
-#endif /* PHGeomUtility_HH__ */
+#endif /* PHFieldUtility_HH__ */
