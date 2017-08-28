8b8fadeda0cd62eb3f9a8e4d257b6765f1bef77f
Jin Huang <jhuang@bnl.gov>
2017-08-28 17:47:20 -0400

    user wrapper field class

---
+++ offline/packages/PHGenFitPkg/GenFitExp/Field.h
@@ -0,0 +1,59 @@
+/*
+   Authors: Haiwang Yu
+*/
+/** @addtogroup genfit
+ * @{
+ */
+
+#ifndef genfit_Field_h
+#define genfit_Field_h
+
+#include "GenFit/AbsBField.h"
+
+#include "boost/tuple/tuple.hpp"
+#include "boost/tuple/tuple_comparison.hpp"
+#include <map>
+
+#include <TH2D.h>
+
+class PHField;
+
+namespace genfit {
+
+/** @brief Field Wrapper
+ *
+ *  @author Haiwang Yu (New Mexico State University)
+ * 
+ */
+class Field : public AbsBField {
+ public:
+  Field(const PHField* field) : field_(field)
+  { }
+
+  virtual ~Field() {}
+
+
+//  void plot(std::string option = "");
+
+  //! return value at position
+  TVector3 get(const TVector3& pos) const;
+  void get(const double& posX, const double& posY, const double& posZ, double& Bx, double& By, double& Bz) const;
+
+  const PHField* get_field() const
+  {
+    return field_;
+  }
+
+  void set_field(const PHField* field)
+  {
+    field_ = field;
+  }
+
+ private:
+  const PHField* field_;
+};
+
+} /* End of namespace genfit */
+/** @} */
+
+#endif // genfit_Field_h
