ec30dbd3c296941e1fbfde205280d03cb9ea1ed6
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-09-29 22:38:35 -0400

    move to new tpc code

--- simulation/g4simulation/g4tpc/TPCbaseLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class TPCPadMap+;
-#pragma link C++ class TPCHit+;
-#pragma link C++ class TPCDigit+;
-#pragma link C++ class TPCHitsContainer+;
-#pragma link C++ class TPCDigitsContainer+;
-
-#endif /* __CINT__ */
