d7ab19b2cbab8a73e2e4d46c5776a7d9dde62a61
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-11-14 22:23:30 -0500

    add queue to preload seeds, maximize entropy by always returning random values, cleanup

--- offline/framework/phool/PHRandomSeed.cc
+++ offline/framework/phool/PHRandomSeed.cc
@@ -1,29 +1,39 @@
 #include "PHRandomSeed.h"
 #include "recoConsts.h"
 
-#include <gsl/gsl_const.h>
-#include <gsl/gsl_randist.h>
-#include <gsl/gsl_rng.h>
-
 #include <random>
-#include <cassert>
-#include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+static queue<unsigned int> seedqueue;
+static std::mt19937 fRandomGenerator;
+static std::uniform_int_distribution<unsigned int> fDistribution;
+
 bool PHRandomSeed::fInitialized(false);
-mt19937 PHRandomSeed::fRandomGenerator;
-uniform_int_distribution<unsigned int> PHRandomSeed::fDistribution;
+bool PHRandomSeed::fFixed(false);
 
 unsigned int PHRandomSeed::GetSeed()
 {
+  if (! seedqueue.empty())
+  {
+    unsigned int iseed = seedqueue.front();
+    seedqueue.pop();
+    return iseed;
+  }
   if (!fInitialized)
+  {
     InitSeed();
-
-  assert(fInitialized);
+  }
+  if (fFixed)
+  {
   return fDistribution(fRandomGenerator);
+  }
+     std::random_device rdev;
+      uint32_t random_seed = rdev();
+      return random_seed;
 }
 
 void PHRandomSeed::InitSeed()
@@ -33,43 +43,14 @@ void PHRandomSeed::InitSeed()
   {
     // fixed init seed
     const int seed = rc->get_IntFlag("RANDOMSEED");
+    cout << "PHRandomSeed: using fixed seed " << seed << endl;
     fRandomGenerator.seed(seed);
+    fFixed = true;
     fInitialized = true;
-
-    cout << "PHRandomSeed::InitSeed - initialized with fixed random seed " << seed << " from recoConsts.RANDOMSEED. "<<endl
-        <<  "                         To reproduce this random number sequences, please add this line to Fun4All macro: "<<endl
-        <<  "                         recoConsts::instance()->set_IntFlag(\"RANDOMSEED\", " << seed << ");"<<endl;
   }
-  else
-  {
-    // random init seed
+}
 
-    //  that's when we switch to c++11
-    //  std::random_device rdev;
-    //  uint32_t random_seed = rdev();
-    unsigned int random_seed(0);
-    // use /dev/urandom which unlike /dev/random is non blocking
-    // even if entropy is too low. Not for use in cryptographic apps but
-    // good enough for a random seed
-    FILE *fp = fopen("/dev/urandom", "r");
-    if (!fp)
-    {
-      cout << "could not open /dev/urandom for seed" << endl;
-      exit(1);
-    }
-    size_t readbytes = fread(&random_seed, sizeof(random_seed), 1, fp);
-    fclose(fp);
-    if (!readbytes)
-    {
-      cout << "PHRandomSeed: reading /dev/urandom failed" << endl;
-      exit(1);
-    }
-
-    fRandomGenerator.seed(random_seed);
-    fInitialized = true;
-
-    cout << "PHRandomSeed::InitSeed - initialized with random seed " << random_seed << " from /dev/urandom. "<<endl
-         << "                         To reproduce this random number sequences, please add this line to Fun4All macro"<<endl
-         << "                         recoConsts::instance()->set_IntFlag(\"RANDOMSEED\", " << random_seed << ");";
-  }
+void PHRandomSeed::LoadSeed(unsigned int iseed)
+{
+  seedqueue.push(iseed);
 }
