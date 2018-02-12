6fc1f5be0c64573800b99cecf00bf57b94bcb29f
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-02-12 14:02:13 -0500

    Add TPCPadPlane base class and sim ple implementation

---
+++ simulation/g4simulation/g4tpc/PHG4TPCPadPlaneSimple.h
@@ -0,0 +1,27 @@
+#ifndef PHG4TPCPadPlaneSimple_h
+#define PHG4TPCPadPlaneSimple_h
+
+#include "PHG4TPCPadPlane.h"
+
+class PHG4CellContainer;
+
+class PHG4TPCPadPlaneSimple: public PHG4TPCPadPlane
+{
+public:
+  PHG4TPCPadPlaneSimple(const std::string& name = "SimplePadPlane");
+  virtual ~PHG4TPCPadPlaneSimple(){}
+
+  void MapToPadPlane(PHG4CellContainer *g4cells, const double x_gem, const double y_gem, const double t_gem);
+
+  void SetDefaultParameters();
+  void UpdateInternalParameters();
+
+protected:
+  double max_active_radius;
+  double min_active_radius;
+  double rbinwidth;
+  double phibinwidth;
+  double tbinwidth;
+};
+
+#endif
