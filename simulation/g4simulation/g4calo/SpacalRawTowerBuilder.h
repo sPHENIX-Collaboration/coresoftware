8c2b2d7ade785e8f1617439a014c65eb650359ff
Jin Huang <jhuang@bnl.gov>
2018-02-12 14:08:49 -0500

    add SPACAL tower maker

---
+++ simulation/g4simulation/g4calo/SpacalRawTowerBuilder.h
@@ -0,0 +1,88 @@
+#ifndef SpacalRawTowerBuilder_H__
+#define SpacalRawTowerBuilder_H__
+
+#include <fun4all/SubsysReco.h>
+
+
+#include <phool/PHTimeServer.h>
+#include <string>
+
+class PHCompositeNode;
+class RawTowerContainer;
+class RawTowerGeomContainer;
+
+class SpacalRawTowerBuilder : public SubsysReco {
+
+ public:
+  SpacalRawTowerBuilder(const std::string& name="SpacalRawTowerBuilder");
+  virtual ~SpacalRawTowerBuilder(){}
+
+  int InitRun(PHCompositeNode *topNode);
+  int process_event(PHCompositeNode *topNode);
+  void Detector(const std::string &d) {detector = d;}
+  void EminCut(const double e) {emin = e;}
+  void checkenergy(const int i = 1) {chkenergyconservation = i;}
+
+  enum enu_tower_energy_src
+  {
+    //! save Geant4 energy deposition as the weight of the cells
+      kEnergyDeposition,
+
+      //! save light yield as the weight of the cells
+      kLightYield
+
+  };
+
+  enu_tower_energy_src
+  get_tower_energy_src() const
+  {
+    return _tower_energy_src;
+  }
+
+  void
+  set_tower_energy_src(enu_tower_energy_src towerEnergySrc)
+  {
+    _tower_energy_src = towerEnergySrc;
+  }
+
+
+  std::string
+  get_sim_tower_node_prefix() const
+  {
+    return _sim_tower_node_prefix;
+  }
+
+  void
+  set_sim_tower_node_prefix(std::string simTowerNodePrefix)
+  {
+    _sim_tower_node_prefix = simTowerNodePrefix;
+  }
+
+ protected:
+  void CreateNodes(PHCompositeNode *topNode);
+
+  RawTowerContainer* _towers;
+  RawTowerGeomContainer *rawtowergeom;
+
+  std::string detector;
+  std::string TowerNodeName;
+  std::string TowerGeomNodeName;
+  std::string _sim_tower_node_prefix;
+
+  int _cell_binning;
+  double emin;	
+  int chkenergyconservation;
+  int _nlayers;
+  int _nphibins;
+  int _netabins;
+  double _etamin;
+  double _phimin;
+  double _etastep;
+  double _phistep;
+  enu_tower_energy_src _tower_energy_src;
+
+  PHTimeServer::timer _timer;
+
+};
+
+#endif /* SpacalRawTowerBuilder_H__ */
