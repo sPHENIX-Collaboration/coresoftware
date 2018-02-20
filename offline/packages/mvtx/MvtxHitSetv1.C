359c0172d59f605f8c7dff5f8fa930b78b92bd7a
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2018-02-20 13:09:58 -0500

    fix compile errors

--- offline/packages/mvtx/MvtxHitSetv1.C
+++ offline/packages/mvtx/MvtxHitSetv1.C
@@ -1,21 +1,45 @@
 #include "MvtxHitSetv1.h"
 
-  void 
-  MvtxHitSetv1::AddHit(const uint16_t col, const uint16_t row)
-  {
-    hits_.insert(std::make_pair(col, row));
-  }
 
-  ConstRange 
-  MvtxHitSetv1::GetHits( void )
-  {
-    return std::make_pair(hits_.begin(), hits_.end());
-  }
+MvtxHitSetv1::MvtxHitSetv1()
+{
 
-  ConstRange 
-  MvtxHitSetv1::GetHits(const uint16_t col)
-  { 
-    return std::make_pair(hits_.lower_bound(col), hits_.upper_bound(col));
-  }
+}
+
+void 
+MvtxHitSetv1::identify(std::ostream& os) const
+{
+
+}
+
+void 
+MvtxHitSetv1::Reset()
+{
+
+}
+
+void
+MvtxHitSetv1::print() const
+{
+
+}
+
+void 
+MvtxHitSetv1::AddHit(const uint16_t col, const uint16_t row)
+{
+  hits_.insert(std::make_pair(col, row));
+}
+
+MvtxHitSetv1::ConstRange 
+MvtxHitSetv1::GetHits( void )
+{
+  return std::make_pair(hits_.begin(), hits_.end());
+}
+
+MvtxHitSetv1::ConstRange 
+MvtxHitSetv1::GetHits(const uint16_t col)
+{ 
+  return std::make_pair(hits_.lower_bound(col), hits_.upper_bound(col));
+}
 
   
