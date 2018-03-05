1fe513f7b2d6a1b2aad8c501e356acf837ed90e6
HaiwangYu <yuhw.pku@gmail.com>
2018-03-04 20:16:11 -0500

    init PHTrackSeeding

---
+++ offline/packages/TrackReco/PHTrackSeeding.h
@@ -0,0 +1,64 @@
+/*!
+ *  \file		  PHTrackSeeding.h
+ *  \brief		Base class for track seeding
+ *  \author		Haiwang Yu <yuhw@nmsu.edu>
+ */
+
+#ifndef __H_PHTrackSeeding_H__
+#define __H_PHTrackSeeding_H__
+
+// PHENIX includes
+#include <fun4all/SubsysReco.h>
+
+// STL includes
+#include <string>
+
+// forward declarations
+class PHCompositeNode;
+
+class SvtxClusterMap;
+class SvtxVertexMap;
+class SvtxTrackMap;
+class AssocInfoContainer;
+
+/// \class PHTrackSeeding
+///
+/// \brief Base class for track seeding
+///
+class PHTrackSeeding : public SubsysReco
+{
+
+ public:
+
+  PHTrackSeeding(const std::string &name = "PHTrackSeeding");
+  virtual ~PHTrackSeeding() {}
+
+  int Init(PHCompositeNode *topNode);
+  int InitRun(PHCompositeNode *topNode);
+  int process_event(PHCompositeNode *topNode);
+  int End(PHCompositeNode *topNode);
+
+ protected:
+  /// setup interface for trackers, called in InitRun, setup things like pointers to nodes.
+  /// overrided in derived classes
+  virtual int Setup(PHCompositeNode *topNode);
+
+  /// process event interface for trackers, called in process_event.
+  /// implemented in derived classes
+  virtual int Process() = 0;
+
+  SvtxClusterMap *_cluster_map;
+  SvtxVertexMap *_vertex_map;
+  SvtxTrackMap *_track_map;
+  AssocInfoContainer *_assoc_container;
+
+ private:
+
+	/// create new node output pointers
+	int CreateNodes(PHCompositeNode *topNode);
+
+	/// fetch node pointers
+	int GetNodes(PHCompositeNode *topNode);
+};
+
+#endif // __H_PHTrackSeeding_H__
