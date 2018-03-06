8bf195b35be99dce207c96945cb14691d5d0cd29
Darren McGlinchey <darren.mcglinchey@colorado.edu>
2018-03-05 20:04:56 -0500

    Implement functions

--- offline/packages/mvtx/MvtxHitSetv1.C
+++ offline/packages/mvtx/MvtxHitSetv1.C
@@ -2,44 +2,52 @@
 
 
 MvtxHitSetv1::MvtxHitSetv1()
-{
-
-}
-
-void 
-MvtxHitSetv1::identify(std::ostream& os) const
-{
-
-}
-
-void 
-MvtxHitSetv1::Reset()
+  : TrkrHitSetv1()
 {
 
 }
 
 void
-MvtxHitSetv1::print() const
+MvtxHitSetv1::identify(std::ostream& os) const
 {
+  os << "MvtxHitSetv1:" << std::endl
+     << "       hitid: 0x" << std::hex << GetHitSetKey() << std::dec << std::endl
+     << "     truthid: 0x" << std::hex << GetTruthMapKey() << std::dec << std::endl
+     << "       nhits: " << hits_.size() << std::endl;
+  for ( ConstIterator itr = hits_.begin(); itr != hits_.end(); ++itr)
+    os << "            col:" << itr->first << " row:" << itr->second << std::endl;
 
 }
 
-void 
+void
+MvtxHitSetv1::Reset()
+{
+  TrkrHitSetv1::Reset();
+  hits_.clear();
+}
+
+void
+MvtxHitSetv1::print() const
+{
+  identify(std::cout);
+}
+
+void
 MvtxHitSetv1::AddHit(const uint16_t col, const uint16_t row)
 {
   hits_.insert(std::make_pair(col, row));
 }
 
-MvtxHitSetv1::ConstRange 
+MvtxHitSetv1::ConstRange
 MvtxHitSetv1::GetHits( void )
 {
   return std::make_pair(hits_.begin(), hits_.end());
 }
 
-MvtxHitSetv1::ConstRange 
+MvtxHitSetv1::ConstRange
 MvtxHitSetv1::GetHits(const uint16_t col)
-{ 
+{
   return std::make_pair(hits_.lower_bound(col), hits_.upper_bound(col));
 }
 
-  
+
