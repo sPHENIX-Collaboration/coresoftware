10f31df0158295abdb051005ad1fc98e91f72230
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-04-26 16:34:42 -0400

    bugfix, row and column were swapped in copy ctor

--- simulation/g4simulation/g4histos/G4RootScintillatorTower.h
+++ simulation/g4simulation/g4histos/G4RootScintillatorTower.h
@@ -1,32 +1,31 @@
-#ifndef G4RootScintillatorTower_H_
-#define G4RootScintillatorTower_H_
+// Tell emacs that this is a C++ source
+// This file is really -*- C++ -*-.
+#ifndef G4HISTOS_G4ROOTSCINTILLATORTOWER_H
+#define G4HISTOS_G4ROOTSCINTILLATORTOWER_H
 
-#include "phool/PHObject.h"
+#include <phool/PHObject.h>
 
 class RawTower;
 
-class G4RootScintillatorTower : public PHObject {
-
+class G4RootScintillatorTower : public PHObject
+{
  public:
   G4RootScintillatorTower();
-  G4RootScintillatorTower(const RawTower &tower);
+  G4RootScintillatorTower(const RawTower& tower);
   virtual ~G4RootScintillatorTower() {}
-
   void Reset();
   int isValid() const;
-  void identify(std::ostream& os=std::cout) const;
+  void identify(std::ostream& os = std::cout) const;
 
-  int get_row() const {return row;}
-  int get_column() const {return column;}
-
-  double get_energy() const {return energy;}
-
+  int get_row() const { return row; }
+  int get_column() const { return column; }
+  double get_energy() const { return energy; }
  protected:
   short row;
   short column;
   double energy;
 
-  ClassDef(G4RootScintillatorTower,1)
+  ClassDef(G4RootScintillatorTower, 1)
 };
- 
-#endif /* G4RootScintillatorTower_H_ */
+
+#endif  // G4HISTOS_G4ROOTSCINTILLATORTOWER_H
