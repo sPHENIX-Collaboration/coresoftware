65e0b9003d0f0b2f9efe1448c38f3b88937bf5a9
dvperepelitsa <dvp@bnl.gov>
2018-06-23 13:57:46 -0400

    Adding layer-integrated Psi2 & v2 calculation in UE background determination

--- offline/packages/jetbackground/TowerBackground_v1.h
+++ offline/packages/jetbackground/TowerBackground_v1.h
@@ -16,18 +16,18 @@ class TowerBackground_v1 : public TowerBackground
   int isValid() const { return 1; }
 
   virtual void set_UE( int layer, const std::vector<float> & UE ) { _UE[layer] = UE; }
-  virtual void set_v2( int layer, float v2 ) { _v2[layer] = v2; }
-  virtual void set_Psi2( int layer, float Psi2 ) { _Psi2[layer] = Psi2; }
+  virtual void set_v2( float v2 ) { _v2 = v2; }
+  virtual void set_Psi2( float Psi2 ) { _Psi2= Psi2; }
   
   virtual std::vector<float> get_UE( int layer ) { return _UE[layer]; }
-  virtual float get_v2( int layer ) { return _v2[layer]; }
-  virtual float get_Psi2( int layer ) { return _Psi2[layer]; }
+  virtual float get_v2( ) { return _v2; }
+  virtual float get_Psi2( ) { return _Psi2; }
 
  private:
 
   std::vector< std::vector< float> > _UE;
-  float _v2[3];
-  float _Psi2[3];
+  float _v2;
+  float _Psi2;
 
   ClassDef(TowerBackground_v1, 1);
 };
