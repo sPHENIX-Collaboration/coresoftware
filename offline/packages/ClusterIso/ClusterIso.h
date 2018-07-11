5fa9e137faef9c6d91102a78b6f585771b1773b2
Chase Smith <chsm5267@colorado.edu>
2018-07-11 10:29:13 -0600

    added doxygen comments

--- offline/packages/ClusterIso/ClusterIso.h
+++ offline/packages/ClusterIso/ClusterIso.h
@@ -2,19 +2,18 @@
 #ifndef ClusterIso_h 
 #define ClusterIso_h
 
-// --- need to check all these includes...
-
 #include <fun4all/SubsysReco.h>
 
 #include <phool/getClass.h>         
 #include <phool/PHCompositeNode.h>  
 
-#include <calobase/RawClusterContainer.h> 
 #include <calobase/RawCluster.h>          
 #include <calobase/RawClusterUtility.h>   
-#include <calobase/RawTowerContainer.h> 
-#include <calobase/RawTowerGeom.h>
+#include <calobase/RawClusterContainer.h> 
+
 #include <calobase/RawTower.h>
+#include <calobase/RawTowerGeom.h>
+#include <calobase/RawTowerContainer.h> 
 #include <calobase/RawTowerGeomContainer.h>  
 
 #include <g4vertex/GlobalVertex.h>    
@@ -23,32 +22,46 @@
 #include <cmath>
 class PHCompositeNode;
 
+/** \Brief Tool to find isolation energy of each EMCal cluster.
+ * 
+ * This tool finds isoET of clusters by summing towers energy
+ * in a cone of radius R around the cluster and subtracting 
+ * the cluster from the sum
+ */
 
 class ClusterIso: public SubsysReco
 {
-
 public:
+/**
+ * Constructor for ClusterIso Class
+ */
+ClusterIso(const std::string& ,float eTCut, float coneSize);
 
-  ClusterIso(const std::string& ,float eTCut, float coneSize);
+virtual int Init(PHCompositeNode*);
+virtual int process_event(PHCompositeNode*);
+virtual int End(PHCompositeNode*);
 
-  virtual int Init(PHCompositeNode*);
-  virtual int process_event(PHCompositeNode*);
-  virtual int End(PHCompositeNode*);
-  void seteTCut(float x);
-  void setConeSize(float x);
-  const float geteTCut();
-  const CLHEP::Hep3Vector getVertex();
-  const float getConeSize();
+void seteTCut(float x);
+void setConeSize(float x);
+const float geteTCut();
+const float getConeSize();
+const CLHEP::Hep3Vector getVertex();
 
 
 private:
-  float m_eTCut;
-  float m_coneSize;
-  float m_vx;
-  float m_vy;
-  float m_vz;
+  float m_eTCut; ///< cluster must be over this energy set in constructor
+  float m_coneSize; ///< delta R around cluster that is considered
+  float m_vx; ///< new vertex x value  
+  float m_vy; ///< new vertex y value 
+  float m_vz; ///< new vertex z value
 };
 
+/** \Brief Function to get correct tower eta
+ *
+ * Each tower is calculated using the vertex (0,0,0)
+ * which is incorrect in many collisions. This function 
+ * uses geometry to find eta using correct vertex.
+ */
 inline double getTowerEta(RawTowerGeom* tower_geom, double vx, double vy, double vz) 
   {
     if(vx==0&&vy==0&&vz==0){
@@ -64,14 +77,20 @@ inline double getTowerEta(RawTowerGeom* tower_geom, double vx, double vy, double
     }
 }
 
+/** \Brief Function to find delta R between 2 objects
+ *
+ * Takes eta and phi of each object and returns the difference 
+ * of the etas and phis added in quadrature. Used to find towers
+ * inside a cone of delta R around a cluster.
+ */
 inline const float deltaR( float eta1, float eta2, float phi1, float phi2 ) {
 
     float deta = eta1 - eta2;
     float dphi = phi1 - phi2;
-    if ( dphi > 3.14159 ) dphi -= 2 * 3.14159;
-    if ( dphi < -3.14159 ) dphi += 2 * 3.14159;
+    if ( dphi > 3.14159 ) dphi -= 2 * 3.14159; //corrects to keep range -pi to pi
+    if ( dphi < -3.14159 ) dphi += 2 * 3.14159; //corrects to keep range -pi to pi
 
     return sqrt( deta*deta + dphi*dphi);
-
 }
-#endif //ClusterIso_h
+
+#endif
