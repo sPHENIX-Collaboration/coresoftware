060505dfed769db90bfac004216f70fc2645093c
Jin Huang <jhuang@bnl.gov>
2018-09-15 11:35:25 -0400

    node structure

--- simulation/g4simulation/g4jets/JetHepMCLoader.C
+++ simulation/g4simulation/g4jets/JetHepMCLoader.C
@@ -10,13 +10,102 @@
 
 #include "JetHepMCLoader.h"
 
-JetHepMCLoader::JetHepMCLoader(const std::string &name)
-  : SubsysReco(name)
+#include "Jet.h"
+#include "JetMapV1.h"
+
+#include <fun4all/Fun4AllReturnCodes.h>
+#include <phool/PHCompositeNode.h>
+#include <phool/PHIODataNode.h>
+#include <phool/PHNodeIterator.h>
+#include <phool/PHTypedNodeIterator.h>
+#include <phool/getClass.h>
+
+#include <TH2F.h>
+
+using namespace std;
+
+JetHepMCLoader::JetHepMCLoader(const std::string &jetInputCategory)
+  : SubsysReco("JetHepMCLoader_" + jetInputCategory)
+  , m_jetInputCategory(jetInputCategory)
+  , m_saveQAPlots(false)
 {
-  // TODO Auto-generated constructor stub
 }
 
 JetHepMCLoader::~JetHepMCLoader()
 {
-  // TODO Auto-generated destructor stub
+}
+
+int JetHepMCLoader::Init(PHCompositeNode *topNode)
+{
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int JetHepMCLoader::InitRun(PHCompositeNode *topNode)
+{
+  return CreateNodes(topNode);
+}
+
+int JetHepMCLoader::process_event(PHCompositeNode *topNode)
+{
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int JetHepMCLoader::End(PHCompositeNode *topNode)
+{
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+void JetHepMCLoader::addJet(
+    const std::string &name,
+    int embeddingID,
+    const std::string &algorithmName,
+    double parameter,
+    int tagPID,
+    int tagStatus)
+{
+  hepmc_jet_src src{name, embeddingID, algorithmName, parameter, tagPID, tagStatus};
+
+  m_jetSrc.insert(src);
+}
+
+int JetHepMCLoader::CreateNodes(PHCompositeNode *topNode)
+{
+  PHNodeIterator iter(topNode);
+
+  // Looking for the DST node
+  PHCompositeNode *dstNode = static_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", "DST"));
+  if (!dstNode)
+  {
+    cout << PHWHERE << "DST Node missing, doing nothing." << endl;
+    return Fun4AllReturnCodes::ABORTRUN;
+  }
+
+  for (const hepmc_jet_src &src : m_jetSrc)
+  {
+    // Create the AntiKt node if required
+    PHCompositeNode *AlgoNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", src.m_algorithmName.c_str()));
+    if (!AlgoNode)
+    {
+      AlgoNode = new PHCompositeNode(src.m_algorithmName.c_str());
+      dstNode->addNode(AlgoNode);
+    }
+
+    // Create the Input node if required
+    PHCompositeNode *InputNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", m_jetInputCategory.c_str()));
+    if (!InputNode)
+    {
+      InputNode = new PHCompositeNode(m_jetInputCategory.c_str());
+      AlgoNode->addNode(InputNode);
+    }
+
+    JetMap *jets = findNode::getClass<JetMap>(topNode, src.m_name);
+    if (!jets)
+    {
+      jets = new JetMapV1();
+      PHIODataNode<PHObject> *JetMapNode = new PHIODataNode<PHObject>(jets, src.m_name.c_str(), "PHObject");
+      InputNode->addNode(JetMapNode);
+    }
+  }
+
+  return Fun4AllReturnCodes::EVENT_OK;
 }
--- simulation/g4simulation/g4jets/JetHepMCLoader.h
+++ simulation/g4simulation/g4jets/JetHepMCLoader.h
@@ -12,6 +12,10 @@
 #define JETHEPMCLOADER_H_
 
 #include <fun4all/SubsysReco.h>
+#include <set>
+#include <string>
+
+class PHCompositeNode;
 
 /*!
  * \brief JetHepMCLoader loads special jet objects encoded in HepMC records to DST Jet nodes. Example use are loading sHijing HIJFRG jets
@@ -19,8 +23,66 @@
 class JetHepMCLoader : public SubsysReco
 {
  public:
-  JetHepMCLoader(const std::string &name = "JetHepMCLoader");
+
+  //! \param[in] jetInputCategory is the DST PHCompositeNode name that list the output jet maps, e.g. sHijing_HIJFRG for sHijing HIJFRG truth jets
+  JetHepMCLoader(const std::string &jetInputCategory);
   virtual ~JetHepMCLoader();
+
+  int Init(PHCompositeNode *topNode);
+  int InitRun(PHCompositeNode *topNode);
+  int process_event(PHCompositeNode *topNode);
+  int End(PHCompositeNode *topNode);
+
+  //! \brief addJet add HepMC entries for a particular type of jets
+  //! Example of adding sHijing HIJFRG truth jets with R=0.4:
+  //!   \code{.cpp}
+  //!     addJet("AntiKt_sHijing_HIJFRG_r04",0,"ANTIKT",0.4,4000000,103);
+  //!   \endcode
+  //! \param[in] name name of the jet category
+  //! \param[in] embeddingID hepmc event's embedding ID
+  //! \param[in] parameter jet parameter, e.g. radius
+  //! \param[in] tagPID HepMC entry identifying tag on PID
+  //! \param[in] tagStatus HepMC entry identifying tag on status
+  void addJet(
+      const std::string &name,
+      int embeddingID,
+      const std::string &algorithmName,
+      double parameter,
+      int tagPID,
+      int tagStatus);
+
+  void saveQAPlots(bool b) { m_saveQAPlots = b; }
+
+ private:
+  std::string m_jetInputCategory;
+
+  bool m_saveQAPlots;
+
+  struct hepmc_jet_src
+  {
+    //! name
+    std::string m_name;
+
+    //! hepmc event's embedding ID
+    int m_embeddingID;
+
+    //! Name of jet algorithm
+    std::string m_algorithmName;
+
+    //! jet parameter, e.g. radius
+    double m_parameter;
+
+    //! HepMC entry identifying tag on PID
+    int m_tagPID;
+
+    //! HepMC entry identifying tag on status
+    int m_tagStatus;
+  };
+
+  int CreateNodes(PHCompositeNode *topNode);
+
+  std::set< hepmc_jet_src> m_jetSrc;
+
 };
 
 #endif /* JETHEPMCLOADER_H_ */
