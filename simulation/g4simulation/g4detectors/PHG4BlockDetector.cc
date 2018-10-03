7266d48b61e933fef6b5cdca3ad5bd4a46eeaf3f
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-10-02 22:38:20 -0400

    clang-format

--- simulation/g4simulation/g4detectors/PHG4BlockDetector.cc
+++ simulation/g4simulation/g4detectors/PHG4BlockDetector.cc
@@ -4,7 +4,6 @@
 
 #include <g4main/PHG4Utils.h>
 
-
 #include <phool/PHCompositeNode.h>
 #include <phool/PHIODataNode.h>
 #include <phool/getClass.h>
@@ -23,15 +22,16 @@
 using namespace std;
 
 //_______________________________________________________________
-PHG4BlockDetector::PHG4BlockDetector( PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam, const int lyr):
-  PHG4Detector(Node, dnam),
-  m_Params(parameters),
-  m_BlockPhysi(nullptr),
-  m_Layer(lyr)
-{}
+PHG4BlockDetector::PHG4BlockDetector(PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam, const int lyr)
+  : PHG4Detector(Node, dnam)
+  , m_Params(parameters)
+  , m_BlockPhysi(nullptr)
+  , m_Layer(lyr)
+{
+}
 
 //_______________________________________________________________
-bool PHG4BlockDetector::IsInBlock(G4VPhysicalVolume * volume) const
+bool PHG4BlockDetector::IsInBlock(G4VPhysicalVolume *volume) const
 {
   if (volume == m_BlockPhysi)
   {
@@ -41,22 +41,20 @@ bool PHG4BlockDetector::IsInBlock(G4VPhysicalVolume * volume) const
 }
 
 //_______________________________________________________________
-void PHG4BlockDetector::Construct( G4LogicalVolume* logicWorld )
+void PHG4BlockDetector::Construct(G4LogicalVolume *logicWorld)
 {
+  G4Material *TrackerMaterial = G4Material::GetMaterial(m_Params->get_string_param("material"));
 
-  G4Material* TrackerMaterial = G4Material::GetMaterial(m_Params->get_string_param("material"));
-
-
-  if ( ! TrackerMaterial )
-    {
-      std::cout << "Error: Can not set material" << std::endl;
-      exit(-1);
-    }
+  if (!TrackerMaterial)
+  {
+    std::cout << "Error: Can not set material" << std::endl;
+    exit(-1);
+  }
 
   G4VSolid *block_solid = new G4Box(G4String(GetName().c_str()),
-                          m_Params->get_double_param("size_x")/2.*cm,
-                          m_Params->get_double_param("size_y")/2.*cm,
-			  m_Params->get_double_param("size_z")/2.*cm);
+                                    m_Params->get_double_param("size_x") / 2. * cm,
+                                    m_Params->get_double_param("size_y") / 2. * cm,
+                                    m_Params->get_double_param("size_z") / 2. * cm);
 
   double steplimits = m_Params->get_double_param("steplimits") * cm;
   G4UserLimits *g4userlimits = nullptr;
@@ -66,30 +64,28 @@ void PHG4BlockDetector::Construct( G4LogicalVolume* logicWorld )
   }
 
   G4LogicalVolume *block_logic = new G4LogicalVolume(block_solid,
-                                    TrackerMaterial,
-                                    G4String(GetName().c_str()),
-                                    nullptr, nullptr, g4userlimits);
+                                                     TrackerMaterial,
+                                                     G4String(GetName().c_str()),
+                                                     nullptr, nullptr, g4userlimits);
   G4VisAttributes matVis;
   if (m_Params->get_int_param("blackhole"))
-    {
-      PHG4Utils::SetColour(&matVis, "BlackHole");
-      matVis.SetVisibility(false);
-      matVis.SetForceSolid(false);
-    }
+  {
+    PHG4Utils::SetColour(&matVis, "BlackHole");
+    matVis.SetVisibility(false);
+    matVis.SetForceSolid(false);
+  }
   else
-    {
-      PHG4Utils::SetColour(&matVis, m_Params->get_string_param("material"));
-      matVis.SetVisibility(true);
-      matVis.SetForceSolid(true);
-    }
+  {
+    PHG4Utils::SetColour(&matVis, m_Params->get_string_param("material"));
+    matVis.SetVisibility(true);
+    matVis.SetForceSolid(true);
+  }
   block_logic->SetVisAttributes(matVis);
 
-  G4RotationMatrix *rotm  = new G4RotationMatrix();
-  rotm->rotateZ(m_Params->get_double_param("rot_z")*deg);
-  m_BlockPhysi = new G4PVPlacement(rotm, G4ThreeVector(m_Params->get_double_param("place_x")*cm,
-                                                      m_Params->get_double_param("place_y")*cm,
-						      m_Params->get_double_param("place_z")*cm),
-                                  block_logic,
-                                  G4String(GetName().c_str()),
-                                  logicWorld, 0, false, OverlapCheck());
+  G4RotationMatrix *rotm = new G4RotationMatrix();
+  rotm->rotateZ(m_Params->get_double_param("rot_z") * deg);
+  m_BlockPhysi = new G4PVPlacement(rotm, G4ThreeVector(m_Params->get_double_param("place_x") * cm, m_Params->get_double_param("place_y") * cm, m_Params->get_double_param("place_z") * cm),
+                                   block_logic,
+                                   G4String(GetName().c_str()),
+                                   logicWorld, 0, false, OverlapCheck());
 }
--- simulation/g4simulation/g4detectors/PHG4BlockDetector.h
+++ simulation/g4simulation/g4detectors/PHG4BlockDetector.h
@@ -9,40 +9,36 @@ class G4LogicalVolume;
 class PHParameters;
 class G4VPhysicalVolume;
 
-class PHG4BlockDetector: public PHG4Detector
+class PHG4BlockDetector : public PHG4Detector
 {
-
-  public:
-
+ public:
   //! constructor
-  PHG4BlockDetector( PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam="BLOCK", const int lyr = 0 );
+  PHG4BlockDetector(PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam = "BLOCK", const int lyr = 0);
 
   //! destructor
-  virtual ~PHG4BlockDetector( void )
-  {}
+  virtual ~PHG4BlockDetector(void)
+  {
+  }
 
   //! construct
-  virtual void Construct( G4LogicalVolume* world );
+  virtual void Construct(G4LogicalVolume *world);
 
   //!@name volume accessors
   //@{
-  bool IsInBlock(G4VPhysicalVolume*) const;
+  bool IsInBlock(G4VPhysicalVolume *) const;
   //@}
 
-  void SuperDetector(const std::string &name) {m_SuperDetector = name;}
-  const std::string SuperDetector() const {return m_SuperDetector;}
-  int get_Layer() const {return m_Layer;}
+  void SuperDetector(const std::string &name) { m_SuperDetector = name; }
+  const std::string SuperDetector() const { return m_SuperDetector; }
+  int get_Layer() const { return m_Layer; }
 
-  private:
-
+ private:
   PHParameters *m_Params;
- 
-  G4VPhysicalVolume* m_BlockPhysi;
 
+  G4VPhysicalVolume *m_BlockPhysi;
 
   int m_Layer;
   std::string m_SuperDetector;
-  
 };
 
 #endif
