69b3439e824a8bcf2505f8feec1a789edd304348
Chris Pinkenburg <pinkenburg@bnl.gov>
2018-11-05 22:14:27 -0500

    use double instead of floats (it makes a difference), use initializers, rename variables according to sphenix coding conventions, remove unused methods

--- offline/packages/trigger/CaloTriggerInfo_v1.h
+++ offline/packages/trigger/CaloTriggerInfo_v1.h
@@ -1,5 +1,5 @@
-#ifndef __CALOTRIGGERINFO_V1_H__
-#define __CALOTRIGGERINFO_V1_H__
+#ifndef TRIGGER_CALOTRIGGERINFOV1_H
+#define TRIGGER_CALOTRIGGERINFOV1_H
 
 #include "CaloTriggerInfo.h"
 
@@ -16,109 +16,109 @@ class CaloTriggerInfo_v1 : public CaloTriggerInfo
   int isValid() const { return 1; }
 
   // EMCal 2x2
-  void set_best_EMCal_2x2_E(float E) { _EMCAL_2x2_BEST_E = E; }
-  void set_best_EMCal_2x2_eta(float eta) { _EMCAL_2x2_BEST_ETA = eta; }
-  void set_best_EMCal_2x2_phi(float phi) { _EMCAL_2x2_BEST_PHI = phi; }
+  void set_best_EMCal_2x2_E(const float E) { m_EMCAL_2x2_BEST_E = E; }
+  void set_best_EMCal_2x2_eta(const float eta) { m_EMCAL_2x2_BEST_ETA = eta; }
+  void set_best_EMCal_2x2_phi(const float phi) { m_EMCAL_2x2_BEST_PHI = phi; }
 
-  float get_best_EMCal_2x2_E() { return _EMCAL_2x2_BEST_E; }
-  float get_best_EMCal_2x2_eta() { return _EMCAL_2x2_BEST_ETA; }
-  float get_best_EMCal_2x2_phi() { return _EMCAL_2x2_BEST_PHI; }
+  float get_best_EMCal_2x2_E() const { return m_EMCAL_2x2_BEST_E; }
+  float get_best_EMCal_2x2_eta() const { return m_EMCAL_2x2_BEST_ETA; }
+  float get_best_EMCal_2x2_phi() const { return m_EMCAL_2x2_BEST_PHI; }
 
   // EMCal 4x4
-  void set_best_EMCal_4x4_E(float E) { _EMCAL_4x4_BEST_E = E; }
-  void set_best_EMCal_4x4_eta(float eta) { _EMCAL_4x4_BEST_ETA = eta; }
-  void set_best_EMCal_4x4_phi(float phi) { _EMCAL_4x4_BEST_PHI = phi; }
+  void set_best_EMCal_4x4_E(const float E) { m_EMCAL_4x4_BEST_E = E; }
+  void set_best_EMCal_4x4_eta(const float eta) { m_EMCAL_4x4_BEST_ETA = eta; }
+  void set_best_EMCal_4x4_phi(const float phi) { m_EMCAL_4x4_BEST_PHI = phi; }
 
-  float get_best_EMCal_4x4_E() { return _EMCAL_4x4_BEST_E; }
-  float get_best_EMCal_4x4_eta() { return _EMCAL_4x4_BEST_ETA; }
-  float get_best_EMCal_4x4_phi() { return _EMCAL_4x4_BEST_PHI; }
+  float get_best_EMCal_4x4_E() const { return m_EMCAL_4x4_BEST_E; }
+  float get_best_EMCal_4x4_eta() const { return m_EMCAL_4x4_BEST_ETA; }
+  float get_best_EMCal_4x4_phi() const { return m_EMCAL_4x4_BEST_PHI; }
 
   // 2nd best EMCal 4x4
-  void set_best2_EMCal_4x4_E(float E) { _EMCAL_4x4_BEST2_E = E; }
-  void set_best2_EMCal_4x4_eta(float eta) { _EMCAL_4x4_BEST2_ETA = eta; }
-  void set_best2_EMCal_4x4_phi(float phi) { _EMCAL_4x4_BEST2_PHI = phi; }
+  void set_best2_EMCal_4x4_E(const float E) { m_EMCAL_4x4_BEST2_E = E; }
+  void set_best2_EMCal_4x4_eta(const float eta) { m_EMCAL_4x4_BEST2_ETA = eta; }
+  void set_best2_EMCal_4x4_phi(const float phi) { m_EMCAL_4x4_BEST2_PHI = phi; }
 
-  float get_best2_EMCal_4x4_E() { return _EMCAL_4x4_BEST2_E; }
-  float get_best2_EMCal_4x4_eta() { return _EMCAL_4x4_BEST2_ETA; }
-  float get_best2_EMCal_4x4_phi() { return _EMCAL_4x4_BEST2_PHI; }
+  float get_best2_EMCal_4x4_E() const { return m_EMCAL_4x4_BEST2_E; }
+  float get_best2_EMCal_4x4_eta() const { return m_EMCAL_4x4_BEST2_ETA; }
+  float get_best2_EMCal_4x4_phi() const { return m_EMCAL_4x4_BEST2_PHI; }
 
   // FullCalo 0.2x0.2
-  void set_best_FullCalo_0p2x0p2_E(float E) { _FULLCALO_0p2x0p2_BEST_E = E; }
-  void set_best_FullCalo_0p2x0p2_eta(float eta) { _FULLCALO_0p2x0p2_BEST_ETA = eta; }
-  void set_best_FullCalo_0p2x0p2_phi(float phi) { _FULLCALO_0p2x0p2_BEST_PHI = phi; }
+  void set_best_FullCalo_0p2x0p2_E(const float E) { m_FULLCALO_0p2x0p2_BEST_E = E; }
+  void set_best_FullCalo_0p2x0p2_eta(const float eta) { m_FULLCALO_0p2x0p2_BEST_ETA = eta; }
+  void set_best_FullCalo_0p2x0p2_phi(const float phi) { m_FULLCALO_0p2x0p2_BEST_PHI = phi; }
 
-  float get_best_FullCalo_0p2x0p2_E() { return _FULLCALO_0p2x0p2_BEST_E; }
-  float get_best_FullCalo_0p2x0p2_eta() { return _FULLCALO_0p2x0p2_BEST_ETA; }
-  float get_best_FullCalo_0p2x0p2_phi() { return _FULLCALO_0p2x0p2_BEST_PHI; }
+  float get_best_FullCalo_0p2x0p2_E() const { return m_FULLCALO_0p2x0p2_BEST_E; }
+  float get_best_FullCalo_0p2x0p2_eta() const { return m_FULLCALO_0p2x0p2_BEST_ETA; }
+  float get_best_FullCalo_0p2x0p2_phi() const { return m_FULLCALO_0p2x0p2_BEST_PHI; }
 
   // FullCalo 0.4x0.4
-  void set_best_FullCalo_0p4x0p4_E(float E) { _FULLCALO_0p4x0p4_BEST_E = E; }
-  void set_best_FullCalo_0p4x0p4_eta(float eta) { _FULLCALO_0p4x0p4_BEST_ETA = eta; }
-  void set_best_FullCalo_0p4x0p4_phi(float phi) { _FULLCALO_0p4x0p4_BEST_PHI = phi; }
+  void set_best_FullCalo_0p4x0p4_E(const float E) { m_FULLCALO_0p4x0p4_BEST_E = E; }
+  void set_best_FullCalo_0p4x0p4_eta(const float eta) { m_FULLCALO_0p4x0p4_BEST_ETA = eta; }
+  void set_best_FullCalo_0p4x0p4_phi(const float phi) { m_FULLCALO_0p4x0p4_BEST_PHI = phi; }
 
-  float get_best_FullCalo_0p4x0p4_E() { return _FULLCALO_0p4x0p4_BEST_E; }
-  float get_best_FullCalo_0p4x0p4_eta() { return _FULLCALO_0p4x0p4_BEST_ETA; }
-  float get_best_FullCalo_0p4x0p4_phi() { return _FULLCALO_0p4x0p4_BEST_PHI; }
+  float get_best_FullCalo_0p4x0p4_E() const { return m_FULLCALO_0p4x0p4_BEST_E; }
+  float get_best_FullCalo_0p4x0p4_eta() const { return m_FULLCALO_0p4x0p4_BEST_ETA; }
+  float get_best_FullCalo_0p4x0p4_phi() const { return m_FULLCALO_0p4x0p4_BEST_PHI; }
 
   // FullCalo 0.6x0.6
-  void set_best_FullCalo_0p6x0p6_E(float E) { _FULLCALO_0p6x0p6_BEST_E = E; }
-  void set_best_FullCalo_0p6x0p6_eta(float eta) { _FULLCALO_0p6x0p6_BEST_ETA = eta; }
-  void set_best_FullCalo_0p6x0p6_phi(float phi) { _FULLCALO_0p6x0p6_BEST_PHI = phi; }
+  void set_best_FullCalo_0p6x0p6_E(const float E) { m_FULLCALO_0p6x0p6_BEST_E = E; }
+  void set_best_FullCalo_0p6x0p6_eta(const float eta) { m_FULLCALO_0p6x0p6_BEST_ETA = eta; }
+  void set_best_FullCalo_0p6x0p6_phi(const float phi) { m_FULLCALO_0p6x0p6_BEST_PHI = phi; }
 
-  float get_best_FullCalo_0p6x0p6_E() { return _FULLCALO_0p6x0p6_BEST_E; }
-  float get_best_FullCalo_0p6x0p6_eta() { return _FULLCALO_0p6x0p6_BEST_ETA; }
-  float get_best_FullCalo_0p6x0p6_phi() { return _FULLCALO_0p6x0p6_BEST_PHI; }
+  float get_best_FullCalo_0p6x0p6_E() const { return m_FULLCALO_0p6x0p6_BEST_E; }
+  float get_best_FullCalo_0p6x0p6_eta() const { return m_FULLCALO_0p6x0p6_BEST_ETA; }
+  float get_best_FullCalo_0p6x0p6_phi() const { return m_FULLCALO_0p6x0p6_BEST_PHI; }
 
   // FullCalo 0.8x0.8
-  void set_best_FullCalo_0p8x0p8_E(float E) { _FULLCALO_0p8x0p8_BEST_E = E; }
-  void set_best_FullCalo_0p8x0p8_eta(float eta) { _FULLCALO_0p8x0p8_BEST_ETA = eta; }
-  void set_best_FullCalo_0p8x0p8_phi(float phi) { _FULLCALO_0p8x0p8_BEST_PHI = phi; }
+  void set_best_FullCalo_0p8x0p8_E(const float E) { m_FULLCALO_0p8x0p8_BEST_E = E; }
+  void set_best_FullCalo_0p8x0p8_eta(const float eta) { m_FULLCALO_0p8x0p8_BEST_ETA = eta; }
+  void set_best_FullCalo_0p8x0p8_phi(const float phi) { m_FULLCALO_0p8x0p8_BEST_PHI = phi; }
 
-  float get_best_FullCalo_0p8x0p8_E() { return _FULLCALO_0p8x0p8_BEST_E; }
-  float get_best_FullCalo_0p8x0p8_eta() { return _FULLCALO_0p8x0p8_BEST_ETA; }
-  float get_best_FullCalo_0p8x0p8_phi() { return _FULLCALO_0p8x0p8_BEST_PHI; }
+  float get_best_FullCalo_0p8x0p8_E() const { return m_FULLCALO_0p8x0p8_BEST_E; }
+  float get_best_FullCalo_0p8x0p8_eta() const { return m_FULLCALO_0p8x0p8_BEST_ETA; }
+  float get_best_FullCalo_0p8x0p8_phi() const { return m_FULLCALO_0p8x0p8_BEST_PHI; }
 
   // FullCalo 1.0x1.0
-  void set_best_FullCalo_1p0x1p0_E(float E) { _FULLCALO_1p0x1p0_BEST_E = E; }
-  void set_best_FullCalo_1p0x1p0_eta(float eta) { _FULLCALO_1p0x1p0_BEST_ETA = eta; }
-  void set_best_FullCalo_1p0x1p0_phi(float phi) { _FULLCALO_1p0x1p0_BEST_PHI = phi; }
+  void set_best_FullCalo_1p0x1p0_E(const float E) { m_FULLCALO_1p0x1p0_BEST_E = E; }
+  void set_best_FullCalo_1p0x1p0_eta(const float eta) { m_FULLCALO_1p0x1p0_BEST_ETA = eta; }
+  void set_best_FullCalo_1p0x1p0_phi(const float phi) { m_FULLCALO_1p0x1p0_BEST_PHI = phi; }
 
-  float get_best_FullCalo_1p0x1p0_E() { return _FULLCALO_1p0x1p0_BEST_E; }
-  float get_best_FullCalo_1p0x1p0_eta() { return _FULLCALO_1p0x1p0_BEST_ETA; }
-  float get_best_FullCalo_1p0x1p0_phi() { return _FULLCALO_1p0x1p0_BEST_PHI; }
+  float get_best_FullCalo_1p0x1p0_E() const { return m_FULLCALO_1p0x1p0_BEST_E; }
+  float get_best_FullCalo_1p0x1p0_eta() const { return m_FULLCALO_1p0x1p0_BEST_ETA; }
+  float get_best_FullCalo_1p0x1p0_phi() const { return m_FULLCALO_1p0x1p0_BEST_PHI; }
 
  private:
-  float _EMCAL_2x2_BEST_E;
-  float _EMCAL_2x2_BEST_ETA;
-  float _EMCAL_2x2_BEST_PHI;
+  float m_EMCAL_2x2_BEST_E;
+  float m_EMCAL_2x2_BEST_ETA;
+  float m_EMCAL_2x2_BEST_PHI;
 
-  float _EMCAL_4x4_BEST_E;
-  float _EMCAL_4x4_BEST_ETA;
-  float _EMCAL_4x4_BEST_PHI;
+  float m_EMCAL_4x4_BEST_E;
+  float m_EMCAL_4x4_BEST_ETA;
+  float m_EMCAL_4x4_BEST_PHI;
 
-  float _EMCAL_4x4_BEST2_E;
-  float _EMCAL_4x4_BEST2_ETA;
-  float _EMCAL_4x4_BEST2_PHI;
+  float m_EMCAL_4x4_BEST2_E;
+  float m_EMCAL_4x4_BEST2_ETA;
+  float m_EMCAL_4x4_BEST2_PHI;
 
-  float _FULLCALO_0p2x0p2_BEST_E;
-  float _FULLCALO_0p2x0p2_BEST_ETA;
-  float _FULLCALO_0p2x0p2_BEST_PHI;
+  float m_FULLCALO_0p2x0p2_BEST_E;
+  float m_FULLCALO_0p2x0p2_BEST_ETA;
+  float m_FULLCALO_0p2x0p2_BEST_PHI;
 
-  float _FULLCALO_0p4x0p4_BEST_E;
-  float _FULLCALO_0p4x0p4_BEST_ETA;
-  float _FULLCALO_0p4x0p4_BEST_PHI;
+  float m_FULLCALO_0p4x0p4_BEST_E;
+  float m_FULLCALO_0p4x0p4_BEST_ETA;
+  float m_FULLCALO_0p4x0p4_BEST_PHI;
 
-  float _FULLCALO_0p6x0p6_BEST_E;
-  float _FULLCALO_0p6x0p6_BEST_ETA;
-  float _FULLCALO_0p6x0p6_BEST_PHI;
+  float m_FULLCALO_0p6x0p6_BEST_E;
+  float m_FULLCALO_0p6x0p6_BEST_ETA;
+  float m_FULLCALO_0p6x0p6_BEST_PHI;
 
-  float _FULLCALO_0p8x0p8_BEST_E;
-  float _FULLCALO_0p8x0p8_BEST_ETA;
-  float _FULLCALO_0p8x0p8_BEST_PHI;
+  float m_FULLCALO_0p8x0p8_BEST_E;
+  float m_FULLCALO_0p8x0p8_BEST_ETA;
+  float m_FULLCALO_0p8x0p8_BEST_PHI;
 
-  float _FULLCALO_1p0x1p0_BEST_E;
-  float _FULLCALO_1p0x1p0_BEST_ETA;
-  float _FULLCALO_1p0x1p0_BEST_PHI;
+  float m_FULLCALO_1p0x1p0_BEST_E;
+  float m_FULLCALO_1p0x1p0_BEST_ETA;
+  float m_FULLCALO_1p0x1p0_BEST_PHI;
 
   ClassDef(CaloTriggerInfo_v1, 2);
 };
