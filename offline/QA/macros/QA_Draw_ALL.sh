152d28895890577e72a401efc63cff9c37be5879
Jin Huang <blackcathj@gmail.com>
2018-12-29 22:12:33 -0500

    Update QA_Draw_ALL.sh

--- offline/QA/macros/QA_Draw_ALL.sh
+++ offline/QA/macros/QA_Draw_ALL.sh
@@ -28,12 +28,19 @@ set macros = (\
 	QA_Draw_HCALOUT_G4Hit.C \
 	QA_Draw_HCALOUT_TowerCluster.C \
 	QA_Draw_Sum_Cluster.C \
+	QA_Draw_Calorimeter_Sum_TrackProj.C \
+	QA_Draw_Calorimeter_Sum_TrackProjEP.C \
 );
 
 # imake nstall-data
 foreach macro ($macros)
 	root -b -q "$macro($new_QA_file, $reference_QA_file)"
 end
+
+# last all jet stuff
+root -b -q "QA_Draw_Jet_Spectrum.C(${q}h_QAG4SimJet_AntiKt_Truth_r07${q}, $new_QA_file, $reference_QA_file)"
+root -b -q "QA_Draw_Jet_Spectrum.C(${q}h_QAG4SimJet_AntiKt_Tower_r07${q}, $new_QA_file, $reference_QA_file)"
+root -b -q "QA_Draw_Jet_Summary.C(${q}AntiKt_Tower${q},$new_QA_file, $reference_QA_file)"
 
 echo "$0 - Output plots:";
 ls -lh $1*.png;
