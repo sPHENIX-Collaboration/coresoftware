f6ba2ca5598c5033b91db86fc69d895a5dc46d19
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-01-17 10:36:06 -0500

    split linkdef files, build root5/root6 specific dictionaries

---
+++ offline/framework/phool/PHMessageLinkDef.h
@@ -0,0 +1,8 @@
+#ifdef __CINT__
+
+#pragma link C++ enum PHMessageType;
+#pragma link C++ enum PHAccessType;
+#pragma link C++ enum PHTreeType;
+#pragma link C++ function PHMessage(const std::string &, int, const std::string &);
+
+#endif /* __CINT__ */
