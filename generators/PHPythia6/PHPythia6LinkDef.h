d5430fd4ee73cdb202015350be552cffa8802b21
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-01-24 23:33:57 -0500

    create dictionaries only for root5

--- generators/PHPythia6/PHPythia6LinkDef.h
+++ generators/PHPythia6/PHPythia6LinkDef.h
@@ -1,10 +1,6 @@
 #ifdef __CINT__
 
 #pragma link C++ class PHPythia6-!;
-#pragma link C++ class PHPy6GenTrigger-!;
-#pragma link C++ class PHPy6JetTrigger-!;
-#pragma link C++ class PHPy6ForwardElectronTrig-!;
-#pragma link C++ class PHPy6ParticleTrigger-!;
 
 #endif
 
