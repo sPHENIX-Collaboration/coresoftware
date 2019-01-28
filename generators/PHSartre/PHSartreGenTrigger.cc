b0243c53daddcf0fa710be1b492badc22de7d208
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-01-27 21:49:45 -0500

    clang-format

--- generators/PHSartre/PHSartreGenTrigger.cc
+++ generators/PHSartre/PHSartreGenTrigger.cc
@@ -7,25 +7,27 @@
 using namespace std;
 
 //__________________________________________________________
-PHSartreGenTrigger::PHSartreGenTrigger(const std::string &name):
-  m_Verbosity(0),
-  m_Name(name)
- {}
+PHSartreGenTrigger::PHSartreGenTrigger(const std::string &name)
+  : m_Verbosity(0)
+  , m_Name(name)
+{
+}
 
 //__________________________________________________________
 PHSartreGenTrigger::~PHSartreGenTrigger() {}
 
-std::vector<int> PHSartreGenTrigger::convertToInts(std::string s) {
-  
+std::vector<int> PHSartreGenTrigger::convertToInts(std::string s)
+{
   vector<int> theVec;
   stringstream ss(s);
-  int i;  
-  while (ss >> i) {
+  int i;
+  while (ss >> i)
+  {
     theVec.push_back(i);
     if (ss.peek() == ',' ||
-	ss.peek() == ' ' ||
-	ss.peek() == ':' ||
-	ss.peek() == ';') ss.ignore();
+        ss.peek() == ' ' ||
+        ss.peek() == ':' ||
+        ss.peek() == ';') ss.ignore();
   }
 
   return theVec;
