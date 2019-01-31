6a0eec1b0de50698297c256d945e96ac127f7e70
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-01-30 22:43:18 -0500

    clang-format

--- offline/QA/modules/QAHistManagerDef.cc
+++ offline/QA/modules/QAHistManagerDef.cc
@@ -1,4 +1,4 @@
-// $Id: $                                                                                             
+// $Id: $
 
 /*!
  * \file QAHistManagerDef.h
@@ -10,8 +10,8 @@
 
 #include "QAHistManagerDef.h"
 
-#include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllHistoManager.h>
+#include <fun4all/Fun4AllServer.h>
 
 #include <TAxis.h>
 #include <TMath.h>
@@ -23,57 +23,51 @@ using namespace std;
 
 namespace QAHistManagerDef
 {
+//! Get a pointer to the default hist manager for QA modules
+Fun4AllHistoManager *
+getHistoManager()
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  Fun4AllHistoManager *hm = se->getHistoManager(HistoManagerName);
 
-  //! Get a pointer to the default hist manager for QA modules
-  Fun4AllHistoManager *
-  getHistoManager()
+  if (not hm)
   {
-
-    Fun4AllServer *se = Fun4AllServer::instance();
-    Fun4AllHistoManager *hm = se->getHistoManager(HistoManagerName);
-
-    if (not hm)
-      {
-//        cout
-//            << "QAHistManagerDef::get_HistoManager - Making Fun4AllHistoManager EMCalAna_HISTOS"
-//            << endl;
-        hm = new Fun4AllHistoManager(HistoManagerName);
-        se->registerHistoManager(hm);
-      }
-
-    assert(hm);
-
-    return hm;
+    //        cout
+    //            << "QAHistManagerDef::get_HistoManager - Making Fun4AllHistoManager EMCalAna_HISTOS"
+    //            << endl;
+    hm = new Fun4AllHistoManager(HistoManagerName);
+    se->registerHistoManager(hm);
   }
 
-  //! Save hist to root files
-  void
-  saveQARootFile(const std::string & file_name)
-  {
-    getHistoManager()->dumpHistos(file_name);
-  }
+  assert(hm);
 
-//! utility function to
-  void
-  useLogBins(TAxis * axis)
-  {
-    assert(axis);
-    assert(axis->GetXmin()>0);
-    assert( axis->GetXmax()>0);
-
-    const int bins = axis->GetNbins();
-
-    Axis_t from = log10(axis->GetXmin());
-    Axis_t to = log10(axis->GetXmax());
-    Axis_t width = (to - from) / bins;
-    vector<Axis_t> new_bins(bins + 1);
-
-    for (int i = 0; i <= bins; i++)
-      {
-        new_bins[i] = TMath::Power(10, from + i * width);
-      }
-    axis->Set(bins, new_bins.data());
-
-  }
+  return hm;
 }
 
+//! Save hist to root files
+void saveQARootFile(const std::string &file_name)
+{
+  getHistoManager()->dumpHistos(file_name);
+}
+
+//! utility function to
+void useLogBins(TAxis *axis)
+{
+  assert(axis);
+  assert(axis->GetXmin() > 0);
+  assert(axis->GetXmax() > 0);
+
+  const int bins = axis->GetNbins();
+
+  Axis_t from = log10(axis->GetXmin());
+  Axis_t to = log10(axis->GetXmax());
+  Axis_t width = (to - from) / bins;
+  vector<Axis_t> new_bins(bins + 1);
+
+  for (int i = 0; i <= bins; i++)
+  {
+    new_bins[i] = TMath::Power(10, from + i * width);
+  }
+  axis->Set(bins, new_bins.data());
+}
+}  // namespace QAHistManagerDef
