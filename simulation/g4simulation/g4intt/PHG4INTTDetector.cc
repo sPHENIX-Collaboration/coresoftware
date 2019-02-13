e86de162b9e651dde75f9f4de68320a6315474d5
todoroki <todoroki@bnl.gov>
2019-02-12 19:59:49 -0500

    Aluminum end cap rings

--- simulation/g4simulation/g4intt/PHG4INTTDetector.cc
+++ simulation/g4simulation/g4intt/PHG4INTTDetector.cc
@@ -125,7 +125,7 @@ int PHG4INTTDetector::ConstructINTT(G4LogicalVolume *trackerenvelope)
     double fphx_z = params->get_double_param("fphx_z") * cm;
     double fphx_offset_z = params->get_double_param("fphx_offset_z") * cm;
     double pgs_x = params->get_double_param("pgs_x") * cm;
-    double halfladder_z = params->get_double_param("halfladder_z") * cm;
+    double halfladder_inside_z = params->get_double_param("halfladder_inside_z") * cm;
     double stave_straight_outer_y = params->get_double_param("stave_straight_outer_y") * cm;
     double stave_straight_inner_y = params->get_double_param("stave_straight_inner_y") * cm;
     double stave_straight_cooler_y = params->get_double_param("stave_straight_cooler_y") * cm;
@@ -229,8 +229,9 @@ int PHG4INTTDetector::ConstructINTT(G4LogicalVolume *trackerenvelope)
       {
         m_PassiveVolumeTuple.insert(make_pair(hdi_copper_volume, make_tuple(inttlayer, PHG4INTTDefs::HDI_COPPER)));
       }
-      // This is the part of the HDI that extends beyond the sensor
-      const double hdiext_z = (itype == 0) ? 0.000001 : halfladder_z - hdi_z_arr[inttlayer][0] - hdi_z;  // need to assign nonzero value for itype=0
+
+      // This is the part of the HDI that extends beyond the sensor inside the endcap ring
+      const double hdiext_z = (itype == 0) ? 0.000001 : halfladder_inside_z - hdi_z_arr[inttlayer][0] - hdi_z;  // need to assign nonzero value for itype=0
       G4VSolid *hdiext_kapton_box = new G4Box((boost::format("hdiext_kapton_box_%d_%s") % inttlayer % itype).str(),
                                               hdi_kapton_x / 2., hdi_y / 2., hdiext_z / 2.0);
       G4LogicalVolume *hdiext_kapton_volume = new G4LogicalVolume(hdiext_kapton_box, G4Material::GetMaterial("G4_KAPTON"),  // was "FPC"
@@ -786,6 +787,7 @@ int PHG4INTTDetector::ConstructINTT(G4LogicalVolume *trackerenvelope)
         if (itype != 0)
         {
           // We have added the outer sensor above, now we add the HDI extension tab to the end of the outer sensor HDI
+          //const double posz_ext = (hdi_z_arr[inttlayer][0] + hdi_z) + hdiext_z / 2.;
           const double posz_ext = (hdi_z_arr[inttlayer][0] + hdi_z) + hdiext_z / 2.;
 
           new G4PVPlacement(G4Transform3D(ladderrotation, G4ThreeVector(posx, posy, -posz_ext)), ladderext_volume,
@@ -883,6 +885,82 @@ int PHG4INTTDetector::ConstructINTT(G4LogicalVolume *trackerenvelope)
   inner_skin_volume->SetVisAttributes(rail_vis);
   new G4PVPlacement(0, G4ThreeVector(0, 0.0), inner_skin_volume,
                     "si_support_inner_skin", trackerenvelope, false, 0, OverlapCheck());
+
+  // Endcap ring in simulations = Endcap rings + endcap staves 
+  
+  // Aluminum ring
+  G4Tubs *endcap_Al_ring = new G4Tubs("endcap_Al_ring",
+                                       supportparams->get_double_param("endcap_Alring_inner_radius") * cm,
+                                       supportparams->get_double_param("endcap_Alring_outer_radius") * cm,
+                                       supportparams->get_double_param("endcap_Alring_length") * cm / 2.,
+                                       -M_PI, 2.0 * M_PI);
+
+  G4LogicalVolume *endcap_Al_ring_volume = new G4LogicalVolume(endcap_Al_ring, G4Material::GetMaterial("Al6061T6"),
+                                                               "endcap_Al_ring_volume", 0, 0, 0);
+  
+  // Stainlees steal ring
+  G4Tubs *endcap_SS_ring = new G4Tubs("endcap_SS_ring",
+                                      supportparams->get_double_param("endcap_SSring_inner_radius") * cm,
+                                      supportparams->get_double_param("endcap_SSring_outer_radius") * cm,
+                                      supportparams->get_double_param("endcap_SSring_length") * cm / 2.,
+                                      -M_PI, 2.0 * M_PI);
+
+  G4LogicalVolume *endcap_SS_ring_volume = new G4LogicalVolume(endcap_SS_ring, G4Material::GetMaterial("SS316"),
+                                                               "endcap_SS_ring_volume", 0, 0, 0);
+  
+  // Water Glycol ring
+  G4Tubs *endcap_WG_ring = new G4Tubs("endcap_WG_ring",
+                                       supportparams->get_double_param("endcap_WGring_inner_radius") * cm,
+                                       supportparams->get_double_param("endcap_WGring_outer_radius") * cm,
+                                       supportparams->get_double_param("endcap_WGring_length") * cm / 2.,
+                                       -M_PI, 2.0 * M_PI);
+
+  G4LogicalVolume *endcap_WG_ring_volume = new G4LogicalVolume(endcap_WG_ring, G4Material::GetMaterial("WaterGlycol_INTT"),
+                                                               "endcap_WG_ring_volume", 0, 0, 0);
+
+  // Place endcap rings
+  G4RotationMatrix ringrotation;
+  ringrotation.rotateY( 0 * deg / rad );
+
+  double endcap_ring_z = supportparams->get_double_param("endcap_ring_z") * cm; 
+  for(int i = 0; i < 2; i++) // i=0 : positive z, i=1 negative z
+  {
+
+    endcap_ring_z = (i==0) ?  endcap_ring_z : -1.0 * endcap_ring_z;
+
+    double width_WGring_z = supportparams->get_double_param("endcap_WGring_length") * cm;
+    double width_SSring_z = supportparams->get_double_param("endcap_SSring_length") * cm;
+    double width_Alring_z = supportparams->get_double_param("endcap_Alring_length") * cm;
+
+    for(int j = 0; j < 2; j++) // j=0 : positive side z, j=1 negative side z
+    {
+      width_WGring_z = (j==0) ? width_WGring_z : -1.0 * width_WGring_z; 
+      width_SSring_z = (j==0) ? width_SSring_z : -1.0 * width_SSring_z;
+      width_Alring_z = (j==0) ? width_Alring_z : -1.0 * width_Alring_z;
+    
+      double cent_WGring_z = endcap_ring_z + width_WGring_z / 2.;
+      double cent_SSring_z = endcap_ring_z + width_WGring_z + width_SSring_z / 2.;
+      double cent_Alring_z = endcap_ring_z + width_WGring_z + width_SSring_z + width_Alring_z / 2.;
+
+
+      new G4PVPlacement(G4Transform3D(ringrotation, G4ThreeVector(0, 0, cent_WGring_z)),
+          endcap_WG_ring_volume,
+          (boost::format("endcap_WG_ring_pv_%d_%d") %i %j).str(),
+          trackerenvelope, false, 0, OverlapCheck());
+
+      new G4PVPlacement(G4Transform3D(ringrotation, G4ThreeVector(0, 0, cent_SSring_z)),
+          endcap_SS_ring_volume,
+          (boost::format("endcap_SS_ring_pv_%d_%d") %i %j).str(),
+          trackerenvelope, false, 0, OverlapCheck());
+
+      new G4PVPlacement(G4Transform3D(ringrotation, G4ThreeVector(0, 0, cent_Alring_z)),
+          endcap_Al_ring_volume,
+          (boost::format("endcap_Al_ring_pv_%d_%d") %i %j).str(),
+          trackerenvelope, false, 0, OverlapCheck());
+    }
+  }
+
+
 
   //=======================================================
   // Add an outer shell for the MVTX - move this to the MVTX detector module
