95a43604ae05182b7257a0c6a276a1f26b81531a
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-03-01 16:27:47 -0500

    this change makes the cling loading complaint about JetMap.h go away

--- simulation/g4simulation/g4jets/JetMapv1LinkDef.h
+++ simulation/g4simulation/g4jets/JetMapv1LinkDef.h
@@ -1,7 +1,9 @@
 #ifdef __CINT__
+#pragma link C++ class JetMapv1 + ;
 
+#ifndef __CLING__
 #pragma link C++ class std::pair < unsigned int, Jet* > +;
 #pragma link C++ class std::map < unsigned int, Jet* > +;
-#pragma link C++ class JetMapv1 + ;
+#endif
 
 #endif /* __CINT__ */
