ce4d2be1c7bbf10b65555894892e9c07b68a6084
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-04-12 16:21:30 -0400

    clang-format

--- simulation/g4simulation/g4main/PHG4IonGun.cc
+++ simulation/g4simulation/g4main/PHG4IonGun.cc
@@ -1,13 +1,13 @@
 #include "PHG4IonGun.h"
 
-#include "PHG4Particlev3.h"
 #include "PHG4InEvent.h"
+#include "PHG4Particlev3.h"
 
 #include <fun4all/Fun4AllReturnCodes.h>
 
-#include <phool/getClass.h>
 #include <phool/PHCompositeNode.h>
 #include <phool/PHIODataNode.h>
+#include <phool/getClass.h>
 
 #include <Geant4/G4Event.hh>
 #include <Geant4/G4IonTable.hh>
@@ -26,7 +26,7 @@ PHG4IonGun::PHG4IonGun(const string &name)
   , ioncharge(0)
   , excitEnergy(0)
 {
-  fill(begin(mom),end(mom),NAN);
+  fill(begin(mom), end(mom), NAN);
 }
 
 void PHG4IonGun::SetCharge(const int c)
@@ -43,17 +43,17 @@ void PHG4IonGun::SetMom(const double px, const double py, const double pz)
 
 int PHG4IonGun::process_event(PHCompositeNode *topNode)
 {
-// get pdg code for ions
+  // get pdg code for ions
   UpdateParticle();
-  PHG4InEvent *ineve = findNode::getClass<PHG4InEvent>(topNode,"PHG4INEVENT");
-  ReuseExistingVertex(topNode); // checks if we should reuse existing vertex
-  int vtxindex = ineve->AddVtx(vtx_x,vtx_y,vtx_z,t0);
-//   G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z, A, excitEnergy);
-   //G4PrimaryParticle* g4part = new G4PrimaryParticle(ion);
-//   cout << "name: " << ion->GetParticleName() << ", pdgcode: " << ion->GetPDGEncoding() << endl;
-      PHG4Particle *particle = new PHG4Particlev3(ion);
-      SetParticleId(particle,ineve);
-      ineve->AddParticle(vtxindex, particle);
+  PHG4InEvent *ineve = findNode::getClass<PHG4InEvent>(topNode, "PHG4INEVENT");
+  ReuseExistingVertex(topNode);  // checks if we should reuse existing vertex
+  int vtxindex = ineve->AddVtx(vtx_x, vtx_y, vtx_z, t0);
+  //   G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z, A, excitEnergy);
+  //G4PrimaryParticle* g4part = new G4PrimaryParticle(ion);
+  //   cout << "name: " << ion->GetParticleName() << ", pdgcode: " << ion->GetPDGEncoding() << endl;
+  PHG4Particle *particle = new PHG4Particlev3(ion);
+  SetParticleId(particle, ineve);
+  ineve->AddParticle(vtxindex, particle);
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
@@ -66,7 +66,7 @@ void PHG4IonGun::UpdateParticle()
   ion->set_px(mom[0]);
   ion->set_py(mom[1]);
   ion->set_pz(mom[2]);
-  G4ParticleDefinition* iondef = G4IonTable::GetIonTable()->GetIon(Z, A, excitEnergy);
+  G4ParticleDefinition *iondef = G4IonTable::GetIonTable()->GetIon(Z, A, excitEnergy);
   ion->set_name(iondef->GetParticleName());
   ion->set_pid(iondef->GetPDGEncoding());
   return;
@@ -75,7 +75,7 @@ void PHG4IonGun::UpdateParticle()
 void PHG4IonGun::Print(const string &what) const
 {
   cout << "PHG4IonGun, using ions of" << endl;
-  cout << "A: " << A << ", Z: " << Z << ", charge: " << ioncharge 
+  cout << "A: " << A << ", Z: " << Z << ", charge: " << ioncharge
        << ", excitation Energy: " << excitEnergy << endl;
-  cout << "px: " << mom[0]/GeV << " GeV, py: " << mom[1]/GeV << " GeV, pz: " << mom[2]/GeV << " GeV" << endl;
+  cout << "px: " << mom[0] / GeV << " GeV, py: " << mom[1] / GeV << " GeV, pz: " << mom[2] / GeV << " GeV" << endl;
 }
