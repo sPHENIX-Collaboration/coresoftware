6d1d43d434d7c8a57758fe6afc7743f5bbaf5c83
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-05-04 20:29:44 -0400

    apply datamember naming convention

--- offline/database/PHParameter/PHParameterInterface.cc
+++ offline/database/PHParameter/PHParameterInterface.cc
@@ -9,26 +9,26 @@
 using namespace std;
 
 PHParameterInterface::PHParameterInterface(const std::string &name):
-params(new PHParameters(name))
+m_Params(new PHParameters(name))
 {}
 
 void
 PHParameterInterface::set_paramname(const string &name)
 {
-  params->set_name(name);
+  m_Params->set_name(name);
 }
 
 void
 PHParameterInterface::set_default_double_param( const std::string &name, const double dval)
 {
-  if (default_double.find(name) == default_double.end())
+  if (m_DefaultDoubleParMap.find(name) == m_DefaultDoubleParMap.end())
     {
-      default_double[name] = dval;
+      m_DefaultDoubleParMap[name] = dval;
     }
   else
     {
       cout << "trying to overwrite default double " << name << " " 
-	   << default_double[name] << " with " << dval << endl;
+	   << m_DefaultDoubleParMap[name] << " with " << dval << endl;
       gSystem->Exit(1);
     }
   return;
@@ -37,14 +37,14 @@ PHParameterInterface::set_default_double_param( const std::string &name, const d
 void
 PHParameterInterface::set_default_int_param( const std::string &name, const int ival)
 {
-  if (default_int.find(name) == default_int.end())
+  if (m_DefaultIntParMap.find(name) == m_DefaultIntParMap.end())
     {
-      default_int[name] = ival;
+      m_DefaultIntParMap[name] = ival;
     }
   else
     {
       cout << "trying to overwrite default int " << name << " " 
-	   << default_int[name] << " with " << ival << endl;
+	   << m_DefaultIntParMap[name] << " with " << ival << endl;
       gSystem->Exit(1);
     }
   return;
@@ -53,14 +53,14 @@ PHParameterInterface::set_default_int_param( const std::string &name, const int 
 void
 PHParameterInterface::set_default_string_param( const std::string &name, const string &sval)
 {
-  if (default_string.find(name) == default_string.end())
+  if (m_DefaultStringParMap.find(name) == m_DefaultStringParMap.end())
     {
-      default_string[name] = sval;
+      m_DefaultStringParMap[name] = sval;
     }
   else
     {
       cout << "trying to overwrite default string " << name << " " 
-	   << default_string[name] << " with " << sval << endl;
+	   << m_DefaultStringParMap[name] << " with " << sval << endl;
       gSystem->Exit(1);
     }
   return;
@@ -68,83 +68,83 @@ PHParameterInterface::set_default_string_param( const std::string &name, const s
 void
 PHParameterInterface::set_double_param(const std::string &name, const double dval)
 {
-  if (default_double.find(name) == default_double.end())
+  if (m_DefaultDoubleParMap.find(name) == m_DefaultDoubleParMap.end())
     {
       cout << "double parameter " << name << " not implemented" << endl;
       cout << "implemented double parameters are:" << endl;
-      for (map<const string, double>::const_iterator iter = default_double.begin(); iter != default_double.end(); ++iter)
+      for (map<const string, double>::const_iterator iter = m_DefaultDoubleParMap.begin(); iter != m_DefaultDoubleParMap.end(); ++iter)
 	{
 	  cout << iter->first << endl;
 	}
       return;
     }
-  dparams[name] = dval;
+  m_DoubleParMap[name] = dval;
 }
 
 double
 PHParameterInterface::get_double_param(const std::string &name) const
 {
-  return params->get_double_param(name);
+  return m_Params->get_double_param(name);
 }
 
 void
 PHParameterInterface::set_int_param(const std::string &name, const int ival)
 {
-  if (default_int.find(name) == default_int.end())
+  if (m_DefaultIntParMap.find(name) == m_DefaultIntParMap.end())
     {
       cout << "integer parameter " << name << " not implemented" << endl;
       cout << "implemented integer parameters are:" << endl;
-      for (map<const string, int>::const_iterator iter = default_int.begin(); iter != default_int.end(); ++iter)
+      for (map<const string, int>::const_iterator iter = m_DefaultIntParMap.begin(); iter != m_DefaultIntParMap.end(); ++iter)
 	{
 	  cout << iter->first << endl;
 	}
       return;
     }
-  iparams[name] = ival;
+  m_IntParMap[name] = ival;
 }
 
 int
 PHParameterInterface::get_int_param(const std::string &name) const
 {
-  return params->get_int_param(name);
+  return m_Params->get_int_param(name);
 }
 
 void
 PHParameterInterface::set_string_param(const std::string &name, const string &sval)
 {
-  if (default_string.find(name) == default_string.end())
+  if (m_DefaultStringParMap.find(name) == m_DefaultStringParMap.end())
     {
       cout << "string parameter " << name << " not implemented" << endl;
       cout << "implemented string parameters are:" << endl;
-      for (map<const string, string>::const_iterator iter = default_string.begin(); iter != default_string.end(); ++iter)
+      for (map<const string, string>::const_iterator iter = m_DefaultStringParMap.begin(); iter != m_DefaultStringParMap.end(); ++iter)
 	{
 	  cout << iter->first << endl;
 	}
       return;
     }
-  cparams[name] = sval;
+  m_StringParMap[name] = sval;
 }
 
 string
 PHParameterInterface::get_string_param(const std::string &name) const
 {
-  return params->get_string_param(name);
+  return m_Params->get_string_param(name);
 }
 
 void
 PHParameterInterface::UpdateParametersWithMacro()
 {
-  for (map<const string,double>::const_iterator iter = dparams.begin(); iter != dparams.end(); ++iter)
+  for (map<const string,double>::const_iterator iter = m_DoubleParMap.begin(); iter != m_DoubleParMap.end(); ++iter)
     {
-      params->set_double_param(iter->first,iter->second);
+      m_Params->set_double_param(iter->first,iter->second);
     }
-  for (map<const string,int>::const_iterator iter = iparams.begin(); iter != iparams.end(); ++iter)
+  for (map<const string,int>::const_iterator iter = m_IntParMap.begin(); iter != m_IntParMap.end(); ++iter)
     {
-      params->set_int_param(iter->first,iter->second);
+      m_Params->set_int_param(iter->first,iter->second);
     }
-  for (map<const string,string>::const_iterator iter = cparams.begin(); iter != cparams.end(); ++iter)
+  for (map<const string,string>::const_iterator iter = m_StringParMap.begin(); iter != m_StringParMap.end(); ++iter)
     {
-      params->set_string_param(iter->first,iter->second);
+      m_Params->set_string_param(iter->first,iter->second);
     }
   return;
 }
@@ -152,14 +152,14 @@ PHParameterInterface::UpdateParametersWithMacro()
 void
 PHParameterInterface::SaveToNodeTree(PHCompositeNode *runNode, const string &nodename)
 {
-  params->SaveToNodeTree(runNode, nodename);
+  m_Params->SaveToNodeTree(runNode, nodename);
   return;
 }
 
 void
 PHParameterInterface::PutOnParNode(PHCompositeNode *parNode, const string &nodename)
 {
-  parNode->addNode(new PHDataNode<PHParameters>(params,nodename));
+  parNode->addNode(new PHDataNode<PHParameters>(m_Params,nodename));
 }
 
 void
@@ -167,16 +167,16 @@ PHParameterInterface::InitializeParameters()
 {
   SetDefaultParameters(); // call method from specific subsystem
   // now load those parameters to our params class
-  for (map<const string,double>::const_iterator iter = default_double.begin(); iter != default_double.end(); ++iter)
+  for (map<const string,double>::const_iterator iter = m_DefaultDoubleParMap.begin(); iter != m_DefaultDoubleParMap.end(); ++iter)
     {
-      params->set_double_param(iter->first,iter->second);
+      m_Params->set_double_param(iter->first,iter->second);
     }
-  for (map<const string,int>::const_iterator iter = default_int.begin(); iter != default_int.end(); ++iter)
+  for (map<const string,int>::const_iterator iter = m_DefaultIntParMap.begin(); iter != m_DefaultIntParMap.end(); ++iter)
     {
-      params->set_int_param(iter->first,iter->second);
+      m_Params->set_int_param(iter->first,iter->second);
     }
-  for (map<const string,string>::const_iterator iter = default_string.begin(); iter != default_string.end(); ++iter)
+  for (map<const string,string>::const_iterator iter = m_DefaultStringParMap.begin(); iter != m_DefaultStringParMap.end(); ++iter)
     {
-      params->set_string_param(iter->first,iter->second);
+      m_Params->set_string_param(iter->first,iter->second);
     }
 }
