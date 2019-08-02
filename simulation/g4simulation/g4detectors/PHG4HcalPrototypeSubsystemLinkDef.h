5bf988228829d48be2edf91164be545cf33df4e2
Jin Huang <jhuang@bnl.gov>
2019-08-02 09:16:02 -0400

    relocating prototype code

--- simulation/g4simulation/g4detectors/PHG4HcalPrototypeSubsystemLinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class PHG4HcalPrototypeSubsystem-!;
-
-#endif /* __CINT__ */
--- simulation/g4simulation/g4detectors/PHG4Prototype2InnerHcalSubsystemLinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class PHG4Prototype2InnerHcalSubsystem-!;
-
-#endif /* __CINT__ */
