aeaad80266c09337484f3f6792c990d60df1134f
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-10-04 16:58:43 -0400

    use qt based gui which has to run in main thread, execute Fun4AllServer::run() from G4 cmd line

---
+++ simulation/g4simulation/g4main/Fun4AllMessenger.cc
@@ -0,0 +1,34 @@
+#include "Fun4AllMessenger.h"
+
+#include <fun4all/Fun4AllServer.h>
+
+#include <Geant4/G4UIcmdWithAnInteger.hh>
+
+using namespace std;
+
+Fun4AllMessenger::Fun4AllMessenger(Fun4AllServer *ffa):
+  se(ffa)
+{
+  m_Fun4AllDir = new G4UIdirectory("/Fun4All/");
+  m_Fun4AllDir->SetGuidance("UI commands to run Fun4All commands"); 
+  m_RunCmd = new G4UIcmdWithAnInteger("/Fun4All/run",this);
+  m_RunCmd->SetGuidance("Run Event(s)");
+  m_RunCmd->SetParameterName("nEvents",1); 
+  m_RunCmd->AvailableForStates(G4State_Idle);
+}
+
+Fun4AllMessenger::~Fun4AllMessenger()
+{
+  delete m_RunCmd;
+  delete m_Fun4AllDir;
+}
+
+void Fun4AllMessenger::SetNewValue(G4UIcommand* command,G4String newValue)
+{
+
+  if (command == m_RunCmd)
+  {
+    se->run(m_RunCmd->GetNewIntValue(newValue));
+  }
+}
+
