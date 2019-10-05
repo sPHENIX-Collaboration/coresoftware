a4cf3fc801587685f2ed285aa771ff00ba2fdc91
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-10-05 10:23:43 -0400

    move raw data node handling to phoolraw

---
+++ offline/framework/phoolraw/PHRawDataNode.cc
@@ -0,0 +1,41 @@
+//  Implementation of class PHRawDataNode
+//  Author: Matthias Messer
+
+#include "PHRawDataNode.h"
+
+#include "PHIOManager.h"
+#include "PHRawOManager.h"
+
+#include <Event/phenixTypes.h>
+
+using namespace std;
+
+PHRawDataNode::PHRawDataNode(PHDWORD* d, const string& n,
+                             const int l, const int i, const int w, const int h)
+  : PHDataNode<PHDWORD>(d, n)
+  , length(l)
+  , ID(i)
+  , wordLength(w)
+  , hitFormat(h)
+{
+}
+
+PHRawDataNode::~PHRawDataNode()
+{
+  // set the data poitner to 0 so
+  // the dtor of the PHDataNode parent class doesn't try
+  // to delete it
+  setData(nullptr);
+}
+
+bool PHRawDataNode::write(PHIOManager* IOManager, const string&)
+{
+  PHRawOManager* rawOManager = dynamic_cast<PHRawOManager*>(IOManager);
+  bool bret = false;
+  if (rawOManager)
+  {
+    bret = rawOManager->write(this);
+  }
+
+  return bret;
+}
