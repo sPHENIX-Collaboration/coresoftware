fbb1d343e5d6c3626e14bfba369eb45aba84c051
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-10-07 13:13:07 -0400

    clang-format

--- simulation/g4simulation/g4gdml/PHG4GDMLUtility.cc
+++ simulation/g4simulation/g4gdml/PHG4GDMLUtility.cc
@@ -9,39 +9,36 @@
  */
 
 #include "PHG4GDMLUtility.hh"
-#include "PHG4GDMLWriteStructure.hh"
 #include "PHG4GDMLConfig.hh"
+#include "PHG4GDMLWriteStructure.hh"
 
 #include <fun4all/Fun4AllServer.h>
 
-#include <phool/PHNodeIterator.h>
 #include <phool/PHCompositeNode.h>
 #include <phool/PHDataNode.h>
-#include <phool/PHObject.h>                // for PHObject
+#include <phool/PHNodeIterator.h>
+#include <phool/PHObject.h>  // for PHObject
 #include <phool/getClass.h>
-
 
 #include <Geant4/G4VPhysicalVolume.hh>
 
 #include <cassert>
-#include <iostream>                        // for operator<<, stringstream
+#include <iostream>  // for operator<<, stringstream
 #include <sstream>
 #include <stdexcept>
 
 using namespace std;
 
-void PHG4GDMLUtility::Dump_GDML(const std::string &filename, G4VPhysicalVolume * vol, PHCompositeNode *topNode)
+void PHG4GDMLUtility::Dump_GDML(const std::string &filename, G4VPhysicalVolume *vol, PHCompositeNode *topNode)
 {
   if (topNode == nullptr)
-    {
+  {
+    Fun4AllServer *se = Fun4AllServer::instance();
+    topNode = se->topNode();
+  }
 
-      Fun4AllServer *se = Fun4AllServer::instance();
-      topNode = se->topNode();
-
-    }
-
-  const PHG4GDMLConfig * config =
-  GetOrMakeConfigNode(topNode);
+  const PHG4GDMLConfig *config =
+      GetOrMakeConfigNode(topNode);
   assert(config);
 
   PHG4GDMLWriteStructure gdml_parser(config);
@@ -49,38 +46,37 @@ void PHG4GDMLUtility::Dump_GDML(const std::string &filename, G4VPhysicalVolume *
   assert(vol->GetLogicalVolume());
 
   xercesc::XMLPlatformUtils::Initialize();
-  gdml_parser.Write(filename, vol->GetLogicalVolume(),get_PHG4GDML_Schema(), 0, true);
+  gdml_parser.Write(filename, vol->GetLogicalVolume(), get_PHG4GDML_Schema(), 0, true);
   xercesc::XMLPlatformUtils::Terminate();
 }
 
-
-PHG4GDMLConfig * PHG4GDMLUtility::GetOrMakeConfigNode(PHCompositeNode *topNode, bool build_new )
+PHG4GDMLConfig *PHG4GDMLUtility::GetOrMakeConfigNode(PHCompositeNode *topNode, bool build_new)
 {
   PHNodeIterator iter(topNode);
 
   // Looking for the RUN node
-  PHCompositeNode *parNode = static_cast<PHCompositeNode*>(iter.findFirst(
+  PHCompositeNode *parNode = static_cast<PHCompositeNode *>(iter.findFirst(
       "PHCompositeNode", "PAR"));
   if (!parNode)
-    {
-      stringstream serr;
-      serr << __PRETTY_FUNCTION__ << ": PAR Node missing, request aborting.";
-      cout << serr.str() << endl;
+  {
+    stringstream serr;
+    serr << __PRETTY_FUNCTION__ << ": PAR Node missing, request aborting.";
+    cout << serr.str() << endl;
 
-      throw runtime_error(serr.str());
+    throw runtime_error(serr.str());
 
-      return nullptr;
-    }
+    return nullptr;
+  }
 
   PHG4GDMLConfig *config = findNode::getClass<PHG4GDMLConfig>(parNode,
-      getDSTNodeName());
+                                                              getDSTNodeName());
   if (!config and build_new)
-    {
-      config = new PHG4GDMLConfig();
-      PHDataNode<PHObject> *node = new PHDataNode<PHObject>(config,
-          getDSTNodeName(), "PHObject");
-      parNode->addNode(node);
-    }
+  {
+    config = new PHG4GDMLConfig();
+    PHDataNode<PHObject> *node = new PHDataNode<PHObject>(config,
+                                                          getDSTNodeName(), "PHObject");
+    parNode->addNode(node);
+  }
 
   return config;
 }
