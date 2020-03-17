88db39b50d73b37fc57e46034ba7c5f300d78922
Anthony Denis Frawley <afrawley@fsu.edu>
2020-03-16 20:34:45 -0400

    Removed obsolete classes: trackbase - removed cell association classes. trackbase_historic - removed all SvtxHit and SvtxCluster related classes.

--- offline/packages/trackbase_historic/SvtxClusterMapLinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class SvtxClusterMap + ;
-
-#endif /* __CINT__ */
--- offline/packages/trackbase_historic/SvtxCluster_v1LinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class SvtxCluster_v1 + ;
-
-#endif /* __CINT__ */
