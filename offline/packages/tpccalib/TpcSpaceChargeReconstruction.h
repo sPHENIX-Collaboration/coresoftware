693dec464c8315e60f313af29e7fb4c9ddbff7a4
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-03-31 11:59:34 -0400

    added documentation and a few TODO

--- offline/packages/tpccalib/TpcSpaceChargeReconstruction.h
+++ offline/packages/tpccalib/TpcSpaceChargeReconstruction.h
@@ -1,6 +1,10 @@
 #ifndef TPCCALIB_TPCSPACECHARGERECONSTRUCTION_H
 #define TPCCALIB_TPCSPACECHARGERECONSTRUCTION_H
-
+/**
+ * \file TpcSpaceChargeReconstruction.h
+ * \brief performs space charge distortion reconstruction using tracks
+ * \author Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
+ */
 #include <fun4all/SubsysReco.h>
 #include <trackbase/TrkrDefs.h>
 
@@ -15,6 +19,20 @@ class SvtxTrackMap;
 class TrkrCluster;
 class TrkrClusterContainer;
 
+/**
+ * \class TpcSpaceChargeReconstruction
+ * \brief performs space charge distortion reconstruction using tracks
+ * \detail To reconstruct the distortions dr0, drphi0 and dz0 in a given volume element, the following chisquare is minimized
+ chisquare = sum_cluster (drphi - (drphi0 + dr0 tan alpha))**2/error**2 + sum_cluster ( dz - (dz0 + dr0 tan beta))**2/error**2
+ with
+ - drphi and dz the residuals (track - cluster) measured for a given cluster
+ - alpha and beta the track angles at the cluster in the rphi,r plane and the z,r plane, respectively
+ The chisquare being quadratic in dr0, drphi0 and dz0, it can be minimized analytically.
+ This results in a linear equation lhs[i].[corrections] = rhs[i], and thus [corrections] = lhs[i]**(-1).rhs[i]
+ The lhs and rhs matrices are filled in TpcSpaceChargeReconstruction::process_track
+ The inversion is performed in TpcSpaceChargeReconstruction::calculate_distortions
+ */
+
 class TpcSpaceChargeReconstruction: public SubsysReco
 {
   public:
@@ -26,9 +44,11 @@ class TpcSpaceChargeReconstruction: public SubsysReco
   //@{
 
   /// set tpc layers
+  // TODO: use recoconst instead ?
   void set_tpc_layers( unsigned int first_layer, unsigned int n_layers );
 
   /// set grid dimensions
+  // TODO: use recoconst instead ?
   /**
   \param zbins the number of bins along z
   \param rbins the number of bins in the radial direction. It must be a divider of the number of tpc layers (by default 48), e.g. 1, 12, 24, 48
