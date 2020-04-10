fdd33e31c76e3c7443bd9aa1f09105dd6f500f45
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-04-10 19:49:05 -0400

    clang-format

--- offline/packages/PHField/PHFieldBeast.cc
+++ offline/packages/PHField/PHFieldBeast.cc
@@ -15,7 +15,7 @@ PHFieldBeast::PHFieldBeast(const string &filename, const int verb, const float m
   , m_MagFieldScale(magfield_rescale)
 {
   m_BeastMagneticField = new BeastMagneticField(filename.c_str());
-  if (m_BeastMagneticField->ValidMapImported()) 
+  if (m_BeastMagneticField->ValidMapImported())
   {
     // Turn linear interpolation on;
     m_BeastMagneticField->UseInterpolation();
@@ -29,12 +29,11 @@ PHFieldBeast::PHFieldBeast(const string &filename, const int verb, const float m
 
 void PHFieldBeast::GetFieldValue(const double point[4], double *Bfield) const
 {
-  double x = point[0]/cm;
-  double y = point[1]/cm;
-  double z = point[2]/cm;
+  double x = point[0] / cm;
+  double y = point[1] / cm;
+  double z = point[2] / cm;
 
-
-  if ( !m_BeastMagneticField->GetFieldValue(x,y,z,Bfield[0],Bfield[1],Bfield[2]))
+  if (!m_BeastMagneticField->GetFieldValue(x, y, z, Bfield[0], Bfield[1], Bfield[2]))
   {
     Bfield[0] = 0.0;
     Bfield[1] = 0.0;
@@ -48,8 +47,8 @@ void PHFieldBeast::GetFieldValue(const double point[4], double *Bfield) const
   }
   if (Verbosity() > 2)
   {
-    cout << "PHFieldBeast::GetFieldValue: bx: " << Bfield[0]/tesla
-	 << ", by: " << Bfield[1]/tesla << ", bz: " << Bfield[2]/tesla << endl;
+    cout << "PHFieldBeast::GetFieldValue: bx: " << Bfield[0] / tesla
+         << ", by: " << Bfield[1] / tesla << ", bz: " << Bfield[2] / tesla << endl;
   }
   return;
 }
