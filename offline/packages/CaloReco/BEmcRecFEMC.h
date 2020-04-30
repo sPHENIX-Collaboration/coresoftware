1327af71ab5f87d8d756858d17c2419ccbb00be9
shura <shura@bnl.gov>
2020-04-30 15:39:26 -0400

    Minor modifications to follow the changes in BEmcRec, BEmcCluster and BEmcProfile

--- offline/packages/CaloReco/BEmcRecFEMC.h
+++ offline/packages/CaloReco/BEmcRecFEMC.h
@@ -12,7 +12,7 @@ class BEmcRecFEMC : public BEmcRec
   ~BEmcRecFEMC();
   void CorrectEnergy(float energy, float x, float y, float *ecorr);
   void CorrectECore(float ecore, float x, float y, float *ecorecorr);
-  void CorrectPosition(float energy, float x, float y, float *xcorr, float *ycorr);
+  void CorrectPosition(float energy, float x, float y, float& xcorr, float& ycorr);
   void CorrectShowerDepth(float energy, float x, float y, float z, float& xc, float& yc, float& zc );
   static float GetImpactAngle(float e, float x, float y);
 
