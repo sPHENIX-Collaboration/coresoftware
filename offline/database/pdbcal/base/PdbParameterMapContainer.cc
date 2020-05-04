0ebe264b7d155face2de97707803286576116234
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-04 14:34:40 -0400

    Add stacktrace to help locating offending code

--- offline/database/pdbcal/base/PdbParameterMapContainer.cc
+++ offline/database/pdbcal/base/PdbParameterMapContainer.cc
@@ -9,6 +9,8 @@
 #include <TFile.h>
 #include <TSystem.h>
 
+#include <boost/stacktrace.hpp>
+
 #include <algorithm>
 #include <cctype>
 #include <ctime>
@@ -53,6 +55,10 @@ void PdbParameterMapContainer::AddPdbParameterMap(const int layer, PdbParameterM
   if (parametermap.find(layer) != parametermap.end())
   {
     cout << PHWHERE << " layer " << layer << " already exists" << endl;
+    cout << "Here is the stacktrace: " << endl;
+    cout << boost::stacktrace::stacktrace();
+    cout << endl << "DO NOT PANIC - this is not a segfault" << endl;
+    cout << "Check the stacktrace for the guilty party (typically #2)" << endl;
     gSystem->Exit(1);
   }
   parametermap[layer] = params;
