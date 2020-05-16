50df6810c06a2e87334b7838a7cd6ff28d9d67cd
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-05-16 18:05:17 -0400

    gcc 8.3 barfs with %s/F in sprintf, using stl strings instead

--- simulation/g4simulation/g4trackfastsim/PHG4TrackFastSimEval.cc
+++ simulation/g4simulation/g4trackfastsim/PHG4TrackFastSimEval.cc
@@ -97,31 +97,32 @@ int PHG4TrackFastSimEval::Init(PHCompositeNode *topNode)
   const string xyz[3] = {"x", "y", "z"};
   for (map<string, int>::const_iterator iter = m_ProjectionNameMap.begin(); iter != m_ProjectionNameMap.end(); ++iter)
   {
-    char bname[100];
-    char bdef[100];
+    string bname;
+    string bdef;
     for (int i = 0; i < 3; i++)
     {
-      sprintf(bname, "%s_%s", iter->first.c_str(), xyz[i].c_str());
-      sprintf(bdef, "%s/F", bname);
-      m_TracksEvalTree->Branch(bname, &m_TTree_ref[i][iter->second], bdef);
+      bname = iter->first + "_" + xyz[i];
+      bdef = bname +"/F";
+      m_TracksEvalTree->Branch(bname.c_str(), &m_TTree_ref[i][iter->second], bdef.c_str());
     }
     for (int i = 0; i < 3; i++)
     {
-      sprintf(bname, "%s_p%s", iter->first.c_str(), xyz[i].c_str());
-      sprintf(bdef, "%s/F", bname);
-      m_TracksEvalTree->Branch(bname, &m_TTree_ref_p[i][iter->second], bdef);
+      bname = iter->first + "_p" + xyz[i];
+      bdef = bname +"/F";
+
+      m_TracksEvalTree->Branch(bname.c_str(), &m_TTree_ref_p[i][iter->second], bdef.c_str());
     }
     for (int i = 0; i < 3; i++)
     {
-      sprintf(bname, "%s_proj_%s", iter->first.c_str(), xyz[i].c_str());
-      sprintf(bdef, "%s/F", bname);
-      m_TracksEvalTree->Branch(bname, &m_TTree_proj[i][iter->second], bdef);
+      bname = iter->first + "_proj_" + xyz[i];
+      bdef = bname +"/F";
+      m_TracksEvalTree->Branch(bname.c_str(), &m_TTree_proj[i][iter->second], bdef.c_str());
     }
     for (int i = 0; i < 3; i++)
     {
-      sprintf(bname, "%s_proj_p%s", iter->first.c_str(), xyz[i].c_str());
-      sprintf(bdef, "%s/F", bname);
-      m_TracksEvalTree->Branch(bname, &m_TTree_proj_p[i][iter->second], bdef);
+      bname = iter->first + "_proj_p" + xyz[i];
+      bdef = bname +"/F";
+      m_TracksEvalTree->Branch(bname.c_str(), &m_TTree_proj_p[i][iter->second], bdef.c_str());
     }
   }
 
