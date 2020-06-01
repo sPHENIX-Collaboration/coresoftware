384f6d837b050932292c7dd9cfa6a3483497ef6e
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-06-01 13:22:07 -0400

    clang-format

--- simulation/g4simulation/g4main/EicEventHeaderv1.cc
+++ simulation/g4simulation/g4main/EicEventHeaderv1.cc
@@ -2,12 +2,12 @@
 
 #include <phool/phool.h>
 
-#include <cmath>
 #include <climits>
+#include <cmath>
 #include <cstdlib>
-#include <iostream>       // for operator<<, basic_ostream, basic_ostream::o...
-#include <string>         // for operator<<, string, char_traits
-#include <utility>        // for pair
+#include <iostream>  // for operator<<, basic_ostream, basic_ostream::o...
+#include <string>    // for operator<<, string, char_traits
+#include <utility>   // for pair
 
 using namespace std;
 
