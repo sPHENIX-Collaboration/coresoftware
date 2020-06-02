e2a47443808618036d666cfed442c7f2feb66b6d
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-06-02 14:27:41 -0600

    - start cleanup stepping action - use std::unique_ptr for running g4hit. This allows to get rid of destructor

--- simulation/g4simulation/g4micromegas/PHG4MicromegasSteppingAction.h
+++ simulation/g4simulation/g4micromegas/PHG4MicromegasSteppingAction.h
@@ -4,6 +4,7 @@
 #define PHG4MICROMEGASSTEPPINGACTION_H
 
 #include <g4main/PHG4SteppingAction.h>
+#include <memory>
 
 class PHG4MicromegasDetector;
 
@@ -16,37 +17,42 @@ class PHParameters;
 
 class PHG4MicromegasSteppingAction : public PHG4SteppingAction
 {
- public:
+  
+  public:
+  
   //! constructor
   PHG4MicromegasSteppingAction(PHG4MicromegasDetector*, const PHParameters* parameters);
 
-  //! destructor
-  virtual ~PHG4MicromegasSteppingAction();
-
   //! stepping action
-  virtual bool UserSteppingAction(const G4Step*, bool);
+  bool UserSteppingAction(const G4Step*, bool) override;
 
   //! reimplemented from base class
-  virtual void SetInterfacePointers(PHCompositeNode*);
+  void SetInterfacePointers(PHCompositeNode*) override;
 
- private:
+  private:
+  
   //! pointer to the detector
-  PHG4MicromegasDetector* m_Detector;
-  const PHParameters* m_Params;
-  //! pointer to hit container
-  PHG4HitContainer* m_HitContainer;
-  PHG4Hit* m_Hit;
-  PHG4HitContainer* m_SaveHitContainer;
-  G4VPhysicalVolume* m_SaveVolPre;
-  G4VPhysicalVolume* m_SaveVolPost;
+  PHG4MicromegasDetector* m_Detector = nullptr;
+  
+  const PHParameters* m_Params = nullptr;
 
-  int m_SaveTrackId;
-  int m_SavePreStepStatus;
-  int m_SavePostStepStatus;
-  int m_ActiveFlag;
-  int m_BlackHoleFlag;
-  double m_EdepSum;
-  double m_EionSum;
+  //! pointer to hit container
+  PHG4HitContainer* m_HitContainer = nullptr;
+
+  // running hit
+  std::unique_ptr<PHG4Hit> m_Hit;
+  
+  PHG4HitContainer* m_SaveHitContainer = nullptr;
+  G4VPhysicalVolume* m_SaveVolPre = nullptr;
+  G4VPhysicalVolume* m_SaveVolPost = nullptr;
+
+  int m_SaveTrackId = -1;
+  int m_SavePreStepStatus =  -1;
+  int m_SavePostStepStatus =  -1;
+  int m_ActiveFlag = 0;
+  int m_BlackHoleFlag = 0;
+  double m_EdepSum = 0;
+  double m_EionSum = 0;
 };
 
 #endif // MICROMEGASSTEPPINGACTION_H
