318b3284450faa6abd68e80ec3569506f5cc2baf
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-06-03 15:52:51 -0600

    - use system includes rather than local, to make rootcint happy - added ::identify method

--- offline/packages/micromegas/CylinderGeomMicromegas.cc
+++ offline/packages/micromegas/CylinderGeomMicromegas.cc
@@ -5,3 +5,14 @@
 
 #include "CylinderGeomMicromegas.h"
 
+void CylinderGeomMicromegas::identify( std::ostream& out ) const
+{
+  out << "CylinderGeomMicromegas" << std::endl;
+  out << "layer: " << m_layer << std::endl;
+  out << "segmentation_type: " << (m_segmentation_type == MicromegasDefs::SegmentationType::SEGMENTATION_PHI ? "SEGMENTATION_PHI":"SEGMENTATION_Z") << std::endl;
+  out << "radius: " << m_radius << "cm" << std::endl;
+  out << "thickness: " << m_thickness << "cm" << std::endl;
+  out << "zmin: " << m_zmin << "cm" << std::endl;
+  out << "zmax: " << m_zmax << "cm" << std::endl;
+  out << std::endl;
+}
