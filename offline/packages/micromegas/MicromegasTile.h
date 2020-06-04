58f440ef04ae40b7e22ab34e6a916a275331fc15
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-06-04 13:57:04 -0600

    used direct constructor rather than via array

--- offline/packages/micromegas/MicromegasTile.h
+++ offline/packages/micromegas/MicromegasTile.h
@@ -6,7 +6,6 @@
  * \author Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
  */
 
-#include <array>
 #include <vector>
 
 //! header only class that contains information about a given Tile location inside CylinderGeom
@@ -21,12 +20,12 @@ class MicromegasTile
   MicromegasTile()
   {}
 
-  //! convenient construct from std::array
-  MicromegasTile( const std::array<double, 4> values )
-    :m_centerPhi( values[0] )
-    ,m_centerZ( values[1] )
-    ,m_sizePhi( values[2] )
-    ,m_sizeZ( values[3] )
+  //! constructor
+  MicromegasTile( double centerPhi, double centerZ, double sizePhi, double sizeZ )
+    :m_centerPhi( centerPhi )
+    ,m_centerZ( centerZ )
+    ,m_sizePhi( sizePhi )
+    ,m_sizeZ( sizeZ )
   {}
 
   double m_centerPhi = 0;
