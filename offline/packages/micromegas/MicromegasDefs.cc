e87b4fb02aeeb290b3649a06ae341a5f00488b08
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-06-05 15:35:22 -0600

    Simple clusterinzing: one cluster per hit

--- offline/packages/micromegas/MicromegasDefs.cc
+++ offline/packages/micromegas/MicromegasDefs.cc
@@ -10,13 +10,6 @@ namespace MicromegasDefs
 {
 
   //________________________________________________________________
-  uint8_t getTileId(TrkrDefs::hitsetkey key)
-  {
-    TrkrDefs::hitsetkey tmp = (key >> kBitShiftTileId);
-    return tmp;
-  }
-
-  //________________________________________________________________
   TrkrDefs::hitsetkey genHitSetKey(uint8_t layer, uint8_t tile )
   {
     TrkrDefs::hitsetkey key = TrkrDefs::genHitSetKey(TrkrDefs::TrkrId::micromegasId, layer);
@@ -25,10 +18,33 @@ namespace MicromegasDefs
     return key;
   }
 
-    //________________________________________________________________
+  //________________________________________________________________
+  uint8_t getTileId(TrkrDefs::hitsetkey key)
+  {
+    TrkrDefs::hitsetkey tmp = (key >> kBitShiftTileId);
+    return tmp;
+  }
+
+  //________________________________________________________________
   TrkrDefs::hitkey genHitKey(uint16_t strip)
   {
     TrkrDefs::hitkey key = strip << kBitShiftStrip;
+    return key;
+  }
+
+  //________________________________________________________________
+  uint16_t getStrip( TrkrDefs::hitkey key )
+  {
+    TrkrDefs::hitkey tmp = (key >> kBitShiftStrip);
+    return tmp;
+  }
+
+  //________________________________________________________________
+  TrkrDefs::cluskey genClusterKey(TrkrDefs::hitsetkey hitsetkey, uint32_t clusid)
+  {
+    TrkrDefs::cluskey tmp = hitsetkey;
+    TrkrDefs::cluskey key = (tmp << TrkrDefs::kBitShiftClusId);
+    key |= clusid;
     return key;
   }
 
