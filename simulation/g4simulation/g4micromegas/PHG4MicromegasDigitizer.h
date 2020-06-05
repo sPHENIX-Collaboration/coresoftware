d5645497f1c18c5ce155797e37729bc2ae68aa2b
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-06-05 10:44:32 -0600

    added Digitizer skeleton

---
+++ simulation/g4simulation/g4micromegas/PHG4MicromegasDigitizer.h
@@ -0,0 +1,64 @@
+// Tell emacs that this is a C++ source
+// -*- C++ -*-.
+
+#ifndef PHG4MICROMEGASDIGITIZER_H
+#define PHG4MICROMEGASDIGITIZER_H
+
+/*!
+ * \file PHG4MicromegasDigitizer.h
+ * \author Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
+ */
+
+#include <fun4all/SubsysReco.h>
+
+#include <gsl/gsl_rng.h>
+#include <memory>
+
+class PHCompositeNode;
+
+class PHG4MicromegasDigitizer : public SubsysReco
+{
+
+  public:
+  PHG4MicromegasDigitizer(const std::string &name = "PHG4MicromegasDigitizer");
+
+  //! run initialization
+  int InitRun(PHCompositeNode*) override;
+
+  //! event processing
+  int process_event(PHCompositeNode *topNode) override;
+
+  //! adc parameters
+  void set_adc_scale(unsigned int max_adc, double energy_scale)
+  {
+    m_max_adc  = max_adc;
+    m_energy_scale = energy_scale;
+  }
+
+  //! threshold
+  void set_energy_threshold(double value)
+  { m_energy_threshold = value; }
+
+  private:
+
+  // settings
+  unsigned int m_max_adc = 0;
+  double m_energy_scale = 1;
+  double m_energy_threshold = 0;
+
+  //! rng de-allocator
+  class Deleter
+  {
+    public:
+    //! deletion operator
+    void operator() (gsl_rng* rng) const
+    { gsl_rng_free(rng); }
+  };
+
+  //! random generator that conform with sPHENIX standard
+  /*! using a unique_ptr with custom Deleter ensures that the structure is properly freed when parent object is destroyed */
+  std::unique_ptr<gsl_rng, Deleter> m_rng;
+
+};
+
+#endif
