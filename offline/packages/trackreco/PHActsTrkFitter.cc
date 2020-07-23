130a7cdc9cc960aa29c6ea4283864a79855c1227
Anthony Denis Frawley <afrawley@fsu.edu>
2020-07-23 16:40:13 -0400

    Code to complete the update of SvtxTracks with acts fitted track results. This allows evaluation of the acts fitted tracks using SvtxEvaluator.

--- offline/packages/trackreco/PHActsTrkFitter.cc
+++ offline/packages/trackreco/PHActsTrkFitter.cc
@@ -12,6 +12,7 @@
 
 /// Tracking includes
 #include <trackbase_historic/SvtxTrack.h>
+#include <trackbase_historic/SvtxTrackState_v1.h>
 #include <trackbase_historic/SvtxTrackMap.h>
 
 #include <fun4all/Fun4AllReturnCodes.h>
@@ -29,6 +30,7 @@
 #include <Acts/Surfaces/Surface.hpp>
 #include <Acts/EventData/MultiTrajectory.hpp>
 #include <Acts/EventData/MultiTrajectoryHelpers.hpp>
+#include <Acts/Utilities/Definitions.hpp>
 
 #include <ACTFW/EventData/Track.hpp>
 #include <ACTFW/Framework/AlgorithmContext.hpp>
@@ -255,21 +257,77 @@ void PHActsTrkFitter::updateSvtxTrack(Trajectory traj,
   const auto &[trackTips, mj] = traj.trajectory();
   /// only one track tip in the track fit Trajectory
   auto &trackTip = trackTips.front();
-  auto trajState =
-    Acts::MultiTrajectoryHelpers::trajectoryState(mj, trackTip);
 
-  const auto& params = traj.trackParameters(trackTip);
- 
   SvtxTrackMap::Iter trackIter = m_trackMap->find(trackKey);
   SvtxTrack *track = trackIter->second;
+
+  if(Verbosity() > 2)
+    {
+      std::cout << "Identify (proto) track before updating with acts results " << std::endl;
+      track->identify();
+      std::cout << " cluster keys size " << track->size_cluster_keys() << std::endl;  
+    }
+
+  track->clear_states();
+  track->clear_cluster_keys();
+
+  // create a state at pathlength = 0.0
+  // This state holds the track parametyers, which will be updated below
+  float pathlength = 0.0;
+  SvtxTrackState_v1 out( pathlength);
+  out.set_x(0.0);
+  out.set_y(0.0);
+  out.set_z(0.0);
+  track->insert_state(&out);   
+
+  // begin temporary kluge because the smoothed momenta are screwed up
+  // use first pass filter result for momentum for now
+  int istate = 0;
+  mj.visitBackwards(trackTip, [&](const auto &state) {
+      /// Only fill the track states with non-outlier measurement
+      auto typeFlags = state.typeFlags();
+      if (not typeFlags.test(Acts::TrackStateFlag::MeasurementFlag))
+	{
+	  std::cout << " failed typeFlags: " << typeFlags << std::endl;
+	}
+
+      if (state.hasFiltered())
+	{
+	  if(istate != 0) return true;
+
+	  Acts::BoundParameters parameter(
+					  m_tGeometry->geoContext,
+					  state.filteredCovariance(), state.filtered(),
+					  state.referenceSurface().getSharedPtr());
+
+	  // We take the momentum (only) from the first pass filter results
+	  track->set_px(parameter.momentum()(0));
+	  track->set_py(parameter.momentum()(1));
+	  track->set_pz(parameter.momentum()(2));
+
+	  istate++;
+	}
+      return true;
+    }
+    );
+  // end temporary kluge
+
+  auto trajState =
+    Acts::MultiTrajectoryHelpers::trajectoryState(mj, trackTip);
+ 
+  const auto& params = traj.trackParameters(trackTip);
 
   /// Acts default unit is mm. So convert to cm
   track->set_x(params.position()(0) / Acts::UnitConstants::cm);
   track->set_y(params.position()(1) / Acts::UnitConstants::cm);
   track->set_z(params.position()(2) / Acts::UnitConstants::cm);
+
+  /*
+  // See above: we get these from the first pass filter output instead for now, until the smoothing is fixed 
   track->set_px(params.momentum()(0));
   track->set_py(params.momentum()(1));
   track->set_pz(params.momentum()(2));
+  */
   track->set_chisq(trajState.chi2Sum);
   track->set_ndf(trajState.NDF);
 
@@ -287,9 +345,173 @@ void PHActsTrkFitter::updateSvtxTrack(Trajectory traj,
 	    }
 	}
     }
- 
-  return;
 
+  calculateDCA(params);
+
+  // convert from mm to cm
+  track->set_dca3d_xy(m_dca3Dxy / Acts::UnitConstants::cm);
+  track->set_dca3d_z(m_dca3Dz / Acts::UnitConstants::cm);
+  track->set_dca3d_xy_error(m_dca3DxyCov / Acts::UnitConstants::cm);
+  track->set_dca3d_z_error(m_dca3DzCov / Acts::UnitConstants::cm);
+  
+  // Also need to update the state list for all measurements associated with the acts track  
+  // loop over acts track states, copy over to SvtxTrackStates, and add to SvtxTrack
+  fillSvtxTrackStates(traj, trackTip, track);  
+
+  // Also need to update list of associated cluster keys ( HitIDClusIDActsMap on node tree) 
+
+  if(Verbosity() > 2)
+    {  
+      std::cout << " Identify fitted track after updating track states:" << std::endl;
+      track->identify();
+      std::cout << " cluster keys size " << track->size_cluster_keys() << std::endl;  
+    }
+ 
+ return;
+  
+}
+
+void PHActsTrkFitter::fillSvtxTrackStates(const Trajectory traj, const size_t &trackTip, SvtxTrack *svtx_track)
+{
+  const auto &[trackTips, mj] = traj.trajectory();
+  
+  mj.visitBackwards(trackTip, [&](const auto &state) {
+      /// Only fill the track states with non-outlier measurement
+      auto typeFlags = state.typeFlags();
+      if (not typeFlags.test(Acts::TrackStateFlag::MeasurementFlag))
+	{
+	  return true;
+	}
+      
+      auto meas = std::get<Measurement>(*state.uncalibrated());
+      
+      /// Get local position
+      Acts::Vector2D local(meas.parameters()[Acts::ParDef::eLOC_0],
+			   meas.parameters()[Acts::ParDef::eLOC_1]);
+      /// Get global position
+      Acts::Vector3D global(0, 0, 0);
+      /// This is an arbitrary vector. Doesn't matter in coordinate transformation
+      /// in Acts code
+      Acts::Vector3D mom(1, 1, 1);
+      meas.referenceSurface().localToGlobal(m_tGeometry->geoContext,
+					    local, mom, global);
+      
+      /// Get measurement covariance
+      //auto cov = meas.covariance();
+      
+      float pathlength = sqrt(global.x()*global.x() + global.y()*global.y());  // use measurement radius for now, how to get pathlength from Acts?
+      SvtxTrackState_v1 out( pathlength );
+      out.set_x(global.x() / Acts::UnitConstants::cm);
+      out.set_y(global.y() / Acts::UnitConstants::cm);
+      out.set_z(global.z() /  Acts::UnitConstants::cm);
+      
+      if (state.hasSmoothed())
+	{
+	  Acts::BoundParameters parameter(
+					  m_tGeometry->geoContext,
+					  state.smoothedCovariance(), state.smoothed(),
+					  state.referenceSurface().getSharedPtr());
+	  
+	  out.set_px(parameter.momentum().x());
+	  out.set_py(parameter.momentum().y());
+	  out.set_pz(parameter.momentum().z());
+	  
+
+	  // set covariance to zero for now
+	  for (int i = 0; i < 6; i++)
+	    {
+	      for (int j = i; j < 6; j++)
+		{ out.set_error(i, j, 0.0); }
+	    }
+	  	  
+	  const unsigned int hitId = state.uncalibrated().hitID();
+	  TrkrDefs::cluskey cluskey = getClusKey(hitId);
+	  svtx_track->insert_cluster_key(cluskey);
+	  
+	  if(Verbosity() > 2)
+	    {
+	      std::cout << " inserting state with x,y,z = " << global.x() /  Acts::UnitConstants::cm 
+			<< "  " << global.y() /  Acts::UnitConstants::cm << "  " 
+			<< global.z() /  Acts::UnitConstants::cm 
+			<< " pathlength " << pathlength
+			<< " cluskey " << cluskey
+			<< std::endl; 
+	    }
+	  
+	  svtx_track->insert_state(&out);      
+	}
+  
+      return true;      
+    }
+    );
+
+  return;
+}
+
+TrkrDefs::cluskey PHActsTrkFitter::getClusKey(const unsigned int hitID)
+{
+  TrkrDefs::cluskey clusKey = 0;
+  /// Unfortunately the map is backwards for looking up cluster key from
+  /// hit ID. So we need to iterate over it. There won't be duplicates since
+  /// the cluster key and hit id are a one-to-one map
+  std::map<TrkrDefs::cluskey, unsigned int>::iterator
+      hitIter = m_hitIdClusKey->begin();
+  while (hitIter != m_hitIdClusKey->end())
+  {
+    if (hitIter->second == hitID)
+    {
+      clusKey = hitIter->first;
+      break;
+    }
+    ++hitIter;
+  }
+
+  return clusKey;
+}
+    
+void PHActsTrkFitter::calculateDCA(const Acts::BoundParameters param)
+{
+
+  Acts::Vector3D pos = param.position();
+  Acts::Vector3D mom = param.momentum();
+  Acts::BoundSymMatrix cov = Acts::BoundSymMatrix::Zero();
+  if(param.covariance())
+    cov = param.covariance().value();
+
+  Acts::ActsSymMatrixD<3> posCov;
+  for(int i = 0; i < 3; ++i)
+    {
+      for(int j = 0; j < 3; ++j)
+	{
+	  posCov(i,j) = cov(i,j);
+	} 
+    }
+
+  Acts::Vector3D r = mom.cross(Acts::Vector3D(0.,0.,1.));
+  float phi = atan2(r(1), r(0));
+
+  Acts::RotationMatrix3D rot;
+  Acts::RotationMatrix3D rot_T;
+  rot(0,0) = cos(phi);
+  rot(0,1) = -sin(phi);
+  rot(0,2) = 0;
+  rot(1,0) = sin(phi);
+  rot(1,1) = cos(phi);
+  rot(1,2) = 0;
+  rot(2,0) = 0;
+  rot(2,1) = 0;
+  rot(2,2) = 1;
+  
+  rot_T = rot.transpose();
+
+  Acts::Vector3D pos_R = rot * pos;
+  Acts::ActsSymMatrixD<3> rotCov = rot * posCov * rot_T;
+
+  m_dca3Dxy = pos_R(0);
+  m_dca3Dz = pos_R(2);
+  m_dca3DxyCov = rotCov(0,0);
+  m_dca3DzCov = rotCov(2,2);
+  
 }
 
 int PHActsTrkFitter::createNodes(PHCompositeNode* topNode)
@@ -362,5 +584,17 @@ int PHActsTrkFitter::getNodes(PHCompositeNode* topNode)
       return Fun4AllReturnCodes::ABORTEVENT;
     }
 
+  m_hitIdClusKey = findNode::getClass<std::map<TrkrDefs::cluskey, unsigned int>>(topNode, "HitIDClusIDActsMap");
+  
+  if (!m_hitIdClusKey)
+    {
+      std::cout << PHWHERE << "No HitID:ClusKey map on node tree. Bailing."
+		<< std::endl;
+      
+      return Fun4AllReturnCodes::EVENT_OK;
+    }
+
+
   return Fun4AllReturnCodes::EVENT_OK;
 }
+
--- offline/packages/trackreco/PHActsTrkFitter.h
+++ offline/packages/trackreco/PHActsTrkFitter.h
@@ -44,7 +44,9 @@ class SvtxTrackMap;
 using SourceLink = FW::Data::TrkrClusterSourceLink;
 using FitResult = Acts::KalmanFitterResult<SourceLink>;
 using Trajectory = FW::TrkrClusterMultiTrajectory;
-
+using Measurement = Acts::Measurement<FW::Data::TrkrClusterSourceLink,
+                                      Acts::ParDef::eLOC_0,
+                                      Acts::ParDef::eLOC_1>;
 class PHActsTrkFitter : public PHTrackFitting
 {
  public:
@@ -68,6 +70,14 @@ class PHActsTrkFitter : public PHTrackFitting
   void setTimeAnalysis(bool time){m_timeAnalysis = time;}
 
  private:
+
+  // DCA 
+  void calculateDCA(const Acts::BoundParameters param);
+
+  void fillSvtxTrackStates(const Trajectory traj, const size_t &trackTip, SvtxTrack *svtx_track);
+
+  TrkrDefs::cluskey getClusKey(const unsigned int hitID);
+
   /// Event counter
   int m_event;
 
@@ -96,7 +106,16 @@ class PHActsTrkFitter : public PHTrackFitting
   /// TrackMap containing SvtxTracks
   SvtxTrackMap *m_trackMap;
 
+  // map relating acts hitid's to clusterkeys
+  std::map<TrkrDefs::cluskey, unsigned int> *m_hitIdClusKey{nullptr};
+
   int m_nBadFits;
+
+  float m_dca3Dxy;          /// fitted parameter 3D DCA in xy plane
+  float m_dca3Dz;           /// fitted parameter 3D DCA in z plane
+  float m_dca3DxyCov;       /// fitted parameter 3D DCA covariance in xy
+  float m_dca3DzCov;        /// fitted parameter 3D DCA covariance in z
+
 
   /// Variables for doing event time execution analysis
   bool m_timeAnalysis;
