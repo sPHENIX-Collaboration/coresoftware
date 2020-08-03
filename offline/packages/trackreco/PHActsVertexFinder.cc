3b233a0c601c753cac6e6806103d6e7da9145180
Joe Osborn <osbornjd@ornl.gov>
2020-08-03 10:42:58 -0400

    skeleton acts vertex finder

---
+++ offline/packages/trackreco/PHActsVertexFinder.cc
@@ -0,0 +1,25 @@
+#include "PHActsVertexFinder.h"
+
+#include <fun4all/Fun4AllReturnCodes.h>
+
+
+
+
+
+PHActsVertexFinder::PHActsVertexFinder()
+{
+
+}
+
+
+int PHActsVertexFinder::Setup(PHCompositeNode *topNode)
+{
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int PHActsVertexFinder::Process(PHCompositeNode *topNode)
+{
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
---
+++ offline/packages/trackreco/PHActsVertexFinder.h
@@ -0,0 +1,22 @@
+#ifndef TRACKRECO_PHACTSVERTEXFINDER_H
+#define TRACKRECO_PHACTSVERTEXFINDER_H
+
+#include "PHInitVertexing.h"
+
+
+
+#include <trackbase/TrkrDefs.h>
+
+class PHActsVertexFinder: public PHInitVertexing 
+{
+  
+ public:
+  PHActsVertexFinder()
+  virtual ~PHActsVertexFinder() {}
+  
+ private:
+  
+  
+};
+
+#endif // TRACKRECO_PHACTSVERTEXFINDER_H
