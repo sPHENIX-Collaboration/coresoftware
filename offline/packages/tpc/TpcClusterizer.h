39d52e6fe1257ba9df0dc1e337991ebfe8708027
bogui56 <cer@mit.edu>
2020-09-01 09:09:25 -0400

    Clusterizer Update

--- offline/packages/tpc/TpcClusterizer.h
+++ offline/packages/tpc/TpcClusterizer.h
@@ -2,16 +2,20 @@
 #define TPC_TPCCLUSTERIZER_H
 
 #include <fun4all/SubsysReco.h>
-
+#include <map> 
 #include <vector>
 #include <string>
 
 class PHCompositeNode;
+class TrkrHitSet;
 class TrkrHitSetContainer;
 class TrkrClusterContainer;
 class TrkrClusterHitAssoc;
 class TNtuple;
 class PHG4CylinderCellGeom;
+
+typedef std::pair<int, int> iphiz;
+typedef std::pair<double, iphiz> ihit;
 
 class TpcClusterizer : public SubsysReco
 {
@@ -30,6 +34,13 @@ class TpcClusterizer : public SubsysReco
   bool is_local_maximum(int phi, int z, std::vector<std::vector<double>> &adcval);
   bool is_in_sector_boundary(int phibin, int sector, PHG4CylinderCellGeom *layergeom);
   void get_cluster(int phibin, int zbin, int &phiup, int &phidown, int &zup, int &zdown, std::vector<std::vector<double>> &adcval);
+  void get_cluster2(int phibin, int zbin, std::vector<std::vector<double>> &adcval, std::vector<ihit> &ihit_list);
+  void find_z_range(int phibin, int zbin, std::vector<std::vector<double>> &adcval, int& zdown, int& zup);
+  void find_phi_range(int phibin, int zbin, std::vector<std::vector<double>> &adcval, int& phidown, int& phiup);
+  void remove_hit(double adc, int phibin, int zbin, std::multimap<double, ihit> &all_hit_map, std::vector<std::vector<double>> &adcval );
+  void remove_hits(std::vector<ihit> &ihit_list, std::multimap<double, ihit> &all_hit_map, std::vector<std::vector<double>> &adcval);
+  void calc_cluster_parameter(std::vector<ihit> &ihit_list, int iclus, PHG4CylinderCellGeom *layergeom, TrkrHitSet *hitset);
+  void print_cluster(std::vector<ihit> &ihit_list);
 
   TrkrHitSetContainer *m_hits;
   TrkrClusterContainer *m_clusterlist;
