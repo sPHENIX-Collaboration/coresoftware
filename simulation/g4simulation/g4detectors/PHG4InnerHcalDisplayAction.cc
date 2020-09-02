697a720af03922f06dbf3799ef9b3595c4dc76df
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-09-02 08:30:33 -0400

    clang-format

--- simulation/g4simulation/g4detectors/PHG4InnerHcalDisplayAction.cc
+++ simulation/g4simulation/g4detectors/PHG4InnerHcalDisplayAction.cc
@@ -17,7 +17,7 @@ PHG4InnerHcalDisplayAction::PHG4InnerHcalDisplayAction(const string &name)
 
 PHG4InnerHcalDisplayAction::~PHG4InnerHcalDisplayAction()
 {
-  for (auto &it:m_VisAttVec)
+  for (auto &it : m_VisAttVec)
   {
     delete it;
   }
@@ -27,7 +27,7 @@ PHG4InnerHcalDisplayAction::~PHG4InnerHcalDisplayAction()
 
 void PHG4InnerHcalDisplayAction::ApplyDisplayAction(G4VPhysicalVolume *physvol)
 {
-  for (auto &it:m_ScintiLogVolSet)
+  for (auto &it : m_ScintiLogVolSet)
   {
     if (it->GetVisAttributes())
     {
