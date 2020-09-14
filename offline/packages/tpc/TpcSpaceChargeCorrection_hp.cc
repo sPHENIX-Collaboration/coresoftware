c065cc74598ef44e32f9f85dcad9e9b52b1c5590
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2020-09-14 09:48:01 -0600

    renamed TPC misaligner into TpcSpaceChargeCorrection use histograms to load the corrections

---
+++ offline/packages/tpc/TpcSpaceChargeCorrection_hp.cc
@@ -0,0 +1,109 @@
+#include "TpcSpaceChargeCorrection_hp.h"
+
+#include <fun4all/Fun4AllReturnCodes.h>
+#include <phool/getClass.h>
+#include <trackbase/TrkrCluster.h>
+#include <trackbase/TrkrClusterContainer.h>
+#include <trackbase/TrkrDefs.h>
+
+#include <TFile.h>
+#include <TH3.h>
+
+#include <math.h>
+
+namespace 
+{
+  //_____________________________________________________________________
+  template<class T> constexpr T square( const T& x ) { return x*x; }
+}
+
+//_____________________________________________________________________
+TpcSpaceChargeCorrection_hp::TpcSpaceChargeCorrection_hp( const std::string& name ):
+  SubsysReco( name)
+  {}
+
+//_____________________________________________________________________
+int TpcSpaceChargeCorrection_hp::InitRun(PHCompositeNode*)
+{ 
+  
+  std::cout << "TpcSpaceChargeCorrection_hp::InitRun - reading distortions from " << m_distortion_filename << std::endl;
+  m_distortion_tfile = TFile::Open( m_distortion_filename.c_str());
+  if( !m_distortion_tfile )
+  {
+    std::cout << "TpcSpaceChargeCorrection_hp::InitRun - cannot open " << m_distortion_filename << std::endl;
+    exit(1);
+  }
+  
+  
+  // Open TH3F files only once that contain distortions due to space charge
+  hDPint= dynamic_cast<TH3*>(m_distortion_tfile->Get("hIntDistortionP")); assert( hDPint );
+  hDRint= dynamic_cast<TH3*>(m_distortion_tfile->Get("hIntDistortionR")); assert( hDRint );
+  hDZint= dynamic_cast<TH3*>(m_distortion_tfile->Get("hIntDistortionZ")); assert( hDZint );    
+
+  // dump axis limits
+  for(const auto& axis:{ hDPint->GetXaxis(), hDPint->GetYaxis(), hDPint->GetZaxis() })
+  { std::cout << "TpcSpaceChargeCorrection_hp::InitRun - axis: " << axis->GetTitle() << " bins: " << axis->GetNbins() << " limits: " << axis->GetXmin() << " " << axis->GetXmax() << std::endl; }
+  
+  return Fun4AllReturnCodes::EVENT_OK; 
+}
+
+//_____________________________________________________________________
+int TpcSpaceChargeCorrection_hp::process_event(PHCompositeNode* topNode)
+{
+  // load nodes
+  auto res =  load_nodes(topNode);
+  if( res != Fun4AllReturnCodes::EVENT_OK ) return res;
+
+  transform_clusters();
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+//_____________________________________________________________________
+int TpcSpaceChargeCorrection_hp::load_nodes( PHCompositeNode* topNode )
+{
+  // cluster map
+  _cluster_map = findNode::getClass<TrkrClusterContainer>(topNode, "TRKR_CLUSTER");
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+//_____________________________________________________________________
+void TpcSpaceChargeCorrection_hp::transform_clusters()
+{
+  if( !_cluster_map ) return;
+
+  auto range = _cluster_map->getClusters();
+  for( auto clusterIter = range.first; clusterIter != range.second; ++clusterIter )
+  {
+    // check if cluster belongs to TPC
+    const auto& key = clusterIter->first;
+    const auto trkrid = TrkrDefs::getTrkrId(key);
+    if( trkrid == TrkrDefs::tpcId )
+    { transform_cluster( clusterIter->second ); }
+  }
+
+  return;
+}
+
+//_____________________________________________________________________
+void TpcSpaceChargeCorrection_hp::transform_cluster( TrkrCluster* cluster )
+{
+  // get cluster radius, phi and z
+  const auto r = std::sqrt( square( cluster->getX() ) + square( cluster->getY() ) );
+  auto phi = std::atan2( cluster->getY(), cluster->getX() );
+  if( phi < 0 ) phi += 2*M_PI;
+
+  const auto z = cluster->getZ();
+  
+  // apply corrections 
+  const auto phi_new = phi - hDPint->Interpolate(phi,r,z);
+  const auto r_new = r - hDRint->Interpolate(phi,r,z);
+  const auto z_new = z - hDZint->Interpolate(phi,r,z);
+
+  // update cluster
+  const auto x_new = r_new*std::cos( phi_new );
+  const auto y_new = r_new*std::sin( phi_new );
+
+  cluster->setX( x_new );
+  cluster->setY( y_new );
+  cluster->setZ( z_new );
+}
---
+++ offline/packages/tpc/TpcSpaceChargeCorrection_hp.h
@@ -0,0 +1,58 @@
+#ifndef TPC_TpcSpaceChargeCorrection_hp_H
+#define G4EVAL_TRACKINGEVALUATOR_HP_H
+
+#include <fun4all/SubsysReco.h>
+#include <phool/PHObject.h>
+#include <phool/PHTimer.h>
+#include <trackbase/TrkrDefs.h>
+
+class TrkrCluster;
+class TrkrClusterContainer;
+class TFile;
+class TH3;
+
+class TpcSpaceChargeCorrection_hp : public SubsysReco
+{
+  public:
+
+  //! constructor
+  TpcSpaceChargeCorrection_hp(  const std::string& = "TpcSpaceChargeCorrection_hp" );
+
+  //! global initialization
+  virtual int InitRun(PHCompositeNode*);
+
+  //! event processing
+  virtual int process_event(PHCompositeNode*);
+
+  //! distortion filename
+  void set_distortion_filename( const std::string& value )
+  { m_distortion_filename = value; }
+
+  private:
+
+  //! load nodes
+  int load_nodes( PHCompositeNode* );
+
+  //! transform clusters
+  void transform_clusters();
+
+  //! transform clusters
+  void transform_cluster( TrkrCluster* );
+
+  //! cluster container
+  TrkrClusterContainer* _cluster_map = nullptr;
+  
+  //! space charge correction file name
+  std::string m_distortion_filename;
+  TFile *m_distortion_tfile = nullptr;
+
+  //!@name space charge distortion histograms
+  //@{
+  TH3 *hDRint = nullptr;
+  TH3 *hDPint = nullptr;
+  TH3 *hDZint = nullptr;
+  //@}
+
+};
+
+#endif
