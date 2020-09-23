4d02f30bd0ac12e56da943c24f2506bdcae77403
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-09-23 10:14:52 -0400

    implement skipping for non syncing input managers

--- generators/phhepmc/Fun4AllHepMCPileupInputManager.cc
+++ generators/phhepmc/Fun4AllHepMCPileupInputManager.cc
@@ -60,7 +60,14 @@ Fun4AllHepMCPileupInputManager::~Fun4AllHepMCPileupInputManager()
   gsl_rng_free(RandomGenerator);
 }
 
-int Fun4AllHepMCPileupInputManager::run(const int nevents)
+int Fun4AllHepMCPileupInputManager::SkipForThisManager(const int nevents)
+{
+
+  int iret = run(nevents, true);
+  return iret;
+}
+
+int Fun4AllHepMCPileupInputManager::run(const int nevents, const bool skip)
 {
   if (_first_run)
   {
@@ -145,9 +152,9 @@ int Fun4AllHepMCPileupInputManager::run(const int nevents)
         }
         else
         {
-          MySyncManager()->CurrentEvent(evt->event_number());
           if (Verbosity() > 0)
           {
+	    cout << "Fun4AllHepMCPileupInputManager::run::" << Name();
             cout << "hepmc evt no: " << evt->event_number() << endl;
           }
           events_total++;
@@ -164,7 +171,8 @@ int Fun4AllHepMCPileupInputManager::run(const int nevents)
           }
         }
       }  // loop until retrieve a valid event
-
+      if (!skip)
+      {
       PHHepMCGenEventMap *geneventmap = hepmc_helper.get_geneventmap();
       PHHepMCGenEvent *genevent = nullptr;
       if (hepmc_helper.get_embedding_id() > 0)
@@ -185,7 +193,7 @@ int Fun4AllHepMCPileupInputManager::run(const int nevents)
       hepmc_helper.move_vertex(genevent);
       // place to the crossing center in time
       genevent->moveVertex(0, 0, 0, t0);
-
+      }
     }  //    for (int icollision = 0; icollision < ncollisions; ++icollision)
 
   }  //  for (int icrossing = _min_crossing; icrossing <= _max_crossing; ++icrossing)
