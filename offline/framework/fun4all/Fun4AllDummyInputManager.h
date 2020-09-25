6458bfd022738037e127324a8a30c8dbca1deb3a
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-09-25 09:40:30 -0400

    fake up correct sync behavior for dummy input manager

--- offline/framework/fun4all/Fun4AllDummyInputManager.h
+++ offline/framework/fun4all/Fun4AllDummyInputManager.h
@@ -17,17 +17,19 @@ class Fun4AllDummyInputManager : public Fun4AllInputManager
  public:
   Fun4AllDummyInputManager(const std::string& name = "DUMMY", const std::string& nodename = "DST");
   virtual ~Fun4AllDummyInputManager() {}
-  int fileopen(const std::string& /*filename*/) { return 0; }
+  int fileopen(const std::string&) {return 0;}
   int fileclose() { return 0; }
   int isOpen() { return 1; }
   int run(const int /*nevents=0*/);
   int GetSyncObject(SyncObject** /*mastersync*/) { return Fun4AllReturnCodes::SYNC_NOOBJECT; }
   int SyncIt(const SyncObject* /*mastersync*/) { return Fun4AllReturnCodes::SYNC_OK; }
   void setSyncManager(Fun4AllSyncManager* master);
-  int PushBackEvents(const int /*nevt*/) { return 0; }
+  int PushBackEvents(const int nevt);
+  int ResetFileList(); 
 
  private:
-  int m_NumEvents;
+  int m_NumEvents = 0;
+  int m_SumEvents = 0;
 };
 
 #endif
