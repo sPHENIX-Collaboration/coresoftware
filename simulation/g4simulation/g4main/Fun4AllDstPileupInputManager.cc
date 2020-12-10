07ab98d270a93493b1f00ef28834ee7f8a95e316
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-12-10 18:22:32 -0500

    need to create dst nodes in first call to ::run, in order for the merger to be able to add missing nodes there. If done later, fun4all barks with "Someone changed the number of Output Nodes on the fly" and quits.

--- simulation/g4simulation/g4main/Fun4AllDstPileupInputManager.cc
+++ simulation/g4simulation/g4main/Fun4AllDstPileupInputManager.cc
@@ -161,6 +161,23 @@ int Fun4AllDstPileupInputManager::run(const int nevents)
     if( result != 0 ) return result;
   }
 
+
+  /* create internal dst node if not already there
+   * this usually happen in fileopen however, when the file is not oppened during first event, for instance because background rate is too low,
+   * this can cause fun4all server to bark with "Someone changed the number of Output Nodes on the fly"
+   */
+
+  if( !m_dstNode )
+  {
+    auto se = Fun4AllServer::instance();
+    m_dstNode = se->getNode(InputNode(), TopNodeName());
+  }
+
+  if (!m_dstNodeInternal)
+  {
+    m_dstNodeInternal.reset(new PHCompositeNode("DST_INTERNAL"));
+  }
+
   // create merger node
   Fun4AllDstPileupMerger merger;
   merger.load_nodes(m_dstNode);
