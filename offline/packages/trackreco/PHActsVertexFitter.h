3928296cf02313645b802e03836f3748ee3551c0
Joe Osborn <osbornjd@ornl.gov>
2020-12-18 09:44:45 -0500

    clang-format

--- offline/packages/trackreco/PHActsVertexFitter.h
+++ offline/packages/trackreco/PHActsVertexFitter.h
@@ -21,12 +21,11 @@ namespace Acts
 
 using Trajectory = ActsExamples::TrkrClusterMultiTrajectory;
 
-using BoundTrackParamVec = std::vector<const Acts::BoundTrackParameters*>;
-using VertexTrackMap = std::map<const unsigned int, 
+using BoundTrackParamVec = std::vector<const Acts::BoundTrackParameters *>;
+using VertexTrackMap = std::map<const unsigned int,
                                 BoundTrackParamVec>;
 
 using ActsVertex = const Acts::Vertex<Acts::BoundTrackParameters>;
-
 
 /**
  * This class runs the Acts vertex fitter on the final tracks. It is
@@ -36,41 +35,42 @@ using ActsVertex = const Acts::Vertex<Acts::BoundTrackParameters>;
 class PHActsVertexFitter : public SubsysReco
 {
  public:
-  PHActsVertexFitter(const std::string& name = "PHActsVertexFitter");
-  virtual ~PHActsVertexFitter(){}
+  PHActsVertexFitter(const std::string &name = "PHActsVertexFitter");
+  virtual ~PHActsVertexFitter() {}
   int process_event(PHCompositeNode *topNode);
   int Init(PHCompositeNode *topNode);
   int InitRun(PHCompositeNode *topNode);
   int ResetEvent(PHCompositeNode *topNode);
-  int End (PHCompositeNode *topNode);
+  int End(PHCompositeNode *topNode);
 
   void updateSvtxVertexMap(bool updateSvtxVertexMap)
-    { m_updateSvtxVertexMap = updateSvtxVertexMap; }
+  {
+    m_updateSvtxVertexMap = updateSvtxVertexMap;
+  }
 
  private:
-  
   int getNodes(PHCompositeNode *topNode);
   int createNodes(PHCompositeNode *topNode);
-  
+
   /// Get the tracks with their associated vertex Ids
-  VertexTrackMap getTracks();  
+  VertexTrackMap getTracks();
 
   /// Turn the SvtxTrack object into an Acts::TrackParameters object
-  const Acts::BoundTrackParameters* makeTrackParam(const SvtxTrack* track) const;
+  const Acts::BoundTrackParameters *makeTrackParam(const SvtxTrack *track) const;
 
   /// Run the Acts vertex fitter
-  ActsVertex fitVertex(BoundTrackParamVec tracks, 
-		       Acts::Logging::Level logLevel) const;
+  ActsVertex fitVertex(BoundTrackParamVec tracks,
+                       Acts::Logging::Level logLevel) const;
 
   /// Runs Acts vertex fitter
-  void fitVertices(std::vector<const Acts::BoundTrackParameters*> tracks);
-  
+  void fitVertices(std::vector<const Acts::BoundTrackParameters *> tracks);
+
   /// Update SvtxVertex or create new SvtxVertexMap
   void createActsSvtxVertex(const unsigned int,
-			    ActsVertex vertex);
+                            ActsVertex vertex);
   void updateSvtxVertex(const unsigned int,
-			ActsVertex vertex);
- 
+                        ActsVertex vertex);
+
   int m_event = 0;
 
   std::map<const unsigned int, Trajectory> *m_actsFitResults;
@@ -84,4 +84,4 @@ class PHActsVertexFitter : public SubsysReco
   bool m_updateSvtxVertexMap = false;
 };
 
-#endif //TRACKRECO_PHACTSVERTEXFITTER_H 
+#endif  //TRACKRECO_PHACTSVERTEXFITTER_H
