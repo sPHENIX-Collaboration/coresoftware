425b763273784ac2db7adfa65318c86e9702bdec
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-01-08 16:52:23 -0500

    cleanup, separate writing file from finding common segments, randomize segemtn selection enabled

--- offline/framework/frog/CreateFileList.pl
+++ offline/framework/frog/CreateFileList.pl
@@ -4,6 +4,7 @@ use DBI;
 use strict;
 use Getopt::Long;
 use Data::Dumper;
+use List::Util qw(shuffle);
 
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","argouser") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
@@ -22,16 +23,26 @@ while(my @res = $getdsttypes->fetchrow_array())
     }
 }
 
+my %proddesc = (
+"1" => "hijing MB (0-12fm)",
+"2" => "hijing central (0-4.88fm)",
+"3" => "pythia8 pp MB"
+);
+
+
 if ($#ARGV < 0)
 {
-    print "usage: CreateFileLists.pl -fm <fermi range> <filetypes>\n";
+    print "usage: CreateFileLists.pl -type <production type> <filetypes>\n";
     print "parameters:\n";
-    print "-fm : Hijing fermi range, 0-12 = 1, 0-4.88 = 2\n";
     print "-n  : <number of events>\n";
-    print "-pp : pythia8 selection: MinBias = 1\n";
+    print "-r  : randomize segments used\n";
     print "-s  : <starting segment>\n";
-#    print "-r : pick random segments until nEvents is reached\n";
-    print "\navailable types:\n";
+    print "-type : production type\n";
+    foreach my $pd (sort keys %proddesc)
+    {
+	print "    $pd : $proddesc{$pd}\n";
+    }
+    print "\navailable file types:\n";
     foreach my $tp (sort keys %dsttype)
     {
 	print "$tp\n";
@@ -41,49 +52,60 @@ if ($#ARGV < 0)
 my $nEvents;
 my $start_segment;
 my $randomize;
-my $fmrange;
-my $pptype;
-GetOptions('fm:i' =>\$fmrange, 'n=i' => \$nEvents, 'pp:i' => \$pptype, 'r' => \$randomize, 's=i' => \$start_segment);
-if (! defined $fmrange && ! defined $pptype)
+my $prodtype;
+GetOptions('type:i' =>\$prodtype, 'n:i' => \$nEvents, 'r' => \$randomize, 's:i' => \$start_segment);
+
+if (defined $randomize && ! defined $nEvents)
 {
-    print "either fermi range for hijing with -fm or pp type with -pp\n";
+    print "randomizing segments only if number of events is selected\n";
     exit(0);
 }
-if (defined $fmrange && ($fmrange < 1 || $fmrange > 2))
+
+if (! defined $prodtype )
 {
-    print "-fm $fmrange invalid, need to give valid fermi range\n";
-    print "-fm 1 : 0-12fm\n";
-    print "-fm 2 : 0-4.88fm\n";
+    print "need to give production type\n";
+    print "-type : production type\n";
+    foreach my $pd (sort keys %proddesc)
+    {
+	print "    $pd : $proddesc{$pd}\n";
+    }
     exit(0);
 }
-if (defined $pptype && ($pptype < 1 || $pptype > 1))
+if (! exists $proddesc{$prodtype})
 {
-    print "-pp pp type invalid, need to give valid pp type\n";
-    print "-pp 1 : pythia8 MinBias\n";
+    print  "invalid production type $prodtype, valid values\n";
+    print "-type : production type\n";
+    foreach my $pd (sort keys %proddesc)
+    {
+	print "    $pd : $proddesc{$pd}\n";
+    }
     exit(0);
 }
 
 my $filenamestring;
-if ($fmrange == 1)
+if ($prodtype == 1)
 {
     $filenamestring = "sHijing_0_12fm";
 }
-elsif ($fmrange == 2)
+elsif ($prodtype == 2)
 {
     $filenamestring = "sHijing_0_488fm";
 }
-elsif ($pptype == 1)
+elsif ($prodtype == 3)
 {
     $filenamestring = "pythia8_mb";
 }
 else
 {
-    print "no string for fermi range $fmrange\n";
+    print "no file substring for production type $prodtype\n";
     exit(1);
 }
 #check dst types
 my %req_types = ();
+
+# hash of {dsttype} containing hash of segment,filename
 my %allfilehash = ();
+
 my %allevthash = ();
 
 while($#ARGV >= 0)
@@ -114,6 +136,7 @@ my $getfilesql = sprintf("select filename,segment,events from datasets where %s 
 
 my $getfiles = $dbh->prepare($getfilesql);
 
+# here we fill the big hash with all segments/files for all requested filetypes
 foreach my $tp (sort keys %req_types)
 {
     my %dsthash = ();
@@ -134,8 +157,10 @@ foreach my $tp (sort keys %req_types)
     $allevthash{$tp} = \%evthash;
 }
 
-my $entries = 100000;
+my $entries = 100000; # given that we have 1000 files max, this value is always higher
 my $lowtype;
+# here we find the dst type with the smallest number of entries (segments)
+# so we do not loop too much when finding matches for the other types
 foreach my $tp (sort keys %allfilehash)
 {
     if ($entries > keys %{$allfilehash{$tp}})
@@ -144,11 +169,13 @@ foreach my $tp (sort keys %allfilehash)
 	$lowtype = $tp;
     }
 }
+# here $lowtype is the dst type with the smallest number of segments
 #print "lowest entries: $entries, type: $lowtype\n";
 #print Dumper(%allevthash);
+
+my @segarray = ();
 foreach my $seg (sort keys %{$allfilehash{$lowtype}})
 {
-    my $isgood = 1;
     foreach my $tp (sort keys %allfilehash)
     {
 	if ($tp eq $lowtype)
@@ -157,36 +184,49 @@ foreach my $seg (sort keys %{$allfilehash{$lowtype}})
 	}
 	if (! exists  $allfilehash{$tp}{$seg})
 	{
-	    $isgood = 0;
 	    last;
 	}
     }
+    push(@segarray,$seg);
+}
+# in segarray we have the common segments of all files now
 
-    if ($isgood == 1)
+# remove segments from array when number of events is reached
+# randomize segments when -r is set
+if (defined $nEvents)
+{
+    if (defined $randomize)
     {
+	@segarray = shuffle(@segarray);
+    }
+    my @tmparray = ();
+    foreach my $seg (@segarray)
+    {
+	push(@tmparray,$seg);
+	$nEvents -= $allevthash{$lowtype}{$allfilehash{$lowtype}{$seg}};
+	if ($nEvents <= 0)
+	{
+	    last;
+	}
+    }
+    @segarray = @tmparray;
+}
+# sort list of segments and write to output file
+foreach my $seg (sort @segarray)
+{
 #	print "segment $seg is good\n";
-	foreach my $tp (sort keys %allfilehash)
-	{
-#	    print "using $allfilehash{$tp}{$seg}\n";
-	    my $printcmd = sprintf("echo %s >> %s",$allfilehash{$tp}{$seg},$dsttype{$tp});
-	    system($printcmd);
-	}
-
-	if (defined $nEvents)
-	{
-#	print "events: from $allfilehash{$lowtype}{$seg}: $allevthash{$lowtype}{$allfilehash{$lowtype}{$seg}}\n";
-	    $nEvents = $nEvents - $allevthash{$lowtype}{$allfilehash{$lowtype}{$seg}};
-	    if ($nEvents <= 0)
-	    {
-		last;
-	    }
-	}
-    }
-    else
+    foreach my $tp (sort keys %allfilehash)
     {
-	print "segment $seg is bad\n";
+#	    print "using $allfilehash{$tp}{$seg}\n";
+	my $printcmd = sprintf("echo %s >> %s",$allfilehash{$tp}{$seg},$dsttype{$tp});
+	system($printcmd);
     }
 
+}
+print "wrote the following list files\n";
+foreach my $tp (sort keys %allfilehash)
+{
+    print "$dsttype{$tp}\n";
 }
 
 
