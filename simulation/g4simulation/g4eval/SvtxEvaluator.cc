e031984300874f30e141629ff3bcdcd688efda3e
bogui56 <cer@mit.edu>
2021-01-12 10:07:20 -0500

    minor update

--- simulation/g4simulation/g4eval/SvtxEvaluator.cc
+++ simulation/g4simulation/g4eval/SvtxEvaluator.cc
@@ -112,6 +112,7 @@ int SvtxEvaluator::Init(PHCompositeNode* topNode)
   _ievent = 0;
 
   _tfile = new TFile(_filename.c_str(), "RECREATE");
+  _tfile->SetCompressionLevel(0);
   if (_do_info_eval) _ntp_info = new TNtuple("ntp_info", "event info",
                                                  "event:seed:"
 					         "occ11:occ116:occ21:occ216:occ31:occ316:"
