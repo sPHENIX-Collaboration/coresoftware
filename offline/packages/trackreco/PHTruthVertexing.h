50bbbfbc930a95703d32035f9d45553826ee73b4
Joe Osborn <osbornjd@ornl.gov>
2021-01-25 13:18:15 -0500

    add setter

--- offline/packages/trackreco/PHTruthVertexing.h
+++ offline/packages/trackreco/PHTruthVertexing.h
@@ -43,7 +43,10 @@ class PHTruthVertexing : public PHInitVertexing
   {
     return _vertex_error;
   }
-
+  void set_acts_silicon(bool acts_silicon)
+  {
+    _acts_silicon = acts_silicon;
+  }
   void set_embed_only(bool embed_only)
   {
     _embed_only = embed_only;
@@ -69,7 +72,7 @@ class PHTruthVertexing : public PHInitVertexing
   std::vector<float> _vertex_error;
 
   bool _embed_only;
-  bool _acts_silicon = true;
+  bool _acts_silicon = false;
 
   gsl_rng *m_RandomGenerator;
 };
