8e942387f7127e1346d3079ecdd9a1835eca9db7
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2021-04-29 17:22:08 -0400

    - rearanged access methods for clarity - call baseclass Identify in ::Itentify to avoid duplication - mark class as final to prevent inheriting from this class. Future version have to inherit from whatever the last version of SvtxTrack_vXXX is.

--- offline/packages/trackbase_historic/SvtxTrack_FastSim_v1.h
+++ offline/packages/trackbase_historic/SvtxTrack_FastSim_v1.h
@@ -8,16 +8,16 @@
 #include "SvtxTrack_FastSim.h"
 
 // SvtxTrack_FastSim with recording of associated G4hits
-class SvtxTrack_FastSim_v1 : public SvtxTrack_FastSim
+class SvtxTrack_FastSim_v1 final: public SvtxTrack_FastSim
 {
  public:
-  
+
   //* constructor
-  SvtxTrack_FastSim_v1() {}
-  
+  SvtxTrack_FastSim_v1() = default;
+
   //* base class copy constructor
   SvtxTrack_FastSim_v1( const SvtxTrack& );
-  
+
   //* destructor
   virtual ~SvtxTrack_FastSim_v1() = default;
 
@@ -25,29 +25,71 @@ class SvtxTrack_FastSim_v1 : public SvtxTrack_FastSim
   virtual void CopyFrom( const SvtxTrack& );
   virtual void CopyFrom( SvtxTrack* source )
   { CopyFrom( *source ); }
-  
+
   // The "standard PHObject response" functions...
   void identify(std::ostream& os = std::cout) const;
-  void Reset() { *this = SvtxTrack_FastSim_v1(); }
+  void Reset() 
+  { *this = SvtxTrack_FastSim_v1(); }
+  
   int isValid() const;
-  PHObject* CloneMe() const { return new SvtxTrack_FastSim_v1(*this); }
+  
+  PHObject* CloneMe() const 
+  { return new SvtxTrack_FastSim_v1(*this); }
 
-  bool empty_g4hit_id() const { return _g4hit_ids.empty(); }
-  size_t size_g4hit_id() const { return _g4hit_ids.size(); }
-  void add_g4hit_id(int volume, PHG4HitDefs::keytype id) { _g4hit_ids[volume].insert(id); }
-  SvtxTrack::HitIdIter begin_g4hit_id() { return _g4hit_ids.begin(); }
-  SvtxTrack::HitIdConstIter begin_g4hit_id() const { return _g4hit_ids.begin(); }
-  SvtxTrack::HitIdIter find_g4hit_id(int volume) { return _g4hit_ids.find(volume); }
-  SvtxTrack::HitIdConstIter find_g4hit_id(int volume) const { return _g4hit_ids.find(volume); }
-  SvtxTrack::HitIdIter end_g4hit_id() { return _g4hit_ids.end(); }
-  SvtxTrack::HitIdConstIter end_g4hit_id() const { return _g4hit_ids.end(); }
-  size_t remove_g4hit_id(int volume, PHG4HitDefs::keytype id) { return _g4hit_ids[volume].erase(id); }
-  size_t remove_g4hit_volume(int volume) { return _g4hit_ids.erase(volume); }
-  void clear_g4hit_id() { return _g4hit_ids.clear(); }
-  const HitIdMap& g4hit_ids() const { return _g4hit_ids; }
+
+  //!@name accessors
+  //@{
+
+  const HitIdMap& g4hit_ids() const
+  { return _g4hit_ids; }
+
+  bool empty_g4hit_id() const
+  { return _g4hit_ids.empty(); }
+
+  size_t size_g4hit_id() const
+  { return _g4hit_ids.size(); }
+
+  SvtxTrack::HitIdConstIter begin_g4hit_id() const
+  { return _g4hit_ids.begin(); }
+
+  SvtxTrack::HitIdConstIter end_g4hit_id() const
+  { return _g4hit_ids.end(); }
+
+  SvtxTrack::HitIdConstIter find_g4hit_id(int volume) const
+  { return _g4hit_ids.find(volume); }
+
+  //@}
+
+
+  //!@name modifiers
+  //@{
+
+  void add_g4hit_id(int volume, PHG4HitDefs::keytype id)
+  { _g4hit_ids[volume].insert(id); }
+
+  size_t remove_g4hit_id(int volume, PHG4HitDefs::keytype id)
+  { return _g4hit_ids[volume].erase(id); }
+
+  size_t remove_g4hit_volume(int volume)
+  { return _g4hit_ids.erase(volume); }
+
+  SvtxTrack::HitIdIter begin_g4hit_id()
+  { return _g4hit_ids.begin(); }
+
+  SvtxTrack::HitIdIter end_g4hit_id()
+  { return _g4hit_ids.end(); }
+
+  SvtxTrack::HitIdIter find_g4hit_id(int volume)
+  { return _g4hit_ids.find(volume); }
+
+  void clear_g4hit_id()
+  { return _g4hit_ids.clear(); }
+
+  //@}
 
  private:
-  HitIdMap _g4hit_ids;  //< contained hit ids
+
+  HitIdMap _g4hit_ids;
 
   ClassDef(SvtxTrack_FastSim_v1, 1)
 };
