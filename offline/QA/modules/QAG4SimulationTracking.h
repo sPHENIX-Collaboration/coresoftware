cb81fdd34ece10c49d04d35b671b8a133227367c
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2021-05-01 09:45:03 -0400

    also loop over Micromegas G4Hits when looking for clusters/layers/truth track.

--- offline/QA/modules/QAG4SimulationTracking.h
+++ offline/QA/modules/QAG4SimulationTracking.h
@@ -82,6 +82,7 @@ class QAG4SimulationTracking : public SubsysReco
   PHG4HitContainer *m_g4hits_tpc = nullptr;
   PHG4HitContainer *m_g4hits_intt = nullptr;
   PHG4HitContainer *m_g4hits_mvtx = nullptr;
+  PHG4HitContainer *m_g4hits_micromegas = nullptr;
 };
 
 #endif  // QA_QAG4SimulationTracking_H
