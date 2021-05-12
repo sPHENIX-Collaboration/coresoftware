719f1a279933bdd4464361bdb8c2163d654f4fc0
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2021-05-12 00:34:52 -0400

    - removed using namespace std - removed hard coded micromegas first layer and layer radii. Use micromegas cylindergeomcontainer instead - use convenient square and get_r methods rather than pow(x,2) - prepare modifications needed to accomodate new geometry

--- offline/packages/trackreco/PHMicromegasTpcTrackMatching.h
+++ offline/packages/trackreco/PHMicromegasTpcTrackMatching.h
@@ -9,6 +9,7 @@
 #include <vector>
 
 class PHCompositeNode;
+class PHG4CylinderGeomContainer;
 class SvtxTrack;
 class TrkrCluster;
 class TF1;
@@ -16,11 +17,10 @@ class TrkrHitSetContainer;
 
 class PHMicromegasTpcTrackMatching : public PHTrackPropagating
 {
- public:
-
+  
+  public:
   PHMicromegasTpcTrackMatching(const std::string &name = "PHMicromegasTpcTrackMatching");
-
-  virtual ~PHMicromegasTpcTrackMatching();
+  virtual ~PHMicromegasTpcTrackMatching() = default;
 
   void set_rphi_search_window_lyr1(const double win){_rphi_search_win[0] = win;}
   void set_z_search_window_lyr1(const double win){_z_search_win[0] = win;}
@@ -31,37 +31,33 @@ class PHMicromegasTpcTrackMatching : public PHTrackPropagating
   void set_sc_calib_mode(const bool mode){_sc_calib_mode = mode;}
   void set_collision_rate(const double rate){_collision_rate = rate;}
 
- protected:
+  protected:
   int Setup(PHCompositeNode* topNode) override;
-
   int Process() override;
-
   int End() override;
   
  private:
 
   int GetNodes(PHCompositeNode* topNode);
 
-   void CircleFitByTaubin (std::vector<TrkrCluster*> clusters, double &R, double &X0, double &Y0);
+  void CircleFitByTaubin (std::vector<TrkrCluster*> clusters, double &R, double &X0, double &Y0);
   void circle_circle_intersection(double r1, double r2, double x2, double y2, double &xplus, double &yplus, double &xminus, double &yminus);
-  void  line_fit(std::vector<TrkrCluster*> clusters, double &a, double &b);
-
-  unsigned int _n_mm_layers = 2;
+  void line_fit(std::vector<TrkrCluster*> clusters, double &a, double &b);
   
   // default values, can be replaced from the macro, all in cm
   // rhese correspond to the "baseline" configuration tiles
   double _rphi_search_win[2] = {0.25, 13.0}; 
   double _z_search_win[2] = {26.0, 0.25};
 
+  /// micromegas layer radii
+  /** these are reset in ::Setup using actual micromegas geometry */
   double _mm_layer_radius[2] = { 82.2565, 82.6998};
-
-  double _z_proj[2]={0,0}; 
-  double _y_proj[2]={0,0}; 
-  double _x_proj[2]={0,0}; 
-  double _rphi_proj[2]={0,0};
 
   // range of TPC layers to use in projection to micromegas
   unsigned int _min_tpc_layer = 38;
+  
+  /// first micromegas layer
+  /** it is reset in ::Setup using actual micromegas geometry */
   unsigned int _min_mm_layer = 55;
 
   bool _test_windows = false;   // true for testing only
@@ -72,11 +68,17 @@ class PHMicromegasTpcTrackMatching : public PHTrackPropagating
 
   int _event = -1;
   
+  //! micomegas geometry
+  PHG4CylinderGeomContainer* _geomContainerMicromegas = nullptr;
+
+  //! current track
   SvtxTrack *_tracklet_tpc{nullptr};
 
   TF1 *fdrphi{nullptr};
   double _par0 = -0.36619;
   double _par1 = 0.00375714;
+  
+  //! hitset container
   TrkrHitSetContainer  *_mm_hitsets = nullptr;
 };
 
