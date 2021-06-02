44e5e3278ca54df13724fc52a1b2ee588226ecd5
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2021-06-02 16:06:44 -0400

    added code to perform matrix inversion independently from sc reconstruction module this will - ensure that the same inversion code is used for genfit and acts. - allow to aggregate the matrices from several reconstruction jobs before inversion

---
+++ offline/packages/tpccalib/TpcSpaceChargeMatrixInversion.h
@@ -0,0 +1,70 @@
+#ifndef TPCCALIB_TPCSPACECHARGEMATRIXINVERSION_H
+#define TPCCALIB_TPCSPACECHARGEMATRIXINVERSION_H
+/**
+ * \file TpcSpaceChargeMatrixInversion.h
+ * \brief aggregate space charge reconstruction matrices from several jobs and perform the inversion
+ * \author Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
+ */
+#include <fun4all/Fun4AllBase.h>
+
+#include <TString.h>
+
+#include <memory>
+#include <vector>
+
+// forward declaration
+class SvtxTrack;
+class SvtxTrackMap;
+class TH3;
+class TpcSpaceChargeMatrixContainer;
+class TrkrCluster;
+class TrkrClusterContainer;
+
+/**
+ * \class TpcSpaceChargeMatrixInversion
+ * \brief performs space charge distortion reconstruction using tracks
+ */
+
+class TpcSpaceChargeMatrixInversion: public Fun4AllBase
+{
+  public:
+
+  /// constructor
+  TpcSpaceChargeMatrixInversion( const std::string& = "TPCSPACECHARGEMATRIXINVERSION" );
+
+  ///@name modifiers
+  //@{
+
+  /// set whether to use only tracks with micromegas or not
+  void set_use_micromegas( bool value )
+  { m_use_micromegas = value; }
+
+  /// output file
+  /**
+   * this is the file where space charge correction 3D histograms are stored
+   * they are suitable for being read by TpcClusterizer
+   */
+  void set_outputfile( const std::string& filename );
+
+  /// add space charge correction matrix to current
+  void add( const TpcSpaceChargeMatrixContainer& );
+
+  /// calculate distortions by inverting stored matrices, and save relevant histograms
+  void calculate_distortions();
+
+  //@}
+
+  private:
+
+  /// output file
+  std::string m_outputfile = "DistortionCorrections.root";
+
+  /// true if only tracks with micromegas must be used
+  bool m_use_micromegas = true;
+
+  /// matrix container
+  std::unique_ptr<TpcSpaceChargeMatrixContainer> m_matrix_container;
+
+};
+
+#endif
