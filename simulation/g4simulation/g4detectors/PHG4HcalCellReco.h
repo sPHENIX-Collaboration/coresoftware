b26bb5f7adae5ca944349bba20a1f426e238854f
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-07-07 15:07:16 -0400

    increase column dim to 27 to cover ecce inner hcal

--- simulation/g4simulation/g4detectors/PHG4HcalCellReco.h
+++ simulation/g4simulation/g4detectors/PHG4HcalCellReco.h
@@ -7,6 +7,7 @@
 
 #include <fun4all/SubsysReco.h>
 
+#include <cmath>
 #include <string>
 
 class PHCompositeNode;
@@ -14,37 +15,36 @@ class PHCompositeNode;
 class PHG4HcalCellReco : public SubsysReco, public PHParameterInterface
 {
  public:
-
   PHG4HcalCellReco(const std::string &name = "HcalCellReco");
 
-  ~PHG4HcalCellReco() override{}
-  
+  ~PHG4HcalCellReco() override {}
+
   //! module initialization
   int InitRun(PHCompositeNode *topNode) override;
-  
-    //! event processing
+
+  //! event processing
   int process_event(PHCompositeNode *topNode) override;
-  
+
   //! end of process
   int End(PHCompositeNode *topNode) override;
-  
+
   void SetDefaultParameters() override;
 
-  void Detector(const std::string &d) {detector = d;}
-  void checkenergy(const int i=1) {chkenergyconservation = i;}
+  void Detector(const std::string &d) { detector = d; }
+  void checkenergy(const int i = 1) { chkenergyconservation = i; }
 
-  void   set_timing_window(const double tmi, const double tma);
-  
+  void set_timing_window(const double tmi, const double tma);
+
  protected:
   int CheckEnergy(PHCompositeNode *topNode);
   std::string detector;
   std::string hitnodename;
   std::string cellnodename;
 
-  int chkenergyconservation;
+  int chkenergyconservation = 0;
 
-  double tmin;
-  double tmax;
+  double tmin = NAN;
+  double tmax = NAN;
 };
 
 #endif
