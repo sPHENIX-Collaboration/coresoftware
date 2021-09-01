49a14cc99bd87bc971d315648c32a4e7ed5a82f4
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-09-01 16:54:49 -0400

    use -Wextra for g4intt

--- simulation/g4simulation/g4mvtx/PHG4MvtxDisplayAction.cc
+++ simulation/g4simulation/g4mvtx/PHG4MvtxDisplayAction.cc
@@ -24,7 +24,7 @@ PHG4MvtxDisplayAction::~PHG4MvtxDisplayAction()
   m_VisAttVec.clear();
 }
 
-void PHG4MvtxDisplayAction::ApplyDisplayAction(G4VPhysicalVolume *physvol)
+void PHG4MvtxDisplayAction::ApplyDisplayAction(G4VPhysicalVolume */*physvol*/)
 {
   // check if vis attributes exist, if so someone else has set them and we do nothing
   for (auto it : m_LogicalVolumeMap)
