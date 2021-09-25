34a55ce7f5f4e7d5ebffd95cd83729e545b66c64
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-09-25 10:37:31 -0400

    PHG4EPDSteppingAction.h no changes in result

--- simulation/g4simulation/g4epd/PHG4EPDSteppingAction.h
+++ simulation/g4simulation/g4epd/PHG4EPDSteppingAction.h
@@ -14,25 +14,29 @@ class PHCompositeNode;
 class PHG4EPDDetector;
 class PHG4Hit;
 class PHG4HitContainer;
+class PHParameters;
 class PHParametersContainer;
 
 class PHG4EPDSteppingAction : public PHG4SteppingAction
 {
  public:
-  PHG4EPDSteppingAction(PHG4EPDDetector*, PHParametersContainer const*);
+   PHG4EPDSteppingAction(PHG4EPDDetector*, PHParametersContainer const*);
+//  PHG4EPDSteppingAction(PHG4EPDDetector*, const PHParameters* parameters);
   ~PHG4EPDSteppingAction() override;
 
   bool UserSteppingAction(const G4Step*, bool) override;
 
   void SetInterfacePointers(PHCompositeNode*) override;
 
+  // void SetHitNodeName(const std::string& type, const std::string& name) override;
  private:
-  PHG4EPDDetector* m_detector;
+  PHG4EPDDetector* m_detector = nullptr;
 
-  PHG4HitContainer* m_hit_container;
-  PHG4Hit* m_hit;
-
+  PHG4HitContainer* m_hit_container = nullptr;
+  PHG4Hit* m_hit = nullptr;
   int32_t poststatus;
+  std::string m_HitNodeName;
+  std::string m_SupportNodeName;
 };
 
 #endif /* G4EPD_PHG4EPSTEPPINGACTION_H */
