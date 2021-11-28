be17c101a0f26942ce80770b7d6676c1f41782da
dvperepelitsa <dvp@bnl.gov>
2021-11-28 00:21:51 -0500

    Read in centrality calibrations from file and determine centiles

--- simulation/g4simulation/g4centrality/PHG4CentralityReco.h
+++ simulation/g4simulation/g4centrality/PHG4CentralityReco.h
@@ -8,6 +8,7 @@
 //===========================================================
 
 #include <fun4all/SubsysReco.h>
+#include <phparameter/PHParameters.h>
 
 #include <cmath>
 #include <string>
@@ -24,17 +25,38 @@ class PHG4CentralityReco : public SubsysReco
   int process_event(PHCompositeNode *topNode) override;
   int End(PHCompositeNode *topNode) override;
 
+  void DoCentralityCalibration(bool do_centrality_calibration ) {
+    _do_centrality_calibration = do_centrality_calibration;
+  }
+
+  PHParameters& GetCalibrationParameters() {
+    return  _centrality_calibration_params;
+  }
+  
  private:
   void CreateNode(PHCompositeNode *topNode);
   void FillNode(PHCompositeNode *topNode);
 
+  PHParameters _centrality_calibration_params;
+
+  bool _do_centrality_calibration = true;
+
+  std::map<float,int> _cent_cal_bimp;
+  std::map<float,int> _cent_cal_epd;
+  std::map<float,int> _cent_cal_mbd;
+
+  float _bimp = NAN;
+  float _bimp_cent = NAN;
+
   float _epd_N = NAN;
   float _epd_S = NAN;
   float _epd_NS = NAN;
+  float _epd_cent = NAN;
 
   float _mbd_N = NAN;
   float _mbd_S = NAN;
   float _mbd_NS = NAN;
+  float _mbd_cent = NAN;
 };
 
 #endif
