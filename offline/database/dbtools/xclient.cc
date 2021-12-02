d4ac85ea0d3c7a563a7a56c455cb846770ec5d27
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-12-02 14:49:04 -0500

    update for xpload 0.5.0

--- offline/database/dbtools/xclient.cc
+++ offline/database/dbtools/xclient.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 #include <string>
@@ -5,15 +6,30 @@
 
 #include <xpload/xpload.h>
 
+using namespace std;
 
-int main()
+class ArgParser
 {
-  using namespace std;
+ public:
+  ArgParser(int &argc, char **argv);
+  string get_value(const string &option) const;
+  int verify() const;
+ private:
+   vector<string> args;
+};
 
-  string tag = "example_tag_1";
-  uint64_t timestamp = 9999999999;
 
-  vector<string> paths = xpload::fetch(tag, timestamp);
+int main(int argc, char **argv)
+{
+  ArgParser arg_parser(argc, argv);
+
+  string cfg = arg_parser.get_value("-c");
+  string tag = arg_parser.get_value("-t");
+  uint64_t timestamp = std::stoul(arg_parser.get_value("-s"));
+
+  xpload::Configurator config(cfg);
+
+  vector<string> paths = xpload::fetch(tag, timestamp, config);
 
   if (paths.empty())
   {
@@ -29,3 +45,28 @@ int main()
 
   return EXIT_SUCCESS;
 } 
+
+
+ArgParser::ArgParser(int &argc, char **argv)
+{
+  for (int i=1; i < argc; ++i)
+    args.push_back( string(argv[i]) );
+}
+
+string ArgParser::get_value(const string &option) const
+{
+  auto itr = find(args.begin(), args.end(), option);
+
+  string value{""};
+
+  if (itr != args.end() && ++itr != args.end()) {
+    value = *itr;
+  }
+
+  // Default values
+  if (value.empty() && option == "-c") return "test";
+  if (value.empty() && option == "-t") return "example_tag_1";
+  if (value.empty() && option == "-s") return "9999999999";
+
+  return value;
+}
