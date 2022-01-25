dc85d5caa998569283178d5112fabefdcce168c1
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2022-01-25 16:28:29 -0500

    removed some commented lines

--- offline/packages/trackreco/MakeActsGeometry.cc
+++ offline/packages/trackreco/MakeActsGeometry.cc
@@ -555,11 +555,9 @@ void MakeActsGeometry::unpackVolumes()
   if(m_buildMMs)
   {
     // micromegas
-    //std::cout << "Before: m_clusterSurfaceMapMm size    " << m_clusterSurfaceMapMmEdit.size() << std::endl;
     auto mmBarrel = find_volume_by_name( vol, "MICROMEGAS::Barrel" );
     assert( mmBarrel );
     makeMmMapPairs(mmBarrel);
-    //std::cout << "After: m_clusterSurfaceMapMm size    " << m_clusterSurfaceMapMmEdit.size() << std::endl;
   }
   
   {
@@ -652,7 +650,7 @@ void MakeActsGeometry::makeTpcMapPairs(TrackingVolumePtr &tpcVolume)
 //____________________________________________________________________________________________
 void MakeActsGeometry::makeMmMapPairs(TrackingVolumePtr &mmVolume)
 {
-  if(Verbosity() > 10)
+  // if(Verbosity() > 10)
   { std::cout << "MakeActsGeometry::makeMmMapPairs - mmVolume: " << mmVolume->volumeName() << std::endl; }
   const auto mmLayerArray = mmVolume->confinedLayers();
   const auto mmLayerVector = mmLayerArray->arrayObjects();
