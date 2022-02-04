141faac4287227fd66b3259d3b7da61f34d7f8b5
jtaou <ousphenix@gmail.com>
2022-02-04 18:00:27 -0500

    initial changes for simple dbfile mode rawtowercalib: dbfileaccessor

---
+++ calibrations/dbfile_accessor_calo/HcalCaloCalibSimpleCorrFilev1.h
@@ -0,0 +1,47 @@
+#ifndef CALOCALIB_HCALSIMPLECORRFILEv1_H
+#define CALOCALIB_HCALSIMPLECORRFILEv1_H
+
+#include "CaloCalibSimpleCorrFile.h"
+
+#include "calobase/RawTowerDefs.h"
+
+#include <cstddef>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <string>
+
+class HcalCaloCalibSimpleCorrFilev1 : public CaloCalibSimpleCorrFile
+{
+ public:
+  HcalCaloCalibSimpleCorrFilev1(){}
+  ~HcalCaloCalibSimpleCorrFilev1(){}
+
+  /*
+  void Reset();
+  int isValid() const override;
+  void identify(std::ostream& os = std::cout) const override;
+  */
+
+  void Open(const char * ) override;
+  void View() override;
+  void ViewReadable() override;
+  
+  float getCorr(const unsigned int ieta, const unsigned int iphi) override;
+
+
+  ConstIterator AddCorr(const unsigned int ieta, const unsigned int iphi, float corr);
+  void set_CalibrationFileName(const char * inFileName) {m_CalibrationFileName = inFileName;}
+
+  
+
+ protected:
+  //  std::array< std::array<double,64>, 24> m_RecalArray;
+  // use towerid  map instead of 2d array
+
+  std::string m_CalibrationFileName;
+//    ClassDefOverride(HcalCaloCalibSimpleCorrFilev1, 2);
+
+};
+
+#endif
