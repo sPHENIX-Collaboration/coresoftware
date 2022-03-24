0a0a0b4b5636acdc4ab6632f173200824eadee6b
Chris Pinkenburg <pinkenburg@bnl.gov>
2022-03-24 19:46:32 -0400

    cleanup

--- offline/framework/ffarawmodules/EventCombiner.cc
+++ offline/framework/ffarawmodules/EventCombiner.cc
@@ -16,15 +16,7 @@
 //____________________________________________________________________________..
 EventCombiner::EventCombiner(const std::string &name):
  SubsysReco(name)
-{
-  std::cout << "EventCombiner::EventCombiner(const std::string &name) Calling ctor" << std::endl;
-}
-
-//____________________________________________________________________________..
-EventCombiner::~EventCombiner()
-{
-  std::cout << "EventCombiner::~EventCombiner() Calling dtor" << std::endl;
-}
+{}
 
 //____________________________________________________________________________..
 int EventCombiner::Init(PHCompositeNode *topNode)
@@ -43,24 +35,15 @@ int EventCombiner::Init(PHCompositeNode *topNode)
 }
 
 //____________________________________________________________________________..
-int EventCombiner::InitRun(PHCompositeNode *topNode)
-{
-  std::cout << "EventCombiner::InitRun(PHCompositeNode *topNode) Initializing for Run XXX" << std::endl;
-  return Fun4AllReturnCodes::EVENT_OK;
-}
-
-//____________________________________________________________________________..
 int EventCombiner::process_event(PHCompositeNode *topNode)
 {
   std::vector<Event *> subeventeventvec;
   unsigned int total_length = 0;
   for (auto &nam : m_PrdfInputNodeNameSet)
   {
-//    std::cout << "name : " << nam << std::endl;
     Event *evt = findNode::getClass<Event>(topNode,nam);
     subeventeventvec.push_back(evt);
     total_length+= evt->getEvtLength();
-//    std::cout << nam << " run number " <<  evt->getRunNumber() << " event no: " <<  evt->getEvtSequence() << std::endl;
   }
 // safety belts
   int eventno = subeventeventvec[0]->getEvtSequence();
@@ -72,37 +55,32 @@ int EventCombiner::process_event(PHCompositeNode *topNode)
 		<< " current subevt: " << e->getEvtSequence() << std::endl;
     }
   }
+
 // lets copy them all together
   int nwout;
   int current = 0;
-  std::cout << "total length: " <<  total_length << std::endl;
   m_OutArray = new int[total_length];
+// the first Copy is without "DATA", "DATA" skips the first few words which contain event number, length, etc
   subeventeventvec[0]->Copy( m_OutArray , total_length , &nwout);
   current = nwout;
   for (unsigned int icnt = 1; icnt < subeventeventvec.size(); icnt++)
   {
     subeventeventvec[icnt]->Copy(&m_OutArray[current], total_length-current, &nwout, "DATA");
-    std::cout << "current " << current << " total length-current: " <<  total_length-current << ", nwout: " << nwout << std::endl;
     current += nwout;
     m_OutArray[0] +=  nwout;
   }  
+
   PHNodeIterator iter(topNode);
-PHDataNode<Event> *PrdfNode = dynamic_cast<PHDataNode<Event> *>(iter.findFirst("PHDataNode", m_PrdfOutputNodeName));
+  PHDataNode<Event> *PrdfNode = dynamic_cast<PHDataNode<Event> *>(iter.findFirst("PHDataNode", m_PrdfOutputNodeName));
 
-std::cout << "Looking at prdfnode 0x" << std::hex << PrdfNode << std::dec << std::endl;
-
-    m_Event = new oncsEvent(m_OutArray);
-PrdfNode->setData(m_Event);
-m_Event->identify();
-std::cout << "fetching event from tree" << std::endl;
-  Event *evts = findNode::getClass<Event>(topNode,m_PrdfOutputNodeName);
-  evts->identify();
-  std::cout << "evt at " << std::hex << evts << std::dec << std::endl;
-
+  m_Event = new oncsEvent(m_OutArray);
+  PrdfNode->setData(m_Event);
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 //____________________________________________________________________________..
+// the events need to be cleaned up outside of the framework
+// in principal this is a prdf input manager which has to clear the data it puts on the tree
 int EventCombiner::ResetEvent(PHCompositeNode *topNode)
 {
   PHNodeIterator iter(topNode);
@@ -112,33 +90,6 @@ int EventCombiner::ResetEvent(PHCompositeNode *topNode)
   m_Event = nullptr;
   delete [] m_OutArray;
   return Fun4AllReturnCodes::EVENT_OK;
-}
-
-//____________________________________________________________________________..
-int EventCombiner::EndRun(const int runnumber)
-{
-  std::cout << "EventCombiner::EndRun(const int runnumber) Ending Run for Run " << runnumber << std::endl;
-  return Fun4AllReturnCodes::EVENT_OK;
-}
-
-//____________________________________________________________________________..
-int EventCombiner::End(PHCompositeNode *topNode)
-{
-  std::cout << "EventCombiner::End(PHCompositeNode *topNode) This is the End..." << std::endl;
-  return Fun4AllReturnCodes::EVENT_OK;
-}
-
-//____________________________________________________________________________..
-int EventCombiner::Reset(PHCompositeNode *topNode)
-{
- std::cout << "EventCombiner::Reset(PHCompositeNode *topNode) being Reset" << std::endl;
-  return Fun4AllReturnCodes::EVENT_OK;
-}
-
-//____________________________________________________________________________..
-void EventCombiner::Print(const std::string &what) const
-{
-  std::cout << "EventCombiner::Print(const std::string &what) const Printing info for " << what << std::endl;
 }
 
 void EventCombiner::AddPrdfInputNodeName(const std::string &name)
--- offline/framework/ffarawmodules/EventCombiner.h
+++ offline/framework/ffarawmodules/EventCombiner.h
@@ -18,40 +18,13 @@ class EventCombiner : public SubsysReco
 
   EventCombiner(const std::string &name = "EventCombiner");
 
-  virtual ~EventCombiner();
+  ~EventCombiner() override {}
 
-  /** Called during initialization.
-      Typically this is where you can book histograms, and e.g.
-      register them to Fun4AllServer (so they can be output to file
-      using Fun4AllServer::dumpHistos() method).
-   */
   int Init(PHCompositeNode *topNode) override;
 
-  /** Called for first event when run number is known.
-      Typically this is where you may want to fetch data from
-      database, because you know the run number. A place
-      to book histograms which have to know the run number.
-   */
-  int InitRun(PHCompositeNode *topNode) override;
-
-  /** Called for each event.
-      This is where you do the real work.
-   */
   int process_event(PHCompositeNode *topNode) override;
 
-  /// Clean up internals after each event.
   int ResetEvent(PHCompositeNode *topNode) override;
-
-  /// Called at the end of each run.
-  int EndRun(const int runnumber) override;
-
-  /// Called at the end of all processing.
-  int End(PHCompositeNode *topNode) override;
-
-  /// Reset
-  int Reset(PHCompositeNode * /*topNode*/) override;
-
-  void Print(const std::string &what = "ALL") const override;
 
   void AddPrdfInputNodeFromManager(const Fun4AllInputManager *in); 
   void AddPrdfInputNodeName(const std::string &name);
