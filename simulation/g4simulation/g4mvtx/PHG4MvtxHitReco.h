f9c54094444899bc1d1c62a5f91688e22cfa92dd
cdean-github <cameron.dean@cern.ch>
2022-03-25 17:45:22 -0400

    First version of strobed MVTX

--- simulation/g4simulation/g4mvtx/PHG4MvtxHitReco.h
+++ simulation/g4simulation/g4mvtx/PHG4MvtxHitReco.h
@@ -5,6 +5,8 @@
 
 #include <fun4all/SubsysReco.h>
 
+#include <gsl/gsl_rng.h>
+
 #include <map>
 #include <string>
 #include <utility>  // for pair
@@ -16,7 +18,7 @@ class PHG4MvtxHitReco : public SubsysReco, public PHParameterContainerInterface
  public:
   explicit PHG4MvtxHitReco(const std::string &name = "PHG4MvtxRECO");
 
-  ~PHG4MvtxHitReco() override {}
+  ~PHG4MvtxHitReco();
 
   //! module initialization
   int InitRun(PHCompositeNode *topNode) override;
@@ -28,7 +30,11 @@ class PHG4MvtxHitReco : public SubsysReco, public PHParameterContainerInterface
 
   double get_timing_window_min(const int i) { return tmin_max[i].first; }
   double get_timing_window_max(const int i) { return tmin_max[i].second; }
+  std::pair<double, double> generate_alpide_pulse(const double energy_deposited);
+  double generate_strobe();
   void set_timing_window(const int detid, const double tmin, const double tmax);
+  void use_strobed_mode(bool use) {m_use_strobe = use;}
+  void set_strobe_properties(double width, double separation) {m_strobe_width = width; m_strobe_separation = separation;}
 
   void SetDefaultParameters() override;
 
@@ -37,6 +43,12 @@ class PHG4MvtxHitReco : public SubsysReco, public PHParameterContainerInterface
   std::string hitnodename;
   std::string geonodename;
   std::map<int, std::pair<double, double> > tmin_max;
+  bool m_use_strobe = false;
+  double m_strobe_width = 5.;
+  double m_strobe_separation = 0.;
+
+private:
+  gsl_rng *m_RandomGenerator = nullptr;
 };
 
 #endif
