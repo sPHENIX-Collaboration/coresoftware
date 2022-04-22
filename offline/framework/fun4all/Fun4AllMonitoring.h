ea34b264ddf26e45b07090a635e7fbb14eaead8b
Chris Pinkenburg <pinkenburg@bnl.gov>
2022-04-22 12:03:42 -0400

    add new heap memory monitoring

---
+++ offline/framework/fun4all/Fun4AllMonitoring.h
@@ -0,0 +1,39 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef FUN4ALL_FUN4ALLMONITORING_H
+#define FUN4ALL_FUN4ALLMONITORING_H
+
+#include "Fun4AllBase.h"
+
+#include <map>
+#include <string>
+#include <tuple>
+#include <vector>
+
+class Fun4AllMonitoring : public Fun4AllBase
+{
+ public:
+  static Fun4AllMonitoring *instance()
+  {
+    if (mInstance) return mInstance;
+    mInstance = new Fun4AllMonitoring();
+    return mInstance;
+  }
+  ~Fun4AllMonitoring() override;
+  void Snapshot(const std::string &what = "AfterProcessEvent");
+
+  void PrintsMaps() const;
+
+  void Get_Memory();
+  void OutFileName(const std::string &fname);
+
+ private:
+  Fun4AllMonitoring();
+  static Fun4AllMonitoring *mInstance;
+  uint64_t mEvent = 0;
+  uint64_t mHeapPss = 0;
+  uint64_t mOtherPss = 0;
+  std::string mOutFileName;
+};
+
+#endif
