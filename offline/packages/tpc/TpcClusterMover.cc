3fd3470e683abb113618db87a0373cebdfc9f68c
Joe Osborn <osbornjd@ornl.gov>
2022-05-24 09:06:43 -0400

    fix cluster mover verbosity

--- offline/packages/tpc/TpcClusterMover.cc
+++ offline/packages/tpc/TpcClusterMover.cc
@@ -63,7 +63,7 @@ std::vector<std::pair<TrkrDefs::cluskey, Acts::Vector3>> TpcClusterMover::proces
   // need at least 3 clusters to fit a circle
   if(tpc_global_vec.size() < 3)
     {
-      if(Verbosity() > 0)
+      if(_verbosity > 0)
 	{ std::cout << "  -- skip this tpc track, not enough clusters: " << tpc_global_vec.size() << std::endl; }
       return global_in;
     }
