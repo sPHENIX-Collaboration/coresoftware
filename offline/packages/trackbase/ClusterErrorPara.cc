99d68e5787bfa00a74c955eb5d5eb107db72e8c5
bogui56 <cer@mit.edu>
2022-07-26 05:16:58 -0400

    updated error parametrization

--- offline/packages/trackbase/ClusterErrorPara.cc
+++ offline/packages/trackbase/ClusterErrorPara.cc
@@ -19,29 +19,32 @@ namespace
 ClusterErrorPara::ClusterErrorPara()
 {
   f0 = new TF1("f0","[0] + x*[1]",0,10);
-  f0->SetParameter(0,0.00050639);
-  f0->SetParameter(1,0.000772036);
+  f0->SetParameter(0,0.01305);
+  f0->SetParameter(1,0.0213599);
+
   f1 = new TF1("f1","[0] + x*[1]",0,10);
-  f1->SetParameter(0,0.000197808);
-  f1->SetParameter(1,0.0012378);
+  f1->SetParameter(0,0.00827729);
+  f1->SetParameter(1,0.0341472);
+
   f2 = new TF1("f2","[0] + x*[1]",0,10);
-  f2->SetParameter(0,0.000119418);
-  f2->SetParameter(1,0.001058);
+  f2->SetParameter(0,0.00939903);
+  f2->SetParameter(1,0.0276867);
+
   fz = new TF1("fz","pol2",-2,2);
-  fz->SetParameter(0,0.0458407);
-  fz->SetParameter(1,0.000764133);
-  fz->SetParameter(2,0.087525);
+  fz->SetParameter(0,0.0430592);
+  fz->SetParameter(1,-0.000177174);
+  fz->SetParameter(2,0.0914288);
 
   static const double invsqrt12 = 1./std::sqrt(12);
 
   pitcherr_phi_mvtx = 0.002688 * invsqrt12;
   pitcherr_phi_intt = 0.0078 * invsqrt12;
-  pitcherr_phi_mm1 = 0.1;
-  pitcherr_phi_mm2 = 31.6;
-  pitcherr_z_mvtx = 0.002924;
-  pitcherr_z_intt = 1.6;
-  pitcherr_z_mm1 = 54.2;
-  pitcherr_z_mm2 = 0.2;
+  pitcherr_phi_mm1 = 0.1* invsqrt12 ;
+  pitcherr_phi_mm2 = 31.6* invsqrt12 ;
+  pitcherr_z_mvtx = 0.002924* invsqrt12 ;
+  pitcherr_z_intt = 1.6* invsqrt12 ;
+  pitcherr_z_mm1 = 54.2* invsqrt12 ;
+  pitcherr_z_mm2 = 0.2* invsqrt12 ;
 
 }
 
@@ -79,12 +82,11 @@ ClusterErrorPara::error_t ClusterErrorPara::get_cluster_error(TrackSeed *seed, T
       else if( cluster->getPhiSize() == 3 && cluster->getZSize() == 3 )  phierror = pitcherr_phi_mvtx*scalefactors_mvtx_phi[3];
       else phierror = pitcherr_phi_mvtx*cluster->getPhiSize();
       static constexpr std::array<double, 4> scalefactors_z = {{ 0.47, 0.48, 0.71, 0.55 }};
-      if( cluster->getZSize() == 2 && cluster->getPhiSize() == 2 ) zerror = pitcherr_z_mvtx*scalefactors_z[0];
+      if( cluster->getZSize() == 2 && cluster->getPhiSize() == 2 )       zerror = pitcherr_z_mvtx*scalefactors_z[0];
       else if( cluster->getZSize() == 2 && cluster->getPhiSize() == 3 )  zerror = pitcherr_z_mvtx*scalefactors_z[1];
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 2 )  zerror = pitcherr_z_mvtx*scalefactors_z[2];
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 3 )  zerror = pitcherr_z_mvtx*scalefactors_z[3];
       else zerror = pitcherr_z_mvtx*cluster->getZSize();
-
       break;
       
     case TrkrDefs::inttId:
@@ -131,7 +133,7 @@ ClusterErrorPara::error_t ClusterErrorPara::get_cluster_error(TrackSeed *seed, T
       double beta = atan(seed->get_slope());
       zerror = fz->Eval(beta);
       //      std::cout << "zerror: " << zerror << " beta: "<< beta << std::endl;
-      phierror*=cluster_r;
+      //      phierror;
       TrkrClusterv4 *clusterv4 = dynamic_cast<TrkrClusterv4 *>(cluster);
       if(clusterv4->getEdge()>=1)
 	phierror *= 2;
@@ -152,7 +154,9 @@ ClusterErrorPara::error_t ClusterErrorPara::get_simple_cluster_error(TrkrCluster
   int layer = TrkrDefs::getLayer(key);
   double phierror = 0;
   double zerror   = 0;
-
+  if(cluster_r>100){
+    phierror *=1;
+  }
   switch( TrkrDefs::getTrkrId( key ) )
     {
    
@@ -163,8 +167,8 @@ ClusterErrorPara::error_t ClusterErrorPara::get_simple_cluster_error(TrkrCluster
 	phierror = pitcherr_phi_mm1;
 	zerror = pitcherr_z_mm1;
       }else if(layer==56){
-	phierror =pitcherr_phi_mm2 = 31.6;
-	zerror = pitcherr_z_mm2 = 0.2;
+	phierror =pitcherr_phi_mm2;// = 31.6;
+	zerror = pitcherr_z_mm2;// = 0.2;
       }
       break;
     case TrkrDefs::mvtxId:
@@ -184,7 +188,6 @@ ClusterErrorPara::error_t ClusterErrorPara::get_simple_cluster_error(TrkrCluster
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 2 )  zerror = pitcherr_z_mvtx*scalefactors_z[2];
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 3 )  zerror = pitcherr_z_mvtx*scalefactors_z[3];
       else zerror = pitcherr_z_mvtx*cluster->getZSize();
-
       break;
       
     case TrkrDefs::inttId:
@@ -224,7 +227,7 @@ ClusterErrorPara::error_t ClusterErrorPara::get_simple_cluster_error(TrkrCluster
 	phierror = f2->Eval(alpha);
       }
       zerror = fz->Eval(beta);
-      phierror*=cluster_r;
+      //      phierror*=cluster_r;
       TrkrClusterv4 *clusterv4 = dynamic_cast<TrkrClusterv4 *>(cluster);
       if(clusterv4->getEdge()>=1)
 	phierror *= 2;
@@ -271,10 +274,8 @@ ClusterErrorPara::error_t ClusterErrorPara::get_fix_tpc_cluster_error(TrkrCluste
   if(sector==2){
     phierror = f2->Eval(alpha);
   }
-  double beta = 0.2;
+  double beta = 0.1;
   zerror = fz->Eval(beta);
-
-
 
   TrkrClusterv4 *clusterv4 = dynamic_cast<TrkrClusterv4 *>(cluster);
   if(clusterv4->getEdge()>=1)
@@ -312,12 +313,12 @@ ClusterErrorPara::error_t ClusterErrorPara::get_si_cluster_error(const TrkrClust
       else if( cluster->getPhiSize() == 3 && cluster->getZSize() == 3 )  phierror = pitcherr_phi_mvtx*scalefactors_mvtx_phi[3];
       else phierror = pitcherr_phi_mvtx*cluster->getPhiSize();
       static constexpr std::array<double, 4> scalefactors_z = {{ 0.47, 0.48, 0.71, 0.55 }};
-      if( cluster->getZSize() == 2 && cluster->getPhiSize() == 2 ) zerror = pitcherr_z_mvtx*scalefactors_z[0];
+      if( cluster->getZSize() == 2 && cluster->getPhiSize() == 2 )       zerror = pitcherr_z_mvtx*scalefactors_z[0];
       else if( cluster->getZSize() == 2 && cluster->getPhiSize() == 3 )  zerror = pitcherr_z_mvtx*scalefactors_z[1];
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 2 )  zerror = pitcherr_z_mvtx*scalefactors_z[2];
       else if( cluster->getZSize() == 3 && cluster->getPhiSize() == 3 )  zerror = pitcherr_z_mvtx*scalefactors_z[3];
       else zerror = pitcherr_z_mvtx*cluster->getZSize();
-
+ 
       break;
       
     case TrkrDefs::inttId:
