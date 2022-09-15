f6edcfcc2fae981d8715fc677e854fc812797b02
VVerkest <vverkest@gmail.com>
2022-09-14 20:20:32 -0400

    fix these deleted files

---
+++ simulation/g4simulation/g4jets/JetMapv1.h
@@ -0,0 +1,51 @@
+#ifndef G4JET_JETMAP_H
+#define G4JET_JETMAP_H
+
+#include "Jet.h"
+
+#include <phool/PHObject.h>
+
+#include <cmath>
+#include <cstddef>  // for size_t
+#include <iostream>
+#include <map>
+#include <vector>
+#include <functional>
+#include <set>
+
+class JetMap : public PHObject
+{
+ public:
+  // jet object iterators
+  typedef std::map<unsigned int, Jet*> typ_JetMap;
+  typedef typ_JetMap::const_iterator ConstIter;
+  typedef typ_JetMap::iterator Iter;
+
+  typedef std::vector<Jet*> vec_JetMap; // to be used when sorting typ_JetMap to iterate over
+
+  // source identifier iterators
+  typedef std::set<Jet::SRC>::const_iterator ConstSrcIter;
+  typedef std::set<Jet::SRC>::iterator SrcIter;
+
+  JetMap() {}
+  ~JetMap() override {}
+
+  void identify(std::ostream& os = std::cout) const override;
+  int isValid() const override { return 0; }
+  PHObject* CloneMe() const override { return nullptr; }
+
+  // map content info ----------------------------------------------------------
+
+  virtual void set_algo(Jet::ALGO /*algo*/) { return; }
+  virtual Jet::ALGO get_algo() const { return Jet::NONE; }
+
+  virtual void set_par(float) { return; }
+  virtual float get_par() const { return NAN; }
+
+  // set access to list of source identifiers ----------------------------------
+
+  virtual bool empty_src() const { return true; }
+  virtual void insert_src(Jet::SRC /*src*/) { return; }
+
+  virtual ConstSrcIter begin_src() const;
+
