06920ec7b6adb81ddf79e2eeae8ce837d32afcc7
Shuonli <sli17@ole.augie.edu>
2022-09-22 16:25:33 -0400

    clean up

--- simulation/g4simulation/g4ohcal/PHG4OHCalDetector.cc
+++ simulation/g4simulation/g4ohcal/PHG4OHCalDetector.cc
@@ -146,29 +146,29 @@ int PHG4OHCalDetector::ConstructOHCal(G4LogicalVolume *hcalenvelope)
   std::vector<G4VPhysicalVolume *>::iterator it1 = abs_asym->GetVolumesIterator();
   for (unsigned int isector = 0; isector < abs_asym->TotalImprintedVolumes(); isector++)
     {
-        m_DisplayAction->AddSteelVolume((*it1)->GetLogicalVolume());
-	m_SteelAbsorberLogVolSet.insert((*it1)->GetLogicalVolume());
-	hcalenvelope->AddDaughter((*it1));
-	m_VolumeSteel += (*it1)->GetLogicalVolume()->GetSolid()->GetCubicVolume();
-	std::vector<G4VPhysicalVolume *>::iterator it3 = m_ScintiMotherAssembly->GetVolumesIterator();
-	unsigned int ncnt = 24 * 5 * 2;
-	unsigned int ioff = isector * ncnt;
-	// ok we always have to skip to the scintillators we want to add for every hcal sector
-	for (unsigned int j = 0; j < ioff; j++)
-	  {
-	    ++it3;
+      m_DisplayAction->AddSteelVolume((*it1)->GetLogicalVolume());
+      m_SteelAbsorberLogVolSet.insert((*it1)->GetLogicalVolume());
+      hcalenvelope->AddDaughter((*it1));
+      m_VolumeSteel += (*it1)->GetLogicalVolume()->GetSolid()->GetCubicVolume();
+      std::vector<G4VPhysicalVolume *>::iterator it3 = m_ScintiMotherAssembly->GetVolumesIterator();
+      unsigned int ncnt = 24 * 5 * 2;
+      unsigned int ioff = isector * ncnt;
+      // ok we always have to skip to the scintillators we want to add for every hcal sector
+      for (unsigned int j = 0; j < ioff; j++)
+	{
+	  ++it3;
 	  }
-	for (unsigned int j = ioff; j < ioff + ncnt; j++)
-	  {
-	    m_DisplayAction->AddScintiVolume((*it3)->GetLogicalVolume());
-	    m_ScintiTileLogVolSet.insert((*it3)->GetLogicalVolume());
-	    hcalenvelope->AddDaughter((*it3));
-	    m_ScintiTilePhysVolMap.insert(std::make_pair(*it3, ExtractLayerTowerId(isector, *it3)));
-	    m_VolumeScintillator += (*it3)->GetLogicalVolume()->GetSolid()->GetCubicVolume();
-            ++it3;
+      for (unsigned int j = ioff; j < ioff + ncnt; j++)
+	{
+	  m_DisplayAction->AddScintiVolume((*it3)->GetLogicalVolume());
+	  m_ScintiTileLogVolSet.insert((*it3)->GetLogicalVolume());
+	  hcalenvelope->AddDaughter((*it3));
+	  m_ScintiTilePhysVolMap.insert(std::make_pair(*it3, ExtractLayerTowerId(isector, *it3)));
+	  m_VolumeScintillator += (*it3)->GetLogicalVolume()->GetSolid()->GetCubicVolume();
+	  ++it3;
 	  }
-	
-	++it1;
+      
+      ++it1;
     }
   // Chimney assemblies
   G4AssemblyVolume *chimAbs_asym = reader->GetAssembly("sectorChimney");         //absorber
