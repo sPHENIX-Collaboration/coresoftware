7a9b1b3d5851ad86da747f08d0795d8642d0a7b9
E Shulga <mohaas33@gmail.com>
2022-09-22 10:14:48 -0400

    Phi spacing and electron redistribution is introduced, not fully working yet

--- simulation/g4simulation/g4tpc/PHG4TpcPadPlaneReadout.cc
+++ simulation/g4simulation/g4tpc/PHG4TpcPadPlaneReadout.cc
@@ -91,7 +91,7 @@ int PHG4TpcPadPlaneReadout::CreateReadoutGeometry(PHCompositeNode * /*topNode*/,
       double sec_min_phi = sec_max_phi - SectorPhi[iregion];
       sector_min_Phi[zside].push_back(sec_min_phi);
       sector_max_Phi[zside].push_back(sec_max_phi);
-      std::cout << "R" << iregion << " sector " << isector << ": "<< sec_min_phi << " < phi < " << sec_max_phi  << std::endl;
+      //std::cout << "R" << iregion << " sector " << isector << ": "<< sec_min_phi << " < phi < " << sec_max_phi  << std::endl;
       sector_min_Phi_sectors[zside][iregion].push_back(sec_min_phi);
       sector_max_Phi_sectors[zside][iregion].push_back(sec_max_phi);
 
@@ -196,7 +196,7 @@ void PHG4TpcPadPlaneReadout::MapToPadPlane(TrkrHitSetContainer *single_hitsetcon
   //std::cout << "Enter new MapToPadPlane with rad_gem " << rad_gem << std::endl;
 
   // Moving electrons from dead area to a closest pad 
-  int p_region=-1;
+  //int p_region=-1;
   for (int iregion = 0; iregion < 3; ++iregion)
   {
     //std::cout<< "R "<<iregion<<std::endl;
@@ -218,44 +218,48 @@ void PHG4TpcPadPlaneReadout::MapToPadPlane(TrkrHitSetContainer *single_hitsetcon
       //std::cout<< "R after" << rad_gem << std::endl;
 
     }
-    if (rad_gem < MaxRadius[iregion] && rad_gem > MinRadius[iregion]) p_region = iregion;
+    //if (rad_gem < MaxRadius[iregion] && rad_gem > MinRadius[iregion]) p_region = iregion;
 
   }
+  /*
   for (int iregion = 0; iregion < 3; ++iregion)
   {  
     if (rad_gem < MaxRadius[iregion] && rad_gem > MinRadius[iregion]) p_region = iregion;
     if(p_region>0){
-      std::cout<< "p_region" << p_region << std::endl;
+      //std::cout<< "p_region" << p_region << std::endl;
       for(int s=0; s<12;s++){
         double daPhi = 0;
-      std::cout<< "1" << std::endl;
-      std::cout<< "s=" <<s<< std::endl;
-      std::cout<< "iregion=" <<iregion<< std::endl;
-      std::cout << "s min = " << sector_min_Phi_sectors[side][p_region][11] << std::endl;
-      std::cout << "s max = " << sector_max_Phi_sectors[side][p_region][s] << std::endl;
-      std::cout << "Ds = " << sector_min_Phi_sectors[side][p_region][11] - sector_max_Phi_sectors[side][p_region][s] << std::endl;
+      //std::cout<< "1" << std::endl;
+      //std::cout<< "s=" <<s<< std::endl;
+      //std::cout<< "iregion=" <<iregion<< std::endl;
+      //std::cout << "s min = " << sector_min_Phi_sectors[side][p_region][11] << std::endl;
+      //std::cout << "s max = " << sector_max_Phi_sectors[side][p_region][s] << std::endl;
+      //std::cout << "Ds = " << sector_min_Phi_sectors[side][p_region][11] - sector_max_Phi_sectors[side][p_region][s] << std::endl;
         if (s==0){
           //std::cout<< "ector_max_Phi_sectors[side][p_region][11] " << sector_max_Phi_sectors[side][p_region][11] << std::endl;
           daPhi = fabs(sector_min_Phi_sectors[side][p_region][11] - sector_max_Phi_sectors[side][p_region][s]);
         }else{
           daPhi = fabs(sector_min_Phi_sectors[side][p_region][s-1] - sector_max_Phi_sectors[side][p_region][s]);
         }
-      std::cout<< "2" << std::endl;
-      std::cout<< "daPhi = " << daPhi << std::endl;
+      //std::cout<< "2" << std::endl;
+      //std::cout<< "daPhi = " << daPhi << std::endl;
         if (phi<=sector_min_Phi_sectors[side][p_region][s] && phi>=sector_min_Phi_sectors[side][p_region][s]-daPhi){
-          std::cout<< "daPhi = " << daPhi <<std::endl;
-          std::cout<< "phi before" << phi << std::endl;
-          if(phi >= sector_min_Phi_sectors[side][p_region][s] + daPhi/2){
-            phi -= phi - sector_min_Phi_sectors[side][p_region][s];
-          }else{
-            phi += (sector_min_Phi_sectors[side][p_region][s] + daPhi/2) - phi;
+          //std::cout<< "daPhi = " << daPhi <<std::endl;
+          //std::cout<< "phi before" << phi << std::endl;
+          if (daPhi<1){ 
+            if(phi >= sector_min_Phi_sectors[side][p_region][s] + daPhi/2){
+              phi -= phi - sector_min_Phi_sectors[side][p_region][s];
+            }else{
+              phi += (sector_min_Phi_sectors[side][p_region][s] + daPhi/2) - phi;
+            }
           }
-          std::cout<< "phi after" << phi << std::endl;
+          //std::cout<< "phi after" << phi << std::endl;
          }
 
       }
     }
   }
+  */
   unsigned int layernum = 0;
 
   // Find which readout layer this electron ends up in
