71375900a731b8105aa340c069fcce08a3bbd2be
bschaefer <brs521@lehigh.edu>
2022-10-10 15:35:34 -0400

    sEPD moved inward (z-axis), avoid overlap with beamline

--- simulation/g4simulation/g4epd/PHG4EPDSubsystem.cc
+++ simulation/g4simulation/g4epd/PHG4EPDSubsystem.cc
@@ -116,5 +116,5 @@ PHG4Detector* PHG4EPDSubsystem::GetDetector() const
 
 void PHG4EPDSubsystem::SetDefaultParameters()
 {
-  set_default_double_param("place_z", 316.);
+  set_default_double_param("place_z", 308.);
 }
