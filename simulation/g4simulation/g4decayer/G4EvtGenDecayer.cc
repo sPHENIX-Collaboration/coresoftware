798ba3da2b75f266314bc4237d1a84b40729492d
MYOMAO <zshi2@mail.ccsf.edu>
2022-10-28 03:08:42 -0400

    Clean Up with Latest EvtGen Codes optimizing codes suggested by Jin

--- simulation/g4simulation/g4decayer/G4EvtGenDecayer.cc
+++ simulation/g4simulation/g4decayer/G4EvtGenDecayer.cc
@@ -49,167 +49,195 @@
 
 #include <cstdlib>   // for abs
 #include <iostream>  // for operator<<, basic_ostream:...
-#include <queue>     // for operator<<
-#include <stack>     // for operator<<
 #include <string>    // for operator<<
+#include <stack>    // for operator<<
+#include <queue>    // for operator<<
 
 #include <phool/PHRandomSeed.h>  //Introduce Random Number with Dedicated PHENIX Random Seed
+
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4EvtGenDecayer::G4EvtGenDecayer()
-  : G4VExtDecayer("G4EvtGenDecayer")
-  , fVerboseLevel(0)
+	: G4VExtDecayer("G4EvtGenDecayer")
+	  , fVerboseLevel(0)
 {
-  mEvtGenRandomEngine = new PHEvtGenRandomEngine();
 
-  EvtRandom::setRandomEngine((EvtRandomEngine*) mEvtGenRandomEngine);
+	mEvtGenRandomEngine = new PHEvtGenRandomEngine();
 
-  radCorrEngine = genList.getPhotosModel();
+	EvtRandom::setRandomEngine((EvtRandomEngine*) mEvtGenRandomEngine);
 
-  extraModels = genList.getListOfModels();
+	radCorrEngine = genList.getPhotosModel();
 
-  // the hardcoded paths are temporary
-  const string decay = string(getenv("OFFLINE_MAIN")) + "/share/EvtGen/DECAY.DEC";  // Using PDG 2019 reference as the input for now
-  const string evt = string(getenv("OFFLINE_MAIN")) + "/share/EvtGen/evt.pdl";
 
-  mEvtGen = new EvtGen(decay, evt, (EvtRandomEngine*) mEvtGenRandomEngine, radCorrEngine, &extraModels);
-  extraModels.clear();
+	extraModels = genList.getListOfModels();
+
+
+	// the hardcoded paths are temporary
+	const string decay = 	  string(getenv("OFFLINE_MAIN"))  + "/share/EvtGen/DECAY.DEC"; // Using PDG 2019 reference as the input for now
+	const string evt   =  string(getenv("OFFLINE_MAIN"))  + "/share/EvtGen/evt.pdl";
+
+	mEvtGen = new EvtGen(decay,  evt, (EvtRandomEngine*) mEvtGenRandomEngine,radCorrEngine,&extraModels);
+	extraModels.clear();
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4EvtGenDecayer::~G4EvtGenDecayer()
 {
-  delete mEvtGen;
 
-  delete radCorrEngine;
+	delete mEvtGen;		
+
+	delete radCorrEngine;
+
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4ParticleDefinition* G4EvtGenDecayer::
-    GetParticleDefinition(int ParPDGID) const
+GetParticleDefinition(int ParPDGID) const
 {
-  /// Return G4 particle definition for given TParticle
+	/// Return G4 particle definition for given TParticle
 
-  // get particle definition from G4ParticleTable
-  G4int pdgEncoding = ParPDGID;
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-  G4ParticleDefinition* particleDefinition = 0;
-  if (pdgEncoding != 0) particleDefinition = particleTable->FindParticle(pdgEncoding);
+	// get particle definition from G4ParticleTable
+	G4int pdgEncoding = ParPDGID;
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	G4ParticleDefinition* particleDefinition = 0;
+	if (pdgEncoding != 0)	particleDefinition = particleTable->FindParticle(pdgEncoding);
 
-  return particleDefinition;
+
+
+	return particleDefinition;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4DecayProducts* G4EvtGenDecayer::ImportDecayProducts(const G4Track& track)
 {
-  /// Import decay products
+	/// Import decay products
 
-  std::cout << "Indeed Using EvtGen Decayer with STACK CheckQA Simplied Detectable - Jin's Comment Implemented - Final" << std::endl;
 
-  G4ThreeVector momentum = track.GetMomentum();
-  G4double etot = track.GetDynamicParticle()->GetTotalEnergy();
+	G4ThreeVector momentum = track.GetMomentum();
+	G4double etot = track.GetDynamicParticle()->GetTotalEnergy();
 
-  G4ParticleDefinition* particleDef = track.GetDefinition();
-  G4int pdgEncoding = particleDef->GetPDGEncoding();
+	G4ParticleDefinition* particleDef = track.GetDefinition();
+	G4int pdgEncoding = particleDef->GetPDGEncoding();
 
-  EvtVector4R p_init(etot / GeV, momentum.x() / GeV, momentum.y() / GeV, momentum.z() / GeV);
 
-  EvtId parentID = EvtPDL::evtIdFromLundKC(pdgEncoding);
+	EvtVector4R p_init(etot/GeV,momentum.x()/GeV,momentum.y()/GeV,momentum.z()/GeV);
 
-  auto mParticle = EvtParticleFactory::particleFactory(parentID, p_init);
+	EvtId parentID = EvtPDL::evtIdFromLundKC(pdgEncoding);
 
-  // Decay the particle
-  mEvtGen->generateDecay(mParticle);
+	auto mParticle = EvtParticleFactory::particleFactory(parentID, p_init);
 
-  EvtHepMCEvent theEvent;
 
-  theEvent.constructEvent(mParticle);
 
-  HepMC3::GenEvent* evt = theEvent.getEvent();  //Directly use HepMC3 records
+	// Decay the particle
+	mEvtGen->generateDecay(mParticle);
 
-  G4DecayProducts* decayProducts = new G4DecayProducts(*(track.GetDynamicParticle()));
 
-  std::stack<HepMC3::GenParticlePtr> stack;
+	EvtHepMCEvent theEvent;
 
-  auto part = evt->particles()[0];
-  int partpdg = part->pdg_id();
 
-  if (part->pdg_id() != pdgEncoding) std::cout << "Issue Found: The first particle in the decay chain is NOT the incoming particle decayed by EvtGen" << std::endl;
+	theEvent.constructEvent(mParticle);
 
-  stack.push(part);
 
-  while (!stack.empty())
-  {
-    auto particle = stack.top();
-    stack.pop();
 
-    for (auto children : particle->children())
-    {
-      int pdg = children->pdg_id();
+	HepMC3::GenEvent * evt = theEvent.getEvent(); //Directly use HepMC3 records
 
-      G4ParticleDefinition* particleDefinition = GetParticleDefinition(pdg);
 
-      if (particleDefinition)
-      {
-        bool SameVtxPos = 1;
+	G4DecayProducts* decayProducts = new G4DecayProducts(*(track.GetDynamicParticle()));
 
-        if (children->production_vertex()->position().x() != particle->end_vertex()->position().x() || children->production_vertex()->position().y() != particle->end_vertex()->position().y() || children->production_vertex()->position().z() != particle->end_vertex()->position().z() || children->production_vertex()->position().t() != particle->end_vertex()->position().t()) SameVtxPos = 0;
 
-        G4ThreeVector G4Mom = G4ThreeVector(part->momentum().px() * GeV, part->momentum().py() * GeV, part->momentum().pz() * GeV);
-        G4DynamicParticle* dynamicParticle = new G4DynamicParticle(particleDefinition, G4Mom);
+	std::stack<HepMC3::GenParticlePtr> stack;
 
-        if (dynamicParticle)
-        {
-          if (fVerboseLevel > 0)
-          {
-            std::cout << "  G4 particle name: "
-                      << dynamicParticle->GetDefinition()->GetParticleName()
-                      << std::endl;
-          }
 
-          if (SameVtxPos)
-          {
-            decayProducts->PushProducts(dynamicParticle);
-          }
-          if (!SameVtxPos)
-          {
-            std::cout << "Issue Found: in this vertex, particles pushed to GEANT have different production positions, need to check and understand why!!!" << std::endl;
-            std::cout << "This is due to the particle PDGID: " << children->pdg_id() << "  from the decay vertex of the parent particle:" << particle->pdg_id() << std::endl;
-            std::cout << "Here is the Event Content" << std::endl;
-            const HepMC3::GenEvent& CheckEvent = *evt;
-            HepMC3::Print::content(CheckEvent);
-          }
-        }
-        else
-        {
-          std::cout << "Dynamical particle to be pushed from EvtGen to GEANT 4 is not properly defined: need to check why this happens!" << std::endl;
-          exit(1);
-        }
-      }
-      else
-      {
-        stack.push(children);
-      }
+	auto part =  evt->particles()[0];
 
-      if (particleDefinition)
-      {
-        CheckWhat.push_back(pdg);
-      }
-    }
-  }
+	if(part->pdg_id() != pdgEncoding) std::cout << "Issue Found: The first particle in the decay chain is NOT the incoming particle decayed by EvtGen" << std::endl;
 
-  evt->clear();
-  mParticle->deleteTree();
-  return decayProducts;
+	stack.push(part);
+
+
+	while(!stack.empty()){
+
+		auto particle = stack.top();
+		stack.pop();
+
+
+
+		for (auto children: particle->children()) {
+			int pdg = children->pdg_id();
+
+			G4ParticleDefinition* particleDefinition = GetParticleDefinition(pdg);
+
+
+
+			if(particleDefinition){
+
+				bool SameVtxPos = 1;
+
+				if(children->production_vertex()->position().x() != particle->end_vertex()->position().x() || children->production_vertex()->position().y() != particle->end_vertex()->position().y() || children->production_vertex()->position().z() != particle->end_vertex()->position().z() || children->production_vertex()->position().t() != particle->end_vertex()->position().t() ) SameVtxPos = 0;		
+
+				G4ThreeVector G4Mom = G4ThreeVector(part->momentum().px() * GeV, part->momentum().py() * GeV, part->momentum().pz() * GeV);
+				G4DynamicParticle* dynamicParticle = new G4DynamicParticle(particleDefinition, G4Mom);
+
+				if (dynamicParticle)
+				{
+					if (fVerboseLevel > 0)
+					{
+						std::cout << "  G4 particle name: "
+							<< dynamicParticle->GetDefinition()->GetParticleName()
+							<< std::endl;
+					}
+
+
+					if(SameVtxPos){
+
+						decayProducts->PushProducts(dynamicParticle); 
+
+					}
+					if(!SameVtxPos){
+
+						std::cout << "Issue Found: in this vertex, particles pushed to GEANT have different production positions, need to check and understand why!!!" << std::endl;
+						std::cout << "This is due to the particle PDGID: " <<  children->pdg_id()  << "  from the decay vertex of the parent particle:" << particle->pdg_id() << std::endl;
+						std::cout << "Here is the Event Content" << std::endl;
+						const HepMC3::GenEvent& CheckEvent =  *evt;
+						HepMC3::Print::content(CheckEvent);
+					}
+				}else{
+
+					std::cout << "Dynamical particle to be pushed from EvtGen to GEANT 4 is not properly defined: need to check why this happens!" << std::endl;
+					exit(1);
+
+				}
+
+			}else{
+
+				stack.push(children);
+
+			}
+
+		}
+
+
+	}
+
+
+
+
+	evt->clear();
+	mParticle->deleteTree();
+	return decayProducts;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void G4EvtGenDecayer::SetDecayTable(const string decayTable, bool useXml)
 {
-  mEvtGen->readUDecay(decayTable, useXml);
+	mEvtGen->readUDecay(decayTable, useXml);
 }
+
+
