0705d715f578ef58d4bcf6808a0a452499e01a58
E Shulga <mohaas33@gmail.com>
2022-11-02 10:43:17 -0400

    Implemented new binning in the tpc/fillSpaceChargeMaps

--- calibrations/tpc/fillSpaceChargeMaps/macros/run_files_300evts_AA_MDC2.sh
+++ calibrations/tpc/fillSpaceChargeMaps/macros/run_files_300evts_AA_MDC2.sh
@@ -2,7 +2,7 @@
 start=${1?Error: no start file \# given}
 stop=${2?Error: no stop file \# given}
 bX=${3?Error: no beamX \# given}
-source /opt/sphenix/core/bin/sphenix_setup.sh -n
+source /opt/sphenix/core/bin/sphenix_setup.sh -n new
 export MYINSTALL=/sphenix/user/shulga/tpc2019_install
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
  
@@ -11,7 +11,8 @@ do
     let Xstart=f XevtStart=f*100;
     A=$( printf '%05d' $Xstart )
     #B=$( printf '%06d' $Xend )
-    fname="G4Hits_sHijing_0_20fm-0000000002-"$A".root" ;
+    #fname="G4Hits_sHijing_0_20fm-0000000002-"$A".root" ;
+    fname="G4Hits_sHijing_0_20fm-0000000040-"$A".root" ;
     foutputname="./Files/mdc2_ADCBins_UseFieldMaps_hist_G4Hits_sHijing_0-12fm_bX"$bX"_"$A".root" ;
     #foutputname="./Files/mdc2_ADCBins_NoFieldMaps_hist_G4Hits_sHijing_0-12fm_bX"$bX"_"$A".root" ;
     echo $fname ;
