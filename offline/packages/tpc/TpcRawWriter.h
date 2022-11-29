5503dd7ab35f1aa8809fe634bec9683b42093ebc
bogui56 <cer@mit.edu>
2022-11-29 13:25:49 -0500

    add emulated raw data format

--- offline/packages/tpc/TpcRawWriter.h
+++ offline/packages/tpc/TpcRawWriter.h
@@ -1,8 +1,9 @@
-#ifndef TPC_TPCCLUSTERIZER_H
-#define TPC_TPCCLUSTERIZER_H
+#ifndef TPC_TPCRAWWRITER_H
+#define TPC_TPCRAWWRITER_H
 
 #include <fun4all/SubsysReco.h>
 #include <trackbase/TrkrCluster.h>
+#include <trackbase/ActsSurfaceMaps.h>
 #include <trackbase/ActsGeometry.h>
 
 #include <map> 
@@ -12,11 +13,10 @@
 class PHCompositeNode;
 class TrkrHitSet;
 class TrkrHitSetContainer;
-class RawHitSet;
-class RawHitSetContainer;
+class RawHitSetContainerv1;
 class TrkrClusterContainer;
 class TrkrClusterHitAssoc;
-class PHG4TpcCylinderGeom;
+class PHG4CylinderCellGeom;
 class PHG4TpcCylinderGeomContainer;
 
 //typedef std::pair<int, int> iphiz;
@@ -24,50 +24,50 @@ class PHG4TpcCylinderGeomContainer;
 typedef std::pair<unsigned short, unsigned short> iphiz;
 typedef std::pair<unsigned short, iphiz> ihit;
 
-class TpcClusterizer : public SubsysReco
+class TpcRawWriter : public SubsysReco
 {
  public:
-  TpcClusterizer(const std::string &name = "TpcClusterizer");
-  ~TpcClusterizer() override = default;
+  TpcRawWriter(const std::string &name = "TpcRawWriter");
+  ~TpcRawWriter() override = default;
 
   int InitRun(PHCompositeNode *topNode) override;
   int process_event(PHCompositeNode *topNode) override;
   int End(PHCompositeNode *topNode) override;
-
+  
   void set_sector_fiducial_cut(const double cut){SectorFiducialCut = cut; }
   void set_do_hit_association(bool do_assoc){do_hit_assoc = do_assoc;}
   void set_do_wedge_emulation(bool do_wedge){ do_wedge_emulation = do_wedge;}
   void set_do_sequential(bool do_seq){ do_sequential = do_seq;}
-  void set_remove_singles(bool do_sing){ do_singles = do_sing;}
-  void set_read_raw(bool read_raw){ do_read_raw = read_raw;}
   void set_max_cluster_half_size_phi(unsigned short size) { MaxClusterHalfSizePhi = size ;}
-  void set_max_cluster_half_size_z(unsigned short size) { MaxClusterHalfSizeT = size ;}
+  void set_max_cluster_half_size_z(unsigned short size) { MaxClusterHalfSizeZ = size ;}
+  void set_drift_velocity_scale(double value) { m_drift_velocity_scale = value; }
   void set_cluster_version(int value) { cluster_version = value; }
   
  private:
-  bool is_in_sector_boundary(int phibin, int sector, PHG4TpcCylinderGeom *layergeom) const;
-
+  
   TrkrHitSetContainer *m_hits = nullptr;
-  RawHitSetContainer *m_rawhits = nullptr;
+  RawHitSetContainerv1 *m_rawhits = nullptr;
   TrkrClusterContainer *m_clusterlist = nullptr;
   TrkrClusterHitAssoc *m_clusterhitassoc = nullptr;
+  ActsSurfaceMaps *m_surfMaps = nullptr;
   ActsGeometry *m_tGeometry = nullptr;
   bool do_hit_assoc = true;
-  bool do_wedge_emulation = false;
+  bool do_wedge_emulation = true;
   bool do_sequential = false;
-  bool do_read_raw = false;
-  bool do_singles = false;
   double pedestal = 74.4;
   double SectorFiducialCut = 0.5;
   unsigned short MaxClusterHalfSizePhi = 3;
-  unsigned short MaxClusterHalfSizeT = 5;
-  int cluster_version = 4;
-  double m_tdriftmax = 0;
-  double AdcClockPeriod = 53.0;   // ns 
+  unsigned short MaxClusterHalfSizeZ = 5;
+  int cluster_version = 3;
+  /// drift velocity scale factor
 
-  // TPC shaping offset correction parameter
-  // From Tony Frawley July 5, 2022
-  double m_sampa_tbias = 39.6;  // ns  
+  double m_drift_velocity_scale = 1.0;
+  
+  // TPC shaping offset correction parameters
+  // From Tony Frawley May 13, 2021
+  double par0_neg = 0.0503;
+  double par0_pos = -0.0503;
+  
 };
 
 #endif
