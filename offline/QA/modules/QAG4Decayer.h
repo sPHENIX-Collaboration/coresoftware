c33268d5c0cd2024e1b765289b4b8f213ffef488
MYOMAO <zshi2@mail.ccsf.edu>
2022-11-30 20:09:35 -0500

    Clang Format Fixed

--- offline/QA/modules/QAG4Decayer.h
+++ offline/QA/modules/QAG4Decayer.h
@@ -1,24 +1,22 @@
 #include <fun4all/SubsysReco.h>
 #include <g4main/PHG4Particle.h>
-#include <g4main/PHG4VtxPoint.h>
 #include <g4main/PHG4TruthInfoContainer.h>
+#include <g4main/PHG4VtxPoint.h>
 
 #include <g4eval/SvtxEvalStack.h>
 
+#include <trackbase_historic/ActsTransformations.h>
 #include <trackbase_historic/SvtxTrack.h>
 #include <trackbase_historic/SvtxTrackMap.h>
 #include <trackbase_historic/SvtxVertex.h>
 #include <trackbase_historic/SvtxVertexMap.h>
-#include <trackbase_historic/ActsTransformations.h>
 #include <trackbase_historic/TrackSeed.h>
-
-
 
 #include <TBranch.h>
 #include <TFile.h>
-#include <TTree.h>
 #include <TH1.h>
 #include <TH2.h>
+#include <TTree.h>
 
 #include <string>
 
@@ -27,50 +25,48 @@ class PHG4TruthInfoContainer;
 class PHG4Particle;
 class PHG4VtxPoint;
 
-class QAG4Decayer : public SubsysReco {
-	public:
+class QAG4Decayer : public SubsysReco
+{
+ public:
+  QAG4Decayer();
+  virtual ~QAG4Decayer();
 
-		QAG4Decayer();
-		virtual ~QAG4Decayer();
+  int Init(PHCompositeNode *topNode) override;
+  //		int Init() override;
+  int process_event(PHCompositeNode *topNode) override;
+  int End(PHCompositeNode *topNode) override;
+  int Channel(int pdgid, std::vector<int> Daughter);
 
-		int Init(PHCompositeNode *topNode) override;
-//		int Init() override;
-		int process_event(PHCompositeNode *topNode) override;
-		int End(PHCompositeNode *topNode) override;
-		int Channel(int pdgid, std::vector<int> Daughter);
-	
- 
-	private:
-		  PHG4TruthInfoContainer *m_truth_info;
-	  //  PHG4Particle *m_g4particle;
-		  SvtxEvalStack * _svtxevalstack = NULL;
+ private:
+  PHG4TruthInfoContainer *m_truth_info;
+  //  PHG4Particle *m_g4particle;
+  SvtxEvalStack *_svtxevalstack = NULL;
 
-		  TH1D * QAPx;
-		  TH1D * QAPy;
-		  TH1D * QAPz;
-		  TH1D * QAE;
-		  TH1D * TotalVtxStat;
-		  TH1D * BadVtxStat;
-		  TH1D * BadVtxPercent;
-		  TH1D * HFHadronStat;
+  TH1D *QAPx;
+  TH1D *QAPy;
+  TH1D *QAPz;
+  TH1D *QAE;
+  TH1D *TotalVtxStat;
+  TH1D *BadVtxStat;
+  TH1D *BadVtxPercent;
+  TH1D *HFHadronStat;
 
-		  TH2D * DecayBR;
-		
-		  TH1D * SVtoPVDistance;
-		  TH1D * ProperLifeTime;
-		  TH1D * QACosTheta;
-		  TFile * fout; 
-		  TTree * QATree; 
-		  int EvtID;
-		  float LifeTime;
-		  TH1D * MassHis;
-		  TH1D * NPartHis;
-		  TH1D * BR1DHis;
-	   	  int NParticles;
-		  float D0Px;
-		  float D0Py;
-		  float D0Pz;
-		  float D0E;
-		  std::vector<int> PVDaughtersPDGID;
+  TH2D *DecayBR;
 
+  TH1D *SVtoPVDistance;
+  TH1D *ProperLifeTime;
+  TH1D *QACosTheta;
+  TFile *fout;
+  TTree *QATree;
+  int EvtID;
+  float LifeTime;
+  TH1D *MassHis;
+  TH1D *NPartHis;
+  TH1D *BR1DHis;
+  int NParticles;
+  float D0Px;
+  float D0Py;
+  float D0Pz;
+  float D0E;
+  std::vector<int> PVDaughtersPDGID;
 };
--- simulation/g4simulation/g4decayer/PHEvtGenRandomEngine.cc
+++ simulation/g4simulation/g4decayer/PHEvtGenRandomEngine.cc
@@ -2,27 +2,23 @@
 #include <phool/PHRandomSeed.h>
 //#include <g4main/PHG4ParticleGeneratorBase.h>
 
+#include <iostream>
 
-
- #include <iostream>
-
-PHEvtGenRandomEngine::PHEvtGenRandomEngine()     
+PHEvtGenRandomEngine::PHEvtGenRandomEngine()
 {
-
-     m_RandomGenerator = gsl_rng_alloc(gsl_rng_mt19937);
-	 m_Seed = PHRandomSeed();  // fixed seed is handled in this funtcion
-	 gsl_rng_set(m_RandomGenerator, m_Seed);
+  m_RandomGenerator = gsl_rng_alloc(gsl_rng_mt19937);
+  m_Seed = PHRandomSeed();  // fixed seed is handled in this funtcion
+  gsl_rng_set(m_RandomGenerator, m_Seed);
 }
 
-PHEvtGenRandomEngine::~PHEvtGenRandomEngine(){
-	//std::cout << "Detroy the Random Generator" << std::endl;
-	gsl_rng_free(RandomGenerator());
-	//delete m_RandomGenerator;
+PHEvtGenRandomEngine::~PHEvtGenRandomEngine()
+{
+  //std::cout << "Detroy the Random Generator" << std::endl;
+  gsl_rng_free(RandomGenerator());
+  //delete m_RandomGenerator;
 }
-
 
 double PHEvtGenRandomEngine::random()
 {
-		 
-	return  gsl_rng_uniform_pos(RandomGenerator());
+  return gsl_rng_uniform_pos(RandomGenerator());
 }
