c72c94610adff8159ad34335b8e13c2d7611cc8a
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-02-17 09:52:02 -0500

    fix setting of epdmpv

--- simulation/g4simulation/g4epd/PHG4EPDModuleReco.cc
+++ simulation/g4simulation/g4epd/PHG4EPDModuleReco.cc
@@ -44,6 +44,7 @@ int PHG4EPDModuleReco::InitRun(PHCompositeNode *topNode)
   tmax = get_double_param("tmax");
   m_DeltaT = get_double_param("delta_t");
   m_EpdMpv = get_double_param("epdmpv");
+
   CreateNodes(topNode);
 
   return Fun4AllReturnCodes::EVENT_OK;
