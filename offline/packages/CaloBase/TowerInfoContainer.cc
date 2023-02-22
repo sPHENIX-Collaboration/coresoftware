72103292d34f064902f2fc8d18a2663d2a15517a
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-02-22 13:45:33 -0500

    remove empty source TowerInfoContainer.cc

--- offline/packages/CaloBase/TowerInfoContainer.cc
+++
@@ -1,8 +0,0 @@
-#include "TowerInfoContainer.h"
-
-// TowerInfoContainer::Map DummyTowerMap;
-
-// TowerInfoContainer::Range TowerInfoContainer::getTowers()
-// {
-//   return std::make_pair(DummyTowerMap.begin(), DummyTowerMap.begin());
-// }
