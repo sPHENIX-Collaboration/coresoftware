8d3409518904362d9576ece56c6132f03f760fb9
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-03-07 17:24:00 -0500

    removed "detector_name" it is hardcoded to MICROMEGAS improve debug output

--- simulation/g4simulation/g4micromegas/PHG4MicromegasHitReco.cc
+++ simulation/g4simulation/g4micromegas/PHG4MicromegasHitReco.cc
@@ -102,10 +102,9 @@ namespace
 }
 
 //___________________________________________________________________________
-PHG4MicromegasHitReco::PHG4MicromegasHitReco(const std::string &name, const std::string& detector)
+PHG4MicromegasHitReco::PHG4MicromegasHitReco(const std::string &name)
   : SubsysReco(name)
   , PHParameterInterface(name)
-  , m_detector(detector)
 {
   // initialize rng
   const uint seed = PHRandomSeed();
@@ -148,7 +147,7 @@ int PHG4MicromegasHitReco::InitRun(PHCompositeNode *topNode)
   auto dstNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", "DST"));
   if (!dstNode)
   {
-    std::cout << PHWHERE << "DST Node missing, doing nothing." << std::endl;
+    std::cout << "PHG4MicromegasHitReco::InitRun - DST Node missing, doing nothing." << std::endl;
     exit(1);
   }
 
@@ -156,7 +155,7 @@ int PHG4MicromegasHitReco::InitRun(PHCompositeNode *topNode)
   auto hitsetcontainer = findNode::getClass<TrkrHitSetContainer>(topNode, "TRKR_HITSET");
   if (!hitsetcontainer)
   {
-    std::cout << PHWHERE << "creating TRKR_HITSET." << std::endl;
+    std::cout << "PHG4MicromegasHitReco::InitRun - creating TRKR_HITSET." << std::endl;
 
     // find or create TRKR node
     PHNodeIterator dstiter(dstNode);
@@ -177,7 +176,7 @@ int PHG4MicromegasHitReco::InitRun(PHCompositeNode *topNode)
   auto hittruthassoc = findNode::getClass<TrkrHitTruthAssoc>(topNode, "TRKR_HITTRUTHASSOC");
   if (!hittruthassoc)
   {
-    std::cout << PHWHERE << "creating TRKR_HITTRUTHASSOC." << std::endl;
+    std::cout << "PHG4MicromegasHitReco::InitRun - creating TRKR_HITTRUTHASSOC." << std::endl;
 
     // find or create TRKR node
     PHNodeIterator dstiter(dstNode);
@@ -201,8 +200,7 @@ int PHG4MicromegasHitReco::process_event(PHCompositeNode *topNode)
 {
   // load relevant nodes
   // G4Hits
-  const std::string g4hitnodename = "G4HIT_" + m_detector;
-  auto g4hitcontainer = findNode::getClass<PHG4HitContainer>(topNode, g4hitnodename);
+  auto g4hitcontainer = findNode::getClass<PHG4HitContainer>(topNode, "G4HIT_MICROMEGAS");
   assert(g4hitcontainer);
 
   // acts geometry
