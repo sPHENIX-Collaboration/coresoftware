758cb2f79a24ff4d848f091910878234d5dabeb0
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-03-07 18:18:11 -0500

    - added basic mapping between fee id and fee index. - added rms and pedestal histograms

--- offline/packages/micromegas/MicromegasRawDataDecoder.h
+++ offline/packages/micromegas/MicromegasRawDataDecoder.h
@@ -8,40 +8,65 @@
 
 #include <fun4all/SubsysReco.h>
 
+#include <memory>
 #include <string>        
 
 class PHCompositeNode;
+class TFile;
 class TH1;
 class TH2;
 
 //! micromegas clusterizer
 class MicromegasRawDataDecoder : public SubsysReco
 {
- public:
+  public:
 
   //! constructor
   MicromegasRawDataDecoder( const std::string &name = "MicromegasRawDataDecoder" );
 
+  //! global initialization
+  int Init(PHCompositeNode*) override;
+
   //! run initialization
   int InitRun(PHCompositeNode*) override;
 
-  //! event processing
+  /// event processing
   int process_event(PHCompositeNode*) override;
 
+  /// end of processing
+  int End(PHCompositeNode*) override;
+
+  /// set to true to store evaluation histograms and ntuples
+  void set_save_histograms( bool value ) { m_savehistograms = value; }
+    
+  /// output file name for evaluation histograms
+  void set_histogram_outputfile(const std::string &outputfile) {m_histogramfilename = outputfile;}
+
   private:  
+ 
+  /// create evaluation histograms
+  void create_histograms();
+
   static constexpr int m_nchannels_fee = 256;
-  static constexpr int m_nfee = 16;
+  static constexpr int m_nfee = 8;
   static constexpr int m_nchannels_total = m_nfee*m_nchannels_fee;
   static constexpr int m_max_adc = 1024;
   
   //!@name evaluation histograms
   //@{
-  
+ 
+  /// Output root histograms
+  bool m_savehistograms = true;
+
+  /// histogram output file name
+  std::string m_histogramfilename = "MicromegasRawDataDecoder.root";
+  std::unique_ptr<TFile> m_histogramfile;  
+ 
   //! Fired FEE
-  TH1* m_h_fee = nullptr;
+  TH1* m_h_fee_id = nullptr;
   
   //! ADC distribution vs channel number
-  TH2* m_h_adc = nullptr;
+  TH2* m_h_adc_channel = nullptr;
   
   //@}
   
