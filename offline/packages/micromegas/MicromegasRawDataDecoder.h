650f12b148c2896fbd0ba8d3ebd5f6a1c23d7149
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-03-09 14:20:01 -0500

    Removed all evaluation histograms. They are now in RawDataCalibration

--- offline/packages/micromegas/MicromegasRawDataDecoder.h
+++ offline/packages/micromegas/MicromegasRawDataDecoder.h
@@ -12,9 +12,6 @@
 #include <string>
 
 class PHCompositeNode;
-class TFile;
-class TH1;
-class TH2;
 
 //! micromegas raw data decoder
 class MicromegasRawDataDecoder : public SubsysReco
@@ -32,43 +29,6 @@ class MicromegasRawDataDecoder : public SubsysReco
 
   /// event processing
   int process_event(PHCompositeNode*) override;
-
-  /// end of processing
-  int End(PHCompositeNode*) override;
-
-  /// set to true to store evaluation histograms and ntuples
-  void set_save_histograms( bool value ) { m_savehistograms = value; }
-
-  /// output file name for evaluation histograms
-  void set_histogram_outputfile(const std::string &outputfile) {m_histogramfilename = outputfile;}
-
-  private:
-
-  /// create evaluation histograms
-  void create_histograms();
-
-  static constexpr int m_nchannels_fee = 256;
-  static constexpr int m_nfee = 8;
-  static constexpr int m_nchannels_total = m_nfee*m_nchannels_fee;
-  static constexpr int m_max_adc = 1024;
-
-  //!@name evaluation histograms
-  //@{
-
-  /// Output root histograms
-  bool m_savehistograms = true;
-
-  /// histogram output file name
-  std::string m_histogramfilename = "MicromegasRawDataDecoder.root";
-  std::unique_ptr<TFile> m_histogramfile;
-
-  //! Fired FEE
-  TH1* m_h_fee_id = nullptr;
-
-  //! ADC distribution vs channel number
-  TH2* m_h_adc_channel = nullptr;
-
-  //@}
 
 };
 
