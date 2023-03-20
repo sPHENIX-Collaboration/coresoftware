c5ff3af035b39a74a1cdc5ae514f2f7ab0c66051
Michael Peters <mjpeters@mit.edu>
2023-03-20 08:41:06 -0400

    Added crossing determination

--- offline/packages/trackreco/PHSiliconHelicalPropagator.h
+++ offline/packages/trackreco/PHSiliconHelicalPropagator.h
@@ -1,8 +1,8 @@
-#include <trackreco/PHTrackPropagating.h>
 #include <trackbase/ActsGeometry.h>
 #include <trackbase_historic/TrackSeedContainer.h>
 #include <trackermillepedealignment/HelicalFitter.h>
 #include <trackbase_historic/SvtxTrackMap.h>
+#include <trackbase/TrkrClusterCrossingAssoc.h>
 
 class PHSiliconHelicalPropagator : public SubsysReco
 {
@@ -18,11 +18,14 @@ public:
   void set_track_map_name(std::string name){ _track_map_name = name; }
 private:
   int createSeedContainer(TrackSeedContainer*& container, const std::string container_name, PHCompositeNode *topNode);
+  
   ActsGeometry* _tgeometry = nullptr;
   TrackSeedContainer* _si_seeds = nullptr;
   TrackSeedContainer* _tpc_seeds = nullptr;
   TrackSeedContainer* _svtx_seeds = nullptr;
   HelicalFitter* _fitter = nullptr;
   TrkrClusterContainer* _cluster_map = nullptr;
+  TrkrClusterCrossingAssoc* _cluster_crossing_map = nullptr;
+
   std::string _track_map_name = "SvtxTrackSeedContainer";
 };
