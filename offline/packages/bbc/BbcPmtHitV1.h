954e0993eeb8efb69ad7c82c80ed5eeee2aaa103
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-03-31 18:58:43 -0400

    add missing override

--- offline/packages/bbc/BbcPmtHitV1.h
+++ offline/packages/bbc/BbcPmtHitV1.h
@@ -13,14 +13,14 @@ class BbcPmtHitV1 : public BbcPmtHit
 public:
   BbcPmtHitV1() { }
   BbcPmtHitV1(const Short_t pmt, const Float_t adc, const Float_t tdc0, const Float_t tdc1);
-  virtual ~BbcPmtHitV1() { }
+  ~BbcPmtHitV1() override = default;
 
   Short_t get_pmt()  const override {return pmt;}
   Float_t get_adc()  const override {return adc;}
   Float_t get_tdc0() const override {return tdc0;}
   Float_t get_tdc1() const override {return tdc1;}
 
-  void identify(std::ostream& os = std::cout) const;
+  void identify(std::ostream& os = std::cout) const override;
 
 private:
   Short_t pmt;
