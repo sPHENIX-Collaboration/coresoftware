81355fc5b2cf13b8617015ab7e90861fd8d51770
timothyrinn <trinn2@illinois.edu>
2023-04-03 16:47:25 -0400

    fixed a cppcheck issue

--- offline/packages/CaloBase/TowerInfoDefs.cc
+++ offline/packages/CaloBase/TowerInfoDefs.cc
@@ -233,14 +233,11 @@ unsigned int TowerInfoDefs::encode_epd (const unsigned int arm, const unsigned i
       }
     int epdchnlmap[16][2] = {{0, 0}, {1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}, {11, 12}, {13, 14}, {15, 16}, {17, 18}, {19, 20}, {21, 22}, {23, 24}, {25, 26}, {27, 28}, {29, 30}};
     int channel = 0;
-    if (rbin > 0)
+    if (rbin != 0)
       {
 	channel = epdchnlmap[rbin][phibin - 2 * sector];
       }
-    else
-      {
-	channel = 0;
-      }
+   
     unsigned int key = channel + (sector << 5U) + (arm << 9U);
     return key;
   }
