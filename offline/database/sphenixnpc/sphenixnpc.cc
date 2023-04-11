319c63435a2b97c9b16b66406bd71639a4994ea2
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-04-11 17:37:22 -0400

    clang-format

--- offline/database/sphenixnpc/sphenixnpc.cc
+++ offline/database/sphenixnpc/sphenixnpc.cc
@@ -4,7 +4,7 @@ sphenixnpc *sphenixnpc::__instance = nullptr;
 
 sphenixnpc *sphenixnpc::instance(const std::string &globaltag)
 {
-  if (! __instance) 
+  if (!__instance)
   {
     __instance = new sphenixnpc();
   }
@@ -18,56 +18,64 @@ sphenixnpc::~sphenixnpc()
   __instance = nullptr;
 }
 
-json sphenixnpc::createGlobalTag(const std::string &tagname)
+nlohmann::json sphenixnpc::createGlobalTag(const std::string &tagname)
 {
   setGlobalTag(tagname);
   return nopayloadclient::Client::createGlobalTag();
 }
 
-json sphenixnpc::deleteGlobalTag(const std::string &tagname)
+nlohmann::json sphenixnpc::deleteGlobalTag(const std::string &tagname)
 {
   setGlobalTag(tagname);
   return nopayloadclient::Client::deleteGlobalTag();
 }
 
-json sphenixnpc::getUrlDict(long long iov) {
-    return nopayloadclient::Client::getUrlDict(0, iov);
+nlohmann::json sphenixnpc::getUrlDict(long long iov)
+{
+  return nopayloadclient::Client::getUrlDict(0, iov);
 }
 
-json sphenixnpc::get(std::string pl_type, long long iov) {
-    if (url_dict_.is_null()) {
-        json resp = getUrlDict(iov);
-        if (resp["code"] != 0) return resp;
-        url_dict_ = resp["msg"];
-    }
-    if (not url_dict_.contains(pl_type)) {
-        return DataBaseException("No payload with type " + pl_type + " exists.").jsonify();
-    }
-    return makeResp(url_dict_[pl_type]);
+nlohmann::json sphenixnpc::get(std::string pl_type, long long iov)
+{
+  if (url_dict_.is_null())
+  {
+    nlohmann::json resp = getUrlDict(iov);
+    if (resp["code"] != 0) return resp;
+    url_dict_ = resp["msg"];
+  }
+  if (not url_dict_.contains(pl_type))
+  {
+    return DataBaseException("No payload with type " + pl_type + " exists.").jsonify();
+  }
+  return makeResp(url_dict_[pl_type]);
 }
 
-json sphenixnpc::insertPayload(std::string pl_type, std::string file_url,
-                           long long iov_start) {
-    return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, iov_start);
+nlohmann::json sphenixnpc::insertPayload(std::string pl_type, std::string file_url,
+                                         long long iov_start)
+{
+  return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, iov_start);
 }
 
-json sphenixnpc::insertPayload(std::string pl_type, std::string file_url,
-                           long long iov_start, long long iov_end) {
-    return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, iov_start, 0, iov_end);
+nlohmann::json sphenixnpc::insertPayload(std::string pl_type, std::string file_url,
+                                         long long iov_start, long long iov_end)
+{
+  return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, iov_start, 0, iov_end);
 }
 
-json sphenixnpc::setGlobalTag(std::string name) {
+nlohmann::json sphenixnpc::setGlobalTag(std::string name)
+{
   if (name != m_CachedGlobalTag)
   {
-    url_dict_ = json {};
+    url_dict_ = nlohmann::json{};
     m_CachedGlobalTag = name;
   }
-    return nopayloadclient::Client::setGlobalTag(name);
+  return nopayloadclient::Client::setGlobalTag(name);
 }
 
-json sphenixnpc::clearCache() {
-    url_dict_ = json {};
-    return nopayloadclient::Client::clearCache();
+nlohmann::json sphenixnpc::clearCache()
+{
+  url_dict_ = nlohmann::json{};
+  return nopayloadclient::Client::clearCache();
 }
 
 std::string sphenixnpc::getCalibrationFile(const std::string &type, uint64_t iov)
@@ -78,7 +86,7 @@ std::string sphenixnpc::getCalibrationFile(const std::string &type, uint64_t iov
 
 int sphenixnpc::insertcalib(const std::string &payloadtype, const std::string &fname, uint64_t iov_start)
 {
-  json ret = insertPayload(payloadtype, fname, iov_start);
+  nlohmann::json ret = insertPayload(payloadtype, fname, iov_start);
   if (Verbosity())
   {
     std::cout << ret << std::endl;
@@ -88,7 +96,7 @@ int sphenixnpc::insertcalib(const std::string &payloadtype, const std::string &f
 
 int sphenixnpc::insertcalib(const std::string &payloadtype, const std::string &fname, uint64_t iov_start, uint64_t iov_end)
 {
-  json ret = insertPayload(payloadtype, fname, iov_start, iov_end);
+  nlohmann::json ret = insertPayload(payloadtype, fname, iov_start, iov_end);
   if (Verbosity())
   {
     std::cout << ret << std::endl;
--- offline/database/sphenixnpc/sphenixnpc.h
+++ offline/database/sphenixnpc/sphenixnpc.h
@@ -1,6 +1,5 @@
-#ifndef DBTOOLS_SPHENIXNPC_H
-#define DBTOOLS_SPHENIXNPC_H
-
+#ifndef SPHENIXNPC_SPHENIXNPC_H
+#define SPHENIXNPC_SPHENIXNPC_H
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -11,39 +10,33 @@
 
 #include <iostream>
 
-using json = nlohmann::json;
-
-//namespace sphenixnpc {
-
-class sphenixnpc :  public nopayloadclient::Client
+class sphenixnpc : public nopayloadclient::Client
 {
-
-public: 
+ public:
   static sphenixnpc *instance(const std::string &globaltag = "NONE");
-   ~sphenixnpc(); 
-    json getUrlDict(long long iov);
-    json createGlobalTag(const std::string &tagname);
-    
-    json deleteGlobalTag(const std::string&);
-    json get(std::string pl_type, long long iov);
-    json insertPayload(std::string pl_type, std::string file_url,
-                       long long iov_start);
-    json insertPayload(std::string pl_type, std::string file_url,
-                       long long iov_start, long long iov_end);
-    json setGlobalTag(std::string name);
-    json clearCache();
-    std::string getCalibrationFile(const std::string &type, uint64_t iov);
-    int insertcalib(const std::string &fname, const std::string &payloadtype, uint64_t iov_start);
-    int insertcalib(const std::string &fname, const std::string &payloadtype, uint64_t iov_start, uint64_t iov_end);
-    void Verbosity(int i) {m_Verbosity = i;}
-      int Verbosity() const {return m_Verbosity;}
+  ~sphenixnpc();
+  nlohmann::json getUrlDict(long long iov);
+  nlohmann::json createGlobalTag(const std::string &tagname);
 
-private:
-    static sphenixnpc *__instance;
-    int m_Verbosity = 0;
-    json url_dict_; // valid until global tag is switched
-    std::string m_CachedGlobalTag;
+  nlohmann::json deleteGlobalTag(const std::string &);
+  nlohmann::json get(std::string pl_type, long long iov);
+  nlohmann::json insertPayload(std::string pl_type, std::string file_url,
+                               long long iov_start);
+  nlohmann::json insertPayload(std::string pl_type, std::string file_url,
+                               long long iov_start, long long iov_end) override;
+  nlohmann::json setGlobalTag(std::string name) override;
+  nlohmann::json clearCache() override;
+  std::string getCalibrationFile(const std::string &type, uint64_t iov);
+  int insertcalib(const std::string &fname, const std::string &payloadtype, uint64_t iov_start);
+  int insertcalib(const std::string &fname, const std::string &payloadtype, uint64_t iov_start, uint64_t iov_end);
+  void Verbosity(int i) { m_Verbosity = i; }
+  int Verbosity() const { return m_Verbosity; }
+
+ private:
+  static sphenixnpc *__instance;
+  int m_Verbosity = 0;
+  nlohmann::json url_dict_;  // valid until global tag is switched
+  std::string m_CachedGlobalTag;
 };
 
-//}
-#endif // DBTOOLS_SPHENIXNPC_H
+#endif  // SPHENIXNPC_SPHENIXNPC_H
