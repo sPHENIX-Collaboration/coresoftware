f4bb5219bed263e605c3ca9d40a7c80f411abc74
Ejiro Umaka <ejironaomiumaka@gmail.com>
2023-06-06 11:44:50 -0400

    Update EpFinder.h

--- offline/packages/eventplane/EpFinder.h
+++ offline/packages/eventplane/EpFinder.h
@@ -3,53 +3,51 @@
 #ifndef EVENTPLANE_EPFINDER_H
 #define EVENTPLANE_EPFINDER_H
 
+#include "TH2D.h"
 #include <TString.h>
-
+#include <string>
 #include <array>  // for array
 #include <vector>
 
 class EpInfo;
 
-/*
-\author Mike Lisa
-\date 23 June 2018
-\adjusted for sPHENIX by J. Lajoie
-\24 August 2019
-\Further adjustments for a number of sPHENIX
-\detectors by Ejiro
-\04 June 2022
-*/
-
 typedef struct
 {
   float nMip;
   double phi;
+  int ix;
+  int iy;
+  int wheel;
+  std::vector<std::pair<int,int>> *sameRing;
 
 } EpHit;
 
 class EpFinder
 {
  public:
-  EpFinder(int nEventTypeBins = 10, unsigned int order = 3);
+  EpFinder(int nEventTypeBins = 10, unsigned int order = 3, int ns = 0);
   ~EpFinder(){};
 
-  void SetnMipThreshold(const double thresh) { mThresh = thresh; };
-  void SetMaxTileWeight(const double MAX) { mMax = MAX; };
-  void Results(const std::vector<EpHit> &EpdHits, int EventTypeID, EpInfo *epinfo);
+  void Finish(int thishist = 0);
+  void Results(const std::vector<EpHit> &Hits, int EventTypeID, EpInfo *epinfo);
+  void ResultsEPD(const std::vector<EpHit> &EpdHits, int EventTypeID, EpInfo *epinfo);
   TString Report();
   void ResetEvent();  // clear current event for safety
 
  private:
   double GetPsiInRange(const double Qx, const double Qy, const unsigned int order) const;
-  double mThresh = 0.;
-  double mMax = 100.;
   int mNumberOfEventTypeBins = 0;
   unsigned int m_MaxOrder = 0;
 
-  std::vector<std::array<double, 1>> TotalWeight4Side;
+  std::vector<std::array<double, 2>> TotalWeight4Side;
   std::vector<std::vector<double>> QrawOneSide;
-  std::vector<double> WheelSumWeightsRaw;
+  std::vector<std::vector<double>> QphiWeightedOneSide;
   std::vector<double> PsiRaw;
+  std::vector<double> PsiPhiWeighted;
+
+  TH2* mPhiWeightInput[2];
+  TH2* mPhiWeightOutput[2];
+  TH2* mPhiAveraged[2];
 };
 
 #endif  // EVENTPLANE_EPFINDER_H
