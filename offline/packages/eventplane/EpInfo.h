de8c0b91f68901a263b80c83783a548464b0d4de
Ejiro Umaka <ejironaomiumaka@gmail.com>
2023-06-06 11:46:42 -0400

    Update EpInfo.h

--- offline/packages/eventplane/EpInfo.h
+++ offline/packages/eventplane/EpInfo.h
@@ -15,15 +15,23 @@ class EpInfo : public PHObject
   void Reset() override { return; }
 
   virtual double RawPsi(unsigned int /* order */) const { return NAN; }
-
-  virtual double SWRaw(unsigned int /* order */) const { return NAN; }
+  
+  virtual double PhiWeightedPsi(unsigned int /* order */) const { return NAN; }
 
   virtual std::pair<double, double> RawQ(unsigned int /* order */) const { return std::make_pair(NAN, NAN); }
+    
+  virtual std::pair<double, double> PhiWeightedQ(unsigned int /* order */) const { return std::make_pair(NAN, NAN); }
 
   virtual unsigned int MaxOrder() const { return 0; }
+    
   virtual void CopyQrawOneSide(const std::vector<std::vector<double>>& /*vecvec */) { return; }
-  virtual void CopyWheelSumWeightsRaw(const std::vector<double>& /*vec */) { return; }
+    
+  virtual void CopyQphiWeightedOneSide(const std::vector<std::vector<double>>& /*vecvec */) { return; }
+       
   virtual void CopyPsiRaw(const std::vector<double>& /*vec */) { return; }
+    
+  virtual void CopyPsiPhiWeighted(const std::vector<double>& /*vec */) { return; }
+
 
  private:
   ClassDefOverride(EpInfo, 1);
