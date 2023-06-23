00eea7d4cc11d7ae4bd0a7779f4ac54634bfe069
Anthony Denis Frawley <afrawley@fsu.edu>
2023-06-22 21:37:24 -0400

    Add braces requested by clang.

--- offline/packages/TrackerMillepedeAlignment/AlignmentDefs.h
+++ offline/packages/TrackerMillepedeAlignment/AlignmentDefs.h
@@ -44,12 +44,12 @@ namespace AlignmentDefs
   static constexpr int nstaves_layer_mvtx[3] = {12, 16, 20};
 
   static constexpr int clamshell_stave_list[3][2][10] = {    // [layer][clamshell][stave]
-    3,4,5,6,7,8,0,0,0,0,
-    0,1,2,9,10,11,0,0,0,0,
-    4,5,6,7,8,9,10,11,0,0,
-    0,1,2,3,12,13,14,15,0,0,
-    5,6,7,8,9,10,11,12,13,14,
-    0,1,2,3,4,15,16,17,18,19 };
+    {{3,4,5,6,7,8,0,0,0,0},
+     {0,1,2,9,10,11,0,0,0,0}},
+    {{4,5,6,7,8,9,10,11,0,0},
+     {0,1,2,3,12,13,14,15,0,0}},
+    {{5,6,7,8,9,10,11,12,13,14},
+     {0,1,2,3,4,15,16,17,18,19 }} };
 
   int getTpcRegion(int layer);
   void getMvtxGlobalLabels(Surface surf, int glbl_label[], mvtxGrp grp);
