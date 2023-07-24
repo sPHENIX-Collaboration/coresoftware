4a80303e14c8a779f03effd8e4469dc0a54e21de
bogui56 <cer@mit.edu>
2023-07-24 13:23:01 -0400

    add options for processing real raw data for the first time

--- offline/packages/tpc/TpcClusterizer.h
+++ offline/packages/tpc/TpcClusterizer.h
@@ -39,13 +39,30 @@ class TpcClusterizer : public SubsysReco
   void set_do_hit_association(bool do_assoc){do_hit_assoc = do_assoc;}
   void set_do_wedge_emulation(bool do_wedge){ do_wedge_emulation = do_wedge;}
   void set_do_sequential(bool do_seq){ do_sequential = do_seq;}
-  void set_threshold(float val) { threshold = val;}
+  void set_do_split(bool split){ do_split = split;}
+  void set_seed_threshold(float val) { seed_threshold = val;}
+  void set_edge_threshold(float val) { edge_threshold = val;}
+  void set_min_err_squared(float val) { min_err_squared = val;}
+  void set_min_clus_size(float val) { min_clus_size = val;}
+  void set_min_adc_sum(float val) { min_adc_sum = val;}
   void set_remove_singles(bool do_sing){ do_singles = do_sing;}
   void set_read_raw(bool read_raw){ do_read_raw = read_raw;}
   void set_max_cluster_half_size_phi(unsigned short size) { MaxClusterHalfSizePhi = size ;}
   void set_max_cluster_half_size_z(unsigned short size) { MaxClusterHalfSizeT = size ;}
   void set_cluster_version(int value) { cluster_version = value; }
   void set_ClusHitsVerbose(bool set=true) { record_ClusHitsVerbose = set; };
+  void set_rawdata_reco() {
+    set_do_hit_association(false);
+    set_do_split(false);
+    set_seed_threshold(5);
+    set_edge_threshold(3);
+    set_min_err_squared(0);
+    set_min_clus_size(0);
+    set_min_adc_sum(5);
+    set_remove_singles(false);
+    set_max_cluster_half_size_phi(5);
+    set_max_cluster_half_size_z(8);
+  };
   ClusHitsVerbosev1* mClusHitsVerbose { nullptr };
   
  private:
@@ -62,8 +79,13 @@ class TpcClusterizer : public SubsysReco
   bool do_sequential = false;
   bool do_read_raw = false;
   bool do_singles = false;
+  bool do_split = true;
   double pedestal = 74.4;
-  double threshold = 0;
+  double seed_threshold = 10;
+  double edge_threshold = 5;
+  double min_err_squared = 0.01;
+  double min_clus_size = 1;
+  double min_adc_sum = 10;
   double SectorFiducialCut = 0.5;
   unsigned short MaxClusterHalfSizePhi = 3;
   unsigned short MaxClusterHalfSizeT = 5;
