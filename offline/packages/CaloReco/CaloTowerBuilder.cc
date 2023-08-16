f1dea91d0aa971234e1fed887830e6d5fc16516f
Evan Croft <ewc223@lehigh.edu>
2023-08-16 11:52:14 -0400

    Masked empty channels 0-63, 78, 110

--- offline/packages/CaloReco/CaloTowerBuilder.cc
+++ offline/packages/CaloReco/CaloTowerBuilder.cc
@@ -164,6 +164,17 @@ int CaloTowerBuilder::process_event(PHCompositeNode *topNode)
         }
         for (int channel = 0; channel < nchannels; channel++)
         {
+          //mask empty channels
+            
+          if(m_dettype == CaloTowerBuilder::EPD){
+            /*
+            if (pid > 9001) {
+              continue;
+            }*/
+            if (pid == 9001 && (channel < 64 || channel == 78 || channel == 110)) {
+              continue;
+            }
+          }
           std::vector<float> waveform;
           waveform.reserve(m_nsamples);
           for (int samp = 0; samp < m_nsamples; samp++)
@@ -177,12 +188,6 @@ int CaloTowerBuilder::process_event(PHCompositeNode *topNode)
         {
           for (int channel = 0; channel < m_nchannels - nchannels; channel++)
           {
-            //mask empty channels
-            if (m_dettype == CaloTowerBuilder::EPD && pid == 9001 && (channel < 64 || channel == 78 || channel == 110)) {
-              continue;
-            }
-
-
             std::vector<float> waveform;
             waveform.reserve(m_nsamples);
             for (int samp = 0; samp < m_nzerosuppsamples; samp++)
