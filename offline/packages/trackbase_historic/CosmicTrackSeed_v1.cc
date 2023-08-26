f89544bb3571a78332752182fd9d79bee155bae5
Joe Osborn <osbornjd91@gmail.com>
2023-08-26 17:56:12 -0400

    add cosmic track seed object

---
+++ offline/packages/trackbase_historic/CosmicTrackSeed_v1.cc
@@ -0,0 +1,38 @@
+#include "CosmicTrackSeed_v1.h"
+
+CosmicTrackSeed_v1::CosmicTrackSeed_v1()
+{}
+
+// have to suppress uninitMenberVar from cppcheck since it triggers many false positive
+// cppcheck-suppress uninitMemberVar
+CosmicTrackSeed_v1::CosmicTrackSeed_v1(const CosmicTrackSeed_v1& seed)
+{ CosmicTrackSeed_v1::CopyFrom( seed ); }
+
+CosmicTrackSeed_v1& CosmicTrackSeed_v1::operator=(const CosmicTrackSeed_v1& seed)
+{ if( this != &seed ) CopyFrom( seed ); return *this; }
+
+CosmicTrackSeed_v1::~CosmicTrackSeed_v1()
+{}
+
+void CosmicTrackSeed_v1::CopyFrom( const TrackSeed& seed )
+{
+  if( this == &seed ) return;
+  TrackSeed::CopyFrom( seed );
+
+  m_silicon_seed1 = seed.get_silicon_seed_index();
+  m_tpc_seed1 = seed.get_tpc_seed_index();
+  m_silicon_seed2 = seed.get_silicon_seed_index2();
+  m_tpc_seed2 = seed.get_tpc_seed_index2();
+
+}
+
+void CosmicTrackSeed_v1::identify(std::ostream& os) const
+{
+
+  os << "CosmicTrackSeed_v1 object ";
+  os << "Silicon seed index1: " << m_silicon_seed1;
+  os << "tpc seed index1: " << m_tpc_seed1;
+  os << "Silicon seed index2: " << m_silicon_seed2;
+  os << "tpc seed index2: " << m_tpc_seed2;
+
+}
---
+++ offline/packages/trackbase_historic/CosmicTrackSeed_v1.h
@@ -0,0 +1,48 @@
+#ifndef TRACKBASEHISTORIC_COSMICTRACKSEED_V1_H
+#define TRACKBASEHISTORIC_COSMICTRACKSEED_V1_H
+
+#include <phool/PHObject.h>
+
+#include "TrackSeed.h"
+
+#include <cmath>
+#include <iostream>
+
+class CosmicTrackSeed_v1 : public TrackSeed
+{
+ public: 
+  CosmicTrackSeed_v1();
+  ~CosmicTrackSeed_v1() override;
+  
+  CosmicTrackSeed_v1( const CosmicTrackSeed_v1& );
+  CosmicTrackSeed_v1& operator=(const CosmicTrackSeed_v1& seed);
+ 
+  void identify(std::ostream& os = std::cout) const override;
+  void Reset() override { *this = CosmicTrackSeed_v1(); }
+  int isValid() const override { return 1; }
+  void CopyFrom( const TrackSeed&) override;
+  void CopyFrom( TrackSeed* seed) override { CopyFrom( *seed ); }
+  PHObject* CloneMe() const override { return new CosmicTrackSeed_v1(*this); }
+
+  unsigned int get_silicon_seed_index() const override { return m_silicon_seed1; }
+  unsigned int get_tpc_seed_index() const override { return m_tpc_seed1; }
+  void set_silicon_seed_index(const unsigned int index) override { m_silicon_seed1 = index; }
+  void set_tpc_seed_index(const unsigned int index) override { m_tpc_seed1 = index; }
+  unsigned int get_silicon_seed_index2() const override { return m_silicon_seed2; }
+  unsigned int get_tpc_seed_index2() const override { return m_tpc_seed2; }
+  void set_silicon_seed_index2(const unsigned int index) override { m_silicon_seed2 = index; }
+  void set_tpc_seed_index2(const unsigned int index) override { m_tpc_seed2 = index; }
+
+
+ private:
+
+  unsigned int m_silicon_seed1 = std::numeric_limits<unsigned int>::max();
+  unsigned int m_tpc_seed1 = std::numeric_limits<unsigned int>::max();
+  unsigned int m_silicon_seed2 = std::numeric_limits<unsigned int>::max();
+  unsigned int m_tpc_seed2 = std::numeric_limits<unsigned int>::max();
+
+  ClassDefOverride(CosmicTrackSeed_v1, 1);
+
+};
+
+#endif 
