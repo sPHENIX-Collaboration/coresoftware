33be210955d8a6a2acb76a43c6450f30ab75f98a
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-09-07 01:13:57 -0400

    Add InttHit, InttHitv1 class

---
+++ offline/framework/ffarawobjects/InttHitContainer.h
@@ -0,0 +1,22 @@
+#ifndef FUN4ALLRAW_INTTHITCONTAINER_H
+#define FUN4ALLRAW_INTTHITCONTAINER_H
+
+#include <phool/PHObject.h>
+
+class InttHit;
+
+class  InttHitContainer: public PHObject  
+{
+public:
+  InttHitContainer() = default;
+  virtual ~InttHitContainer() = default;
+
+  virtual void AddInttHit(InttHit *) {return;}
+  virtual unsigned int get_nhits() {return 0;}
+  virtual InttHit *get_hit(unsigned int) {return nullptr;}
+
+private:
+  ClassDefOverride(InttHitContainer,1)    
+};
+
+#endif
