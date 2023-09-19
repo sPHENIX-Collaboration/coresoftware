c2ccaf7284d8aae7ab9520cb32fa9c2e7222ecd7
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-09-19 07:42:50 -0400

    clang-format to apply our coding conventions

--- offline/packages/intt/InttCombinedRawDataConverter.h
+++ offline/packages/intt/InttCombinedRawDataConverter.h
@@ -3,43 +3,43 @@
 
 #include <fun4all/SubsysReco.h>
 
-#include <TSystem.h>
 #include <TFile.h>
+#include <TSystem.h>
 #include <TTree.h>
 
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
 
 class InttCombinedRawDataConverter : public SubsysReco
 {
-public:
-	InttCombinedRawDataConverter(std::string const& name = "InttRawDataConverter");
+ public:
+  InttCombinedRawDataConverter(std::string const& name = "InttRawDataConverter");
 
-	int SetOutputFile(std::string const&);
-	int WriteOutputFile();
+  int SetOutputFile(std::string const&);
+  int WriteOutputFile();
 
-	int Init(PHCompositeNode*) override;
-	int InitRun(PHCompositeNode*) override;
-	int process_event(PHCompositeNode*) override;
-	int End(PHCompositeNode*) override;
+  int Init(PHCompositeNode*) override;
+  int InitRun(PHCompositeNode*) override;
+  int process_event(PHCompositeNode*) override;
+  int End(PHCompositeNode*) override;
 
-private:
-	std::string m_InttRawNodeName = "INTTRAWHIT";
+ private:
+  std::string m_InttRawNodeName = "INTTRAWHIT";
 
-	TFile* file = nullptr;
-	TTree* tree = nullptr;
+  TFile* file = nullptr;
+  TTree* tree = nullptr;
 
-	Int_t n_evt = 0;
-	Int_t num_hits = 0;
+  Int_t n_evt = 0;
+  Int_t num_hits = 0;
 
-	typedef std::map<std::string, std::vector<Int_t>*> Branches_i_t;
-	typedef std::map<std::string, std::vector<Long64_t>*> Branches_l_t;
-	typedef std::map<std::string, std::vector<Double_t>*> Branches_d_t;
-	Branches_i_t branches_i;
-	Branches_l_t branches_l;
-	Branches_d_t branches_d;
+  typedef std::map<std::string, std::vector<Int_t>*> Branches_i_t;
+  typedef std::map<std::string, std::vector<Long64_t>*> Branches_l_t;
+  typedef std::map<std::string, std::vector<Double_t>*> Branches_d_t;
+  Branches_i_t branches_i;
+  Branches_l_t branches_l;
+  Branches_d_t branches_d;
 };
 
-#endif//INTT_INTTCOMBINEDRAWDATACONVERTER_H
+#endif  // INTT_INTTCOMBINEDRAWDATACONVERTER_H
