394eb9fa221ce8751486b22d13168920eb253438
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-10-02 14:07:55 -0400

    removed remaining mentions of TPC

--- offline/framework/ffarawobjects/MicromegasRawHitContainerv1.cc
+++ offline/framework/ffarawobjects/MicromegasRawHitContainerv1.cc
@@ -3,11 +3,11 @@
 
 #include <TClonesArray.h>
 
-static const int NTPCHITS = 100;
+static constexpr int NHITS = 100;
 
 MicromegasRawHitContainerv1::MicromegasRawHitContainerv1()
 {
-  MicromegasRawHitsTCArray = new TClonesArray("MicromegasRawHitv1",NTPCHITS);
+  MicromegasRawHitsTCArray = new TClonesArray("MicromegasRawHitv1",NHITS);
 }
 
 MicromegasRawHitContainerv1::~MicromegasRawHitContainerv1()
@@ -19,17 +19,17 @@ MicromegasRawHitContainerv1::~MicromegasRawHitContainerv1()
 void MicromegasRawHitContainerv1::Reset()
 {
  MicromegasRawHitsTCArray->Clear();
- MicromegasRawHitsTCArray->Expand(NTPCHITS);
+ MicromegasRawHitsTCArray->Expand(NHITS);
 }
 
 void MicromegasRawHitContainerv1::identify(std::ostream &os) const
 {
   os << "MicromegasRawHitContainerv1" << std::endl;
-  os << "containing " << MicromegasRawHitsTCArray->GetEntries() << " Tpc hits" << std::endl;
-  MicromegasRawHit *tpchit = static_cast< MicromegasRawHit *> (MicromegasRawHitsTCArray->At(0));
-  if (tpchit)
+  os << "containing " << MicromegasRawHitsTCArray->GetEntries() << " Micromegas hits" << std::endl;
+  auto hit = static_cast< MicromegasRawHit *> (MicromegasRawHitsTCArray->At(0));
+  if (hit)
    {
-     os << "for beam clock: " << std::hex << tpchit->get_bco() << std::dec << std::endl;
+     os << "for beam clock: " << std::hex << hit->get_bco() << std::dec << std::endl;
    }
 }
 
@@ -49,13 +49,13 @@ MicromegasRawHit *MicromegasRawHitContainerv1::AddHit()
   return newhit;
 }
 
-MicromegasRawHit *MicromegasRawHitContainerv1::AddHit(MicromegasRawHit *tpchit)
+MicromegasRawHit *MicromegasRawHitContainerv1::AddHit(MicromegasRawHit *source)
 {
-  MicromegasRawHit *newhit = new ((*MicromegasRawHitsTCArray)[MicromegasRawHitsTCArray->GetLast()+1]) MicromegasRawHitv1(tpchit);
+  auto newhit = new ((*MicromegasRawHitsTCArray)[MicromegasRawHitsTCArray->GetLast()+1]) MicromegasRawHitv1(source);
   return newhit;
 }
 
 MicromegasRawHit *MicromegasRawHitContainerv1::get_hit(unsigned int index)
 {
-  return (MicromegasRawHit *) MicromegasRawHitsTCArray->At(index);
+  return static_cast<MicromegasRawHit*>( MicromegasRawHitsTCArray->At(index) );
 }
