6bb7fe8012aa5f4289bec009566a316ca81e4a54
mchiu-bnl <chiu@bnl.gov>
2023-10-04 13:31:37 -0400

    changes for using calibrations from CDB

--- offline/packages/bbc/BbcDefs.h
+++ offline/packages/bbc/BbcDefs.h
@@ -1,16 +1,15 @@
 #ifndef __BBC_BBCDEFS_H__
 #define __BBC_BBCDEFS_H__
 
-#include <TMath.h>
-#include <iostream>
-
 namespace BbcDefs
 {
-  const Double_t index_refract = 1.4585;
-  const Double_t v_ckov = 1.0 / index_refract;  // velocity threshold for CKOV
-  const Double_t C = 29.9792458;                // cm/ns
+  const double index_refract = 1.4585;        // quartz radiator index of refraction
+  const double v_ckov = 1.0 / index_refract;  // velocity threshold for CKOV
+  const double C = 29.9792458;                // cm/ns
 
   const int BBC_N_PMT = 128;
+  const int BBC_N_FEECH = 256;
+  const int MAX_SAMPLES = 31;
 
 }  // namespace BbcDefs
 
