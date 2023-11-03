c68401588993a0929dd355845f9f87bcc42b13d9
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-11-03 11:18:07 -0400

    ignore non DATAEVENT events

--- offline/framework/fun4allraw/SingleMicromegasPoolInput.cc
+++ offline/framework/fun4allraw/SingleMicromegasPoolInput.cc
@@ -98,7 +98,10 @@ void SingleMicromegasPoolInput::FillPool(const unsigned int /*nbclks*/)
     { evt->identify(); }
 
     if (evt->getEvtType() != DATAEVENT)
-    { m_NumSpecialEvents++; }
+    { 
+      m_NumSpecialEvents++; 
+      continue;
+    }
 
     const int EventSequence = evt->getEvtSequence();
     const int npackets = evt->getPacketList(plist, 10);
