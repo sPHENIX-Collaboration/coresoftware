1ad724b549a01d4887a28dfc77ca4e887cc117d1
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-12-06 10:13:45 -0500

    use (layer,tile,strip) rather than (fee,channel) to locate hot channels

--- offline/packages/micromegas/MicromegasHotChannelMapData.cc
+++ offline/packages/micromegas/MicromegasHotChannelMapData.cc
@@ -15,9 +15,9 @@
 namespace
 {
   static const std::string m_total_entries_key = "total_entries";
-  static const std::string m_fee_id_key = "fee_id";
-  static const std::string m_channel_id_key = "channel_id";
-  static constexpr int m_nchannels_fee = 256;
+  static const std::string m_layer_id_key = "layer_id";
+  static const std::string m_tile_id_key = "tile_id";
+  static const std::string m_strip_id_key = "strip_id";
 }
 
 //________________________________________________________________________-
@@ -25,10 +25,10 @@ std::ostream& operator << (std::ostream& out, const MicromegasHotChannelMapData&
 {
   out << "MicromegasHotChannelMapData" << std::endl;
 
+  out << "total entries: " << calib_data.m_hot_channel_map.size() << std::endl;
   for( const auto& channel_id:calib_data.m_hot_channel_map )
   { out << "  " << channel_id << std::endl; }
 
-  out << "total entries: " << calib_data.m_hot_channel_map.size() << std::endl;
   return out;
 }
 
@@ -57,19 +57,20 @@ void MicromegasHotChannelMapData::read( const std::string& filename )
   const int m_total_entries = cdbttree.GetSingleIntValue( m_total_entries_key );
   for( int i = 0; i < m_total_entries; ++ i )
   {
-
     // read channel id
-    const int fee_id = cdbttree.GetIntValue( i, m_fee_id_key );
-    const int channel_id = cdbttree.GetIntValue( i, m_channel_id_key );
-    if( std::isnan(fee_id) || std::isnan( channel_id ) ) continue;
+    const int layer_id = cdbttree.GetIntValue( i, m_layer_id_key );
+    const int tile_id = cdbttree.GetIntValue( i, m_tile_id_key );
+    const int strip_id = cdbttree.GetIntValue( i, m_strip_id_key );
+    if( std::isnan(layer_id) || std::isnan(tile_id) || std::isnan(strip_id) )
+    { continue; }
 
-    m_hot_channel_map.emplace( fee_id, channel_id );
+    m_hot_channel_map.emplace( layer_id, tile_id, strip_id );
   }
 }
 
 //________________________________________________________________________-
-void MicromegasHotChannelMapData::add_hot_channel( int fee, int channel )
-{ m_hot_channel_map.emplace( fee, channel ); }
+void MicromegasHotChannelMapData::add_hot_channel( int layer, int tile, int strip )
+{ m_hot_channel_map.emplace( layer, tile, strip ); }
 
 //________________________________________________________________________-
 void MicromegasHotChannelMapData::write( const std::string& filename ) const
@@ -84,16 +85,18 @@ void MicromegasHotChannelMapData::write( const std::string& filename ) const
   int index = 0;
   for( const auto& channel_id:m_hot_channel_map )
   {
-    cdbttree.SetIntValue( index, m_fee_id_key, channel_id.m_fee );
-    cdbttree.SetIntValue( index, m_channel_id_key, channel_id.m_channel );
+    cdbttree.SetIntValue( index, m_layer_id_key, channel_id.m_layer );
+    cdbttree.SetIntValue( index, m_tile_id_key, channel_id.m_tile );
+    cdbttree.SetIntValue( index, m_strip_id_key, channel_id.m_strip );
     ++index;
   }
 
   // commit and write
   cdbttree.Commit();
+  cdbttree.CommitSingle();
   cdbttree.WriteCDBTTree();
 }
 
 //________________________________________________________________________-
-bool MicromegasHotChannelMapData::is_hot_channel( int fee, int channel ) const
-{ return m_hot_channel_map.find( {fee, channel } ) != m_hot_channel_map.end(); }
+bool MicromegasHotChannelMapData::is_hot_channel( int layer, int tile, int strip ) const
+{ return m_hot_channel_map.find({layer, tile, strip}) != m_hot_channel_map.end(); }
