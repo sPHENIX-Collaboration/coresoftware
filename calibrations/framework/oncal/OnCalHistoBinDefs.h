0dab870ddea795528e7aabda5a470b6c7d6a8111
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-12-07 12:52:49 -0500

    add OnCal from PHENIX

---
+++ calibrations/framework/oncal/OnCalHistoBinDefs.h
@@ -0,0 +1,13 @@
+#ifndef __ONCALHISTOBINDEFS_H__
+#define __ONCALHISTOBINDEFS_H__
+
+namespace OnCalHistoBinDefs
+{
+  enum {FIRSTRUNBIN = 1,
+        LASTRUNBIN,
+	BORTIMEBIN,
+	EORTIMEBIN,
+        LASTBINPLUSONE};
+};
+
+#endif /* __ONCALHISTOBINDEFS_H__ */
