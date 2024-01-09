a40c11bbfe268e11e113c6adb67016cc8375d95e
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-01-09 12:44:38 -0500

    add some explanation for return codes

--- offline/framework/fun4all/Fun4AllReturnCodes.h
+++ offline/framework/fun4all/Fun4AllReturnCodes.h
@@ -3,6 +3,22 @@
 #ifndef FUN4ALL_FUN4ALLRETURNCODES_H
 #define FUN4ALL_FUN4ALLRETURNCODES_H
 
+// In general negative return codes signal some fatal condition where continuing is just a waste of cpu
+
+// SubsysReco module return codes:
+// ABORTRUN: signals that processing should be aborted for this run, the process will exit with a non zero exit
+// ABORTEVENT: abort reconstruction of the current event, reset everything and process the next event
+// EVENT_OK: generic good return
+// DISCARDEVENT: tell a connected output manager not to save this event but continue processing
+// DONOTREGISTERSUBSYSTEM: during registration, module indicates it doesn't want to be registered
+
+// Synchronization
+// SYNC_OK: all good
+// SYNC_FAIL: synchronization failed (leads to a search for matching events by the sync manager)
+// SYNC_NOOBJECT: no synchronization object in this input manager, take current event without check
+// RESET_NODE_TREE: internal use - under certain conditions during the synchronization process Fun4All needs to reset the node tree to clear the prvious event
+
+
 namespace Fun4AllReturnCodes
 {
   enum
@@ -19,4 +35,4 @@ namespace Fun4AllReturnCodes
   };
 }
 
-#endif /* __FUN4ALLRETURNCODES_H__ */
+#endif /* FUN4ALL_FUN4ALLRETURNCODES_H */
