8c50cfc2048325ff89329a9354462dca938c63a0
Ejiro Umaka <ejironaomiumaka@gmail.com>
2024-01-22 20:31:01 -0500

    Update EventPlaneReco.h

--- offline/packages/eventplaneinfo/EventPlaneReco.h
+++ offline/packages/eventplaneinfo/EventPlaneReco.h
@@ -77,10 +77,7 @@ class EventPlaneReco : public SubsysReco
     
   float _epd_e  = 6.0;
   float _mbd_e = 10.0;
-  float mbd_e_north;
-  float mbd_e_south;
-  float mbdQ;
- 
+
   TProfile * tprof_mean_cos_north_mbd[6] = {};
   TProfile * tprof_mean_sin_north_mbd[6] = {};
   TProfile * tprof_mean_cos_south_mbd[6] = {};
