42574146e461b80831f931276a155f07883ea826
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-03-07 17:23:15 -0500

    Add Gl1 packet check

---
+++ offline/framework/ffarawmodules/Gl1Check.cc
@@ -0,0 +1,48 @@
+#include "Gl1Check.h"
+
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllReturnCodes.h>
+#include <fun4all/SubsysReco.h>  // for SubsysReco
+
+#include <ffarawobjects/Gl1Packet.h>
+
+#include <phool/PHCompositeNode.h>
+#include <phool/PHDataNode.h>
+#include <phool/PHNode.h>          // for PHNode
+#include <phool/PHNodeIterator.h>  // for PHNodeIterator
+#include <phool/getClass.h>
+
+#include <Event/packet.h>
+
+#include <TSystem.h>
+
+#include <iostream>  // for operator<<, endl, basic_ost...
+#include <utility>   // for pair
+#include <vector>    // for vector
+
+//____________________________________________________________________________..
+Gl1Check::Gl1Check(const std::string &name)
+: SubsysReco(name)
+{
+}
+
+//____________________________________________________________________________..
+int Gl1Check::Init(PHCompositeNode *topNode)
+{
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+//____________________________________________________________________________..
+int Gl1Check::process_event(PHCompositeNode *topNode)
+{
+  Gl1Packet *gl1cont = findNode::getClass<Gl1Packet>(topNode,"GL1Packet");
+  if (!gl1cont)
+  {
+    std::cout << "could not find Gl1Packet node" << std::endl;
+  }
+  else
+  {
+    gl1cont->identify();
+  }
+  return Fun4AllReturnCodes::EVENT_OK;
+}
---
+++ offline/framework/ffarawmodules/Gl1Check.h
@@ -0,0 +1,34 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef FFARAWMODULES_GL1CHECK_H
+#define FFARAWMODULES_GL1CHECK_H
+
+#include <fun4all/SubsysReco.h>
+
+#include <set>
+#include <string>
+
+class Fun4AllInputManager;
+class PHCompositeNode;
+
+class Gl1Check : public SubsysReco
+{
+ public:
+  Gl1Check(const std::string &name = "Gl1Check");
+
+  ~Gl1Check() override {}
+
+  int Init(PHCompositeNode *topNode) override;
+
+  int process_event(PHCompositeNode *topNode) override;
+
+//  int ResetEvent(PHCompositeNode *topNode) override;
+
+//  void MyEvtNode(const std::string &name) {m_EvtNodeName = name;}
+
+ private:
+  /* std::string m_EvtNodeName = "GL1RAWHIT"; */
+  /* std::set<uint64_t> bclk_seen; */
+};
+
+#endif  // FFARAWMODULES_GL1CHECK_H
