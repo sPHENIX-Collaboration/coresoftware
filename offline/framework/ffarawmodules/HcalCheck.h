e2d59d31f8d9e597dabc92abaac23a3ea421d1e7
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-03-13 12:36:04 -0400

    add HcalCheck

---
+++ offline/framework/ffarawmodules/HcalCheck.h
@@ -0,0 +1,31 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef FFARAWMODULES_HCALCHECK_H
+#define FFARAWMODULES_HCALCHECK_H
+
+#include <fun4all/SubsysReco.h>
+
+#include <set>
+#include <string>
+
+class Fun4AllInputManager;
+class PHCompositeNode;
+
+class HcalCheck : public SubsysReco
+{
+ public:
+  HcalCheck(const std::string &name = "HcalCheck");
+
+  ~HcalCheck() override {}
+
+  int Init(PHCompositeNode *topNode) override;
+
+  int process_event(PHCompositeNode *topNode) override;
+
+
+ private:
+  /* std::string m_EvtNodeName = "HCALRAWHIT"; */
+  /* std::set<uint64_t> bclk_seen; */
+};
+
+#endif  // FFARAWMODULES_HCALCHECK_H
