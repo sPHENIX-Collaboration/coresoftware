f9d7376042a83acbbd8277fb0d61a21216d9f737
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-03-21 21:13:05 -0400

    clang-format for xingshift

--- calibrations/xingshift/XingShiftCal.h
+++ calibrations/xingshift/XingShiftCal.h
@@ -17,11 +17,9 @@ class XingShiftCal : public SubsysReco
   static const int NBUNCHES = 120;
 
  public:
-
   XingShiftCal(const std::string &name = "XingShiftCal");
 
   ~XingShiftCal() override;
-
 
   int Init(PHCompositeNode *topNode) override;
   int InitRun(PHCompositeNode *topNode) override;
@@ -33,12 +31,10 @@ class XingShiftCal : public SubsysReco
 
   void Print(const std::string &what = "ALL") const override;
 
-  int Calibrate(const int final=0);
-  int CalculateCrossingShift(int& xingshift, uint64_t counts[NTRIG][NBUNCHES], bool& success);
-
+  int Calibrate(const int final = 0);
+  int CalculateCrossingShift(int &xingshift, uint64_t counts[NTRIG][NBUNCHES], bool &success);
 
  private:
-
   Packet *p = nullptr;
   Packet *pBlueSpin = nullptr;
   Packet *pYellSpin = nullptr;
@@ -47,24 +43,19 @@ class XingShiftCal : public SubsysReco
   int evtcap = 50000;
   int done = 0;
 
-
-  
-
   const int packet_BLUESPIN = 14902;
   const int packet_YELLSPIN = 14903;
   const int packet_GL1 = 14001;
 
   bool success = 0;
 
-  //default xingshift
+  // default xingshift
   int xingshift = 5;
-
 
   int blueSpinPattern[NBUNCHES] = {0};
   int yellSpinPattern[NBUNCHES] = {0};
 
   uint64_t scalercounts[NTRIG][NBUNCHES]{};
-
 };
 
-#endif // XINGSHIFTCAL_H
+#endif  // XINGSHIFTCAL_H
