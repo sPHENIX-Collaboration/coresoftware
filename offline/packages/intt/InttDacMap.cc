319d8d3a40590bb521c5b2d8b336a0c25e46e114
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-03-28 08:11:53 -0400

    clang-format

--- offline/packages/intt/InttDacMap.cc
+++ offline/packages/intt/InttDacMap.cc
@@ -4,9 +4,8 @@
 
 #include <cdbobjects/CDBTTree.h>
 
-#include <iostream>
 #include <filesystem>
-
+#include <iostream>
 
 InttDacMap::InttDacMap()
 {
@@ -26,12 +25,12 @@ int InttDacMap::LoadFromCDB(std::string const& calibname)
   if (database.empty())
   {
     std::cout << "InttDacMap::LoadFromCDB(std::string const& name)" << std::endl;
-    std::cout << "\tArgument 'database' is empty string. calibname invalid :" << calibname <<std::endl;
+    std::cout << "\tArgument 'database' is empty string. calibname invalid :" << calibname << std::endl;
     return -1;
   }
 
-  std::cout<<"CDBFile: "<<database<<std::endl;
-  
+  std::cout << "CDBFile: " << database << std::endl;
+
   return LoadFromFile(database);
 }
 
@@ -43,7 +42,7 @@ int InttDacMap::LoadFromFile(std::string const& filename)
     std::cout << "\tArgument 'filename' is empty string" << std::endl;
     return -1;
   }
- 
+
   if (!std::filesystem::exists(filename))
   {
     std::cout << "int InttDacMap::LoadFromFile(std::string const& filename)" << std::endl;
@@ -53,13 +52,13 @@ int InttDacMap::LoadFromFile(std::string const& filename)
 
   CDBTTree cdbttree = CDBTTree(filename);
   cdbttree.LoadCalibrations();
-  
+
   return LoadFromCDBTTree(cdbttree);
 }
 
 int InttDacMap::WriteToFile(std::string const& filename)
 {
-  //std::cout<<"WriteMapToFile"<<std::endl;
+  // std::cout<<"WriteMapToFile"<<std::endl;
   CDBTTree cdbttree = CDBTTree(filename);
 
   FillToCDBTTree(cdbttree);
@@ -68,96 +67,99 @@ int InttDacMap::WriteToFile(std::string const& filename)
   cdbttree.CommitSingle();
   cdbttree.WriteCDBTTree();
 
-  //std::cout<<"WriteMapToFile completed"<<std::endl;
+  // std::cout<<"WriteMapToFile completed"<<std::endl;
   return 0;
 }
 
 int InttDacMap::LoadFromCDBTTree(CDBTTree& cdbttree)
 {
   ///////////////
-  std::cout<<"LoadFromCDBTTree::LoadFromCDBTTree"<<std::endl;
+  std::cout << "LoadFromCDBTTree::LoadFromCDBTTree" << std::endl;
 
   uint64_t N = cdbttree.GetSingleIntValue("size");
   for (uint64_t n = 0; n < N; ++n)
   {
-    int felix_server  = cdbttree.GetIntValue(n, "felix_server");
+    int felix_server = cdbttree.GetIntValue(n, "felix_server");
     int felix_channel = cdbttree.GetIntValue(n, "felix_channel");
-    int chip          = cdbttree.GetIntValue(n, "chip");
-    int adc           = cdbttree.GetIntValue(n, "adc");
-    int dac           = cdbttree.GetIntValue(n, "dac");
+    int chip = cdbttree.GetIntValue(n, "chip");
+    int adc = cdbttree.GetIntValue(n, "adc");
+    int dac = cdbttree.GetIntValue(n, "dac");
     m_dac[felix_server][felix_channel][chip][adc] = dac;
 
-
-    if(m_verbosity>0){
-      std::cout<<"felix_server"<<  felix_server<< " ";
-      std::cout<<"felix_channel"<< felix_channel<< " ";
-      std::cout<<"chip"<<          chip<< " ";
-      std::cout<<"adc"<<           adc<< " ";
-      std::cout<<"dac"<<           dac<< std::endl;
+    if (m_verbosity > 0)
+    {
+      std::cout << "felix_server" << felix_server << " ";
+      std::cout << "felix_channel" << felix_channel << " ";
+      std::cout << "chip" << chip << " ";
+      std::cout << "adc" << adc << " ";
+      std::cout << "dac" << dac << std::endl;
     }
   }
 
   return 0;
 }
 
-
-unsigned short InttDacMap::GetDAC(const uint& felix_server, 
+unsigned short InttDacMap::GetDAC(const uint& felix_server,
                                   const uint& felix_channel,
-                                  const uint& chip,  
-                                  const uint& /*channel*/, 
+                                  const uint& chip,
+                                  const uint& /*channel*/,
                                   const uint& adc)
 {
-  if(felix_server <m_dac.size() &&
-     felix_channel<m_dac[felix_server].size() &&
-     chip         <m_dac[felix_server][chip].size() &&
-     adc          <m_dac[felix_server][chip][adc].size())
+  if (felix_server < m_dac.size() &&
+      felix_channel < m_dac[felix_server].size() &&
+      chip < m_dac[felix_server][chip].size() &&
+      adc < m_dac[felix_server][chip][adc].size())
   {
     return m_dac[felix_server][felix_channel][chip][adc];
   }
-  else {
-    std::cout<<"Range Invalid :"
-             <<" "<<felix_server  
-             <<" "<<felix_channel
-             <<" "<<chip           
-             <<" "<<adc          
-             <<", return -1"<<std::endl;
-    
+  else
+  {
+    std::cout << "Range Invalid :"
+              << " " << felix_server
+              << " " << felix_channel
+              << " " << chip
+              << " " << adc
+              << ", return -1" << std::endl;
+
     return -1;
   }
 }
 
-unsigned short InttDacMap::GetDAC(InttNameSpace::RawData_s const& rawdata, const uint& adc) 
+unsigned short InttDacMap::GetDAC(InttNameSpace::RawData_s const& rawdata, const uint& adc)
 {
-  return GetDAC(rawdata.felix_server, 
-                rawdata.felix_channel, 
+  return GetDAC(rawdata.felix_server,
+                rawdata.felix_channel,
                 rawdata.chip,
-                rawdata.channel, 
-                adc); 
+                rawdata.channel,
+                adc);
 }
 
-unsigned short InttDacMap::GetDAC(InttNameSpace::Offline_s const& offline, const uint& adc) 
+unsigned short InttDacMap::GetDAC(InttNameSpace::Offline_s const& offline, const uint& adc)
 {
-  return GetDAC(InttNameSpace::ToRawData(offline), adc); 
+  return GetDAC(InttNameSpace::ToRawData(offline), adc);
 }
 
-void InttDacMap::SetDefault(const uint& Adc0, 
-                            const uint& Adc1, 
-                            const uint& Adc2, 
+void InttDacMap::SetDefault(const uint& Adc0,
+                            const uint& Adc1,
+                            const uint& Adc2,
                             const uint& Adc3,
-                            const uint& Adc4, 
-                            const uint& Adc5, 
-                            const uint& Adc6, 
+                            const uint& Adc4,
+                            const uint& Adc5,
+                            const uint& Adc6,
                             const uint& Adc7)
 {
-  //std::cout<<m_dac.size()<<std::endl;
-  for(auto& felix_channel : m_dac){
-    //std::cout<<"\t"<<felix_channel.size()<<std::endl;
+  // std::cout<<m_dac.size()<<std::endl;
+  for (auto& felix_channel : m_dac)
+  {
+    // std::cout<<"\t"<<felix_channel.size()<<std::endl;
 
-    for(auto& chip : felix_channel){
-      //std::cout<<"\t\t"<<chip.size()<<std::endl;
+    for (auto& chip : felix_channel)
+    {
+      // std::cout<<"\t\t"<<chip.size()<<std::endl;
 
-      for(auto& adc : chip){
-        //std::cout<<"\t\t\t"<<adc.size()<<std::endl;
+      for (auto& adc : chip)
+      {
+        // std::cout<<"\t\t\t"<<adc.size()<<std::endl;
 
         adc[0] = Adc0;
         adc[1] = Adc1;
@@ -170,37 +172,40 @@ void InttDacMap::SetDefault(const uint& Adc0,
       }
     }
   }
-
 }
 
 void InttDacMap::FillToCDBTTree(CDBTTree& cdbttree)
 {
-  //std::cout<<"InttDacMap::FillToCDBTTree"<<std::endl;
+  // std::cout<<"InttDacMap::FillToCDBTTree"<<std::endl;
 
-  //std::cout<<m_dac.size()<<std::endl;
-  int n=0;
-  for(uint felix_server=0; felix_server<m_dac.size(); felix_server++){
-    for(uint felix_channel=0; felix_channel<m_dac[felix_server].size(); felix_channel++){
-      for(uint chip=0; chip<m_dac[felix_server][felix_channel].size(); chip++){
-        for(uint adc=0; adc<m_dac[felix_server][felix_channel][chip].size(); adc++){
+  // std::cout<<m_dac.size()<<std::endl;
+  int n = 0;
+  for (uint felix_server = 0; felix_server < m_dac.size(); felix_server++)
+  {
+    for (uint felix_channel = 0; felix_channel < m_dac[felix_server].size(); felix_channel++)
+    {
+      for (uint chip = 0; chip < m_dac[felix_server][felix_channel].size(); chip++)
+      {
+        for (uint adc = 0; adc < m_dac[felix_server][felix_channel][chip].size(); adc++)
+        {
           int dac = m_dac[felix_server][felix_channel][chip][adc];
-          cdbttree.SetIntValue(n, "felix_server",  felix_server);
+          cdbttree.SetIntValue(n, "felix_server", felix_server);
           cdbttree.SetIntValue(n, "felix_channel", felix_channel);
-          cdbttree.SetIntValue(n, "chip",          chip);
-          cdbttree.SetIntValue(n, "adc",           adc);
-          cdbttree.SetIntValue(n, "dac",           dac);
-          std::cout<<"felix_server"<<  felix_server<< " ";
-          std::cout<<"felix_channel"<< felix_channel<< " ";
-          std::cout<<"chip"<<          chip<< " ";
-          std::cout<<"adc"<<           adc<< " ";
-          std::cout<<"dac"<<           dac<< std::endl;
+          cdbttree.SetIntValue(n, "chip", chip);
+          cdbttree.SetIntValue(n, "adc", adc);
+          cdbttree.SetIntValue(n, "dac", dac);
+          std::cout << "felix_server" << felix_server << " ";
+          std::cout << "felix_channel" << felix_channel << " ";
+          std::cout << "chip" << chip << " ";
+          std::cout << "adc" << adc << " ";
+          std::cout << "dac" << dac << std::endl;
           n++;
         }
       }
     }
   }
   cdbttree.SetSingleIntValue("size", n);
-  //std::cout<<"size "<<           n<< std::endl;
+  // std::cout<<"size "<<           n<< std::endl;
 
-  //std::cout<<"InttDacMap::FillToCDBTTree done"<<std::endl;
+  // std::cout<<"InttDacMap::FillToCDBTTree done"<<std::endl;
 }
--- offline/packages/intt/InttDacMap.h
+++ offline/packages/intt/InttDacMap.h
@@ -8,50 +8,46 @@
 
 class CDBTTree;
 
-class InttDacMap {
-  public:
-    InttDacMap();
-    virtual ~InttDacMap(){}
+class InttDacMap
+{
+ public:
+  InttDacMap();
+  virtual ~InttDacMap() {}
 
-    virtual int LoadFromCDB( std::string const& calibname);
-    virtual int LoadFromFile(std::string const& filename);
-    virtual int WriteToFile( std::string const& filename); 
+  virtual int LoadFromCDB(std::string const& calibname);
+  virtual int LoadFromFile(std::string const& filename);
+  virtual int WriteToFile(std::string const& filename);
 
+  // Access by OnlineChannel
+  virtual unsigned short GetDAC(const uint& felix_server,
+                                const uint& felix_channel,
+                                const uint& chip,
+                                const uint& channel,
+                                const uint& adc);
+  virtual unsigned short GetDAC(InttNameSpace::RawData_s const& rawdata, const uint& adc);
+  virtual unsigned short GetDAC(InttNameSpace::Offline_s const& offline, const uint& adc);
 
-    // Access by OnlineChannel
-    virtual unsigned short GetDAC(const uint& felix_server, 
-                                  const uint& felix_channel,
-                                  const uint& chip,  
-                                  const uint& channel, 
-                                  const uint& adc);
-    virtual unsigned short GetDAC(InttNameSpace::RawData_s const& rawdata, const uint& adc); 
-    virtual unsigned short GetDAC(InttNameSpace::Offline_s const& offline, const uint& adc); 
+  virtual void Verbosity(int verbosity) { m_verbosity = verbosity; };
 
+  virtual void SetDefault(const uint& Adc0 = 15,
+                          const uint& Adc1 = 30,
+                          const uint& Adc2 = 60,
+                          const uint& Adc3 = 90,
+                          const uint& Adc4 = 120,
+                          const uint& Adc5 = 150,
+                          const uint& Adc6 = 180,
+                          const uint& Adc7 = 210);
 
-    virtual void Verbosity(int verbosity) { m_verbosity = verbosity; };
+ protected:
+  int LoadFromCDBTTree(CDBTTree& cdbttree);
+  void FillToCDBTTree(CDBTTree& cdbttree);
 
-    virtual void SetDefault(const uint& Adc0= 15, 
-                            const uint& Adc1= 30, 
-                            const uint& Adc2= 60, 
-                            const uint& Adc3= 90,
-                            const uint& Adc4=120, 
-                            const uint& Adc5=150, 
-                            const uint& Adc6=180, 
-                            const uint& Adc7=210);
+ private:
+  typedef std::array<std::array<std::array<std::array<int, 8>, 26>, 14>, 8> DacArray;
 
-  protected:
-    int  LoadFromCDBTTree(CDBTTree& cdbttree); 
-    void FillToCDBTTree(  CDBTTree& cdbttree);
+  DacArray m_dac{};  // [FELIX_SERVER:8][FELIX_CHANNEL:14][CHIP:26][DAC:8]
 
-
-  private:
-    typedef std::array< std::array< std::array< std::array<int, 8>, 26>, 14>, 8> DacArray;
-
-    DacArray m_dac {}; // [FELIX_SERVER:8][FELIX_CHANNEL:14][CHIP:26][DAC:8]
-
-    int    m_verbosity {0};
+  int m_verbosity{0};
 };
 
-
-
-#endif // INTT_INTTDACMAP_H
+#endif  // INTT_INTTDACMAP_H
