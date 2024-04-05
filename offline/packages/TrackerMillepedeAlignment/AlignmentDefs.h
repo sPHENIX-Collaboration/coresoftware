274c7ca796cf594a7a35ad7e487bba6bb3dd3e02
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-04-05 10:11:44 -0400

    clang-format for TrackerMillepedeAlignment

--- offline/packages/TrackerMillepedeAlignment/AlignmentDefs.h
+++ offline/packages/TrackerMillepedeAlignment/AlignmentDefs.h
@@ -44,15 +44,15 @@ namespace AlignmentDefs
   static constexpr int nstaves_layer_intt[4] = {12, 12, 16, 16};
   static constexpr int nstaves_layer_mvtx[3] = {12, 16, 20};
 
-  static constexpr int clamshell_stave_list[3][2][10] = {    // [layer][clamshell][stave]
-    {{3,4,5,6,7,8,0,0,0,0},
-     {0,1,2,9,10,11,0,0,0,0}},
-    {{4,5,6,7,8,9,10,11,0,0},
-     {0,1,2,3,12,13,14,15,0,0}},
-    {{5,6,7,8,9,10,11,12,13,14},
-     {0,1,2,3,4,15,16,17,18,19 }} };
+  static constexpr int clamshell_stave_list[3][2][10] = {  // [layer][clamshell][stave]
+      {{3, 4, 5, 6, 7, 8, 0, 0, 0, 0},
+       {0, 1, 2, 9, 10, 11, 0, 0, 0, 0}},
+      {{4, 5, 6, 7, 8, 9, 10, 11, 0, 0},
+       {0, 1, 2, 3, 12, 13, 14, 15, 0, 0}},
+      {{5, 6, 7, 8, 9, 10, 11, 12, 13, 14},
+       {0, 1, 2, 3, 4, 15, 16, 17, 18, 19}}};
 
-  static constexpr int glbl_vtx_label[NGLVTX] = {60000000,60000001,60000002};
+  static constexpr int glbl_vtx_label[NGLVTX] = {60000000, 60000001, 60000002};
 
   int getTpcRegion(int layer);
   void getMvtxGlobalLabels(const Surface& surf, int glbl_label[], mvtxGrp grp);
