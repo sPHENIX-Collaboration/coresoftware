912ef93951225c05086afd94a907ed88b7036a38
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-04-13 14:22:02 -0400

    clang-format

--- offline/packages/tpc/TpcRawDataTree.h
+++ offline/packages/tpc/TpcRawDataTree.h
@@ -49,59 +49,59 @@ class TpcRawDataTree : public SubsysReco
   }
 
  private:
-  TH1 *R1_hist {nullptr};
-  TH1 *R2_hist {nullptr};
-  TH1 *R3_hist {nullptr};
-  TH1 *TotalFEE {nullptr};
-  TH1 *TotalFEEsampa {nullptr};
-  TH1 *TotalFRAME {nullptr};
-  TH1 *checksumError_fee {nullptr};
-  TH1 *checksumError_feesampa {nullptr};
-  TH1 *checksumError_frame {nullptr};
+  TH1 *R1_hist{nullptr};
+  TH1 *R2_hist{nullptr};
+  TH1 *R3_hist{nullptr};
+  TH1 *TotalFEE{nullptr};
+  TH1 *TotalFEEsampa{nullptr};
+  TH1 *TotalFRAME{nullptr};
+  TH1 *checksumError_fee{nullptr};
+  TH1 *checksumError_feesampa{nullptr};
+  TH1 *checksumError_frame{nullptr};
 
-  TH2 *R1_time {nullptr};
-  TH2 *R2_time {nullptr};
-  TH2 *R3_time {nullptr};
+  TH2 *R1_time{nullptr};
+  TH2 *R2_time{nullptr};
+  TH2 *R3_time{nullptr};
 
-  TFile *m_file {nullptr};
+  TFile *m_file{nullptr};
 
-  TTree *m_SampleTree {nullptr};
-  TTree *m_PacketTree {nullptr};
-  TTree *m_TaggerTree {nullptr};
+  TTree *m_SampleTree{nullptr};
+  TTree *m_PacketTree{nullptr};
+  TTree *m_TaggerTree{nullptr};
 
-  uint64_t m_bco {0};
-  uint64_t m_last_bco {0};
+  uint64_t m_bco{0};
+  uint64_t m_last_bco{0};
 
-  double m_xPos {0.};
-  double m_yPos {0.};
+  double m_xPos{0.};
+  double m_yPos{0.};
 
-  uint32_t m_lvl1_count {0};
-  uint32_t m_endat_count {0};
+  uint32_t m_lvl1_count{0};
+  uint32_t m_endat_count{0};
 
-  int m_packet {0};
-  int m_frame {0};
-  int m_nWaveormInFrame {0};
-  int m_maxFEECount {0};
-  int m_nSamples {0};
-  int m_fee {0};
-  int m_sampaAddress {0};
-  int m_sampaChannel {0};
-  int m_Channel {0};
-  int m_BCO {0};
-  int m_checksum {0};
-  int m_checksumError {0};
-  int side {0};
-  int m_nTaggerInFrame {0};
-  int FEE_R[26] {2, 2, 1, 1, 1, 3, 3, 3, 3, 3, 3, 2, 2, 1, 2, 2, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3};
-  int FEE_map[26] {3, 2, 5, 3, 4, 0, 2, 1, 3, 4, 5, 7, 6, 2, 0, 1, 0, 1, 4, 5, 11, 9, 10, 8, 6, 7};
+  int m_packet{0};
+  int m_frame{0};
+  int m_nWaveormInFrame{0};
+  int m_maxFEECount{0};
+  int m_nSamples{0};
+  int m_fee{0};
+  int m_sampaAddress{0};
+  int m_sampaChannel{0};
+  int m_Channel{0};
+  int m_BCO{0};
+  int m_checksum{0};
+  int m_checksumError{0};
+  int side{0};
+  int m_nTaggerInFrame{0};
+  int FEE_R[26]{2, 2, 1, 1, 1, 3, 3, 3, 3, 3, 3, 2, 2, 1, 2, 2, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3};
+  int FEE_map[26]{3, 2, 5, 3, 4, 0, 2, 1, 3, 4, 5, 7, 6, 2, 0, 1, 0, 1, 4, 5, 11, 9, 10, 8, 6, 7};
 
-  uint16_t m_tagger_type {0};
+  uint16_t m_tagger_type{0};
 
-  uint8_t m_is_endat {0};
-  uint8_t m_is_lvl1 {0};
-  uint8_t m_modebits {0};
+  uint8_t m_is_endat{0};
+  uint8_t m_is_lvl1{0};
+  uint8_t m_modebits{0};
 
-  bool m_includeXYPos {true};
+  bool m_includeXYPos{true};
 
   std::string m_fname;
   std::string sectorNum;
