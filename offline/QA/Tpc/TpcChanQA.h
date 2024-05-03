a417e9c7fd24c51cb9d4c24a7d0f854fd8520529
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-05-03 07:39:43 -0400

    clang-tidy and cleanup for offline/QA/Tpc

--- offline/QA/Tpc/TpcChanQA.h
+++ offline/QA/Tpc/TpcChanQA.h
@@ -1,34 +1,33 @@
 // include guard to prevent TpcFFTQA.h from being preprocessed multiple times
-#ifndef TpcChanQA_H
-#define TpcChanQA_H
+#ifndef QA_TPC_TPCCHANQA_H
+#define QA_TPC_TPCCHANQA_H
 //
 
-// Inclusion of necessary header files
 #include <fun4all/SubsysReco.h>
+
 #include <string>
 #include <vector>
-#include <TFile.h>
-#include <TH1.h>
-#include <TH2.h>
-//
 
 // Call classes to be used in code
 class PHCompositeNode;
+class TFile;
+class TH1;
+class TH2;
 //
 
-class TpcChanQA : public SubsysReco // Inherit public parts of SubsysReco
+class TpcChanQA : public SubsysReco  // Inherit public parts of SubsysReco
 {
- // list of public attributes
+  // list of public methods
  public:
   // Function that sets file name and allocated memory to adcSamples
-  explicit TpcChanQA(const std::string &name="TpcChanQA.root");
+  explicit TpcChanQA(const std::string &name = "TpcChanQA.root");
 
   ~TpcChanQA() override = default;
 
   // Initialization of run: where you fetch data and place histograms that
   // care about run number
   int InitRun(PHCompositeNode *topNode) override;
-  
+
   // Where you do the desired work associated with the run (analysis)
   int process_event(PHCompositeNode *topNode) override;
 
@@ -41,25 +40,23 @@ class TpcChanQA : public SubsysReco // Inherit public parts of SubsysReco
     m_packets.push_back(packet);
   }
 
- // Define vector of packets as protected attribute 
- protected:
-  std::vector<int> m_packets;
-
-  // List of private attributes
+  // List of private members
  private:
-  std::string m_fname; // Name of file given to program
-  std::string sectorNum; // Sector number associated with data file
-  TFile *m_file = nullptr; // File being processed
-  TH1F *h_channel_hits = nullptr; // Histogram of hits per channel
-  TH2F *h_channel_ADCs = nullptr; // Histogram of ADC counts per channel
+  TFile *m_file{nullptr};        // File being processed
+  TH1 *h_channel_hits{nullptr};  // Histogram of hits per channel
+  TH2 *h_channel_ADCs{nullptr};  // Histogram of ADC counts per channel
 
-  int side = 0; // Face of the TPC (0==North && 1==South)
-  int m_packet = 0; // packet number
-  int m_nWaveformInFrame = 0; // Number of waveforms in a frame
-  int m_Channel = 0; // Channel number
-  int m_nSamples = 0; // Number of samples in waveform
+  int side{0};                // Face of the TPC (0==North && 1==South)
+  int m_packet{0};            // packet number
+  int m_nWaveformInFrame{0};  // Number of waveforms in a frame
+  int m_Channel{0};           // Channel number
+  int m_nSamples{0};          // Number of samples in waveform
 
-  std::vector<unsigned short> m_adcSamples; // ADC values in waveform
+  std::vector<int> m_packets;
+  std::vector<unsigned short> m_adcSamples;  // ADC values in waveform
+
+  std::string m_fname;    // Name of file given to program
+  std::string sectorNum;  // Sector number associated with data file
 };
 
-#endif //ends preprocessing if TpcFFTQA.h has already been defined
+#endif  // ends preprocessing if TpcFFTQA.h has already been defined
