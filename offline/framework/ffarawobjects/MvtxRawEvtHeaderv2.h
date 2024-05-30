3d65b0ce75cad9a88b94b9746fd66eadcabf8f4e
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2024-05-30 12:46:36 -0400

    ycm - add MvtxFeeIdInfo

--- offline/framework/ffarawobjects/MvtxRawEvtHeaderv2.h
+++ offline/framework/ffarawobjects/MvtxRawEvtHeaderv2.h
@@ -1,11 +1,14 @@
 #ifndef FUN4ALLRAW_MVTXRAWEVTHEADERV2_H
 #define FUN4ALLRAW_MVTXRAWEVTHEADERV2_H
 
-#include "MvtxRawEvtHeaderv1.h"
+#include "MvtxRawEvtHeader.h"
 
 #include <phool/PHObject.h>
 
-class MvtxRawEvtHeaderv2 : public MvtxRawEvtHeaderv1
+class MvtxFeeIdInfo;
+class TCloneArray;
+
+class MvtxRawEvtHeaderv2 : public MvtxRawEvtHeader
 {
  public:
   MvtxRawEvtHeaderv2() = default;
@@ -22,20 +25,17 @@ class MvtxRawEvtHeaderv2 : public MvtxRawEvtHeaderv1
   /// isValid returns non zero if object contains vailid data
   int isValid() const override;
 
-//  void AddFeeId(const int& feeid) override { m_MvtxFeeIdSet.insert(feeid); };
-  void AddDetField(const uint16_t& feeId,
-                   const uint32_t& detField) override { m_MvtxDetField[feeId] = detField; }
-  void AddL1Trg(const uint64_t& gtmL1_bco) override { m_MvtxL1TrgSet.insert(gtmL1_bco); };
+  MvtxFeeIdInfo *AddFeeIdInfo() override;
+  MvtxFeeIdInfo *AddFeeIdInfo(const MvtxFeeIdInfo *feeIdInfo) override;
 
-//  void AddFeeId(const std::set<uint16_t>& mvtxFeeIds) override;
-  void AddDetField(const std::map<uint16_t, uint32_t>& ) override;
+  void AddL1Trg(const uint64_t& gtmL1_bco) override { m_MvtxL1TrgSet.insert(gtmL1_bco); };
   void AddL1Trg(const std::set<uint64_t>& mvtxL1TrgSet) override;
 
-  std::map<uint16_t, uint32_t>& getMvtxDetField() override { return m_MvtxDetField; };
   std::set<uint64_t>& getMvtxLvL1BCO() override { return m_MvtxL1TrgSet; };
 
  private:
-  std::map<uint16_t, uint32_t> m_MvtxDetField;
+  TCloneArray *MvtxFeeIdInfoTCArray = nullptr;
+
   std::set<uint64_t> m_MvtxL1TrgSet;
 
   ClassDefOverride(MvtxRawEvtHeaderv2, 1)
