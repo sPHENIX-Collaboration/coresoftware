8557bcbfb833030b0ab64ace48bd2b506a3e1d64
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-06-07 17:46:57 -0400

    use c++ std::numeric_limits

--- offline/packages/intt/InttVertex.h
+++ offline/packages/intt/InttVertex.h
@@ -22,25 +22,25 @@ class InttVertex : public Vertex
   virtual unsigned int get_id() const override { return 0xFFFFFFFF; }
   virtual void set_id(unsigned int) override {}
 
-  virtual float get_x() const override { return NAN; }
+  virtual float get_x() const override { return std::numeric_limits<float>::quiet_NaN(); }
   virtual void set_x(float) override {}
 
-  virtual float get_y() const override { return NAN; }
+  virtual float get_y() const override { return std::numeric_limits<float>::quiet_NaN(); }
   virtual void set_y(float) override {}
 
-  virtual float get_z() const override { return NAN; }
+  virtual float get_z() const override { return std::numeric_limits<float>::quiet_NaN(); }
   virtual void set_z(float) override {}
 
-  virtual float get_position(unsigned int) const override { return NAN; }
+  virtual float get_position(unsigned int) const override { return std::numeric_limits<float>::quiet_NaN(); }
   virtual void set_position(unsigned int, float) override {}
 
-  virtual float get_error(unsigned int, unsigned int) const override { return NAN; }
+  virtual float get_error(unsigned int, unsigned int) const override { return std::numeric_limits<float>::quiet_NaN(); }
   virtual void set_error(unsigned int, unsigned int, float) override {}
 
-  virtual double get_chi2ndf() const { return NAN; }
+  virtual double get_chi2ndf() const { return std::numeric_limits<double>::quiet_NaN(); }
   virtual void set_chi2ndf(double) {}
 
-  virtual double get_width() const { return NAN; }
+  virtual double get_width() const { return std::numeric_limits<double>::quiet_NaN(); }
   virtual void set_width(double) {}
 
   virtual bool get_good() const { return false; }
@@ -55,10 +55,10 @@ class InttVertex : public Vertex
   virtual unsigned int get_ngroup() const { return 0; }
   virtual void set_ngroup(unsigned int) {}
 
-  virtual double get_peakratio() const { return NAN; }
+  virtual double get_peakratio() const { return std::numeric_limits<double>::quiet_NaN(); }
   virtual void set_peakratio(double) {}
 
-  virtual double get_peakwidth() const { return NAN; }
+  virtual double get_peakwidth() const { return std::numeric_limits<double>::quiet_NaN(); }
   virtual void set_peakwidth(double) {}
 
  protected:
