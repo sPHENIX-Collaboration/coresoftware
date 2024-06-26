78792da44b5ea543c2c0e876e5765cdb7a3562c8
Joe Osborn <osbornjd91@gmail.com>
2024-06-26 10:06:25 -0400

    use class members

--- offline/QA/Calorimeters/CaloValid.h
+++ offline/QA/Calorimeters/CaloValid.h
@@ -9,7 +9,7 @@
 class PHCompositeNode;
 class TH1;
 class TH2;
-
+class TProfile2D;
 class CaloValid : public SubsysReco
 {
  public:
@@ -52,6 +52,61 @@ class CaloValid : public SubsysReco
   TH1* h_cemc_channel_energy[128 * 192]{nullptr};
   TH1* h_ihcal_channel_energy[32 * 48]{nullptr};
   TH1* h_ohcal_channel_energy[32 * 48]{nullptr};
+  TH2* h_emcal_mbd_correlation{nullptr};
+  TH1* h_mbd_hits{nullptr};
+  TH2* h_ohcal_mbd_correlation{nullptr};
+  TH2* h_ihcal_mbd_correlation{nullptr};
+  TH2* h_emcal_hcal_correlation{nullptr};
+  TH2* h_cemc_etaphi{nullptr};
+  TH2* h_ihcal_etaphi{nullptr};
+  TH2* h_ohcal_etaphi{nullptr};
+  TH2* h_cemc_etaphi_wQA{nullptr};
+  TH2* h_ihcal_etaphi_wQA{nullptr};
+  TH2* h_ohcal_etaphi_wQA{nullptr};
+  TH1* h_ihcal_status{nullptr};
+  TH1* h_ohcal_status{nullptr};
+  TH1* h_cemc_status{nullptr};
+  TH2* h_cemc_e_chi2{nullptr};
+  TH2* h_ihcal_e_chi2{nullptr};
+  TH2* h_ohcal_e_chi2{nullptr};
+  TProfile2D* h_cemc_etaphi_time{nullptr};
+  TProfile2D* h_ihcal_etaphi_time{nullptr};
+  TProfile2D* h_ohcal_etaphi_time{nullptr};
+  TProfile2D* h_cemc_etaphi_fracHitADC{nullptr};
+  TProfile2D* h_cemc_etaphi_fracHit{nullptr};
+  TProfile2D* h_ihcal_etaphi_fracHitADC{nullptr};
+  TProfile2D* h_ohcal_etaphi_fracHitADC{nullptr};
+  TProfile2D* h_cemc_etaphi_pedRMS{nullptr};
+  TProfile2D* h_ihcal_etaphi_pedRMS{nullptr};
+  TProfile2D* h_ohcal_etaphi_pedRMS{nullptr};
+  TProfile2D* h_cemc_etaphi_ZSpedRMS{nullptr};
+  TProfile2D* h_ohcal_etaphi_ZSpedRMS{nullptr};
+  TProfile2D* h_ihcal_etaphi_ZSpedRMS{nullptr};
+  TProfile2D* h_cemc_etaphi_badChi2{nullptr};
+  TProfile2D* h_ohcal_etaphi_badChi2{nullptr};
+  TProfile2D* h_ihcal_etaphi_badChi2{nullptr};
+  TH1* h_InvMass{nullptr};
+  TH1* h_channel_pedestal_0{nullptr};
+  TH1* h_zdcSouthraw{nullptr};
+  TH1* h_zdcNorthraw{nullptr};
+  TH1* h_zdcSouthcalib{nullptr};
+  TH1* h_zdcNorthcalib{nullptr};
+  TH1* h_totalzdc_e{nullptr};
+  TH2* h_zdc_emcal_correlation{nullptr};
+  TH1* h_vtx_z_raw{nullptr};
+  TH1* h_vtx_z_cut{nullptr};
+  TH1* h_zdctime_cut{nullptr};
+  TH1* h_emcaltime_cut{nullptr};
+  TH1* h_ihcaltime_cut{nullptr};
+  TH1* h_ohcaltime_cut{nullptr};
+  TH1* h_zdctime{nullptr};
+  TH1* h_emcaltime{nullptr};
+  TH1* h_ihcaltime{nullptr};
+  TH1* h_ohcaltime{nullptr};
+  TH1* h_emcal_tower_e{nullptr};
+  TH2* h_etaphi_clus{nullptr};
+  TH1* h_clusE{nullptr};
+  TH1* h_triggerVec{nullptr};
 
   int _eventcounter{0};
   int _range{1};
