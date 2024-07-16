91194cc941d3a527765d78d1f6dee31d4d797ead
GreyBearKing <aopatton@rcas2061.rcf.bnl.gov>
2024-07-16 13:36:17 -0400

    Updated permissions and made Clang-Tidy change

--- offline/QA/KFParticle/QAKFParticle.cc
+++ offline/QA/KFParticle/QAKFParticle.cc
@@ -59,19 +59,19 @@ QAKFParticle::QAKFParticle(const std::string &name, const std::string &mother_na
   m_min_mass = min_m;
   m_max_mass = max_m;
   m_mother_id = kfpTools.getParticleID(mother_name);
-  //m_mother_name = mother_name;
+  // m_mother_name = mother_name;
 }
 
 int QAKFParticle::InitRun(PHCompositeNode *topNode)
 {
-    /*
-  if (!m_svtxEvalStack)
-  {
-    m_svtxEvalStack.reset(new SvtxEvalStack(topNode));
-    m_svtxEvalStack->set_strict(false);
-    m_svtxEvalStack->set_verbosity(Verbosity());
-  }
-  */
+  /*
+if (!m_svtxEvalStack)
+{
+  m_svtxEvalStack.reset(new SvtxEvalStack(topNode));
+  m_svtxEvalStack->set_strict(false);
+  m_svtxEvalStack->set_verbosity(Verbosity());
+}
+*/
   m_trackMap = findNode::getClass<SvtxTrackMap>(topNode, m_trackMapName);
   if (!m_trackMap)
   {
@@ -91,7 +91,6 @@ int QAKFParticle::Init(PHCompositeNode * /*topNode*/)
 
   TH2 *h2(nullptr);
 
-
   h = new TH1F(TString(get_histo_prefix()) + "InvMass_KFP",  //
                ";mass [GeV/c^{2}];Entries", 100, m_min_mass, m_max_mass);
   hm->registerHisto(h);
@@ -103,23 +102,20 @@ int QAKFParticle::Init(PHCompositeNode * /*topNode*/)
   float pt_min = 0;
   float pt_max = 10;
 
-
   h2 = new TH2F(TString(get_histo_prefix()) + "InvMass_KFP_Eta",  //
-               ";mass [GeV/c^{2}]; Eta", 100, m_min_mass, m_max_mass, 100, eta_min, eta_max);
+                ";mass [GeV/c^{2}]; Eta", 100, m_min_mass, m_max_mass, 100, eta_min, eta_max);
   hm->registerHisto(h2);
 
   h2 = new TH2F(TString(get_histo_prefix()) + "InvMass_KFP_Phi",  //
-               ";mass [GeV/c^{2}]; Phi", 100, m_min_mass, m_max_mass, 100, phi_min, phi_max);
+                ";mass [GeV/c^{2}]; Phi", 100, m_min_mass, m_max_mass, 100, phi_min, phi_max);
   hm->registerHisto(h2);
 
-
   h2 = new TH2F(TString(get_histo_prefix()) + "InvMass_KFP_pT",  //
-               ";pT [GeV]; mass [GeV/c^{2}] ", 100, pt_min, pt_max, 100, m_min_mass, m_max_mass);
+                ";pT [GeV]; mass [GeV/c^{2}] ", 100, pt_min, pt_max, 100, m_min_mass, m_max_mass);
   hm->registerHisto(h2);
 
   h_mass_KFP = dynamic_cast<TH1F *>(hm->getHisto(get_histo_prefix() + "InvMass_KFP"));
   assert(h_mass_KFP);
-
 
   h_mass_KFP_eta = dynamic_cast<TH2F *>(hm->getHisto(get_histo_prefix() + "InvMass_KFP_Eta"));
   assert(h_mass_KFP_eta);
@@ -130,20 +126,21 @@ int QAKFParticle::Init(PHCompositeNode * /*topNode*/)
   h_mass_KFP_pt = dynamic_cast<TH2F *>(hm->getHisto(get_histo_prefix() + "InvMass_KFP_pT"));
   assert(h_mass_KFP_pt);
 
-
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int QAKFParticle::process_event(PHCompositeNode *topNode)
 {
   if (Verbosity() > 2)
+  {
     std::cout << "QAKFParticle::process_event() entered" << std::endl;
+  }
 
   // load relevant nodes from NodeTree
   load_nodes(topNode);
 
   // histogram manager
-  
+
   /*
   if (m_svtxEvalStack)
     m_svtxEvalStack->next_event(topNode);
@@ -170,12 +167,12 @@ int QAKFParticle::process_event(PHCompositeNode *topNode)
   daughters.clear();
   */
 
-  //float m_calculated_mother_decaytime = -1;
-  //float m_calculated_mother_decaytime_err = -1;
-  //const float speedOfLight = 2.99792458e-1;
+  // float m_calculated_mother_decaytime = -1;
+  // float m_calculated_mother_decaytime_err = -1;
+  // const float speedOfLight = 2.99792458e-1;
 
   // std::map<unsigned int, KFParticle*> Map = m_kfpContainer->returnParticlesByPDGid(m_mother_id);
-  
+
   /*
   std::vector<int> d_id;
   std::vector<KFParticle> vertex_vec = kfpTools.makeAllPrimaryVertices(topNode, "SvtxVertexMap");
@@ -206,21 +203,19 @@ int QAKFParticle::process_event(PHCompositeNode *topNode)
   }
   */
 
-  for (KFParticle_Container::Iter iter = m_kfpContainer->begin(); iter != m_kfpContainer->end(); ++iter)
+  for (auto &iter : *m_kfpContainer)
   {
-    if (iter->second->GetPDG() == m_mother_id)
+    if (iter.second->GetPDG() == m_mother_id)
     {
       // filling mother histogram information
-      h_mass_KFP->Fill(iter->second->GetMass());
+      h_mass_KFP->Fill(iter.second->GetMass());
 
-      h_mass_KFP_eta->Fill(iter->second->GetMass(), iter->second->GetEta());
+      h_mass_KFP_eta->Fill(iter.second->GetMass(), iter.second->GetEta());
 
-      h_mass_KFP_phi->Fill(iter->second->GetMass(), iter->second->GetPhi());
+      h_mass_KFP_phi->Fill(iter.second->GetMass(), iter.second->GetPhi());
 
-      h_mass_KFP_pt->Fill(iter->second->GetPt(), iter->second->GetMass());
+      h_mass_KFP_pt->Fill(iter.second->GetPt(), iter.second->GetMass());
       // h_DecayTime->Fill(part->GetLifeTime());
-      
-      
     }
   }
   return Fun4AllReturnCodes::EVENT_OK;
@@ -311,7 +306,6 @@ CLHEP::HepLorentzVector *QAKFParticle::makeHepLV(PHCompositeNode *topNode, int t
 
 int QAKFParticle::load_nodes(PHCompositeNode *topNode)
 {
- 
   m_kfpContainer = findNode::getClass<KFParticle_Container>(topNode, "reconstructedParticles_KFParticle_Container");
   if (!m_kfpContainer)
   {
