1b3dc80a3f8f2ff984a6aa1be6a14864246f8c12
tmengel <tmengel@vols.utk.edu>
2024-07-22 10:49:38 -0400

    Adding a masking pixel class and a method to turn it on and off in offline analysis

---
+++ offline/packages/mvtx/MvtxPixelMask.h
@@ -0,0 +1,38 @@
+#ifndef MVTX_MVTXPIXELMASK_H
+#define MVTX_MVTXPIXELMASK_H
+
+#include "MvtxPixelDefs.h"
+
+#include <vector>
+#include <climits>
+#include <memory>
+
+class MvtxRawHit;
+
+class MvtxPixelMask
+{
+    public:
+
+        MvtxPixelMask(){}
+        ~MvtxPixelMask(){ clear();}
+
+        typedef std::vector<MvtxPixelDefs::pixelkey> hot_pixel_map_t;
+
+        void load_from_CDB();
+
+        void add_pixel(MvtxPixelDefs::pixelkey key);
+        void remove_pixel(MvtxPixelDefs::pixelkey key);
+        
+        void clear();
+
+        bool is_masked(MvtxRawHit* hit) const;
+
+        hot_pixel_map_t get_hot_pixel_map() const { return m_hot_pixel_map; }
+
+    private:
+
+        hot_pixel_map_t m_hot_pixel_map {};
+
+};
+
+#endif // MVTX_MVTXPIXELMASK_H
