d017ae8422e956bbe1c1cc81113f4cfca381e37d
tmengel <tmengel@vols.utk.edu>
2024-07-23 11:26:49 -0400

    ran clang tidy fixer command

--- offline/packages/mvtx/MvtxPixelMask.h
+++ offline/packages/mvtx/MvtxPixelMask.h
@@ -3,36 +3,33 @@
 
 #include "MvtxPixelDefs.h"
 
-#include <vector>
 #include <climits>
 #include <memory>
+#include <vector>
 
 class MvtxRawHit;
 
 class MvtxPixelMask
 {
-    public:
+ public:
+  MvtxPixelMask() {}
+  ~MvtxPixelMask() { clear(); }
 
-        MvtxPixelMask(){}
-        ~MvtxPixelMask(){ clear();}
+  typedef std::vector<MvtxPixelDefs::pixelkey> hot_pixel_map_t;
 
-        typedef std::vector<MvtxPixelDefs::pixelkey> hot_pixel_map_t;
+  void load_from_CDB();
 
-        void load_from_CDB();
+  void add_pixel(MvtxPixelDefs::pixelkey key);
+  void remove_pixel(MvtxPixelDefs::pixelkey key);
 
-        void add_pixel(MvtxPixelDefs::pixelkey key);
-        void remove_pixel(MvtxPixelDefs::pixelkey key);
-        
-        void clear();
+  void clear();
 
-        bool is_masked(MvtxRawHit* hit) const;
+  bool is_masked(MvtxRawHit* hit) const;
 
-        hot_pixel_map_t get_hot_pixel_map() const { return m_hot_pixel_map; }
+  hot_pixel_map_t get_hot_pixel_map() const { return m_hot_pixel_map; }
 
-    private:
-
-        hot_pixel_map_t m_hot_pixel_map {};
-
+ private:
+  hot_pixel_map_t m_hot_pixel_map{};
 };
 
-#endif // MVTX_MVTXPIXELMASK_H
+#endif  // MVTX_MVTXPIXELMASK_H
