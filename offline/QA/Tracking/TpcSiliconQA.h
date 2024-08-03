b67053f2410ae28176eb5aadea2177aac103dedd
rosstom <rosstom@ucla.edu>
2024-08-03 16:43:49 -0400

    Adding geometric cut histograms

--- offline/QA/Tracking/TpcSiliconQA.h
+++ offline/QA/Tracking/TpcSiliconQA.h
@@ -25,6 +25,11 @@ class TpcSiliconQA : public SubsysReco
   int process_event(PHCompositeNode *topNode) override;
   int EndRun(const int runnumber) override;
 
+  void setXCut(float cutVal) {m_xcut = cutVal;}
+  void setYCut(float cutVal) {m_ycut = cutVal;}
+  void setEtaCut(float cutVal) {m_etacut = cutVal;}
+  void setPhiCut(float cutVal) {m_phicut = cutVal;}
+
  private:
   void createHistos();
 
@@ -40,15 +45,20 @@ class TpcSiliconQA : public SubsysReco
   float m_tpcseedphi = std::numeric_limits<float>::quiet_NaN();
   float m_tpcseedeta = std::numeric_limits<float>::quiet_NaN();
 
+  float m_xcut = 1.0;
+  float m_ycut = 1.0;
+  float m_etacut = 0.05;
+  float m_phicut = 0.25;
+
   std::string getHistoPrefix() const;
   int m_event = 0;
 
   TH1 *h_crossing = nullptr;
-  TH1 *h_phiDiff = nullptr;
-  TH1 *h_etaDiff = nullptr;
-  TH1 *h_xDiff = nullptr;
-  TH1 *h_yDiff = nullptr;
-  TH1 *h_zDiff = nullptr; 
+  TH1 *h_phiDiff[4] = {nullptr};
+  TH1 *h_etaDiff[4] = {nullptr};
+  TH1 *h_xDiff[4] = {nullptr};
+  TH1 *h_yDiff[4] = {nullptr};
+  TH1 *h_zDiff[4] = {nullptr}; 
 };
 
 #endif  // QA_TRACKING_TPCSILICONQA_H
