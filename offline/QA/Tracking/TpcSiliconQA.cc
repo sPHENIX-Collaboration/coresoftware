26b079b42d14c245fcd213b66b662a02ddbb6205
Joe Osborn <osbornjd91@gmail.com>
2024-08-06 10:07:52 -0400

    clangify

--- offline/QA/Tracking/TpcSiliconQA.cc
+++ offline/QA/Tracking/TpcSiliconQA.cc
@@ -82,31 +82,40 @@ int TpcSiliconQA::process_event(PHCompositeNode* topNode)
       m_tpcseedz = tpcseed->get_z();
       m_tpcseedphi = tpcseed->get_phi();
       m_tpcseedeta = tpcseed->get_eta();
-      
+
       h_phiDiff[0]->Fill(m_tpcseedphi - m_silseedphi);
       h_etaDiff[0]->Fill(m_tpcseedeta - m_silseedeta);
       h_xDiff[0]->Fill(m_tpcseedx - m_silseedx);
       h_yDiff[0]->Fill(m_tpcseedy - m_silseedy);
       h_zDiff[0]->Fill(m_tpcseedz - m_silseedz);
- 
-      if(abs(m_tpcseedx - m_silseedx) > m_xcut || abs(m_tpcseedy - m_silseedy) > m_ycut) continue;
-        
+
+      if (abs(m_tpcseedx - m_silseedx) > m_xcut || abs(m_tpcseedy - m_silseedy) > m_ycut)
+      {
+        continue;
+      }
+
       h_phiDiff[1]->Fill(m_tpcseedphi - m_silseedphi);
       h_etaDiff[1]->Fill(m_tpcseedeta - m_silseedeta);
       h_xDiff[1]->Fill(m_tpcseedx - m_silseedx);
       h_yDiff[1]->Fill(m_tpcseedy - m_silseedy);
       h_zDiff[1]->Fill(m_tpcseedz - m_silseedz);
-      
-      if(abs(m_tpcseedeta - m_silseedeta) > m_etacut) continue;
-       
+
+      if (abs(m_tpcseedeta - m_silseedeta) > m_etacut)
+      {
+        continue;
+      }
+
       h_phiDiff[2]->Fill(m_tpcseedphi - m_silseedphi);
       h_etaDiff[2]->Fill(m_tpcseedeta - m_silseedeta);
       h_xDiff[2]->Fill(m_tpcseedx - m_silseedx);
       h_yDiff[2]->Fill(m_tpcseedy - m_silseedy);
       h_zDiff[2]->Fill(m_tpcseedz - m_silseedz);
-      
-      if(abs(m_tpcseedphi - m_silseedphi) > m_phicut) continue;
-       
+
+      if (abs(m_tpcseedphi - m_silseedphi) > m_phicut)
+      {
+        continue;
+      }
+
       h_phiDiff[3]->Fill(m_tpcseedphi - m_silseedphi);
       h_etaDiff[3]->Fill(m_tpcseedeta - m_silseedeta);
       h_xDiff[3]->Fill(m_tpcseedx - m_silseedx);
@@ -134,11 +143,11 @@ void TpcSiliconQA::createHistos()
   auto hm = QAHistManagerDef::getHistoManager();
   assert(hm);
 
-  std::vector<std::string> cutNames = {"", "_xyCut", "_etaCut", "_phiCut"}; 
- 
+  std::vector<std::string> cutNames = {"", "_xyCut", "_etaCut", "_phiCut"};
+
   {
     h_crossing = new TH1F(std::string(getHistoPrefix() + "crossing").c_str(),
-                        "Track Crossing Value", 1000, -500, 500);
+                          "Track Crossing Value", 1000, -500, 500);
     h_crossing->GetXaxis()->SetTitle("Track Crossing");
     h_crossing->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_crossing);
@@ -156,7 +165,7 @@ void TpcSiliconQA::createHistos()
   for (const std::string& name : cutNames)
   {
     h_phiDiff[i] = new TH1F(std::string(getHistoPrefix() + "phiDiff" + name).c_str(),
-                        "TPC-Silicon #phi Difference", 100, -0.5, 0.5);
+                            "TPC-Silicon #phi Difference", 100, -0.5, 0.5);
     h_phiDiff[i]->GetXaxis()->SetTitle("TPC Seed #phi - Silicon Seed #phi");
     h_phiDiff[i]->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_phiDiff[i]);
@@ -166,7 +175,7 @@ void TpcSiliconQA::createHistos()
   for (const std::string& name : cutNames)
   {
     h_etaDiff[i] = new TH1F(std::string(getHistoPrefix() + "etaDiff" + name).c_str(),
-                        "TPC-Silicon #eta Difference", 100, -0.1, 0.1);
+                            "TPC-Silicon #eta Difference", 100, -0.1, 0.1);
     h_etaDiff[i]->GetXaxis()->SetTitle("TPC Seed #eta - Silicon Seed #eta");
     h_etaDiff[i]->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_etaDiff[i]);
@@ -176,7 +185,7 @@ void TpcSiliconQA::createHistos()
   for (const std::string& name : cutNames)
   {
     h_xDiff[i] = new TH1F(std::string(getHistoPrefix() + "xDiff" + name).c_str(),
-                        "TPC-Silicon x Difference", 100, -2, 2);
+                          "TPC-Silicon x Difference", 100, -2, 2);
     h_xDiff[i]->GetXaxis()->SetTitle("TPC Seed x - Silicon Seed x [cm]");
     h_xDiff[i]->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_xDiff[i]);
@@ -186,7 +195,7 @@ void TpcSiliconQA::createHistos()
   for (const std::string& name : cutNames)
   {
     h_yDiff[i] = new TH1F(std::string(getHistoPrefix() + "yDiff" + name).c_str(),
-                       "TPC-Silicon y Difference", 100, -2, 2);
+                          "TPC-Silicon y Difference", 100, -2, 2);
     h_yDiff[i]->GetXaxis()->SetTitle("TPC Seed y - Silicon Seed y [cm]");
     h_yDiff[i]->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_yDiff[i]);
@@ -196,12 +205,12 @@ void TpcSiliconQA::createHistos()
   for (const std::string& name : cutNames)
   {
     h_zDiff[i] = new TH1F(std::string(getHistoPrefix() + "zDiff" + name).c_str(),
-                       "TPC-Silicon z Difference", 500, -100, 100);
+                          "TPC-Silicon z Difference", 500, -100, 100);
     h_zDiff[i]->GetXaxis()->SetTitle("TPC Seed z - Silicon Seed z [cm]");
     h_zDiff[i]->GetYaxis()->SetTitle("Entries");
     hm->registerHisto(h_zDiff[i]);
-    i++; 
-  } 
+    i++;
+  }
 
   return;
 }
