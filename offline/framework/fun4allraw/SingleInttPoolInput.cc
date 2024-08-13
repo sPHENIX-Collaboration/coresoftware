c96d4be32c78e94a2770ac3a8e7488cd77108fd7
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-08-13 10:28:45 -0400

    no skipping of zero hit events after initial syncing to gl1 bco

--- offline/framework/fun4allraw/SingleInttPoolInput.cc
+++ offline/framework/fun4allraw/SingleInttPoolInput.cc
@@ -86,7 +86,7 @@ void SingleInttPoolInput::FillPool(const uint64_t minBCO)
     if (evt->getEvtType() != DATAEVENT)
     {
       m_NumSpecialEvents++;
-      if(evt->getEvtType() == ENDRUNEVENT)
+      if (evt->getEvtType() == ENDRUNEVENT)
       {
         std::cout << "End run flag for INTT found, remaining INTT data is corrupted" << std::endl;
         delete evt;
@@ -104,26 +104,24 @@ void SingleInttPoolInput::FillPool(const uint64_t minBCO)
     {
       exit(1);
     }
-    bool skipthis = true;
-
-    for (int i = 0 ; i< npackets; i++)
+    for (int i = 0; i < npackets; i++)
     {
-       int numBCOs = plist[i]->iValue(0, "NR_BCOS");
-       for (int j = 0; j < numBCOs; j++)
-       {
-	 uint64_t bco = plist[i]->lValue(j, "BCOLIST");
-	 if (bco < minBCO)
-	 {
-	   continue;
-	 }
-	 skipthis = false;
-       }
-    }
-    if (skipthis)
-    {
-      for (int i = 0 ; i< npackets; i++)
+      int numBCOs = plist[i]->iValue(0, "NR_BCOS");
+      for (int j = 0; j < numBCOs; j++)
       {
-	delete plist[i];
+        uint64_t bco = plist[i]->lValue(j, "BCOLIST");
+        if (bco < minBCO)
+        {
+          continue;
+        }
+        m_SkipEarlyEvents = false;
+      }
+    }
+    if (m_SkipEarlyEvents)
+    {
+      for (int i = 0; i < npackets; i++)
+      {
+        delete plist[i];
       }
       delete evt;
       continue;
@@ -164,87 +162,87 @@ void SingleInttPoolInput::FillPool(const uint64_t minBCO)
           std::cout << "Number of Hits: " << num_hits << " for packet "
                     << pool->getIdentifier() << std::endl;
         }
-        
-       int numBCOs = pool->iValue(0, "NR_BCOS");
-       uint64_t largest_bco = 0;
-         for (int j = 0; j < numBCOs; j++)
-         {
-           uint64_t bco = pool->lValue(j, "BCOLIST");
-	   if (largest_bco < bco)
-	   {
-	     largest_bco = bco;
-	   }
-	   if (bco < minBCO)
-	   {
-	     continue;
-	   }
-	   skipthis = false;
-           m_BclkStack.insert(bco);
-           m_BclkStackPacketMap[packet_id].insert(bco);
-         }
-	 if (skipthis)
-	 {
-	   if (Verbosity() > 1)
-	   {
-	     std::cout << "largest bco: 0x" << std::hex << largest_bco << ", minbco 0x" << minBCO
-		       << std::dec << ", evtno: " << EventSequence << std::endl;
-	   }
-	 }
-	 else
-	 {
-        for (int j = 0; j < num_hits; j++)
-        {
-          uint64_t gtm_bco = pool->lValue(j, "BCO");
-	  if (gtm_bco < minBCO)
-	  {
-	    // std::cout << "dropping hit with bco 0x" << std::hex
-	    // 	      << gtm_bco << ", min bco: 0x" << minBCO
-	    // 	      << std::endl;
-	     continue;
-	  }
-          InttRawHit *newhit = new InttRawHitv2();
-          int FEE = pool->iValue(j, "FEE");
-          newhit->set_packetid(pool->getIdentifier());
-          newhit->set_fee(FEE);
-          newhit->set_bco(gtm_bco);
-          newhit->set_adc(pool->iValue(j, "ADC"));
-          newhit->set_amplitude(pool->iValue(j, "AMPLITUDE"));
-          newhit->set_chip_id(pool->iValue(j, "CHIP_ID"));
-          newhit->set_channel_id(pool->iValue(j, "CHANNEL_ID"));
-          newhit->set_word(pool->iValue(j, "DATAWORD"));
-          newhit->set_FPHX_BCO(pool->iValue(j, "FPHX_BCO"));
-          newhit->set_full_FPHX(pool->iValue(j, "FULL_FPHX"));
-          newhit->set_full_ROC(pool->iValue(j, "FULL_ROC"));
-          newhit->set_event_counter(pool->iValue(j, "EVENT_COUNTER"));
 
-          gtm_bco += m_Rollover[FEE];
-          
-          if (gtm_bco < m_PreviousClock[FEE])
+        int numBCOs = pool->iValue(0, "NR_BCOS");
+        uint64_t largest_bco = 0;
+        bool skipthis{true};
+        for (int j = 0; j < numBCOs; j++)
+        {
+          uint64_t bco = pool->lValue(j, "BCOLIST");
+          if (largest_bco < bco)
           {
-            m_Rollover[FEE] += 0x10000000000;
-            gtm_bco += 0x10000000000;  // rollover makes sure our bclks are ascending even if we roll over the 40 bit counter
+            largest_bco = bco;
           }
-          m_PreviousClock[FEE] = gtm_bco;
-          m_BeamClockFEE[gtm_bco].insert(FEE);
-          m_FEEBclkMap[FEE] = gtm_bco;
-          if (Verbosity() > 2)
+          if (bco < minBCO)
           {
-            std::cout << "evtno: " << EventSequence
-                      << ", hits: " << j
-                      << ", nr_hits: " << num_hits
-                      << ", FEE: " << FEE
-                      << ", bco: 0x" << std::hex << gtm_bco << std::dec
-		      << ", min bco: 0x" << std::hex << minBCO << std::dec
-                      << ", channel: " << newhit->get_channel_id()
-                      << ", evt_counter: " << newhit->get_event_counter() << std::endl;
+            continue;
           }
-          if (StreamingInputManager())
-          {
-            StreamingInputManager()->AddInttRawHit(gtm_bco, newhit);
-          }
-          m_InttRawHitMap[gtm_bco].push_back(newhit);
+          skipthis = false;
+          m_BclkStack.insert(bco);
+          m_BclkStackPacketMap[packet_id].insert(bco);
         }
-	 }
+        if (skipthis)
+        {
+          if (Verbosity() > 1)
+          {
+            std::cout << "largest bco: 0x" << std::hex << largest_bco << ", minbco 0x" << minBCO
+                      << std::dec << ", evtno: " << EventSequence << std::endl;
+          }
+        }
+        else
+        {
+          for (int j = 0; j < num_hits; j++)
+          {
+            uint64_t gtm_bco = pool->lValue(j, "BCO");
+            if (gtm_bco < minBCO)
+            {
+              // std::cout << "dropping hit with bco 0x" << std::hex
+              // 	      << gtm_bco << ", min bco: 0x" << minBCO
+              // 	      << std::endl;
+              continue;
+            }
+            InttRawHit *newhit = new InttRawHitv2();
+            int FEE = pool->iValue(j, "FEE");
+            newhit->set_packetid(pool->getIdentifier());
+            newhit->set_fee(FEE);
+            newhit->set_bco(gtm_bco);
+            newhit->set_adc(pool->iValue(j, "ADC"));
+            newhit->set_amplitude(pool->iValue(j, "AMPLITUDE"));
+            newhit->set_chip_id(pool->iValue(j, "CHIP_ID"));
+            newhit->set_channel_id(pool->iValue(j, "CHANNEL_ID"));
+            newhit->set_word(pool->iValue(j, "DATAWORD"));
+            newhit->set_FPHX_BCO(pool->iValue(j, "FPHX_BCO"));
+            newhit->set_full_FPHX(pool->iValue(j, "FULL_FPHX"));
+            newhit->set_full_ROC(pool->iValue(j, "FULL_ROC"));
+            newhit->set_event_counter(pool->iValue(j, "EVENT_COUNTER"));
+            gtm_bco += m_Rollover[FEE];
+
+            if (gtm_bco < m_PreviousClock[FEE])
+            {
+              m_Rollover[FEE] += 0x10000000000;
+              gtm_bco += 0x10000000000;  // rollover makes sure our bclks are ascending even if we roll over the 40 bit counter
+            }
+            m_PreviousClock[FEE] = gtm_bco;
+            m_BeamClockFEE[gtm_bco].insert(FEE);
+            m_FEEBclkMap[FEE] = gtm_bco;
+            if (Verbosity() > 2)
+            {
+              std::cout << "evtno: " << EventSequence
+                        << ", hits: " << j
+                        << ", nr_hits: " << num_hits
+                        << ", FEE: " << FEE
+                        << ", bco: 0x" << std::hex << gtm_bco << std::dec
+                        << ", min bco: 0x" << std::hex << minBCO << std::dec
+                        << ", channel: " << newhit->get_channel_id()
+                        << ", evt_counter: " << newhit->get_event_counter() << std::endl;
+            }
+            if (StreamingInputManager())
+            {
+              StreamingInputManager()->AddInttRawHit(gtm_bco, newhit);
+            }
+            m_InttRawHitMap[gtm_bco].push_back(newhit);
+          }
+        }
         //	    Print("FEEBCLK");
       }
       pool->next();
@@ -288,9 +286,9 @@ void SingleInttPoolInput::Print(const std::string &what) const
   }
   if (what == "ALL" || what == "STACK")
   {
-    for(auto& [packetid, bclkstack] : m_BclkStackPacketMap)
+    for (auto &[packetid, bclkstack] : m_BclkStackPacketMap)
     {
-      for(auto& bclk : bclkstack)
+      for (auto &bclk : bclkstack)
       {
         std::cout << "stacked bclk: 0x" << std::hex << bclk << std::dec << std::endl;
       }
@@ -323,7 +321,7 @@ void SingleInttPoolInput::CleanupUsedPackets(const uint64_t bclk)
   for (auto iter : toclearbclk)
   {
     m_BclkStack.erase(iter);
-    for(auto& [packetid , bclkstack] : m_BclkStackPacketMap)
+    for (auto &[packetid, bclkstack] : m_BclkStackPacketMap)
     {
       bclkstack.erase(iter);
     }
@@ -373,23 +371,23 @@ bool SingleInttPoolInput::GetSomeMoreEvents(const uint64_t ibclk)
   }
   if (poolmap.empty())
   {
-//      std::cout << "GetSomeMoreEvents poolmap empty, ret true" << std::endl;
+    //      std::cout << "GetSomeMoreEvents poolmap empty, ret true" << std::endl;
     return true;
   }
-    // for (auto iter : poolmap)
-    // {
-    //   if (!iter.second->depth_ok())
-    //   {
-    //   std::cout << "GetSomeMoreEvents depth not ok, ret true" << std::endl;
-    // 	return true;
-    //   }
-    // }
+  // for (auto iter : poolmap)
+  // {
+  //   if (!iter.second->depth_ok())
+  //   {
+  //   std::cout << "GetSomeMoreEvents depth not ok, ret true" << std::endl;
+  // 	return true;
+  //   }
+  // }
   uint64_t localbclk = ibclk;
   if (ibclk == 0)
   {
     if (m_InttRawHitMap.empty())
     {
-//      std::cout << "GetSomeMoreEvents hitmap empty, ret true" << std::endl;
+      //      std::cout << "GetSomeMoreEvents hitmap empty, ret true" << std::endl;
       return true;
     }
     localbclk = m_InttRawHitMap.begin()->first;
@@ -420,11 +418,11 @@ bool SingleInttPoolInput::GetSomeMoreEvents(const uint64_t ibclk)
       }
     }
   }
-  for(auto iter : toerase)
+  for (auto iter : toerase)
   {
     m_FEEBclkMap.erase(iter);
   }
-//  std::cout << "GetSomeMoreEvents ret false" << std::endl;
+  //  std::cout << "GetSomeMoreEvents ret false" << std::endl;
   return false;
 }
 
