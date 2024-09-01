239a14a7a9eab1e11e4825cfcf7a07e581fb6854
Ejiro Umaka <ejironaomiumaka@gmail.com>
2024-09-01 19:19:26 -0400

    Update Zdcinfov1.h

--- offline/packages/zdcinfo/Zdcinfov1.h
+++ offline/packages/zdcinfo/Zdcinfov1.h
@@ -24,13 +24,10 @@ class Zdcinfov1 : public Zdcinfo
   float get_zdc_energy(int arm) const override;
   void set_radius(int arm, float _r) override;
   float get_radius(int arm) const override;
-  void set_zvertex(float _z) override; 
-  float get_zvertex() const override; 
 
  private:
   float m_zdc_e[2] = {std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN()};
   float m_radius[2] = {std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN()};
-  float m_zvertex = std::numeric_limits<float>::quiet_NaN();
   ClassDefOverride(Zdcinfov1, 1);
 };
 
