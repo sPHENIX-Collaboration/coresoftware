9e785ed342235ea3f69612929c6027d4cbef2f0b
Shuonli <sli17@ole.augie.edu>
2024-09-12 19:45:09 -0400

    check the tower status for the retowered case

--- offline/packages/ClusterIso/ClusterIso.cc
+++ offline/packages/ClusterIso/ClusterIso.cc
@@ -244,6 +244,7 @@ int ClusterIso::process_event(PHCompositeNode *topNode)
             for (unsigned int channel = 0; channel < ntowers; channel++)
             {
               TowerInfo *tower = towersEM3old->get_tower_at_channel(channel);
+              if(!IsAcceptableTower(tower)) continue;
               unsigned int towerkey = towersEM3old->encode_key(channel);
               int ieta = towersEM3old->getTowerEtaBin(towerkey);
               int iphi = towersEM3old->getTowerPhiBin(towerkey);
@@ -266,6 +267,7 @@ int ClusterIso::process_event(PHCompositeNode *topNode)
             for (unsigned int channel = 0; channel < ntowers; channel++)
             {
               TowerInfo *tower = towersIH3->get_tower_at_channel(channel);
+              if(!IsAcceptableTower(tower)) continue;
               unsigned int towerkey = towersIH3->encode_key(channel);
               int ieta = towersIH3->getTowerEtaBin(towerkey);
               int iphi = towersIH3->getTowerPhiBin(towerkey);
@@ -286,6 +288,7 @@ int ClusterIso::process_event(PHCompositeNode *topNode)
             for (unsigned int channel = 0; channel < ntowers; channel++)
             {
               TowerInfo *tower = towersOH3->get_tower_at_channel(channel);
+              if(!IsAcceptableTower(tower)) continue;
               unsigned int towerkey = towersOH3->encode_key(channel);
               int ieta = towersOH3->getTowerEtaBin(towerkey);
               int iphi = towersOH3->getTowerPhiBin(towerkey);
