7f4962992a4d2c6ea1cec2bf1203bc9f1d093ce1
Ejiro Umaka <ejironaomiumaka@gmail.com>
2024-09-14 00:32:44 -0400

    Update GlobalQA.h

--- offline/QA/Global/GlobalQA.h
+++ offline/QA/Global/GlobalQA.h
@@ -1,10 +1,11 @@
 #ifndef GLOBALQA_GLOBALQA_H
 #define GLOBALQA_GLOBALQA_H
 
+#include <cdbobjects/CDBTTree.h>
 #include <fun4all/SubsysReco.h>
 
 #include <string>
-#include <vector> 
+#include <vector>
 
 // Forward declarations
 class PHCompositeNode;
@@ -13,80 +14,84 @@ class TH2;
 class TProfile;
 
 class GlobalQA : public SubsysReco {
-  public:
-    //! constructor
-    GlobalQA(const std::string &name =
-        "GlobalQA"); // const std::string &filename = "testQA.root");
-    // //int nevents = 100);
+public:
+  //! constructor
+  GlobalQA(const std::string &name =
+               "GlobalQA"); // const std::string &filename = "testQA.root");
+  // //int nevents = 100);
 
-    //! destructor
-    ~GlobalQA() override;
+  //! destructor
+  ~GlobalQA() override;
 
-    //! full initialization
-    int Init(PHCompositeNode *) override;
+  //! full initialization
+  int Init(PHCompositeNode *) override;
 
-    //! event processing method
-    int process_event(PHCompositeNode *) override;
+  //! event processing method
+  int process_event(PHCompositeNode *) override;
 
-    //! end of run method
-    int End(PHCompositeNode *) override;
+  //! end of run method
+  int End(PHCompositeNode *) override;
 
-    int process_g4hits(PHCompositeNode *);
-    int process_g4cells(PHCompositeNode *);
-    int process_towers(PHCompositeNode *);
-    int process_clusters(PHCompositeNode *);
+  int process_g4hits(PHCompositeNode *);
+  int process_g4cells(PHCompositeNode *);
+  int process_towers(PHCompositeNode *);
+  int process_clusters(PHCompositeNode *);
 
-    void Detector(const std::string &name) { detector = name; }
-    void set_timing_cut_width(const int &t) { _range = t; }
+  void Detector(const std::string &name) { detector = name; }
+  void set_timing_cut_width(const int &t) { _range = t; }
 
-    void set_debug(bool debug) { m_debug = debug; }
-    TH2 *LogYHist2D(const std::string &name, const std::string &title, int,
-        double, double, int, double, double);
+  void set_debug(bool debug) { m_debug = debug; }
+  TH2 *LogYHist2D(const std::string &name, const std::string &title, int,
+                  double, double, int, double, double);
 
-  private:
-    int evtcount = 0;
-    int Getpeaktime(TH1 *h);
-    void createHistos();
-    float TSAMPLE = 1.0 / (9.4e+6 * 6);
+private:
+  int evtcount = 0;
+  int Getpeaktime(TH1 *h);
+  void createHistos();
 
-    // MBD histos
-    TH1 *h_GlobalQA_mbd_zvtx = nullptr;
-    TH1 *h_GlobalQA_mbd_zvtx_wide = nullptr;
-    TH1 *h_GlobalQA_calc_zvtx = nullptr;
-    TH1 *h_GlobalQA_calc_zvtx_wide = nullptr;
-    TH1 *h_GlobalQA_mbd_charge_s = nullptr;
-    TH1 *h_GlobalQA_mbd_charge_n = nullptr;
+  // MBD histos
+  TH1 *h_GlobalQA_mbd_zvtx = nullptr;
+  TH1 *h_GlobalQA_mbd_zvtx_wide = nullptr;
+  TH1 *h_GlobalQA_calc_zvtx = nullptr;
+  TH1 *h_GlobalQA_calc_zvtx_wide = nullptr;
+  TH1 *h_GlobalQA_mbd_charge_s = nullptr;
+  TH1 *h_GlobalQA_mbd_charge_n = nullptr;
+  TH1 *h_GlobalQA_mbd_charge_sum = nullptr;
+  TH2 *h2_GlobalQA_mbd_charge_NS_correlation = nullptr;
+  TH1 *h_GlobalQA_mbd_nhit_s = nullptr;
+  TH1 *h_GlobalQA_mbd_nhit_n = nullptr;
+  TH2 *h2_GlobalQA_mbd_nhits_NS_correlation = nullptr;
+  TH1 *h_GlobalQA_mbd_zvtxq = nullptr;
 
-    TH1 *h_GlobalQA_mbd_charge_sum = nullptr;     //MBD north + south charge sum
-    TH2 *h2_GlobalQA_mbd_charge_NS_correlation = nullptr;    // North charge vs south charge
+  // sEPD
+  std::string m_sEPDMapName;
+  std::string m_sEPDfieldname;
+  bool m_overrideSEPDMapName{false};
+  bool m_overrideSEPDFieldName{false};
+  CDBTTree *cdbttree{nullptr};
+  unsigned int key = 999;
+  std::vector<unsigned int> v;
+  TH1 *h_GlobalQA_sEPD_tile[744] = {nullptr};
+  TH1 *h_GlobalQA_sEPD_adcsum_s{nullptr};
+  TH1 *h_GlobalQA_sEPD_adcsum_n{nullptr};
+  TH2 *h2_GlobalQA_sEPD_adcsum_ns{nullptr};
 
-    TH1 *h_GlobalQA_mbd_nhit_s = nullptr;
-    TH1 *h_GlobalQA_mbd_nhit_n = nullptr;
+  // ZDC histos
+  TH1 *h_GlobalQA_zdc_zvtx = nullptr;
+  TH1 *h_GlobalQA_zdc_zvtx_wide = nullptr;
+  TH1 *h_GlobalQA_zdc_energy_s = nullptr;
+  TH1 *h_GlobalQA_zdc_energy_n = nullptr;
+  TH1 *h_GlobalQA_triggerVec{nullptr};
+  float zdc_zvtx{std::numeric_limits<float>::quiet_NaN()};
 
-    TH2 *h2_GlobalQA_mbd_nhits_NS_correlation = nullptr; //Number of event that have hits on South & North   
+  int _eventcounter{0};
+  int _range{1};
 
-    TH1 *h_GlobalQA_mbd_zvtxq = nullptr;
+  bool m_debug{false};
 
-
-
-
-    // ZDC histos
-    TH1 *h_GlobalQA_zdc_zvtx = nullptr;
-    TH1 *h_GlobalQA_zdc_zvtx_wide = nullptr;
-    TH1 *h_GlobalQA_zdc_energy_s = nullptr;
-    TH1 *h_GlobalQA_zdc_energy_n = nullptr;
-    TH1* h_GlobalQA_triggerVec{nullptr};
-
-    int _eventcounter{0}; // num events processed
-
-    int _range{1};
-
-    bool m_debug{false};
-
-    std::string detector = "";
-    std::string m_outputFileName = "";
-    std::string OutputFileName = "";
+  std::string detector = "";
+  std::string m_outputFileName = "";
+  std::string OutputFileName = "";
 };
 
 #endif
-
