e6fa64b7675f38e480f155e992ec77e1764a5dfd
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-10-18 14:16:49 -0400

    use std::array in EpdGeomV2 for easy bounds check

---
+++ offline/packages/epd/EpdGeomV2.cc
@@ -0,0 +1,53 @@
+#include "EpdGeomV2.h"
+
+#include <calobase/TowerInfoDefs.h>
+
+#include <phool/PHObject.h>
+
+#include <array>
+
+
+float EpdGeomV2::get_r(unsigned int key) const
+{
+  return tile_r.at(TowerInfoDefs::get_epd_rbin(key));
+}
+
+float EpdGeomV2::get_z(unsigned int key) const
+{
+  return tile_z.at(TowerInfoDefs::get_epd_arm(key));
+}
+
+float EpdGeomV2::get_phi(unsigned int key) const
+{
+
+  if(TowerInfoDefs::get_epd_rbin(key) == 0)
+  {
+    return tile_phi0.at(TowerInfoDefs::get_epd_phibin(key));
+  }
+  else
+  {
+    return tile_phi.at(TowerInfoDefs::get_epd_phibin(key));
+  }
+
+}
+ 
+void EpdGeomV2::set_z(unsigned int key, float z)
+{
+  tile_z.at(TowerInfoDefs::get_epd_arm(key)) = z;
+}
+
+void EpdGeomV2::set_r(unsigned int key, float r)
+{
+  tile_r.at(TowerInfoDefs::get_epd_rbin(key)) = r;
+}
+
+void EpdGeomV2::set_phi(unsigned int key, float f)
+{
+  tile_phi.at(TowerInfoDefs::get_epd_phibin(key)) = f;
+}
+
+void EpdGeomV2::set_phi0(unsigned int key, float f0)
+{
+  tile_phi0.at(TowerInfoDefs::get_epd_phibin(key)) = f0;
+}
+
