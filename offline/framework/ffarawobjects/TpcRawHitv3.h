f2472503fd592dd71a437898e6491ca278317fa8
Jin Huang <jhuang@bnl.gov>
2024-11-17 00:31:21 -0500

    optimize crc and parity calc

--- offline/framework/ffarawobjects/TpcRawHitv3.h
+++ offline/framework/ffarawobjects/TpcRawHitv3.h
@@ -81,11 +81,11 @@ class TpcRawHitv3 : public TpcRawHit
   // uint16_t get_userword() const override { return userword; }
   // void set_userword(const uint16_t i) override { userword = i; }
 
-  uint16_t get_checksum() const override { return checksum; }
-  void set_checksum(const uint16_t i) override { checksum = i; }
+  // uint16_t get_checksum() const override { return checksum; }
+  // void set_checksum(const uint16_t i) override { checksum = i; }
 
-  uint16_t get_parity() const override { return data_parity; }
-  void set_parity(const uint16_t i) override { data_parity = i; }
+  // uint16_t get_parity() const override { return data_parity; }
+  // void set_parity(const uint16_t i) override { data_parity = i; }
 
   bool get_checksumerror() const override { return checksumerror; }
   void set_checksumerror(const bool b) override { checksumerror = b; }
@@ -163,8 +163,8 @@ class TpcRawHitv3 : public TpcRawHit
   uint16_t fee{std::numeric_limits<uint16_t>::max()};
   uint16_t channel{std::numeric_limits<uint16_t>::max()};
   uint16_t type{std::numeric_limits<uint16_t>::max()};
-  uint16_t checksum{std::numeric_limits<uint16_t>::max()};
-  uint16_t data_parity{std::numeric_limits<uint16_t>::max()};
+  // uint16_t checksum{std::numeric_limits<uint16_t>::max()};
+  // uint16_t data_parity{std::numeric_limits<uint16_t>::max()};
 
   bool checksumerror{true};
   bool parityerror{true};
@@ -172,7 +172,7 @@ class TpcRawHitv3 : public TpcRawHit
   //! adc waveform std::vector< uint16_t > for each start time uint16_t
   std::vector<std::pair<uint16_t, std::vector<uint16_t> > > m_adcData;
 
-  ClassDefOverride(TpcRawHitv3, 1)
+  ClassDefOverride(TpcRawHitv3, 2)
 };
 
 #endif
