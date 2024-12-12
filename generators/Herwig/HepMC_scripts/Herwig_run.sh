eb07cbaf5a9aea56b37dffb32dfb09b30d5bd11e
silas-gross <silas.grossberndt@gmail.com>
2024-12-12 15:22:32 -0500

    Fixed bugs, added read file. HepMC generation is now ready to go

---
+++ generators/Herwig/HepMC_scripts/Herwig_run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+filenumber=${3:-'0'}
+filetag=$(printf "%06d" $filenumber)
+echo $filetag
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana
+ 
+/cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/Herwig/bin/Herwig run $1 -N $2 -d1 -t $filetag
