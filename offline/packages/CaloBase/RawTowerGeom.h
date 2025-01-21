224cd0a251e22379d9605b6c50b4bceca15c6aea
Virgile Jean Alain Mahaut <virgilemahaut@sphnx01.sdcc.bnl.gov>
2025-01-21 05:19:22 -0500

    New detailed geometry class in calorimeter reconstruction

--- offline/packages/CaloBase/RawTowerGeom.h
+++ offline/packages/CaloBase/RawTowerGeom.h
@@ -14,6 +14,8 @@ class RawTowerGeom : public PHObject
  public:
   ~RawTowerGeom() override {}
 
+  RawTowerGeom(const RawTowerGeom& /*geom*/) {};
+
   void identify(std::ostream& os = std::cout) const override;
 
   virtual void set_id(RawTowerDefs::keytype) { PHOOL_VIRTUAL_WARN("set_id()"); }
@@ -72,6 +74,12 @@ class RawTowerGeom : public PHObject
     return;
   }
 
+  virtual void set_vertices(const std::vector<double>&)
+  {
+    PHOOL_VIRTUAL_WARN("set_vertices()");
+    return;
+  }
+
   virtual void set_size_x(double)
   {
     PHOOL_VIRTUAL_WARN("set_size_x()");
@@ -105,6 +113,132 @@ class RawTowerGeom : public PHObject
   virtual double get_center_z() const
   {
     PHOOL_VIRTUAL_WARN("get_center_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_int_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_int_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_int_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_int_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_int_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_int_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+  
+  virtual double get_center_ext_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_ext_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_ext_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_ext_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_ext_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_ext_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_eta_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_eta_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_eta_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_eta_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_eta_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_eta_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_eta_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_eta_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_eta_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_eta_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_eta_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_eta_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_phi_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_phi_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_phi_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_phi_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_low_phi_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_low_phi_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_phi_x() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_phi_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_phi_y() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_phi_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_center_high_phi_z() const
+  {
+    PHOOL_VIRTUAL_WARN("get_center_high_phi_z()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_vertex_x(int) const
+  {
+    PHOOL_VIRTUAL_WARN("get_vertex_x()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_vertex_y(int) const
+  {
+    PHOOL_VIRTUAL_WARN("get_vertex_y()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
+
+  virtual double get_vertex_z(int) const
+  {
+    PHOOL_VIRTUAL_WARN("get_vertex_z()");
     return std::numeric_limits<float>::signaling_NaN();
   }
 
@@ -167,6 +301,11 @@ class RawTowerGeom : public PHObject
     return -1;
   }
 
+  virtual double get_rotx() const
+  {
+    PHOOL_VIRTUAL_WARN("get_rotx()");
+    return std::numeric_limits<float>::signaling_NaN();
+  }
   virtual double get_roty() const
   {
     PHOOL_VIRTUAL_WARN("get_roty()");
@@ -178,6 +317,11 @@ class RawTowerGeom : public PHObject
     return std::numeric_limits<float>::signaling_NaN();
   }
 
+  virtual void set_rotx(double)
+  {
+    PHOOL_VIRTUAL_WARN("set_rotx()");
+    return;
+  }
   virtual void set_roty(double)
   {
     PHOOL_VIRTUAL_WARN("set_roty()");
---
+++ offline/packages/CaloBase/RawTowerGeomv5LinkDef.h
@@ -0,0 +1,6 @@
+#ifdef __CINT__
+
+// #pragma link C++ struct point_coordinates + ;
+#pragma link C++ class RawTowerGeomv5 + ;
+
+#endif /* __CINT__ */
