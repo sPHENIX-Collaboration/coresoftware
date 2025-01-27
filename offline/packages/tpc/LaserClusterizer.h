d6a2f0aad0d7eb260d7bb77c89f16ad22569e162
bkimelman <benjamin.kimelman@vanderbilt.edu>
2025-01-27 09:17:14 -0500

    Multi-threading of laser clusterizer. Currently runs 72 threads (one for each module) simultaneously, but need to set option to run all sequentially as well as test out performance for different numbers running together. Also not automatically doing lamination and laser clustering together, needs to be run twice with toggle changed.

--- offline/packages/tpc/LaserClusterizer.h
+++ offline/packages/tpc/LaserClusterizer.h
@@ -30,8 +30,6 @@ class LaserCluster;
 class PHCompositeNode;
 class TrkrHitSet;
 class TrkrHitSetContainer;
-class RawHitSet;
-class RawHitSetContainer;
 class PHG4TpcCylinderGeom;
 class PHG4TpcCylinderGeomContainer;
 
@@ -51,10 +49,8 @@ class LaserClusterizer : public SubsysReco
   int ResetEvent(PHCompositeNode *topNode) override;
   int End(PHCompositeNode *topNode) override;
 
-  // void calc_cluster_parameter(std::vector<pointKeyLaser> &clusHits, std::multimap<unsigned int,std::pair<TrkrDefs::hitkey,TrkrDefs::hitsetkey>> &adcMap);
-  void calc_cluster_parameter(std::vector<pointKeyLaser> &clusHits, std::multimap<unsigned int, std::pair<std::pair<TrkrDefs::hitkey, TrkrDefs::hitsetkey>, std::array<int, 3>>> &adcMap, bool isLamination);
-  // void remove_hits(std::vector<pointKeyLaser> &clusHits,  boost::geometry::index::rtree<pointKeyLaser, boost::geometry::index::quadratic<16> > &rtree, std::multimap <unsigned int, std::pair<TrkrDefs::hitkey,TrkrDefs::hitsetkey>> &adcMap, std::multimap <unsigned int, float*> &adcCoords);
-  void remove_hits(std::vector<pointKeyLaser> &clusHits, boost::geometry::index::rtree<pointKeyLaser, boost::geometry::index::quadratic<16>> &rtree, std::multimap<unsigned int, std::pair<std::pair<TrkrDefs::hitkey, TrkrDefs::hitsetkey>, std::array<int, 3>>> &adcMap);
+  //void calc_cluster_parameter(std::vector<pointKeyLaser> &clusHits, std::multimap<unsigned int, std::pair<std::pair<TrkrDefs::hitkey, TrkrDefs::hitsetkey>, std::array<int, 3>>> &adcMap, bool isLamination);
+  //void remove_hits(std::vector<pointKeyLaser> &clusHits, boost::geometry::index::rtree<pointKeyLaser, boost::geometry::index::quadratic<16>> &rtree, std::multimap<unsigned int, std::pair<std::pair<TrkrDefs::hitkey, TrkrDefs::hitsetkey>, std::array<int, 3>>> &adcMap);
 
   void set_debug(bool debug) { m_debug = debug; }
   void set_debug_name(const std::string &name) { m_debugFileName = name; }
@@ -63,6 +59,8 @@ class LaserClusterizer : public SubsysReco
   void set_min_clus_size(float val) { min_clus_size = val; }
   void set_min_adc_sum(float val) { min_adc_sum = val; }
   void set_max_time_samples(int val) { m_time_samples_max = val; }
+  void set_lamination(bool val) { m_lamination = val; }
+  
 
  private:
   int m_event {-1};
@@ -73,20 +71,18 @@ class LaserClusterizer : public SubsysReco
   LaserEventInfo *m_laserEventInfo {nullptr};
 
   TrkrHitSetContainer *m_hits {nullptr};
-  RawHitSetContainer *m_rawhits {nullptr};
   LaserClusterContainer *m_clusterlist {nullptr};
-  LaserClusterContainer *m_clusterlistLaminations {nullptr};
   ActsGeometry *m_tGeometry {nullptr};
   PHG4TpcCylinderGeomContainer *m_geom_container {nullptr};
   double min_clus_size {1};
   double min_adc_sum {10};
   double m_adc_threshold {74.4};
 
+  bool m_lamination {false};
+
   double m_tdriftmax {0};
   double AdcClockPeriod {53.0};  // ns
   double NZBinsSide {249};
-
-  bool do_read_raw {false};
 
   // TPC shaping offset correction parameter
   // From Tony Frawley July 5, 2022
