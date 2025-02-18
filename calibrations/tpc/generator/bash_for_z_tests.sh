d011b726b3dbd0c31227506683b7b076ed1aed64
Ross Corliss <rcorliss@mit.edu>
2025-02-18 00:01:30 -0500

    added z scan shell script

--- calibrations/tpc/generator/bash_for_z_tests.sh
+++ calibrations/tpc/generator/bash_for_z_tests.sh
@@ -4,13 +4,13 @@
 gainname="not_using_gain"
 
 inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
-outputbase=/direct/star+u/rcorliss/sphenix/generator_output/zerospacecharge_z_test
+outputbase=/direct/star+u/rcorliss/sphenix/generator_output/zerospacecharge_shift_z
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
-export MYINSTALL=/sphenix/u/czhang4/install
+export MYINSTALL=/star/u/rcorliss/install
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
  
-for (( n=0; n<30; n++ ));
+for (( z=-2; z<2; z++ ));
 do 
     outputname=${outputbase}_${z} ;
     echo Processing $inputname index $n to output: $outputname;
