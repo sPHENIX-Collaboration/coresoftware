2adc3e1c6d3ee32dba8e9a03ab0b6ad333e6e012
Ross Corliss <rcorliss@mit.edu>
2025-02-18 14:14:27 -0500

    minor

--- calibrations/tpc/generator/bash_for_condor_submission.sh
+++ calibrations/tpc/generator/bash_for_condor_submission.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+    echo "Usage: bash_for_condor_submission.sh <inputfile> <outputbasename>"
+    echo "or bash_for_condor_submission.sh no"
+    exit 1
+fi
+if [ "$1" == "no" ]; then
+echo "Running in quick test mode"
+inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
+outputbase=/direct/star+u/rcorliss/sphenix/generator_output/quick_test
+gainname="not_using_gain"
+else
 inputname=${1?Error: no input file given}
 outputbase=${2?Error: no output basename given}
 gainname="not_using_gain"
-
-#inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
-#outputbase=/direct/star+u/rcorliss/sphenix/generator_output/zerospacecharge_new_fast
+fi
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
 export MYINSTALL=/sphenix/u/czhang4/install
