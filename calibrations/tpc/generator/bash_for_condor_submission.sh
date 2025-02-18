f0139f45d2e87b9550025353f5bc3e20982adfb8
Ross Corliss <rcorliss@mit.edu>
2025-02-18 14:17:56 -0500

    minor

--- calibrations/tpc/generator/bash_for_condor_submission.sh
+++ calibrations/tpc/generator/bash_for_condor_submission.sh
@@ -4,6 +4,7 @@ echo "Running in quick test mode"
 inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
 outputbase=/direct/star+u/rcorliss/sphenix/generator_output/quick_test
 gainname="not_using_gain"
+nmax=1;
 else
 if [ "$1" == "" ] || [ "$2" == "" ]; then
     echo "Usage: bash_for_condor_submission.sh <inputfile> <outputbasename>"
@@ -12,6 +13,7 @@ if [ "$1" == "" ] || [ "$2" == "" ]; then
 fi
 inputname=${1?Error: no input file given}
 outputbase=${2?Error: no output basename given}
+nmax=30
 gainname="not_using_gain"
 fi
 
@@ -19,7 +21,7 @@ source /opt/sphenix/core/bin/sphenix_setup.sh -n
 export MYINSTALL=/sphenix/u/czhang4/install
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
  
-for (( n=0; n<30; n++ ));
+for (( n=0; n<$nmax; n++ ));
 do 
     outputname=${outputbase}_${n} ;
     echo Processing $inputname index $n to output: $outputname;
