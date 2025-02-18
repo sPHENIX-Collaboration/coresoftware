85b84d505b49cbfb86e64aba94011df30f67874d
Ross Corliss <rcorliss@mit.edu>
2025-02-18 14:07:59 -0500

    roll back accidental change to bash script

--- calibrations/tpc/generator/bash_for_condor_submission.sh
+++ calibrations/tpc/generator/bash_for_condor_submission.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-#inputname=${1?Error: no input file given}
-#outputbase=${2?Error: no output basename given}
+inputname=${1?Error: no input file given}
+outputbase=${2?Error: no output basename given}
 gainname="not_using_gain"
 
-inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
-outputbase=/direct/star+u/rcorliss/sphenix/generator_output/zerospacecharge_z_test
+#inputname=/sphenix/user/shulga/Work/IBF/DistortionMap/Files/Summary_hist_mdc2_UseFieldMaps_AA_event_0_bX10556072.root
+#outputbase=/direct/star+u/rcorliss/sphenix/generator_output/zerospacecharge_new_fast
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
 export MYINSTALL=/sphenix/u/czhang4/install
@@ -12,12 +12,10 @@ source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
  
 for (( n=0; n<30; n++ ));
 do 
-    outputname=${outputbase}_${z} ;
+    outputname=${outputbase}_${n} ;
     echo Processing $inputname index $n to output: $outputname;
     echo $foutputname ;
-    #signature of the function call is: void generate_distortion_map(const char *inputname, const char* gainName, const char *outputname, const char *ibfName, const char *primName, bool hasSpacecharge=true, bool isAdc=false, int nSteps=500, bool scanSteps=false, float zshift=0){
-
-    root -b -q ./generate_distortion_map.C\(\"${inputname}\",\"$gainname\",\"$outputname\",\"_h_SC_ibf_0\",\"_h_SC_prim_0\",0,0,300,0,${z}\)
+    root -b -q ./generate_distortion_map.C\(\"${inputname}\",\"$gainname\",\"$outputname\",\"_h_SC_ibf_${n}\",\"_h_SC_prim_${n}\",1\)
 done
 
 echo all done
