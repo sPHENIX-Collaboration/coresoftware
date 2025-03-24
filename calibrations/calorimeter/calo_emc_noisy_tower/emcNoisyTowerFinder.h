7c536a7c7b197632479a0c66ecfb8f73afa2d79e
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-03-24 07:41:06 -0400

    clang-tidy fixes, cleanup for calo_emc_noisy_tower

--- calibrations/calorimeter/calo_emc_noisy_tower/emcNoisyTowerFinder.h
+++ calibrations/calorimeter/calo_emc_noisy_tower/emcNoisyTowerFinder.h
@@ -4,18 +4,12 @@
 #define CALOEMCNOISYTOWER_EMCNOISYTOWERFINDER_H
 
 #include <fun4all/SubsysReco.h>
-// #include <cdbobjects/CDBTTree.h>
 
-#include <array>
 #include <string>
 #include <vector>
 
-class Fun4AllHistoManager;
-class PHCompositeNode;
-class RawCluster;
 class TFile;
-class TH2F;
-class TowerInfoContainer;
+class TH2;
 class TProfile2D;
 class CDBTTree;
 
@@ -24,15 +18,10 @@ class emcNoisyTowerFinder : public SubsysReco
  public:
   explicit emcNoisyTowerFinder(const std::string& name = "emcNoisyTowerFinder", const std::string& outputName = "emcNoisyTowerFinder.root");
 
-  ~emcNoisyTowerFinder() override;
-  int Init(PHCompositeNode* topNode) override;
+  ~emcNoisyTowerFinder() override = default;
   int InitRun(PHCompositeNode* topNode) override;
   int process_event(PHCompositeNode* topNode) override;
-  int ResetEvent(PHCompositeNode* topNode) override;
-  int EndRun(const int runnumber) override;
   int End(PHCompositeNode* topNode) override;
-  int Reset(PHCompositeNode* /*topNode*/) override;
-  void Print(const std::string& what = "ALL") const override;
   void FillHistograms(const int runnumber, const int segment);
   void CalculateCutOffs(const int runnumber);
   void WriteCDBTree(const int runnumber);
@@ -41,36 +30,45 @@ class emcNoisyTowerFinder : public SubsysReco
   void set_energy_threshold_adc(float val) { energy_threshold_adc = val; }
   void set_energy_threshold_gev(float val) { energy_threshold_gev = val; }
   void set_sigma_bad_thresh(float val) { sigma_bad_thresh = val; }
-  void set_ihcal(){Nphi = 64; Neta = 24; m_caloName = "HCALIN"; return;}
-  void set_ohcal(){Nphi = 64; Neta = 24; m_caloName = "HCALOUT"; return;}
-  float findMedian(const std::vector<float>& arr);
-
+  void set_ihcal()
+  {
+    Nphi = 64;
+    Neta = 24;
+    m_caloName = "HCALIN";
+    return;
+  }
+  void set_ohcal()
+  {
+    Nphi = 64;
+    Neta = 24;
+    m_caloName = "HCALOUT";
+    return;
+  }
+  static float findMedian(const std::vector<float>& arr);
 
  private:
   TFile* out{nullptr};
+  TFile* foutput{nullptr};
 
   CDBTTree* cdbttree{nullptr};
 
-  std::string Outfile{"commissioning.root"};
-
-  std::string m_fieldname = "Femc_datadriven_qm1_correction";
-  std::string m_caloName = "CEMC";
-  
-  TH2F* h_hits_eta_phi_adc{nullptr};
+  TH2* h_hits_eta_phi_adc{nullptr};
   TProfile2D* pr_hits_eta_phi_adc{nullptr};
-  TH2F* h_hits_eta_phi_gev{nullptr};
+  TH2* h_hits_eta_phi_gev{nullptr};
   TProfile2D* pr_hits_eta_phi_gev{nullptr};
 
-  int Nphi = 256;
-  int Neta = 96;
+  int Neta{96};
+  int Nphi{256};
 
-  float energy_threshold_adc = 200;
-  float energy_threshold_gev = 0.25;
+  float energy_threshold_adc{200};
+  float energy_threshold_gev{0.25};
+  float percent_cold_thresh{0.1};
+  float sigma_bad_thresh{5};
 
-  TFile* foutput{nullptr};
+  std::string Outfile{"commissioning.root"};
 
-  float sigma_bad_thresh = 5;
-  float percent_cold_thresh = 0.1;
+  std::string m_fieldname{"Femc_datadriven_qm1_correction"};
+  std::string m_caloName{"CEMC"};
 };
 
 #endif
