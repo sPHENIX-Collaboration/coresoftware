6f7ac722b34b06a24009a3220860c74057feac6b
Anthony Denis Frawley <afrawley@fsu.edu>
2025-05-08 13:35:05 -0400

    Make standard navigation the default, direct navigation enabled from macro.

--- offline/packages/trackreco/PHActsTrkFitter.h
+++ offline/packages/trackreco/PHActsTrkFitter.h
@@ -214,7 +214,7 @@ class PHActsTrkFitter : public SubsysReco
   /// Flag for pp running
   bool m_pp_mode = false;
 
-  bool m_directNavigation = true;
+  bool m_directNavigation = false;
   
   // do we have a constant field
   bool m_ConstField{false};
