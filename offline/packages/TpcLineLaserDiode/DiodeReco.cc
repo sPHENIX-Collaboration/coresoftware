986e1c9c1797035ca65f18f9d0c44e9c2c4afb90
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-05-14 10:11:14 -0400

    cleanup/fixup of tpc laser diode PR

--- offline/packages/TpcLineLaserDiode/DiodeReco.cc
+++ offline/packages/TpcLineLaserDiode/DiodeReco.cc
@@ -11,31 +11,24 @@
 #include <ffarawobjects/TpcDiodev1.h>
 
 #include <Event/Event.h>
-#include <Event/EventTypes.h>
+#include <Event/caen_correction.h>
 #include <Event/packet.h>
-#include "Event/caen_correction.h"
 
-#include <TH2.h>
-#include <TCanvas.h>
+#include <TSystem.h>
 
 #include <algorithm>
+#include <cstdint>                             // for uint16_t
+#include <cstdlib>                             // for exit, size_t
+#include <iostream>                             // for basic_ostream, operat...
 
-using namespace std;
-
-DiodeReco::DiodeReco(const std::string& name)
-  : SubsysReco(name),
-    m_DiodeContainerName("TPCDIODES")
+DiodeReco::DiodeReco(const std::string &name)
+  : SubsysReco(name)
+  , m_DiodeContainerName("TPCDIODES")
 {
-  for(int c=0;c<32;c++)
-    {
-      adc.clear();
-    }
-}
-
-int DiodeReco::Init(PHCompositeNode*)
-{
-  //std::cout << "Init" << std::endl;
-  return Fun4AllReturnCodes::EVENT_OK;
+  for (int c = 0; c < 32; c++)
+  {
+    adc.clear();
+  }
 }
 
 int DiodeReco::InitRun(PHCompositeNode *topNode)
@@ -43,30 +36,30 @@ int DiodeReco::InitRun(PHCompositeNode *topNode)
   PHNodeIterator iter(topNode);
   PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", "DST"));
   if (!dstNode)
-    {
-      dstNode = new PHCompositeNode("DST");
-      topNode->addNode(dstNode);
-    }
-  
+  {
+    dstNode = new PHCompositeNode("DST");
+    topNode->addNode(dstNode);
+  }
+
   PHNodeIterator iterDst(dstNode);
   PHCompositeNode *detNode = dynamic_cast<PHCompositeNode *>(iterDst.findFirst("PHCompositeNode", "TPC"));
   if (!detNode)
-    {
-      detNode = new PHCompositeNode("TPC");
-      dstNode->addNode(detNode);
-    }
-  
+  {
+    detNode = new PHCompositeNode("TPC");
+    dstNode->addNode(detNode);
+  }
+
   TpcDiodeContainer *tpcdiodecont = findNode::getClass<TpcDiodeContainer>(detNode, m_DiodeContainerName);
   if (!tpcdiodecont)
-    {
-      tpcdiodecont = new TpcDiodeContainerv1();
-      PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(tpcdiodecont, m_DiodeContainerName, "PHObject");
-      detNode->addNode(newNode);
-    }
-  
+  {
+    tpcdiodecont = new TpcDiodeContainerv1();
+    PHIODataNode<PHObject> *newNode = new PHIODataNode<PHObject>(tpcdiodecont, m_DiodeContainerName, "PHObject");
+    detNode->addNode(newNode);
+  }
+
   // char name[100];
   // char title[100];
-      
+
   // c_persistency_N = new TCanvas("c_persistency_N","c_persistency_N",800,600);
   // c_persistency_N->Divide(4,4);
   // c_persistency_S = new TCanvas("c_persistency_S","c_persistency_S",800,600);
@@ -79,279 +72,276 @@ int DiodeReco::InitRun(PHCompositeNode *topNode)
   //   persistency[c] = new TH2F(name,title,1024,-0.5,1023.5,256,-250,4250);
   // }
   // waveforms = new TH2F("CAEN Waveforms","CAEN Waveforms;time bins [ns];channels",1024,0,1024,32,0,32);
-  //std::cout << "InitRun" << std::endl;
+  // std::cout << "InitRun" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int DiodeReco::process_event(PHCompositeNode *topNode)
-{ 
+{
   Event *_event = findNode::getClass<Event>(topNode, "PRDF");
   if (!_event)
-    {
-      std::cout << "RawdataUnpacker::Process_Event - Event not found" << std::endl;
-      return Fun4AllReturnCodes::DISCARDEVENT;
-    }
+  {
+    std::cout << "RawdataUnpacker::Process_Event - Event not found" << std::endl;
+    return Fun4AllReturnCodes::DISCARDEVENT;
+  }
 
   diodes = findNode::getClass<TpcDiodeContainer>(topNode, "TPCDIODES");
 
   //_event->identify();
-  caen_correction *cc = 0;
+  caen_correction *cc{nullptr};
   Packet *p = _event->getPacket(2000);
   if (p)
+  {
+    if (!cc)
     {
-      if(!cc)
-	{
-	  switch ( p->iValue(0,"FREQUENCY") )
-	    {
-	    case 0:   // 5GS
-	      cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_5G.dat");
-	      break;
-	    case 1:   // 2.5GS
-	      cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_2G.dat");
-	      break;
-	    case 2:   // 5GS
-	      cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_1G.dat");
-	      break;
-	    }
-	}
-
-      if(cc)
-	{
-	  cc->init(p);
-	}
-
-      //auto newdiode = std::make_unique<TpcDiodev1>();
-      TpcDiodev1 *newdiode = new TpcDiodev1();
-      // uint16_t packetid;
-      // uint16_t channel;
-      // uint16_t maxadc;
-      // uint16_t maxbin;
-      // double integral;
-      // uint16_t nabovethreshold;
-      // double pulsewidth;
-      // uint16_t samples;
-      
-      for(int c=0;c<32;c++)
-	{
-	  for(int t=0;t<1024;t++)
-	    {
-	      adc.push_back(cc->caen_corrected(t,c));
-	    }
-      
-	  PedestalCorrected(0,200);
-	  uint16_t maxadc = MaxAdc(2,200,300);
-	  uint16_t maxbin = MaxBin(2);
-	  double integral = Integral(0,1024);
-	  uint16_t nabovethreshold = NAboveThreshold(200,100);
-	  double pulsewidth = PulseWidth(200,100);
-	  const uint16_t samples = adc.size();
-
-	  uint16_t packetid=2000;
-	  uint16_t channel=c;
-
-	  newdiode->set_packetid(packetid);
-	  newdiode->set_channel(channel);
-	  newdiode->set_maxadc(maxadc);
-	  newdiode->set_maxbin(maxbin);
-	  newdiode->set_integral(integral);
-	  newdiode->set_nabovethreshold(nabovethreshold);
-	  newdiode->set_pulsewidth(pulsewidth);
-	  newdiode->set_samples(samples);
-
-	  for(uint16_t s=0;s<samples;s++)
-	    {
-	      uint16_t adcval = adc[s];
-	      newdiode->set_adc(s,adcval);
-	    }
-
-	  diodes->AddDiode(newdiode);
-	  adc.clear();
-	}
-      
-      // int laser=-1;
-      // int nlaser=0;
-      
-      // for(int c=0;c<32;c++){
-      // 	for(int t=0;t<1024;t++){
-      // 	  adc.push_back(cc->caen_corrected(t,c));
-      // 	}
-
-      // 	PedestalCorrected(0,200);
-
-      // 	if(c<4||(c>15&&c<20))
-      // 	  {
-      // 	    int max = MaxAdc(2,200,300);
-      // 	    if(max>200)
-      // 	      {
-      // 		laser=c;
-      // 		cout << laser << endl;
-      // 		cout << max << endl;
-      // 		nlaser++;
-      // 	      }
-      // 	  }
-      // 	adc.clear();
-      // }
-
-      // cout <<  "Laser: " << laser << endl;
-
-      // if(event==2){
-      // 	  for(int c=0;c<32;c++)
-      // 	    {
-      // 	      for(int t=0;t<1024;t++){
-      // 		adc.push_back(cc->caen_corrected(t,c));
-      // 	      }
-	      
-      // 	      PedestalCorrected(0,200);
-	      
-      // 	      if(nlaser>1)
-      // 		{
-      // 		  cout << "More than one laser fired! Stopping code!" << endl;
-      // 		  return -1;
-      // 		}
-	      
-      // 	      if(laser<0)
-      // 		{
-      // 		  cout << "No laser fired in this event!" << endl;
-      // 		}
-      // 	      for(int s=0;s<static_cast<int>(adc.size());s++){
-      // 		persistency[c]->Fill(s,adc[s]);
-      // 		waveforms->Fill(s,c,adc[s]);
-      // 	      }
-      // 	      if(c<16){
-      // 		c_persistency_N->cd(c+1);
-      // 		persistency[c]->Draw("colz");
-      // 	      }
-      // 	      else
-      // 		{
-      // 		  c_persistency_S->cd(c-15);
-      // 		  persistency[c]->Draw("colz");
-      // 		}
-      // 	      adc.clear();
-      // 	    }
-      // }
-      //p->dump();
-      cout << diodes->get_Laser() << endl;
-      // for(int c=0;c<32;c++){
-      // 	cout << diodes->get_diode(c)->get_maxadc() << endl;
-      // }
-      delete p;
-      //newdiode->Clear();
+      switch (p->iValue(0, "FREQUENCY"))
+      {
+      case 0:  // 5GS
+        cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_5G.dat");
+        break;
+      case 1:  // 2.5GS
+        cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_2G.dat");
+        break;
+      case 2:  // 5GS
+        cc = new caen_correction("/sphenix/user/llegnosky/calib/calib_24974_1G.dat");
+        break;
+      default:
+        std::cout << "Bad selection " << p->iValue(0, "FREQUENCY") << std::endl;
+        gSystem->Exit(1);
+        exit(1);
+        break;
+      }
     }
 
-  //diodes->identify();
-  //cout << "Hello" << endl;
-  //cout << diodes->get_ndiodes() << endl;
-    
-  //cout << diodes->get_Laser() << endl;
-  
+    if (cc)
+    {
+      cc->init(p);
+    }
+
+    // auto newdiode = std::make_unique<TpcDiodev1>();
+    TpcDiodev1 *newdiode = new TpcDiodev1();
+    // uint16_t packetid;
+    // uint16_t channel;
+    // uint16_t maxadc;
+    // uint16_t maxbin;
+    // double integral;
+    // uint16_t nabovethreshold;
+    // double pulsewidth;
+    // uint16_t samples;
+
+    for (int c = 0; c < 32; c++)
+    {
+      for (int t = 0; t < 1024; t++)
+      {
+        adc.push_back(cc->caen_corrected(t, c));
+      }
+
+      PedestalCorrected(0, 200);
+      uint16_t maxadc = MaxAdc(2, 200, 300);
+      uint16_t maxbin = MaxBin(2);
+      double integral = Integral(0, 1024);
+      uint16_t nabovethreshold = NAboveThreshold(200, 100);
+      double pulsewidth = PulseWidth(200, 100);
+      const uint16_t samples = adc.size();
+
+      uint16_t packetid = 2000;
+      uint16_t channel = c;
+
+      newdiode->set_packetid(packetid);
+      newdiode->set_channel(channel);
+      newdiode->set_maxadc(maxadc);
+      newdiode->set_maxbin(maxbin);
+      newdiode->set_integral(integral);
+      newdiode->set_nabovethreshold(nabovethreshold);
+      newdiode->set_pulsewidth(pulsewidth);
+      newdiode->set_samples(samples);
+
+      for (uint16_t s = 0; s < samples; s++)
+      {
+        uint16_t adcval = adc[s];
+        newdiode->set_adc(s, adcval);
+      }
+
+      diodes->AddDiode(newdiode);
+      adc.clear();
+    }
+
+    // int laser=-1;
+    // int nlaser=0;
+
+    // for(int c=0;c<32;c++){
+    // 	for(int t=0;t<1024;t++){
+    // 	  adc.push_back(cc->caen_corrected(t,c));
+    // 	}
+
+    // 	PedestalCorrected(0,200);
+
+    // 	if(c<4||(c>15&&c<20))
+    // 	  {
+    // 	    int max = MaxAdc(2,200,300);
+    // 	    if(max>200)
+    // 	      {
+    // 		laser=c;
+    // 		std::cout << laser << std::endl;
+    // 		std::cout << max << std::endl;
+    // 		nlaser++;
+    // 	      }
+    // 	  }
+    // 	adc.clear();
+    // }
+
+    // std::cout <<  "Laser: " << laser << std::endl;
+
+    // if(event==2){
+    // 	  for(int c=0;c<32;c++)
+    // 	    {
+    // 	      for(int t=0;t<1024;t++){
+    // 		adc.push_back(cc->caen_corrected(t,c));
+    // 	      }
+
+    // 	      PedestalCorrected(0,200);
+
+    // 	      if(nlaser>1)
+    // 		{
+    // 		  std::cout << "More than one laser fired! Stopping code!" << std::endl;
+    // 		  return -1;
+    // 		}
+
+    // 	      if(laser<0)
+    // 		{
+    // 		  std::cout << "No laser fired in this event!" << std::endl;
+    // 		}
+    // 	      for(int s=0;s<static_cast<int>(adc.size());s++){
+    // 		persistency[c]->Fill(s,adc[s]);
+    // 		waveforms->Fill(s,c,adc[s]);
+    // 	      }
+    // 	      if(c<16){
+    // 		c_persistency_N->cd(c+1);
+    // 		persistency[c]->Draw("colz");
+    // 	      }
+    // 	      else
+    // 		{
+    // 		  c_persistency_S->cd(c-15);
+    // 		  persistency[c]->Draw("colz");
+    // 		}
+    // 	      adc.clear();
+    // 	    }
+    // }
+    // p->dump();
+    std::cout << diodes->get_Laser() << std::endl;
+    // for(int c=0;c<32;c++){
+    // 	std::cout << diodes->get_diode(c)->get_maxadc() << std::endl;
+    // }
+    delete p;
+    // newdiode->Clear();
+  }
+
+  // diodes->identify();
+  // std::cout << "Hello" << std::endl;
+  // std::cout << diodes->get_ndiodes() << std::endl;
+
+  // std::cout << diodes->get_Laser() << std::endl;
+
   // c_waveforms->cd();
   // waveforms->Draw("LEGO");
-  
-  //std::cout << "process_event" << std::endl;
-  // event++;
-  return Fun4AllReturnCodes::EVENT_OK;
-}
-  
-int DiodeReco::End(PHCompositeNode*)
-{
-  //std::cout << "End" << std::endl;
+
+  // std::cout << "process_event" << std::endl;
+  //  event++;
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-
-double DiodeReco::MaxAdc(int n,int low_bin,int high_bin)
+double DiodeReco::MaxAdc(int n, int low_bin, int high_bin)
 {
-  double MaxSum = -99999; // Maximum sum over n bins within the bin range 
-  int MaxMid = -1; // Bin number of the middle bin used to calculate MaxSum
+  double MaxSum = -99999;  // Maximum sum over n bins within the bin range
+  int MaxMid = -1;         // Bin number of the middle bin used to calculate MaxSum
 
   // Bracket the limits safely...
-  int start = max(1+n,low_bin+n);
-  int end   = min(int(adc.size())-n,high_bin-n);
-  for (int mid=start; mid<end; mid++)
+  int start = std::max(1 + n, low_bin + n);
+  int end = std::min(int(adc.size()) - n, high_bin - n);
+  for (int mid = start; mid < end; mid++)
+  {
+    double Sum = 0;
+    for (int bin = mid - n; bin <= mid + n; bin++)
     {
-      double Sum=0;
-      for (int bin=mid-n; bin<=mid+n; bin++)
-        {
-          Sum += adc[bin];
-        }
-      if (Sum > MaxSum)
-        {
-          MaxSum = Sum;
-          MaxMid = mid;
-        }
+      Sum += adc[bin];
     }
+    if (Sum > MaxSum)
+    {
+      MaxSum = Sum;
+      MaxMid = mid;
+    }
+  }
 
-  if(MaxMid<0)
-    {
-      cout<<"Error: Maximum ADC could not be found!"<<endl;
-    }
-  return MaxSum/(2.0*n+1.0);
+  if (MaxMid < 0)
+  {
+    std::cout << "Error: Maximum ADC could not be found!" << std::endl;
+  }
+  return MaxSum / (2.0 * n + 1.0);
 }
 
 int DiodeReco::MaxBin(int n)
 {
   double MaxSum = -99999;
   int MaxMid = -1;
-  for (int mid=1+n; mid<static_cast<int>(adc.size())-n; mid++)
+  for (int mid = 1 + n; mid < static_cast<int>(adc.size()) - n; mid++)
+  {
+    double Sum = 0;
+    for (int bin = mid - n; bin <= mid + n; bin++)
     {
-      double Sum=0;
-      for (int bin=mid-n; bin<=mid+n; bin++)
-        {
-          Sum += adc[bin];
-        }
-      if (Sum > MaxSum)
-        {
-          MaxSum = Sum;
-          MaxMid = mid;
-        }
+      Sum += adc[bin];
     }
-  if(MaxMid<0)
+    if (Sum > MaxSum)
     {
-      cout<<"Error: Maximum ADC could not be found!"<<endl;
+      MaxSum = Sum;
+      MaxMid = mid;
     }
+  }
+  if (MaxMid < 0)
+  {
+    std::cout << "Error: Maximum ADC could not be found!" << std::endl;
+  }
   return MaxMid;
 }
 
-double DiodeReco::Integral(int low_bin,int high_bin)
+double DiodeReco::Integral(int low_bin, int high_bin)
 {
-if (low_bin<0) low_bin=0;
- if (high_bin>static_cast<int>(adc.size())) high_bin=static_cast<int>(adc.size());
+  low_bin = std::max(low_bin, 0);
+  high_bin = std::min(high_bin, static_cast<int>(adc.size()));
 
   double SUM = 0;
-  for(int i=low_bin; i<high_bin; i++)
-    {
-      SUM += adc[i];
-    }
+  for (int i = low_bin; i < high_bin; i++)
+  {
+    SUM += adc[i];
+  }
   return SUM;
 }
 
-int DiodeReco::NAboveThreshold( double upper_thr, double lower_thr)
+int DiodeReco::NAboveThreshold(double upper_thr, double lower_thr)
 {
   int nAbove = 0;
 
   bool belowThreshold = true;
 
-  for(int i=0; i<static_cast<int>(adc.size()); i++)
+  for (size_t i = 0; i < adc.size(); i++)
+  {
+    if (belowThreshold && adc[i] >= upper_thr)
     {
-      if (belowThreshold && adc[i] >= upper_thr)
-        {
-          nAbove++;
-          belowThreshold = false;
-        }
-
-      else if (!belowThreshold && adc[i] < lower_thr)
-        {
-          belowThreshold = true;
-        }
+      nAbove++;
+      belowThreshold = false;
     }
+
+    else if (!belowThreshold && adc[i] < lower_thr)
+    {
+      belowThreshold = true;
+    }
+  }
 
   return nAbove;
 }
 
 double DiodeReco::PulseWidth(double upper_thr, double lower_thr)
 {
-
-  //  The results of this routine are ONLY valid 
+  //  The results of this routine are ONLY valid
   //  if NAbove is one.
 
   bool belowThreshold = true;
@@ -359,45 +349,54 @@ double DiodeReco::PulseWidth(double upper_thr, double lower_thr)
   int left = 0;
   int right = 0;
 
-  for(int i=0; i<static_cast<int>(adc.size()); i++)
+  for (int i = 0; i < static_cast<int>(adc.size()); i++)
+  {
+    if (belowThreshold && adc[i] >= upper_thr)
     {
-      if ( belowThreshold && adc[i] >= upper_thr)
-        {
-          left = i;
-          belowThreshold = false;
-        }
-
-      else if ( !belowThreshold && adc[i] < lower_thr)
-        {
-          right = i;
-          belowThreshold = true;
-        }
+      left = i;
+      belowThreshold = false;
     }
 
-  return right-left;
+    else if (!belowThreshold && adc[i] < lower_thr)
+    {
+      right = i;
+      belowThreshold = true;
+    }
+  }
+
+  return right - left;
 }
 
-void DiodeReco::PedestalCorrected(int low_bin=-1, int high_bin=9999)
+void DiodeReco::PedestalCorrected(int low_bin = -1, int high_bin = 9999)
 {
-  if (low_bin<0) low_bin=0;
-  if (high_bin>static_cast<int>(adc.size())) high_bin=adc.size();
+  low_bin = std::max(low_bin, 0);
+  if (high_bin > static_cast<int>(adc.size()))
+  {
+    high_bin = adc.size();
+  }
 
   // Copy all voltages in the pedestal region & sort
-  vector<double> sam;
-  for(int i=low_bin; i<high_bin; i++)
-    {
-      sam.push_back(adc[i]);
-    }
+  std::vector<double> sam;
+  for (int i = low_bin; i < high_bin; i++)
+  {
+    sam.push_back(adc[i]);
+  }
   sort(sam.begin(), sam.end());
 
   // Assign the pedestal as the median of this distribution
-  int n=sam.size();
+  int n = sam.size();
   double PEDESTAL;
-  if(n%2 != 0) PEDESTAL = sam[n/2];
-  else PEDESTAL = (sam[(n-1)/2] + sam[n/2])/2.0;
-  
-  for(int i=0; i<static_cast<int>(adc.size()); i++)
-    {
-      adc[i] = adc[i]-PEDESTAL;
-    }
+  if (n % 2 != 0)
+  {
+    PEDESTAL = sam[n / 2];
+  }
+  else
+  {
+    PEDESTAL = (sam[(n - 1) / 2] + sam[n / 2]) / 2.0;
+  }
+
+  for (size_t i = 0; i < adc.size(); i++)
+  {
+    adc[i] = adc[i] - PEDESTAL;
+  }
 }
--- offline/packages/TpcLineLaserDiode/DiodeReco.h
+++ offline/packages/TpcLineLaserDiode/DiodeReco.h
@@ -4,47 +4,31 @@
 #include <fun4all/SubsysReco.h>
 
 #include <string>
-#include <iostream>
 #include <vector>
 
 class PHCompositeNode;
-class TH2F;
-class TCanvas;
 class TpcDiodeContainer;
-//class TpcDiode;
-//class packet;
 
 class DiodeReco : public SubsysReco
 {
  public:
   DiodeReco(const std::string &name = "DiodeReco");
   ~DiodeReco() override = default;
-  
-  int Init(PHCompositeNode *topNode) override;
+
   int InitRun(PHCompositeNode *topNode) override;
   int process_event(PHCompositeNode *topNode) override;
-  int End(PHCompositeNode *topNode) override;
 
-  std::vector<double> adc;
+  double MaxAdc(int n, int low_bin = 0, int high_bin = 9999);  // Signal is averaged over "avg" number of bins interatively within the bin range [low_bin,high_bin]
+  int MaxBin(int n);
+  double Integral(int low_bin, int high_bin);
+  int NAboveThreshold(double upper_thr, double lower_thr);
+  double PulseWidth(double upper_thr, double lower_thr);
+  void PedestalCorrected(int low_bin, int high_bin);
 
-  double MaxAdc(int avg, int low_bin=0,int high_bin=9999); // Signal is averaged over "avg" number of bins interatively within the bin range [low_bin,high_bin]
-  int MaxBin(int avg);
-  double Integral(int low_bin,int high_bin);
-  int NAboveThreshold(double upper_thr,double lower_thr);
-  double PulseWidth(double upper_thr,double lower_thr);
-  void PedestalCorrected(int low_bin,int high_bin);
-
-private:
-  TpcDiodeContainer *diodes = nullptr;
+ private:
+  TpcDiodeContainer *diodes{nullptr};
   std::string m_DiodeContainerName;
-  
-  // TCanvas *c_waveforms;
-  // TCanvas *c_persistency_N;
-  // TCanvas *c_persistency_S;
-  // TH2F *persistency[32];
-  // TH2F *waveforms;
-
-  // int event=0;
+  std::vector<double> adc;
 };
 
 #endif
