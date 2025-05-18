4e5c4ec5a2c1091fde57fb245d059fa84e83bb20
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-05-18 16:49:14 -0400

    clang-tidy for centrality

--- offline/packages/centrality/CentralityInfov2.cc
+++ offline/packages/centrality/CentralityInfov2.cc
@@ -2,7 +2,7 @@
 
 #include <limits>
 
-void CentralityInfov2::identify(std::ostream& os) const
+void CentralityInfov2::identify(std::ostream &os) const
 {
   os << "  CentralityInfo v2: " << std::endl;
   os << "      Centile: " << (has_centile(CentralityInfo::PROP::mbd_NS) ? get_centile(CentralityInfo::PROP::mbd_NS) : -999.99) << std::endl;
@@ -27,10 +27,8 @@ int CentralityInfov2::get_centrality_bin(const PROP prop_id) const
   {
     return std::numeric_limits<int>::quiet_NaN();
   }
-  else
-  {
-    return _centrality_bin_map.at(prop_id);
-  }
+
+  return _centrality_bin_map.at(prop_id);
 }
 
 void CentralityInfov2::CopyTo(CentralityInfo *info)
@@ -38,6 +36,6 @@ void CentralityInfov2::CopyTo(CentralityInfo *info)
   CentralityInfov1::CopyTo(info);
   for (auto const &it : _centrality_bin_map)
   {
-    info->set_centrality_bin((CentralityInfo::PROP) (it.first),it.second);
+    info->set_centrality_bin((CentralityInfo::PROP)(it.first), it.second);
   }
 }
