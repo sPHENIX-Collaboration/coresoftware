11a9995737ed7dfdcc87d0db47031540229185a6
xyu3 <xyu3@ssh.sdcc.bnl.gov>
2025-05-27 20:05:27 -0400

    change disable function to enable function

--- offline/packages/tpc/TpcGlobalPositionWrapper.h
+++ offline/packages/tpc/TpcGlobalPositionWrapper.h
@@ -43,10 +43,10 @@ class TpcGlobalPositionWrapper
   //! load relevant nodes from tree
   void loadNodes(PHCompositeNode* /*topnode*/);
 
-  void disable_module_edge_corr() { m_disable_module_edge_corr = true; }
-  void disable_static_corr() { m_disable_static_corr = true; }
-  void disable_average_corr() { m_disable_average_corr = true; }
-  void disable_fluctuation_corr() { m_disable_fluctuation_corr = true; }
+  void set_enable_module_edge_corr(bool flag) { m_enable_module_edge_corr = flag; }
+  void set_enable_static_corr(bool flag) { m_enable_static_corr = flag; }
+  void set_enable_average_corr(bool flag) { m_enable_average_corr = flag; }
+  void set_enable_fluctuation_corr(bool flag) { m_enable_fluctuation_corr = flag; }
 
   //! apply all loaded distortion corrections to a given position
   Acts::Vector3 applyDistortionCorrections( Acts::Vector3 /*source*/ ) const;
@@ -73,19 +73,19 @@ class TpcGlobalPositionWrapper
 
   //! module edge distortion correction container
   TpcDistortionCorrectionContainer* m_dcc_module_edge{nullptr};
-  bool m_disable_module_edge_corr = false;
+  bool m_enable_module_edge_corr = true;
 
   //! static distortion correction container
   TpcDistortionCorrectionContainer* m_dcc_static{nullptr};
-  bool m_disable_static_corr = false;
+  bool m_enable_static_corr = true;
 
   //! average distortion correction container
   TpcDistortionCorrectionContainer* m_dcc_average{nullptr};
-  bool m_disable_average_corr = false;
+  bool m_enable_average_corr = true;
 
   //! fluctuation distortion container
   TpcDistortionCorrectionContainer* m_dcc_fluctuation{nullptr};
-  bool m_disable_fluctuation_corr = false;
+  bool m_enable_fluctuation_corr = true;
 
 };
 
