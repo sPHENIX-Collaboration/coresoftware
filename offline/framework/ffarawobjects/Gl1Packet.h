b4b12107523c6b99f52ebc647d98b50ac5aaadce
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-06-15 12:26:09 -0400

    simplify gl1 packet deps, move i/lvalue to base class, remove redundant methods

--- offline/framework/ffarawobjects/Gl1Packet.h
+++ offline/framework/ffarawobjects/Gl1Packet.h
@@ -3,6 +3,7 @@
 
 #include "OfflinePacketv1.h"
 
+#include <cstdint>
 #include <limits>
 
 class Gl1Packet : public OfflinePacketv1
@@ -11,8 +12,11 @@ class Gl1Packet : public OfflinePacketv1
   Gl1Packet() = default;
   ~Gl1Packet() override = default;
   using OfflinePacket::FillFrom;
+  using OfflinePacket::lValue;
   virtual void setScaler(int /*iscal*/, int /*index*/, uint64_t /*lval*/) { return; }
+  virtual uint64_t getScaler(int /*iscal*/, int /*index*/) const { return 0; }
   virtual void setGl1pScaler(int /*iscal*/, int /*index*/, uint64_t /*lval*/) { return; }
+  virtual uint64_t getGl1pScaler(int /*iscal*/, int /*index*/) const { return 0; }
   virtual void setPacketNumber(const unsigned int /* i */) { return; }
   virtual unsigned int getPacketNumber() const { return 0; }
   virtual void setBunchNumber(const uint64_t /*i*/) { return; }
@@ -31,6 +35,10 @@ class Gl1Packet : public OfflinePacketv1
   virtual uint64_t getGTMAllBusyVector() const { return 0; }
 
   virtual void FillFrom(const Gl1Packet* /*pkt*/) { return; }
+// aggregated i/l Values to avoid duplications in the versioned classes
+  int iValue(const int i) const override;
+  long long lValue(const int i, const std::string &what) const override;
+  long long lValue(const int i, const int j) const override;
 
  private:
   ClassDefOverride(Gl1Packet, 2)
--- offline/framework/ffarawobjects/Gl1Packetv3.cc
+++ offline/framework/ffarawobjects/Gl1Packetv3.cc
@@ -6,18 +6,8 @@
 
 void Gl1Packetv3::Reset()
 {
-  OfflinePacketv1::Reset();
-  packet_nr = 0;
-  BunchNumber = std::numeric_limits<uint64_t>::max();
-  TriggerInput = 0;
-  LiveVector = 0;
-  ScaledVector = 0;
-  GTMBusyVector = 0;
+  Gl1Packetv2::Reset();
   GTMAllBusyVector = 0;
-  for (auto &row : scaler)
-  {
-    row.fill(0);
-  }
   return;
 }
 
@@ -25,107 +15,23 @@ void Gl1Packetv3::identify(std::ostream &os) const
 {
   os << "Gl1Packetv3: " << std::endl;
   OfflinePacketv1::identify(os);
-  os << "bunch number: " << BunchNumber << std::endl;
+  os << "bunch number: " << getBunchNumber() << std::endl;
   return;
 }
 
 void Gl1Packetv3::FillFrom(const Gl1Packet *pkt)
 {
-  setBunchNumber(pkt->getBunchNumber());
-  setPacketNumber(pkt->getPacketNumber());
-  setTriggerInput(pkt->getTriggerInput());
-  setLiveVector(pkt->getLiveVector());
-  setScaledVector(pkt->getScaledVector());
-  setGTMBusyVector(pkt->getGTMBusyVector());
+  Gl1Packetv2::FillFrom(pkt);
   setGTMAllBusyVector(pkt->getGTMAllBusyVector());
-  for (int i = 0; i < 64; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      setScaler(i, j, pkt->lValue(i, j));
-    }
-  }
-  std::string gl1p_names[3]{"GL1PRAW", "GL1PLIVE", "GL1PSCALED"};
-  for (int i = 0; i < 16; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      setGl1pScaler(i, j, pkt->lValue(i, gl1p_names[j]));
-    }
-  }
-  OfflinePacketv1::FillFrom(pkt);
 }
 
-int Gl1Packetv3::iValue(const int i) const
-{
-  if (i == 0)
-  {
-    return packet_nr;
-  }
-  std::cout << PHWHERE << " Bad argument for iValue: " << i << std::endl;
-  return std::numeric_limits<int>::min();
-}
 
-long long Gl1Packetv3::lValue(const int i, const int j) const
-{
-  return scaler.at(i).at(j);
-}
-
-long long Gl1Packetv3::lValue(const int i, const std::string &what) const
-{
-  if (what == "BCO")
-  {
-    return getBCO();
-  }
-  if (what == "TriggerInput")
-  {
-    return getTriggerInput();
-  }
-  if (what == "TriggerVector")
-  {
-    return getTriggerVector();
-  }
-  if (what == "LiveVector")
-  {
-    return getLiveVector();
-  }
-  if (what == "ScaledVector")
-  {
-    return getScaledVector();
-  }
-  if (what == "GTMBusyVector")
-  {
-    return getGTMBusyVector();
-  }
-  if (what == "GTMAllBusyVector")
-  {
-    return getGTMAllBusyVector();
-  }
-  if (what == "BunchNumber")
-  {
-    return getBunchNumber();
-  }
-  if (what == "GL1PRAW")
-  {
-    return gl1pscaler.at(i).at(0);
-  }
-  if (what == "GL1PLIVE")
-  {
-    return gl1pscaler.at(i).at(1);
-  }
-  if (what == "GL1PSCALED")
-  {
-    return gl1pscaler.at(i).at(2);
-  }
-  std::cout << "option " << what << " not implemented" << std::endl;
-  return std::numeric_limits<uint64_t>::max();
-}
 
 void Gl1Packetv3::dump(std::ostream &os) const
 {
   os << "packet nr:       " << iValue(0) << std::endl;
   os << "Beam Clock:      "
-     << "0x" << std::hex << lValue(0, "BCO") << std::dec << "   " << lValue(0, "BCO") << std::endl;
+     << "0x" << std::hex << Gl1Packetv2::lValue(0, "BCO") << std::dec << "   " << lValue(0, "BCO") << std::endl;
   os << "Trigger Input:   "
      << "0x" << std::hex << lValue(0, "TriggerInput") << std::dec << "   " << lValue(0, "TriggerInput") << std::endl;
   os << "Live Vector:     "
