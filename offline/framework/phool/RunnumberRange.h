6e3313a19905ecefe06890fccf8d0308601c43e5
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-06-16 17:03:06 -0400

    add include with run ranges

---
+++ offline/framework/phool/RunnumberRange.h
@@ -0,0 +1,13 @@
+#ifndef PHOOL_RUNNUMBERRANGE_H
+#define PHOOL_RUNNUMBERRANGE_H
+
+// first and last physics run
+namespace RunnumberRange
+{
+  static const int RUN2PP_FIRST = 47286;
+  static const int RUN2PP_LAST = 53880;
+  static const int RUN2AUAU_FIRST = 54128;
+  static const int RUN2AUAU_LAST = 54974;
+  static const int RUN3AUAU_FIRST = 66457;
+  static const int RUN3AUAU_LAST = 20000000;
+}
