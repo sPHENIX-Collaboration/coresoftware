4b296a14920f81cba95c9e2309256df58fe73602
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2025-07-11 12:14:08 -0400

    indentations.

--- offline/packages/trackreco/PHSimpleKFProp.cc
+++ offline/packages/trackreco/PHSimpleKFProp.cc
@@ -249,190 +249,189 @@ int PHSimpleKFProp::process_event(PHCompositeNode* topNode)
   std::vector<std::vector<TrkrDefs::cluskey>> new_chains;
   std::vector<TrackSeed_v2> unused_tracks;
 
-#pragma omp parallel num_threads(2)
-{
-  std::vector<std::vector<TrkrDefs::cluskey>> local_chains;
-  std::vector<TrackSeed_v2> local_unused;
-
-// --- build a thread-local copy of the field map ---
- PHFieldConfigv1 fcfg_loc;
- if (!_use_const_field)
- {
-   fcfg_loc.set_field_config(PHFieldConfig::FieldConfigTypes::Field3DCartesian);
-   fcfg_loc.set_filename(m_magField);
- }
- else
- {
-   fcfg_loc.set_field_config(PHFieldConfig::FieldConfigTypes::kFieldUniform);
-   fcfg_loc.set_magfield_rescale(_const_field);
- }
- auto thread_field_map = std::unique_ptr<PHField>(PHFieldUtility::BuildFieldMap(&fcfg_loc));
-
-  //auto thread_field_map = std::make_unique<PHField>(*(_field_map));
-  std::unique_ptr<ALICEKF> fitter_loc = std::make_unique<ALICEKF>(topNode, _cluster_map, thread_field_map.get(), _fieldDir,
-                                     _min_clusters_per_track, _max_sin_phi, Verbosity());
-      fitter_loc->setNeonFraction(Ne_frac);
-      fitter_loc->setArgonFraction(Ar_frac);
-      fitter_loc->setCF4Fraction(CF4_frac);
-      fitter_loc->setNitrogenFraction(N2_frac);
-      fitter_loc->setIsobutaneFraction(isobutane_frac);
-      fitter_loc->useConstBField(_use_const_field);
-      fitter_loc->setConstBField(_const_field);
-      fitter_loc->useFixedClusterError(_use_fixed_clus_err);
-      fitter_loc->setFixedClusterError(0, _fixed_clus_err.at(0));
-      fitter_loc->setFixedClusterError(1, _fixed_clus_err.at(1));
-      fitter_loc->setFixedClusterError(2, _fixed_clus_err.at(2));
-
-  #pragma omp for schedule(static)
-  for (size_t track_it = 0; track_it != _track_map->size(); ++track_it)
+  #pragma omp parallel num_threads(2)
   {
-    if (Verbosity())
+    std::vector<std::vector<TrkrDefs::cluskey>> local_chains;
+    std::vector<TrackSeed_v2> local_unused;
+
+    // --- build a thread-local copy of the field map ---
+    PHFieldConfigv1 fcfg_loc;
+    if (!_use_const_field)
     {
-      std::cout << "TPC seed " << track_it << std::endl;
-    }
-
-    // if not a TPC track, ignore
-    auto track = _track_map->get(track_it);
-    const bool is_tpc = std::any_of(
-        track->begin_cluster_keys(),
-        track->end_cluster_keys(),
-        [](const TrkrDefs::cluskey& key)
-        { return TrkrDefs::getTrkrId(key) == TrkrDefs::tpcId; });
-
-    if (is_tpc)
-    {
-
-      // copy list of seed cluster keys
-      std::vector<std::vector<TrkrDefs::cluskey>> keylist_A(1);
-      std::copy(track->begin_cluster_keys(), track->end_cluster_keys(), std::back_inserter(keylist_A[0]));
-
-      // copy seed clusters position into local map
-      std::map<TrkrDefs::cluskey, Acts::Vector3> trackClusPositions;
-      std::transform(track->begin_cluster_keys(), track->end_cluster_keys(), std::inserter(trackClusPositions, trackClusPositions.end()),
-        [globalPositions](const auto& key)
-        { return std::make_pair(key, globalPositions.at(key)); });
-
-      /// Can't circle fit a seed with less than 3 clusters, skip it
-      if (keylist_A[0].size() < 3)
-      {
-        continue;
-      }
-
-      /// This will by definition return a single pair with each vector
-      /// in the pair length 1 corresponding to the seed info
-      std::vector<float> trackChi2;
-      //timer.stop();
-      //timer.restart();
-
-      auto seedpair = fitter_loc->ALICEKalmanFilter(keylist_A, false, trackClusPositions, trackChi2);
-
-      //timer.stop();
-      if (Verbosity() > 3)
-      {
-        std::cout << "single track ALICEKF time " << timer.elapsed()
-                  << std::endl;
-      }
-      //timer.restart();
-
-      /// circle fit back to update track parameters
-      TrackSeedHelper::circleFitByTaubin(track, trackClusPositions, 7, 55);
-      TrackSeedHelper::lineFit(track, trackClusPositions, 7, 55);
-      track->set_phi(TrackSeedHelper::get_phi(track, trackClusPositions));
-      //timer.stop();
-      if (Verbosity() > 3)
-      {
-        std::cout << "single track circle fit time " << timer.elapsed() << std::endl;
-      }
-      if (seedpair.first.empty()|| seedpair.second.empty())
-      {
-        continue;
-      }
-
-      if (Verbosity())
-      {
-        std::cout << "is tpc track" << std::endl;
-      }
-
-      //timer.stop();
-      //timer.restart();
-
-      if (Verbosity())
-      {
-        std::cout << "propagate first round" << std::endl;
-      }
-
-      auto preseed = PropagateTrack(track, PropagationDirection::Inward, seedpair.second.at(0), globalPositions);
-      if (Verbosity())
-      {
-        std::cout << "preseed size " << preseed.size() << std::endl;
-      }
-
-      std::vector<std::vector<TrkrDefs::cluskey>> kl = {preseed};
-      if (Verbosity())
-      {
-        std::cout << "kl size " << kl.size() << std::endl;
-      }
-      std::vector<float> pretrackChi2;
-
-      auto prepair = fitter_loc->ALICEKalmanFilter(kl, false, globalPositions, pretrackChi2);
-      if (prepair.first.empty() || prepair.second.empty())
-      {
-        continue;
-      }
-
-      std::reverse(kl.at(0).begin(), kl.at(0).end());
-
-      auto pretrack = prepair.first.at(0);
-
-      // copy seed clusters position into local map
-      std::map<TrkrDefs::cluskey, Acts::Vector3> pretrackClusPositions;
-      std::transform(pretrack.begin_cluster_keys(), pretrack.end_cluster_keys(), std::inserter(pretrackClusPositions, pretrackClusPositions.end()),
-        [globalPositions](const auto& key)
-        { return std::make_pair(key, globalPositions.at(key)); });
-
-      // fit seed
-      TrackSeedHelper::circleFitByTaubin(&pretrack,pretrackClusPositions, 7, 55);
-      TrackSeedHelper::lineFit(&pretrack, pretrackClusPositions, 7, 55);
-      pretrack.set_phi(TrackSeedHelper::get_phi(&pretrack, pretrackClusPositions));
-
-      prepair.second.at(0).SetDzDs(-prepair.second.at(0).GetDzDs());
-      auto finalchain = PropagateTrack(&pretrack, kl.at(0), PropagationDirection::Outward, prepair.second.at(0), globalPositions);
-
-      if (finalchain.size() > kl.at(0).size())
-      {
-        local_chains.push_back(finalchain);
-      }
-      else
-      {
-        local_chains.push_back(kl.at(0));
-      }
-
-      //timer.stop();
-
-      if (Verbosity() > 3)
-      {
-        const auto propagatetime = timer.elapsed();
-        std::cout << "propagate track time " << propagatetime << std::endl;
-      }
+      fcfg_loc.set_field_config(PHFieldConfig::FieldConfigTypes::Field3DCartesian);
+      fcfg_loc.set_filename(m_magField);
     }
     else
     {
-      // this is bad: it copies the track to its base class, which is essentially empty
+      fcfg_loc.set_field_config(PHFieldConfig::FieldConfigTypes::kFieldUniform);
+      fcfg_loc.set_magfield_rescale(_const_field);
+    }
+    auto thread_field_map = std::unique_ptr<PHField>(PHFieldUtility::BuildFieldMap(&fcfg_loc));
+
+    //auto thread_field_map = std::make_unique<PHField>(*(_field_map));
+    std::unique_ptr<ALICEKF> fitter_loc = std::make_unique<ALICEKF>(topNode, _cluster_map, thread_field_map.get(), _fieldDir,
+      _min_clusters_per_track, _max_sin_phi, Verbosity());
+    fitter_loc->setNeonFraction(Ne_frac);
+    fitter_loc->setArgonFraction(Ar_frac);
+    fitter_loc->setCF4Fraction(CF4_frac);
+    fitter_loc->setNitrogenFraction(N2_frac);
+    fitter_loc->setIsobutaneFraction(isobutane_frac);
+    fitter_loc->useConstBField(_use_const_field);
+    fitter_loc->setConstBField(_const_field);
+    fitter_loc->useFixedClusterError(_use_fixed_clus_err);
+    fitter_loc->setFixedClusterError(0, _fixed_clus_err.at(0));
+    fitter_loc->setFixedClusterError(1, _fixed_clus_err.at(1));
+    fitter_loc->setFixedClusterError(2, _fixed_clus_err.at(2));
+
+    #pragma omp for schedule(static)
+    for (size_t track_it = 0; track_it != _track_map->size(); ++track_it)
+    {
       if (Verbosity())
       {
-        std::cout << "is NOT tpc track" << std::endl;
+        std::cout << "TPC seed " << track_it << std::endl;
       }
-      local_unused.emplace_back(*track);
+
+      // if not a TPC track, ignore
+      auto track = _track_map->get(track_it);
+      const bool is_tpc = std::any_of(
+        track->begin_cluster_keys(),
+        track->end_cluster_keys(),
+        [](const TrkrDefs::cluskey& key)
+      { return TrkrDefs::getTrkrId(key) == TrkrDefs::tpcId; });
+
+      if (is_tpc)
+      {
+
+        // copy list of seed cluster keys
+        std::vector<std::vector<TrkrDefs::cluskey>> keylist_A(1);
+        std::copy(track->begin_cluster_keys(), track->end_cluster_keys(), std::back_inserter(keylist_A[0]));
+
+        // copy seed clusters position into local map
+        std::map<TrkrDefs::cluskey, Acts::Vector3> trackClusPositions;
+        std::transform(track->begin_cluster_keys(), track->end_cluster_keys(), std::inserter(trackClusPositions, trackClusPositions.end()),
+          [globalPositions](const auto& key)
+        { return std::make_pair(key, globalPositions.at(key)); });
+
+        /// Can't circle fit a seed with less than 3 clusters, skip it
+        if (keylist_A[0].size() < 3)
+        {
+          continue;
+        }
+
+        /// This will by definition return a single pair with each vector
+        /// in the pair length 1 corresponding to the seed info
+        std::vector<float> trackChi2;
+        //timer.stop();
+        //timer.restart();
+
+        auto seedpair = fitter_loc->ALICEKalmanFilter(keylist_A, false, trackClusPositions, trackChi2);
+
+        //timer.stop();
+        if (Verbosity() > 3)
+        {
+          std::cout << "single track ALICEKF time " << timer.elapsed() << std::endl;
+        }
+        //timer.restart();
+
+        /// circle fit back to update track parameters
+        TrackSeedHelper::circleFitByTaubin(track, trackClusPositions, 7, 55);
+        TrackSeedHelper::lineFit(track, trackClusPositions, 7, 55);
+        track->set_phi(TrackSeedHelper::get_phi(track, trackClusPositions));
+        //timer.stop();
+        if (Verbosity() > 3)
+        {
+          std::cout << "single track circle fit time " << timer.elapsed() << std::endl;
+        }
+        if (seedpair.first.empty()|| seedpair.second.empty())
+        {
+          continue;
+        }
+
+        if (Verbosity())
+        {
+          std::cout << "is tpc track" << std::endl;
+        }
+
+        //timer.stop();
+        //timer.restart();
+
+        if (Verbosity())
+        {
+          std::cout << "propagate first round" << std::endl;
+        }
+
+        auto preseed = PropagateTrack(track, PropagationDirection::Inward, seedpair.second.at(0), globalPositions);
+        if (Verbosity())
+        {
+          std::cout << "preseed size " << preseed.size() << std::endl;
+        }
+
+        std::vector<std::vector<TrkrDefs::cluskey>> kl = {preseed};
+        if (Verbosity())
+        {
+          std::cout << "kl size " << kl.size() << std::endl;
+        }
+        std::vector<float> pretrackChi2;
+
+        auto prepair = fitter_loc->ALICEKalmanFilter(kl, false, globalPositions, pretrackChi2);
+        if (prepair.first.empty() || prepair.second.empty())
+        {
+          continue;
+        }
+
+        std::reverse(kl.at(0).begin(), kl.at(0).end());
+
+        auto pretrack = prepair.first.at(0);
+
+        // copy seed clusters position into local map
+        std::map<TrkrDefs::cluskey, Acts::Vector3> pretrackClusPositions;
+        std::transform(pretrack.begin_cluster_keys(), pretrack.end_cluster_keys(), std::inserter(pretrackClusPositions, pretrackClusPositions.end()),
+          [globalPositions](const auto& key)
+          { return std::make_pair(key, globalPositions.at(key)); });
+
+        // fit seed
+        TrackSeedHelper::circleFitByTaubin(&pretrack,pretrackClusPositions, 7, 55);
+        TrackSeedHelper::lineFit(&pretrack, pretrackClusPositions, 7, 55);
+        pretrack.set_phi(TrackSeedHelper::get_phi(&pretrack, pretrackClusPositions));
+
+        prepair.second.at(0).SetDzDs(-prepair.second.at(0).GetDzDs());
+        auto finalchain = PropagateTrack(&pretrack, kl.at(0), PropagationDirection::Outward, prepair.second.at(0), globalPositions);
+
+        if (finalchain.size() > kl.at(0).size())
+        {
+          local_chains.push_back(finalchain);
+        }
+        else
+        {
+          local_chains.push_back(kl.at(0));
+        }
+
+        //timer.stop();
+
+        if (Verbosity() > 3)
+        {
+          const auto propagatetime = timer.elapsed();
+          std::cout << "propagate track time " << propagatetime << std::endl;
+        }
+      }
+      else
+      {
+        // this is bad: it copies the track to its base class, which is essentially empty
+        if (Verbosity())
+        {
+          std::cout << "is NOT tpc track" << std::endl;
+        }
+        local_unused.emplace_back(*track);
+      }
     }
-  }
 
     // Critical sections to merge thread-local results
-  #pragma omp critical
-  {
-    new_chains.insert(new_chains.end(), local_chains.begin(), local_chains.end());
-    unused_tracks.insert(unused_tracks.end(), local_unused.begin(), local_unused.end());
+    #pragma omp critical
+    {
+      new_chains.insert(new_chains.end(), local_chains.begin(), local_chains.end());
+      unused_tracks.insert(unused_tracks.end(), local_unused.begin(), local_unused.end());
+    }
   }
-}
 
   // sort seeds and remove duplicates
   if( Verbosity() )
