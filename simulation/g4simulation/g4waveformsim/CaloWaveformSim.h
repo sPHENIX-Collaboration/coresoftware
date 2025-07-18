5bdf089c68d1f8670207176ab11400327484c413
Shuonli <sli17@ole.augie.edu>
2025-07-18 15:31:41 -0400

    add MC calib and MC timing input

--- simulation/g4simulation/g4waveformsim/CaloWaveformSim.h
+++ simulation/g4simulation/g4waveformsim/CaloWaveformSim.h
@@ -43,27 +43,34 @@ public:
   void set_detector_type(CaloTowerDefs::DetectorSystem dettype) { m_dettype = dettype; }
   void set_detector(const std::string &detector) { m_detector = detector; }
 
-  // Calibration settings (data)
-  void set_fieldname(const std::string &fieldname) { m_fieldname = fieldname; m_overrideCalibName = false; }
+  // Calibration settings (data energy)
+  void set_fieldname(const std::string &fieldname) { m_fieldname = fieldname; m_overrideFieldName = true; }
   void set_calibName(const std::string &calibName) { m_calibName = calibName; m_overrideCalibName = true; }
   void set_directURL_calib(const std::string &url) { m_giveDirectURL = true; m_directURL = url; }
+  void set_overrideCalibName(bool overrideCalib) { m_overrideCalibName = overrideCalib; }
+  void set_overrideFieldName(bool overrideField) { m_overrideFieldName = overrideField; }
 
-  // Calibration settings (MC)
-  void set_MC_fieldname(const std::string &MC_fieldname) { m_MC_fieldname = MC_fieldname; }
-  void set_MC_calibName(const std::string &MC_calibName) { m_MC_calibName = MC_calibName; }
+  // Calibration settings (MC energy)
+  void set_MC_fieldname(const std::string &MC_fieldname) { m_MC_fieldname = MC_fieldname; m_overrideMCFieldName = true; }
+  void set_MC_calibName(const std::string &MC_calibName) { m_MC_calibName = MC_calibName; m_overrideMCCalibName = true; }
   void set_directURL_MCcalib(const std::string &url) { m_giveDirectURL_MC = true; m_directURL_MC = url; }
+  void set_overrideMCFieldName(bool overrideField) { m_overrideMCFieldName = overrideField; }
+  void set_overrideMCCalibName(bool overrideCalib) { m_overrideMCCalibName = overrideCalib; }
 
   // Time calibration (data)
-  void set_overrideFieldName(bool overrideFieldName) { m_overrideFieldName = overrideFieldName; }
-  void set_fieldname_time(const std::string &fieldname_time) { m_fieldname_time = fieldname_time; }
-  void set_calibName_time(const std::string &calibName_time) { m_calibName_time = calibName_time; }
+  void set_fieldname_time(const std::string &fieldname_time) { m_fieldname_time = fieldname_time; m_overrideTimeFieldName = true; }
+  void set_calibName_time(const std::string &calibName_time) { m_calibName_time = calibName_time; m_overrideTimeCalibName = true; }
   void set_directURL_timecalib(const std::string &url) { m_giveDirectURL_time = true; m_directURL_time = url; }
   void set_dotimecalib(bool dotimecalib) { m_dotimecalib = dotimecalib; }
+  void set_overrideTimeFieldName(bool overrideField) { m_overrideTimeFieldName = overrideField; }
+  void set_overrideTimeCalibName(bool overrideCalib) { m_overrideTimeCalibName = overrideCalib; }
 
   // Time calibration (MC)
-  void set_MC_fieldname_time(const std::string &MC_fieldname_time) { m_MC_fieldname_time = MC_fieldname_time; }
-  void set_MC_calibName_time(const std::string &MC_calibName_time) { m_MC_calibName_time = MC_calibName_time; }
+  void set_MC_fieldname_time(const std::string &MC_fieldname_time) { m_MC_fieldname_time = MC_fieldname_time; m_overrideMCTimeFieldName = true; }
+  void set_MC_calibName_time(const std::string &MC_calibName_time) { m_MC_calibName_time = MC_calibName_time; m_overrideMCTimeCalibName = true; }
   void set_directURL_MCtimecalib(const std::string &url) { m_giveDirectURL_MC_time = true; m_directURL_MC_time = url; }
+  void set_overrideMCTimeFieldName(bool overrideField) { m_overrideMCTimeFieldName = overrideField; }
+  void set_overrideMCTimeCalibName(bool overrideCalib) { m_overrideMCTimeCalibName = overrideCalib; }
 
   // Waveform template & sampling
   void set_templatefile(const std::string &templatefile) { m_templatefile = templatefile; }
@@ -94,29 +101,38 @@ private:
   CaloTowerDefs::DetectorSystem m_dettype{CaloTowerDefs::CEMC};
   std::string m_detector{"CEMC"};
 
+  // Data energy calibration
   std::string m_fieldname{"Femc_datadriven_qm1_correction"};
   std::string m_calibName{"cemc_pi0_twrSlope_v1"};
   bool        m_overrideCalibName{false};
+  bool        m_overrideFieldName{false};
   bool        m_giveDirectURL{false};
   std::string m_directURL{""};
-
+  // MC energy calibration
   std::string m_MC_fieldname{"Femc_datadriven_qm1_correction"};
   std::string m_MC_calibName{"cemc_pi0_twrSlope_v1"};
+  bool        m_overrideMCFieldName{false};
+  bool        m_overrideMCCalibName{false};
   bool        m_giveDirectURL_MC{false};
   std::string m_directURL_MC{""};
 
-  bool        m_overrideFieldName{false};
+  // Data time calibration
   std::string m_fieldname_time{"time"};
   std::string m_calibName_time{"CEMC_meanTime"};
-  bool        m_dotimecalib{true};
+  bool        m_overrideTimeFieldName{false};
+  bool        m_overrideTimeCalibName{false};
+  bool        m_dotimecalib{false};
   bool        m_giveDirectURL_time{false};
   std::string m_directURL_time{""};
-
+  // MC time calibration
   std::string m_MC_fieldname_time{"time"};
   std::string m_MC_calibName_time{"CEMC_meanTime"};
+  bool        m_overrideMCTimeFieldName{false};
+  bool        m_overrideMCTimeCalibName{false};
   bool        m_giveDirectURL_MC_time{false};
   std::string m_directURL_MC_time{""};
 
+  // Waveform settings
   std::string m_templatefile{"waveformtemptempohcalcosmic.root"};
   int         m_nsamples{31};
   int         m_pedestalsamples{31};
@@ -124,6 +140,11 @@ private:
   int         m_nchannels{24576};
   float       m_sampling_fraction{1.0f};
 
+  //containers
+  TowerInfoContainer *m_CaloWaveformContainer{nullptr};
+  TowerInfoContainer *m_PedestalContainer{nullptr};
+
+  // Shaping & noise
   int   m_fixpedestal{1500};
   int   m_gaussian_noise{3};
   float m_deltaT{100.};
@@ -146,6 +167,8 @@ private:
   CDBTTree *cdbttree_time{nullptr}, *cdbttree_MC_time{nullptr};
   TProfile *h_template{nullptr};
   LightCollectionModel light_collection_model;
+
+  NoiseType m_noiseType{NOISE_TREE};
 
   void CreateNodeTree(PHCompositeNode *topNode);
   void maphitetaphi(PHG4Hit *g4hit,
