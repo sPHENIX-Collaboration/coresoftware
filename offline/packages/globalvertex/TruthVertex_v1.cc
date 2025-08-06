a4e3bbf4c7eee15beb7392525a127081e7c66c06
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2025-08-05 20:41:07 -0400

    improve identify

--- offline/packages/globalvertex/TruthVertex_v1.cc
+++ offline/packages/globalvertex/TruthVertex_v1.cc
@@ -1,11 +1,16 @@
 #include "TruthVertex_v1.h"
 
 void TruthVertex_v1::identify(std::ostream& os) const
-{
-  os << "TruthVertex_v1 - ID: " << _id
-     << ", z: " << _z << " ± " << _z_err
-     << ", t: " << _t << " ± " << _t_err
-     << std::endl;
+{  
+  
+  os << "---TruthVertex_v1--------------------------------" << std::endl;
+  os << "vertexid: " << get_id() << std::endl;
+  os << " t = " << get_t() << " +/- " << get_t_err() << std::endl;
+  os << " z =  " << get_z() << " +/- " << get_z_err() << std::endl;
+  os << " x =  " << get_x() << " +/- " << get_x_err() << std::endl;
+  os << " y =  " << get_y() << " +/- " << get_y_err() << std::endl;
+  os << "-----------------------------------------------" << std::endl;
+
 }
 
 int TruthVertex_v1::isValid() const
