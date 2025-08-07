aedacbf50b06a4f1e39737decc2d4ba9ff08c20d
Virgile Jean Alain Mahaut <virgilemahaut@sphnxuser01.sdcc.bnl.gov>
2025-08-07 04:36:17 -0400

    Remove point_coordinates so RawTowerGeomv5 is writable

--- offline/packages/CaloBase/RawTowerGeomv5.cc
+++ offline/packages/CaloBase/RawTowerGeomv5.cc
@@ -16,41 +16,40 @@ RawTowerGeomv5::RawTowerGeomv5(const RawTowerGeom& geom0)
   _roty = geom0.get_roty();
   _rotz = geom0.get_rotz();
   
-  _vertices.resize(_nVtx);
   for (int i = 0; i < _nVtx; i++)
   {
-    _vertices[i].x = geom0.get_vertex_x(i);
-    _vertices[i].y = geom0.get_vertex_y(i);
-    _vertices[i].z = geom0.get_vertex_z(i);
+    _vertices_x[i] = geom0.get_vertex_x(i);
+    _vertices_y[i] = geom0.get_vertex_y(i);
+    _vertices_z[i] = geom0.get_vertex_z(i);
   }
 
-  _center.x = geom0.get_center_x();
-  _center.y = geom0.get_center_y();
-  _center.z = geom0.get_center_z();
+  _center_x = geom0.get_center_x();
+  _center_y = geom0.get_center_y();
+  _center_z = geom0.get_center_z();
 
-  _center_int.x = geom0.get_center_int_x();
-  _center_int.y = geom0.get_center_int_y();
-  _center_int.z = geom0.get_center_int_z();
+  _center_int_x = geom0.get_center_int_x();
+  _center_int_y = geom0.get_center_int_y();
+  _center_int_z = geom0.get_center_int_z();
   
-  _center_ext.x = geom0.get_center_ext_x();
-  _center_ext.y = geom0.get_center_ext_y();
-  _center_ext.z = geom0.get_center_ext_z();
+  _center_ext_x = geom0.get_center_ext_x();
+  _center_ext_y = geom0.get_center_ext_y();
+  _center_ext_z = geom0.get_center_ext_z();
 
-  _center_low_eta.x = geom0.get_center_low_eta_x();
-  _center_low_eta.y = geom0.get_center_low_eta_y();
-  _center_low_eta.z = geom0.get_center_low_eta_z();
+  _center_low_eta_x = geom0.get_center_low_eta_x();
+  _center_low_eta_y = geom0.get_center_low_eta_y();
+  _center_low_eta_z = geom0.get_center_low_eta_z();
 
-  _center_high_eta.x = geom0.get_center_high_eta_x();
-  _center_high_eta.y = geom0.get_center_high_eta_y();
-  _center_high_eta.z = geom0.get_center_high_eta_z();
+  _center_high_eta_x = geom0.get_center_high_eta_x();
+  _center_high_eta_y = geom0.get_center_high_eta_y();
+  _center_high_eta_z = geom0.get_center_high_eta_z();
 
-  _center_low_phi.x = geom0.get_center_low_phi_x();
-  _center_low_phi.y = geom0.get_center_low_phi_y();
-  _center_low_phi.z = geom0.get_center_low_phi_z();
+  _center_low_phi_x = geom0.get_center_low_phi_x();
+  _center_low_phi_y = geom0.get_center_low_phi_y();
+  _center_low_phi_z = geom0.get_center_low_phi_z();
 
-  _center_high_phi.x = geom0.get_center_high_phi_x();
-  _center_high_phi.y = geom0.get_center_high_phi_y();
-  _center_high_phi.z = geom0.get_center_high_phi_z();
+  _center_high_phi_x = geom0.get_center_high_phi_x();
+  _center_high_phi_y = geom0.get_center_high_phi_y();
+  _center_high_phi_z = geom0.get_center_high_phi_z();
 }
 
 void RawTowerGeomv5::set_vertices(const std::vector<double>& vertices)
@@ -80,119 +79,116 @@ void RawTowerGeomv5::set_vertices(const std::vector<double>& vertices)
   bool face_low_phi[_nVtx] = {false, true, true, false, false, true, true, false};
   bool face_high_phi[_nVtx] = {true, false, false, true, true, false, false, true};
 
-  _center.x = 0;
-  _center.y = 0;
-  _center.z = 0;
-  _center_int.x = 0;
-  _center_int.y = 0;
-  _center_int.z = 0;
-  _center_ext.x = 0;
-  _center_ext.y = 0;
-  _center_ext.z = 0;
-  _center_low_eta.x = 0;
-  _center_low_eta.y = 0;
-  _center_low_eta.z = 0;
-  _center_high_eta.x = 0;
-  _center_high_eta.y = 0;
-  _center_high_eta.z = 0;
-  _center_low_phi.x = 0;
-  _center_low_phi.y = 0;
-  _center_low_phi.z = 0;
-  _center_high_phi.x = 0;
-  _center_high_phi.y = 0;
-  _center_high_phi.z = 0;
-  _vertices.resize(_nVtx);
+  _center_x = 0;
+  _center_y = 0;
+  _center_z = 0;
+  _center_int_x = 0;
+  _center_int_y = 0;
+  _center_int_z = 0;
+  _center_ext_x = 0;
+  _center_ext_y = 0;
+  _center_ext_z = 0;
+  _center_low_eta_x = 0;
+  _center_low_eta_y = 0;
+  _center_low_eta_z = 0;
+  _center_high_eta_x = 0;
+  _center_high_eta_y = 0;
+  _center_high_eta_z = 0;
+  _center_low_phi_x = 0;
+  _center_low_phi_y = 0;
+  _center_low_phi_z = 0;
+  _center_high_phi_x = 0;
+  _center_high_phi_y = 0;
+  _center_high_phi_z = 0;
   for (int i = 0; i < _nVtx; i++)
   {
-    _vertices[i].x = vertices[i*3+0];
-    _vertices[i].y = vertices[i*3+1];
-    _vertices[i].z = vertices[i*3+2];
+    _vertices_x[i] = vertices[i*3+0];
+    _vertices_y[i] = vertices[i*3+1];
+    _vertices_z[i] = vertices[i*3+2];
 
-    _center.x += _vertices[i].x;
-    _center.y += _vertices[i].y;
-    _center.z += _vertices[i].z;
+    _center_x += _vertices_x[i];
+    _center_y += _vertices_y[i];
+    _center_z += _vertices_z[i];
 
     if (face_inside[i])
     {
-      _center_int.x += _vertices[i].x;
-      _center_int.y += _vertices[i].y;
-      _center_int.z += _vertices[i].z;
+      _center_int_x +=  _vertices_x[i];
+      _center_int_y +=  _vertices_y[i];
+      _center_int_z +=  _vertices_z[i];
     }
     if (face_outside[i])
     {
-      _center_ext.x += _vertices[i].x;
-      _center_ext.y += _vertices[i].y;
-      _center_ext.z += _vertices[i].z;
+      _center_ext_x +=  _vertices_x[i];
+      _center_ext_y +=  _vertices_y[i];
+      _center_ext_z +=  _vertices_z[i];
     }
     if (face_low_eta[i])
     {
-      _center_low_eta.x += _vertices[i].x;
-      _center_low_eta.y += _vertices[i].y;
-      _center_low_eta.z += _vertices[i].z;
+      _center_low_eta_x +=  _vertices_x[i];
+      _center_low_eta_y +=  _vertices_y[i];
+      _center_low_eta_z +=  _vertices_z[i];
     }
     if (face_high_eta[i])
     {
-      _center_high_eta.x += _vertices[i].x;
-      _center_high_eta.y += _vertices[i].y;
-      _center_high_eta.z += _vertices[i].z;
+      _center_high_eta_x +=  _vertices_x[i];
+      _center_high_eta_y +=  _vertices_y[i];
+      _center_high_eta_z +=  _vertices_z[i];
     }
     if (face_low_phi[i])
     {
-      _center_low_phi.x += _vertices[i].x;
-      _center_low_phi.y += _vertices[i].y;
-      _center_low_phi.z += _vertices[i].z;
+      _center_low_phi_x +=  _vertices_x[i];
+      _center_low_phi_y +=  _vertices_y[i];
+      _center_low_phi_z +=  _vertices_z[i];
     }
     if (face_high_phi[i])
     {
-      _center_high_phi.x += _vertices[i].x;
-      _center_high_phi.y += _vertices[i].y;
-      _center_high_phi.z += _vertices[i].z;
+      _center_high_phi_x +=  _vertices_x[i];
+      _center_high_phi_y +=  _vertices_y[i];
+      _center_high_phi_z +=  _vertices_z[i];
     }
   }
 
-  _center.x /= (float) _nVtx;
-  _center.y /= (float) _nVtx;
-  _center.z /= (float) _nVtx;
+  _center_x /= (double) _nVtx;
+  _center_y /= (double) _nVtx;
+  _center_z /= (double) _nVtx;
 
-  _center_int.x /= (float) _nVtx / 2.;
-  _center_int.y /= (float) _nVtx / 2.;
-  _center_int.z /= (float) _nVtx / 2.;
+  _center_int_x /= (double) _nVtx / 2.;
+  _center_int_y /= (double) _nVtx / 2.;
+  _center_int_z /= (double) _nVtx / 2.;
 
-  _center_ext.x /= (float) _nVtx / 2.;
-  _center_ext.y /= (float) _nVtx / 2.;
-  _center_ext.z /= (float) _nVtx / 2.;
+  _center_ext_x /= (double) _nVtx / 2.;
+  _center_ext_y /= (double) _nVtx / 2.;
+  _center_ext_z /= (double) _nVtx / 2.;
   
-  _center_low_eta.x /= (float) _nVtx / 2.;
-  _center_low_eta.y /= (float) _nVtx / 2.;
-  _center_low_eta.z /= (float) _nVtx / 2.;
+  _center_low_eta_x /= (double) _nVtx / 2.;
+  _center_low_eta_y /= (double) _nVtx / 2.;
+  _center_low_eta_z /= (double) _nVtx / 2.;
 
-  _center_high_eta.x /= (float) _nVtx / 2.;
-  _center_high_eta.y /= (float) _nVtx / 2.;
-  _center_high_eta.z /= (float) _nVtx / 2.;
+  _center_high_eta_x /= (double) _nVtx / 2.;
+  _center_high_eta_y /= (double) _nVtx / 2.;
+  _center_high_eta_z /= (double) _nVtx / 2.;
 
-  _center_low_phi.x /= (float) _nVtx / 2.;
-  _center_low_phi.y /= (float) _nVtx / 2.;
-  _center_low_phi.z /= (float) _nVtx / 2.;
+  _center_low_phi_x /= (double) _nVtx / 2.;
+  _center_low_phi_y /= (double) _nVtx / 2.;
+  _center_low_phi_z /= (double) _nVtx / 2.;
 
-  _center_high_phi.x /= (float) _nVtx / 2.;
-  _center_high_phi.y /= (float) _nVtx / 2.;
-  _center_high_phi.z /= (float) _nVtx / 2.;
-
-
+  _center_high_phi_x /= (double) _nVtx / 2.;
+  _center_high_phi_y /= (double) _nVtx / 2.;
+  _center_high_phi_z /= (double) _nVtx / 2.;
 
   return;
 } 
 
 double RawTowerGeomv5::get_center_radius() const
 {
-  return std::sqrt(_center.x * _center.x +
-                   _center.y * _center.y);
+  return std::sqrt(_center_x * _center_x +
+                   _center_y * _center_y);
 }
 
 double RawTowerGeomv5::get_theta() const
 {
-  double radius = sqrt(_center.x * _center.x + _center.y * _center.y);
-  double theta = atan2(radius, _center.z);
+  double radius = sqrt(_center_x * _center_x + _center_y * _center_y);
+  double theta = atan2(radius, _center_z);
   return theta;
 }
 
@@ -206,7 +202,7 @@ double RawTowerGeomv5::get_eta() const
 
 double RawTowerGeomv5::get_phi() const
 {
-  return atan2(_center.y, _center.x);
+  return atan2(_center_y, _center_x);
 }
 
 void RawTowerGeomv5::identify(std::ostream& os) const
