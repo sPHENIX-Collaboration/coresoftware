27d3eb85432e618b638fd48b5e5007658ba76ef3
Anthony Denis Frawley <afrawley@fsu.edu>
2025-10-08 22:02:00 -0400

    New versioned TPC geometry class. Compiles.

---
+++ simulation/g4simulation/g4detectors/PHG4TpcGeom.cc
@@ -0,0 +1,8 @@
+#include "PHG4TpcGeom.h"
+
+void PHG4TpcGeom::identify(std::ostream& os) const
+{
+  os << "virtual PHG4TpcGeom"
+     << std::endl;
+  return;
+}
---
+++ simulation/g4simulation/g4detectors/PHG4TpcGeomContainerLinkDef.h
@@ -0,0 +1,5 @@
+#ifdef __CINT__
+
+#pragma link C++ class PHG4TpcGeomContainer + ;
+
+#endif /* __CINT__ */
