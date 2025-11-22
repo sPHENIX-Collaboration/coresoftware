e58d941c349ea5f4b1803f12c6b4014c745c2500
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-11-22 06:53:38 -0500

    clang-tidy for CosmicSpray

--- simulation/g4simulation/g4main/CosmicSpray.h
+++ simulation/g4simulation/g4main/CosmicSpray.h
@@ -16,22 +16,21 @@ class PHCompositeNode;
 class CosmicSpray : public SubsysReco
 {
  public:
-  bool InDetector(double x, double y, double z);
+  bool InDetector(double x, double y, double z) const;
   CosmicSpray(const std::string &name = "COSMICS", const double R = 650);
-  ~CosmicSpray() override {}
+  ~CosmicSpray() override = default;
   int InitRun(PHCompositeNode *topNode) override;
   int process_event(PHCompositeNode *topNode) override;
-  void set_gen_min_momentum(const double p) { gen.SetMinimumMomentum(p); }  
+  void set_gen_min_momentum(const double p) { gen.SetMinimumMomentum(p); }
   void set_gen_max_momentum(const double p) { gen.SetMaximumMomentum(p); }
-
 
  private:
   EcoMug gen;
 
   double _gun_e = std::numeric_limits<double>::quiet_NaN();
-  double _x_min = std::numeric_limits<double>::quiet_NaN();
+  //  double _x_min = std::numeric_limits<double>::quiet_NaN();
   double _x_max = std::numeric_limits<double>::quiet_NaN();
-  double _z_min = std::numeric_limits<double>::quiet_NaN();
+  //  double _z_min = std::numeric_limits<double>::quiet_NaN();
   double _z_max = std::numeric_limits<double>::quiet_NaN();
   double _y_fix = std::numeric_limits<double>::quiet_NaN();
 
