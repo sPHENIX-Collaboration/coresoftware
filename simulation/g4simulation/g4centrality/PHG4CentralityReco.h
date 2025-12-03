1933b13f6369bc95135da8b46498ade7a851189d
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-12-03 13:33:37 -0500

    clang-tidy for g4centrality

--- simulation/g4simulation/g4centrality/PHG4CentralityReco.h
+++ simulation/g4simulation/g4centrality/PHG4CentralityReco.h
@@ -21,11 +21,10 @@ class PHG4CentralityReco : public SubsysReco
 {
  public:
   PHG4CentralityReco(const std::string &name = "PHG4CentralityReco");
-  ~PHG4CentralityReco() override {}
+  ~PHG4CentralityReco() override = default;
 
   int InitRun(PHCompositeNode *topNode) override;
   int process_event(PHCompositeNode *topNode) override;
-  int End(PHCompositeNode *topNode) override;
 
   void DoCentralityCalibration(bool do_centrality_calibration)
   {
@@ -39,28 +38,28 @@ class PHG4CentralityReco : public SubsysReco
 
  private:
   void CreateNode(PHCompositeNode *topNode);
-  void FillNode(PHCompositeNode *topNode);
+  void FillNode(PHCompositeNode *topNode) const;
 
   PHParameters _centrality_calibration_params;
 
-  bool _do_centrality_calibration {true};
+  bool _do_centrality_calibration{true};
 
   std::map<float, int> _cent_cal_bimp;
   std::map<float, int> _cent_cal_epd;
   std::map<float, int> _cent_cal_mbd;
 
-  float _bimp {std::numeric_limits<float>::quiet_NaN()};
-  float _bimp_cent {std::numeric_limits<float>::quiet_NaN()};
+  float _bimp{std::numeric_limits<float>::quiet_NaN()};
+  float _bimp_cent{std::numeric_limits<float>::quiet_NaN()};
 
-  float _epd_N {std::numeric_limits<float>::quiet_NaN()};
-  float _epd_S {std::numeric_limits<float>::quiet_NaN()};
-  float _epd_NS {std::numeric_limits<float>::quiet_NaN()};
-  float _epd_cent {std::numeric_limits<float>::quiet_NaN()};
+  float _epd_N{std::numeric_limits<float>::quiet_NaN()};
+  float _epd_S{std::numeric_limits<float>::quiet_NaN()};
+  float _epd_NS{std::numeric_limits<float>::quiet_NaN()};
+  float _epd_cent{std::numeric_limits<float>::quiet_NaN()};
 
-  float _mbd_N {std::numeric_limits<float>::quiet_NaN()};
-  float _mbd_S {std::numeric_limits<float>::quiet_NaN()};
-  float _mbd_NS {std::numeric_limits<float>::quiet_NaN()};
-  float _mbd_cent {std::numeric_limits<float>::quiet_NaN()};
+  float _mbd_N{std::numeric_limits<float>::quiet_NaN()};
+  float _mbd_S{std::numeric_limits<float>::quiet_NaN()};
+  float _mbd_NS{std::numeric_limits<float>::quiet_NaN()};
+  float _mbd_cent{std::numeric_limits<float>::quiet_NaN()};
 };
 
 #endif
