8aba0327a011292402e84a1217ff2bd236990b86
Chris Pinkenburg <pinkenburg@users.noreply.github.com>
2025-12-10 16:02:00 -0500

    Add RUN3PP range constants to RunnumberRange.h

--- offline/framework/phool/RunnumberRange.h
+++ offline/framework/phool/RunnumberRange.h
@@ -11,6 +11,8 @@ namespace RunnumberRange
   static const int RUN3_TPCFW_CLOCK_CHANGE = 58667;
   static const int RUN3AUAU_FIRST = 66457;
   static const int RUN3AUAU_LAST = 78954;
+  static const int RUN3PP_FIRST = 79146; // first beam data
+  static const int RUN3PP_LAST = 100000;
 }
 
 #endif
