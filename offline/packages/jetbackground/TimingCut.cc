24b32d04cb306e399d3db156e5ed1b8aa32ba79a
Joseph Clement <jocl6895@colorado.edu>
2026-02-11 18:25:24 -0500

    add protection against missing calib file or function

--- offline/packages/jetbackground/TimingCut.cc
+++ offline/packages/jetbackground/TimingCut.cc
@@ -42,8 +42,21 @@ int TimingCut::Init(PHCompositeNode *topNode)
     }
 
   _fitFile = new CDBTF(CDBInterface::instance()->getUrl("t_ohfrac_calib_Default"));
-  _fitFile->LoadCalibrations();
-  _fitFunc = _fitFile->getTF("t_ohcal_calib_function_Default");
+  if(_fitFile)
+    {
+      _fitFile->LoadCalibrations();
+      _fitFunc = _fitFile->getTF("t_ohcal_calib_function_Default");
+      if(!_fitFunc)
+	{
+	  std::cout << "ERROR: NO CALIBRATION TF1 FOUND FOR TIMING CUT OHCAL FRACTION CORRECTION! This should never happen. ABORT RUN!" << std::endl;
+	  return Fun4AllReturnCodes::ABORTRUN;
+	}
+    }
+  else
+    {
+      std::cout << "ERROR: NO CALIBRATION FILE FOUND FOR TIMING CUT OHCAL FRACTION CORRECTION! ABORT RUN!" << std::endl;
+      return Fun4AllReturnCodes::ABORTRUN;
+    }
 
   return Fun4AllReturnCodes::EVENT_OK;
 }
