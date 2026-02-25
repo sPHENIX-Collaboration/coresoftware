8af50390f772fb46a1489a9c07a74d25ba6f67ad
emclaughlin2 <emlaugh1020@gmail.com>
2026-02-25 14:10:04 -0500

    Change to correct index mapping when setting FitStatus flag for use in all calorimeters

--- offline/packages/CaloReco/CaloTowerBuilder.cc
+++ offline/packages/CaloReco/CaloTowerBuilder.cc
@@ -513,7 +513,7 @@ int CaloTowerBuilder::process_event(PHCompositeNode *topNode)
     {
       towerinfo->set_isRecovered(true);
     }
-    towerinfo->set_FitStatus(static_cast<bool>(processed_waveforms.at(i).at(5)));
+    towerinfo->set_FitStatus(static_cast<bool>(processed_waveforms.at(idx).at(5)));
     int n_samples = waveforms.at(idx).size();
     if (n_samples == m_nzerosuppsamples || SZS)
     {
