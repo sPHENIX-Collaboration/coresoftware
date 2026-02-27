c547c80a0210999d7cf673397f277688c52c9595
Joe Osborn <osbornjd91@gmail.com>
2026-02-27 14:19:02 -0500

    add fwd decl

--- offline/packages/trackbase/IBaseDetector.h
+++ offline/packages/trackbase/IBaseDetector.h
@@ -8,12 +8,20 @@
 
 #pragma once
 
-#include "ActsExamples/Utilities/OptionsFwd.hpp"
-
 #include <memory>
 #include <utility>
 #include <vector>
+namespace boost::program_options
+{
+  class options_description;
+  class variables_map;
+}  // namespace boost::program_options
 
+namespace ActsExamples::Options
+{
+  using Description = ::boost::program_options::options_description;
+  using Variables = ::boost::program_options::variables_map;
+}  // namespace ActsExamples::Options
 namespace Acts {
 class TrackingGeometry;
 class IMaterialDecorator;
