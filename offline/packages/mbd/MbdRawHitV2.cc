37a1689869c7e090405ac006c98da4e2261498fe
mchiu-bnl <chiu@bnl.gov>
2026-03-02 15:10:51 -0500

    added fit info

---
+++ offline/packages/mbd/MbdRawHitV2.cc
@@ -0,0 +1,23 @@
+#include "MbdRawHitV2.h"
+
+void MbdRawHitV2::Reset()
+{
+  Clear();
+}
+
+void MbdRawHitV2::Clear(Option_t* /*unused*/)
+{
+  std::cout << "clearing " << bpmt << std::endl;
+  bpmt = -1;
+  fitstat = 0;
+  badc = std::numeric_limits<float>::quiet_NaN();
+  bttdc = std::numeric_limits<float>::quiet_NaN();
+  bqtdc = std::numeric_limits<float>::quiet_NaN();
+}
+
+void MbdRawHitV2::identify(std::ostream& out) const
+{
+  out << "identify yourself: I am a MbdRawHitV2 object" << std::endl;
+  out << "Pmt: " << bpmt << ", adc: " << badc << ", ttdc: "
+      << bttdc << ", bqtdc: " << bqtdc << std::endl;
+}
