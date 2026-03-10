56be16346d622149b4042ade3bcc64f4ebb16971
Chris Pinkenburg <pinkenburg@bnl.gov>
2026-03-10 15:54:54 -0400

    fix Reset and hex output

--- offline/packages/bcolumicount/BcoInfov1.cc
+++ offline/packages/bcolumicount/BcoInfov1.cc
@@ -3,13 +3,13 @@
 void BcoInfov1::Reset()
 {
   bco.fill(0);
+  evtno.fill(0);
   return;
 }
 
 void BcoInfov1::identify(std::ostream& out) const
 {
   out << "identify yourself: I am an BcoInfov1 Object\n";
-  out << std::hex;
   out << "previous event: " << get_previous_evtno() << std::hex
       << " bco: 0x" << get_previous_bco() << "\n"
       << std::dec
@@ -19,7 +19,6 @@ void BcoInfov1::identify(std::ostream& out) const
       << "future   event:  " << get_future_evtno() << std::hex
       << " bco: 0x" << get_future_bco() << std::dec
       << std::endl;
-
   return;
 }
 
