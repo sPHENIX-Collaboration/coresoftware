55d4b1abf61d12720a0ef7bff3921270c00d6cf4
rosstom <rosstom@ucla.edu>
2026-04-30 20:14:16 -0400

    Last fix

--- offline/packages/KFParticle_sPHENIX/KFParticle_eventReconstruction.h
+++ offline/packages/KFParticle_sPHENIX/KFParticle_eventReconstruction.h
@@ -83,7 +83,6 @@ class KFParticle_eventReconstruction : public KFParticle_Tools
                             std::vector<KFParticle> possibleVertex);
 
   KFParticle createFakePV();
-  std::vector<KFParticle> createTruthPV(PHCompositeNode* topNode);
 
  protected:
   bool m_constrain_to_vertex;
