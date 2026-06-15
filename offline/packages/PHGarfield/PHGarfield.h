61b00a26aadd044c1bf6eee7d012856630519e48
Chris Pinkenburg <pinkenburg@bnl.gov>
2026-06-15 19:38:48 -0400

    use const methods where possible

--- offline/packages/PHGarfield/PHGarfield.h
+++ offline/packages/PHGarfield/PHGarfield.h
@@ -24,25 +24,25 @@ class PHGarfield : public SubsysReco
   ~PHGarfield() override = default;
 
   int InitRun(PHCompositeNode *) override;
-  int process_event(PHCompositeNode *topNode) override;
+  int process_event(PHCompositeNode * /*topNode*/) override;
 
   bool StopHere(const double x, const double y, const double z, const double zPrevious);
 
-  void PrintMaps();
-  void PrintGarfield(double x, double y, double z);
+  void PrintMaps() const;
+  void PrintGarfield(double x, double y, double z) const;
 
   //  These are left in public namespace for easy plotting macros...
   //  The user is encouraged to add more routine to fit their analysis goals...
   TPolyLine3D *ReverseDrift(double x_cm, double y_cm, double z_cm, double step_ns = 50.0);  // Drifts electrons from some initial point until they hit a detector boundary...
 
-  double GetRadius(size_t index) {return radii.at(index);}
+  double GetRadius(size_t index) const {return radii.at(index);}
 
  private:
-  void GetMagneticFieldTesla(double x_cm, double y_cm, double z_cm, double &bx_t, double &by_t, double &bz_t);      // Feeds magnetic field to Garfield
-  void GetElectricFieldVcm(double x_cm, double y_cm, double z_cm, double &ex_vcm, double &ey_vcm, double &ez_vcm);  // Feeds electric field to Garfield
-  void InitializeGas(std::string dir);
+  void GetMagneticFieldTesla(double x_cm, double y_cm, double z_cm, double &bx_t, double &by_t, double &bz_t) const;      // Feeds magnetic field to Garfield
+  void GetElectricFieldVcm(double x_cm, double y_cm, double z_cm, double &ex_vcm, double &ey_vcm, double &ez_vcm) const;  // Feeds electric field to Garfield
+  void InitializeGas(const std::string &dir);
   void FillRadii();
-  double bounder(double phi, double phi_min);
+  static double bounder(double phi, double phi_min);
 
   CDBTTree *m_cdbTPCMAPttree{nullptr};            // Locations of the pads from CDB...
   PHField3DCartesian *m_field{nullptr};           // The stanards sPHENIX field holding container.
