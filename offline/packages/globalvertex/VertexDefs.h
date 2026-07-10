0224b190fc226eae34642d13fc3b71327a9d350c
Daniel J Lis <dlis@sphnxuser05.sdcc.bnl.gov>
2026-07-10 10:10:33 -0400

    djl -- instructions

--- offline/packages/globalvertex/VertexDefs.h
+++ offline/packages/globalvertex/VertexDefs.h
@@ -1,6 +1,13 @@
 #ifndef GLOBALVERTEX_VERTEXDEFS_H
 #define GLOBALVERTEX_VERTEXDEFS_H
 
+// Namespace is used to make the Global Vertex and Calo Vertex object not depend
+//on each other but allows for CaloVtxReco to pass on which algorithm was used to
+// generate the z-vertex
+
+// If you'd like to add an algorithm add it here, then to GlobalVertex::VTXTYPE
+// and also GlobalVertexv4::get_position()
+
 namespace VertexDefs
 {
   enum CALOALGO
