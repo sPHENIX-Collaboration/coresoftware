bf7d5f31fcb6738fd66b43d424cf259906e7abea
Chris Pinkenburg <pinkenburg@bnl.gov>
2026-07-31 18:49:55 -0400

    use DST#Sync for unsplit TTree to read sync object

--- offline/framework/ffaobjects/SyncDefs.h
+++ offline/framework/ffaobjects/SyncDefs.h
@@ -9,7 +9,9 @@ namespace syncdefs
 {
   // __attribute__((unused)) prevents compiler from flagging variable as unused
   static const int NUM_SYNC_VARS __attribute__((unused)) = 4;
-  static const char *SYNCVARS[] __attribute__((unused)) = {"eventcounter", "eventnumber", "runnumber", "segmentnumber"};
+// Sync object in old split TTree
+//  static const char *SYNCVARS[] __attribute__((unused)) = {"eventcounter", "eventnumber", "runnumber", "segmentnumber"};
+  static const char *SYNCVARS[] __attribute__((unused)) = {"DST#Sync"}; // TBranch name of sync object in unsplit TTree
   static const std::string SYNCNODENAME __attribute__((unused)) = "Sync";
 }  // namespace syncdefs
 
