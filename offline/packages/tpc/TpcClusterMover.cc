3d10904cfe02dd623c9a9972dc8ec9a86102b038
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2026-09-09 15:17:12 -0400

    - removed unnecessary members - constructor is now default

--- offline/packages/tpc/TpcClusterMover.cc
+++ offline/packages/tpc/TpcClusterMover.cc
@@ -36,57 +36,21 @@ namespace
   }
 }  // namespace
 
-TpcClusterMover::TpcClusterMover()
-  : inner_tpc_spacing((mid_tpc_min_radius - inner_tpc_min_radius) / 16.0)
-  , mid_tpc_spacing((outer_tpc_min_radius - mid_tpc_min_radius) / 16.0)
-  , outer_tpc_spacing((outer_tpc_max_radius - outer_tpc_min_radius) / 16.0)
+void TpcClusterMover::initialize_geometry(PHG4TpcGeomContainer* /* unused */, ActsGeometry* tGeometry, PHCompositeNode* topNode)
 {
-  // initialize layer radii
 
-  for (int i = 0; i < 16; ++i)
+  if (!tGeometry)
   {
-    layer_radius[i] = inner_tpc_min_radius + (double) i * inner_tpc_spacing + 0.5 * inner_tpc_spacing;
-  }
-  for (int i = 0; i < 16; ++i)
-  {
-    layer_radius[i + 16] = mid_tpc_min_radius + (double) i * mid_tpc_spacing + 0.5 * mid_tpc_spacing;
-  }
-  for (int i = 0; i < 16; ++i)
-  {
-    layer_radius[i + 32] = outer_tpc_min_radius + (double) i * outer_tpc_spacing + 0.5 * outer_tpc_spacing;
-  }
-}
-
-void TpcClusterMover::initialize_geometry(PHG4TpcGeomContainer* cellgeo, ActsGeometry* tGeometry, PHCompositeNode* topNode)
-{
-  _topNode = topNode;
-
-  if (_verbosity > 0)
-  {
-    std::cout << "TpcClusterMover: Getting ActsGeometry, and getting layer radii for Tpc from cell geometry object" << std::endl;
-  }
-
-  if (!tGeometry || !cellgeo)
-  {
-    std::cout << PHWHERE << " Failed to get ActsGeometry or TPC cell geometry, cannot continue - quit!" << std::endl;
+    std::cout << PHWHERE << " Failed to get ActsGeometry cannot continue - quit!" << std::endl;
     exit(1);
   }
 
+  _topNode = topNode;
   _tGeometry = tGeometry;
-
-  int layer = 0;
-  PHG4TpcGeomContainer::ConstRange layerrange = cellgeo->get_begin_end();
-  for (PHG4TpcGeomContainer::ConstIterator layeriter = layerrange.first;
-       layeriter != layerrange.second;
-       ++layeriter)
-  {
-    layer_radius[layer] = layeriter->second->get_radius();
-    layer++;
-  }
 }
 
 //____________________________________________________________________________..
-std::vector<std::pair<TrkrDefs::cluskey, Acts::Vector3>> TpcClusterMover::processTrack(const std::vector<std::pair<TrkrDefs::cluskey, Acts::Vector3>>& global_in)
+std::vector<std::pair<TrkrDefs::cluskey, Acts::Vector3>> TpcClusterMover::processTrack(const std::vector<std::pair<TrkrDefs::cluskey, Acts::Vector3>>& global_in) const
 {
   // Get the global positions of the TPC clusters for this track, already corrected for distortions, and move them to the surfaces
   // The input object contains all clusters for the track in world coordinates
