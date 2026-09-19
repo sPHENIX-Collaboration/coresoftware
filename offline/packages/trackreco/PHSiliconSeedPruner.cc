43420dc6a83774ebd34a88756ee58a0ee8cf83d0
Hao-Ren Jheng <hrjheng@mit.edu>
2026-09-19 15:44:25 -0400

    Store the pruned silicon seeds to a separate container

--- offline/packages/trackreco/PHSiliconSeedPruner.cc
+++ offline/packages/trackreco/PHSiliconSeedPruner.cc
@@ -4,12 +4,16 @@
 #include <fun4all/Fun4AllReturnCodes.h>
 
 #include <phool/PHCompositeNode.h>
+#include <phool/PHIODataNode.h>
+#include <phool/PHNodeIterator.h>
+#include <phool/PHObject.h>
 #include <phool/PHRandomSeed.h>
 #include <phool/getClass.h>
 #include <phool/phool.h>
 
 #include <trackbase_historic/TrackSeed.h>
 #include <trackbase_historic/TrackSeedContainer.h>
+#include <trackbase_historic/TrackSeedContainer_v1.h>
 
 #include <climits>
 #include <iostream>
@@ -37,6 +41,12 @@ void PHSiliconSeedPruner::set_random_seed(unsigned int seed)
 
 int PHSiliconSeedPruner::InitRun(PHCompositeNode *topNode)
 {
+  if (m_trackMapName == m_outputTrackMapName)
+  {
+    std::cout << PHWHERE << " ERROR: Input and output TrackSeedContainer names are both " << m_trackMapName << std::endl;
+    return Fun4AllReturnCodes::ABORTRUN;
+  }
+
   m_siliconSeeds = findNode::getClass<TrackSeedContainer>(topNode, m_trackMapName);
   if (!m_siliconSeeds)
   {
@@ -44,11 +54,37 @@ int PHSiliconSeedPruner::InitRun(PHCompositeNode *topNode)
     return Fun4AllReturnCodes::ABORTRUN;
   }
 
+  m_prunedSiliconSeeds = findNode::getClass<TrackSeedContainer>(topNode, m_outputTrackMapName);
+  if (!m_prunedSiliconSeeds)
+  {
+    PHNodeIterator iter(topNode);
+    PHCompositeNode *dstNode = dynamic_cast<PHCompositeNode *>(iter.findFirst("PHCompositeNode", "DST"));
+    if (!dstNode)
+    {
+      std::cout << PHWHERE << " ERROR: Can't find DST node" << std::endl;
+      return Fun4AllReturnCodes::ABORTRUN;
+    }
+
+    PHNodeIterator dstIter(dstNode);
+    PHCompositeNode *svtxNode = dynamic_cast<PHCompositeNode *>(dstIter.findFirst("PHCompositeNode", "SVTX"));
+    if (!svtxNode)
+    {
+      svtxNode = new PHCompositeNode("SVTX");
+      dstNode->addNode(svtxNode);
+    }
+
+    m_prunedSiliconSeeds = new TrackSeedContainer_v1;
+    auto *outputNode = new PHIODataNode<PHObject>(m_prunedSiliconSeeds, m_outputTrackMapName, "PHObject");
+    svtxNode->addNode(outputNode);
+  }
+
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int PHSiliconSeedPruner::process_event(PHCompositeNode * /*topNode*/)
 {
+  m_prunedSiliconSeeds->Reset();
+
   std::map<short int, std::vector<size_t>> seedIndicesByCrossing;
   std::set<size_t> selectedSeedIndices;
 
@@ -100,13 +136,9 @@ int PHSiliconSeedPruner::process_event(PHCompositeNode * /*topNode*/)
     }
   }
 
-  for (size_t seedIndex = 0; seedIndex < m_siliconSeeds->size(); ++seedIndex)
+  for (const size_t seedIndex : selectedSeedIndices)
   {
-    if (m_siliconSeeds->get(seedIndex) &&
-        selectedSeedIndices.find(seedIndex) == selectedSeedIndices.end())
-    {
-      m_siliconSeeds->erase(seedIndex);
-    }
+    m_prunedSiliconSeeds->insert(m_siliconSeeds->get(seedIndex));
   }
 
   if (Verbosity() > 0)
